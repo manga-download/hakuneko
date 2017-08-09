a04f51ab3878e28868fa945c59150ae6b84b751d
Ronny Wegener <wegener.ronny@gmail.com>
2017-08-09 18:06:40 +0200

    added info creation in build script for darwin package

--- electron/build-darwin.sh
+++ electron/build-darwin.sh
@@ -5,6 +5,14 @@
 DIR_64="${PACKAGE}_${VERSION}_darwin_amd64"
 
 function dmg {
+    sed "/<key>CFBundleName<\/key>/!b;n;c<string>${PRODUCT}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+    sed "/<key>CFBundleDisplayName<\/key>/!b;n;c<string>${PRODUCT}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>CFBundleExecutable<\/key>/!b;n;c<string>${BIN_DARWIN}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>CFBundleIconFile<\/key>/!b;n;c<string>${PACKAGE}.icns</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>CFBundleIdentifier<\/key>/!b;n;c<string>${URL}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>CFBundleShortVersionString<\/key>/!b;n;c<string>${VERSION}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>CFBundleVersion<\/key>/!b;n;c<string>${VERSION}</string>" -i "build/$1/Electron.app/Contents/Info.plist"
+	sed "/<key>LSApplicationCategoryType<\/key>/!b;n;c<string>public.app-category.developer-tools</string>" -i "build/$1/Electron.app/Contents/Info.plist"
     # replace icns
     rm -f "build/$1/Electron.app/Contents/Resources/electron.icns"
     cp "res/icon.icns" "build/$1/Electron.app/Contents/Resources/${PACKAGE}.icns"
