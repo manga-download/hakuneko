897dcbdce1af40e75d215a3378a2e00cbe18e75f
Ronny Wegener <wegener.ronny@gmail.com>
2017-08-11 21:30:54 +0200

    updated graphic layout for osx package

--- electron/build-darwin.sh
+++ electron/build-darwin.sh
@@ -51,14 +51,14 @@ function dmg {
             set current view of container window to icon view
             set toolbar visible of container window to false
             set statusbar visible of container window to false
-            set the bounds of container window to {100, 100, 580, 460}
+            set the bounds of container window to {100, 100, 560, 620}
             set theViewOptions to the icon view options of container window
             set arrangement of theViewOptions to not arranged
-            set icon size of theViewOptions to 96
+            set icon size of theViewOptions to 64
             set background picture of theViewOptions to file ".images:OSXSetup.png"
             make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-            set position of item "'${PRODUCT}'" of container window to {96, 192}
-            set position of item "Applications" of container window to {376, 192}
+            set position of item "'${PRODUCT}'" of container window to {360, 180}
+            set position of item "Applications" of container window to {360, 390}
             update without registering applications
             delay 5
             close
