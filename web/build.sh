e9278fdfc5b92a78b2bf2e2e01ed2988f985efff
Ronny Wegener <wegener.ronny@gmail.com>
2017-09-13 00:57:35 +0200

    updated build script to create a signed archive

--- web/build.sh
+++ web/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+DIR=../cloud/htdocs
+KEY=../../web/hakuneko.key
+PUB=../../web/hakuneko.pem
 REV=$(git log -1 --format="%H")
+VER=$(echo $REV | cut -c 1-6)
+
 echo "<script>" > "lib/hakuneko/version.html"
 echo "var revision = {" >> "lib/hakuneko/version.html"
-echo "    id: '$(echo $REV | cut -c 1-6)'," >> "lib/hakuneko/version.html"
+echo "    id: '${VER}'," >> "lib/hakuneko/version.html"
 echo "    url: 'https://sourceforge.net/p/hakuneko/code/ci/${REV}/'" >> "lib/hakuneko/version.html"
 echo "};" >> "lib/hakuneko/version.html"
 echo "</script>" >> "lib/hakuneko/version.html"
 
+rm -r -f $DIR
 polymer build
+cd $DIR
+zip -r $VER.zip .
+openssl dgst -sha256 -sign $KEY -out $VER.sig $VER.zip
+echo -n $VER > latest
+cp $PUB $VER.pem
