25715466a2c978e02bece9e714a29b67a69f4690
Ronny Wegener <wegener.ronny@gmail.com>
2019-04-22 21:19:57 +0200

    do not change version file in code, but in deployment artifact

--- web/publish.sh
+++ web/publish.sh
@@ -10,13 +10,6 @@ DST=../../../../releases/0.4.0
 REV=$(git log -1 --format="%H")
 VER=$(echo $REV | cut -c 1-6)
 
-echo "<script>" > "lib/hakuneko/version.html"
-echo "var revision = {" >> "lib/hakuneko/version.html"
-echo "    id: '${VER}'," >> "lib/hakuneko/version.html"
-echo "    url: 'https://github.com/manga-download/hakuneko/commits/${REV}'" >> "lib/hakuneko/version.html"
-echo "};" >> "lib/hakuneko/version.html"
-echo "</script>" >> "lib/hakuneko/version.html"
-
 git stash push -u -m 'Cleanup before Deploy'
 polymer build
 # overwrite the polymer minified/obfuscated js files with the original minified/obfuscated js files (e.g. prevent breaking hls.light.min.js when minified by polymer)
@@ -25,7 +18,16 @@ cp -f ./js/*.min.js $DIR/js/
 cp -f ./lib/hakuneko/engine/base/connectors/mangago.html $DIR/lib/hakuneko/engine/base/connectors/mangago.html
 cp -f ./lib/hakuneko/engine/base/connectors/tencentcomic.html $DIR/lib/hakuneko/engine/base/connectors/tencentcomic.html
 git stash pop
+
 cd $DIR
+# update version file before deployment
+echo "<script>" > "lib/hakuneko/version.html"
+echo "var revision = {" >> "lib/hakuneko/version.html"
+echo "    id: '${VER}'," >> "lib/hakuneko/version.html"
+echo "    url: 'https://github.com/manga-download/hakuneko/commits/${REV}'" >> "lib/hakuneko/version.html"
+echo "};" >> "lib/hakuneko/version.html"
+echo "</script>" >> "lib/hakuneko/version.html"
+
 zip -r $VER.zip .
 echo -n $VER.zip?signature=$(openssl dgst -sha256 -hex -sign $KEY $VER.zip | cut -d' ' -f2) > latest
 
