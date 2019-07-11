f4586ff1ddb96352ff04f3e39a6f73bc8a8ab0a8
Ronny Wegener <wegener.ronny@gmail.com>
2019-07-11 22:41:43 +0200

    introduced extractor to get options from commandline arguments

---
+++ client/src/CommandlineArgumentExtractor.js
@@ -0,0 +1,40 @@
+module.exports = class CommandlineArgumentExtractor {
+
+    constructor(argv) {
+        this._argv = argv;
+    }
+
+    _get(key, isFlag) {
+        let option = this._argv.find(arg => arg.startsWith(key + '='));
+        let index = this._argv.findIndex(arg => arg === key);
+
+        if(isFlag) {
+            return index > -1 || option !== undefined;
+        }
+
+        if(option) {
+            let value = option.split('=')[1];
+            return !value ? undefined : value;
+        }
+
+        if(index > -1 && index + 1 < this._argv.length) {
+            let value = this._argv[index + 1];
+            return value.startsWith('-') ? undefined : value;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Extract <Configuration> compatible options from the arguments provided through the commandline
+     */
+    get options() {
+        return {
+            proxyRules:                   this._get('--proxy-server'),
+            applicationUpdateURL:         this._get('--update-url') || this._get('-u'),
+            applicationStartupURL:        this._get('--startup-url'),
+            applicationCacheDirectory:    this._get('--cache-directory') || this._get('-c'),
+            applicationUserDataDirectory: this._get('--user-directory')
+        };
+    }
+}
---
+++ client/test/CommandlineArgumentExtractor.js
@@ -0,0 +1,94 @@
+const assert = require('assert');
+const CommandlineArgumentExtractor = require('../src/CommandlineArgumentExtractor.js');
+
+describe('CommandlineArgumentExtractor', function() {
+
+    describe('_get', function() {
+
+        it('contains flag', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', '-c']);
+            assert.equal(testee._get('-b', true), true);
+        });
+
+        it('does not contain flag', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', '-c']);
+            assert.equal(testee._get('-x', true), false);
+        });
+        
+        it('contains flag when followed by value', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', '1']);
+            assert.equal(testee._get('-b', true), true);
+        });
+        
+        it('followed by value', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', 'x']);
+            assert.equal(testee._get('-b'), 'x');
+        });
+        
+        it('followed by option instead of value', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', '-c']);
+            assert.equal(testee._get('-b'), undefined);
+        });
+        
+        it('no value because at end', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b', '-c']);
+            assert.equal(testee._get('-c'), undefined);
+        });
+        
+        it('assigned value', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b=x', '-c']);
+            assert.equal(testee._get('-b'), 'x');
+        });
+        
+        it('assigned nothing', () => {
+            let testee = new CommandlineArgumentExtractor(['-a', '-b=', '-c']);
+            assert.equal(testee._get('-b'), undefined);
+        });
+    });
+
+    describe('options', function() {
+
+        it('--proxy-server', () => {
+            let expected = 'socks5://localhost:8080';
+            let testee = new CommandlineArgumentExtractor([`--proxy-server=${expected}`]);
+            assert.equal(testee.options.proxyRules, expected);
+        });
+
+        it('--update-url', () => {
+            let expected = 'https://raw.gihub.com/manga-downloader/releases/0.4.0/latest';
+            let testee = new CommandlineArgumentExtractor([`--update-url=${expected}`]);
+            assert.equal(testee.options.applicationUpdateURL, expected);
+        });
+
+        it('-u', () => {
+            let expected = 'https://raw.gihub.com/manga-downloader/releases/0.4.0/latest';
+            let testee = new CommandlineArgumentExtractor(['-u', expected]);
+            assert.equal(testee.options.applicationUpdateURL, expected);
+        });
+
+        it('--startup-url', () => {
+            let expected = 'cache://hakuneko/index.html';
+            let testee = new CommandlineArgumentExtractor([`--startup-url=${expected}`]);
+            assert.equal(testee.options.applicationStartupURL, expected);
+        });
+
+        it('--cache-directory', () => {
+            let expected = './cache';
+            let testee = new CommandlineArgumentExtractor([`--cache-directory=${expected}`]);
+            assert.equal(testee.options.applicationCacheDirectory, expected);
+        });
+
+        it('-c', () => {
+            let expected = './cache';
+            let testee = new CommandlineArgumentExtractor(['-c', expected]);
+            assert.equal(testee.options.applicationCacheDirectory, expected);
+        });
+
+        it('--user-directory', () => {
+            let expected = './user';
+            let testee = new CommandlineArgumentExtractor([`--user-directory=${expected}`]);
+            assert.equal(testee.options.applicationUserDataDirectory, expected);
+        });
+    });
+
+});
