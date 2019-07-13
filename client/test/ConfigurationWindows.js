3147f3b075317cf79d43c125921eca76f81c01c7
Ronny Wegener <wegener.ronny@gmail.com>
2019-07-13 13:54:24 +0200

    tests for windows configuration

--- client/test/ConfigurationWindows.js
+++ client/test/ConfigurationWindows.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs-extra');
 const assert = require('assert');
 const ConfigurationWindows = require('../src/ConfigurationWindows.js');
 
@@ -5,14 +7,90 @@ new ConfigurationWindows(undefined).printInfo();
 
 describe('ConfigurationWindows', function() {
 
-    describe('publicKey', function() {
+    describe('defaults must be valid', function() {
 
-        it('must be valid', () => {
-            let testee = new ConfigurationWindows(undefined);
+        let testee = new ConfigurationWindows(undefined);
+
+        it('isPortableMode', () => {
+            let file = process.execPath + '.portable';
+            fs.createFileSync(file);
+            assert.equal(ConfigurationWindows.isPortableMode, true);
+            fs.unlinkSync(file);
+            assert.equal(ConfigurationWindows.isPortableMode, false);
+        });
+
+        it('publicKey', () => {
             assert.equal(testee.publicKey.startsWith('-----BEGIN PUBLIC KEY-----'), true);
             assert.equal(testee.publicKey.endsWith('-----END PUBLIC KEY-----'), true);
-            assert.equal(testee.publicKey.length, 450);
             assert.equal(testee.publicKey.includes('owIDAQAB'), true);
+            assert.equal(testee.publicKey.length, 450);
+        });
+
+        it('applicationProtocol', () => {
+            assert.equal(testee.applicationProtocol, 'cache');
+        });
+
+        it('applicationUpdateURL', () => {
+            assert.equal(testee.applicationUpdateURL, 'http://static.hakuneko.download/5.0/latest');
+        });
+
+        it('connectorProtocol', () => {
+            assert.equal(testee.connectorProtocol, 'connector');
+        });
+
+        it('applicationStartupURL', () => {
+            assert.equal(testee.applicationStartupURL, 'cache://hakuneko/index.html');
+        });
+
+        (process.platform === 'win32' ? it : it.skip)('applicationCacheDirectory', () => {
+            assert.equal(testee.applicationCacheDirectory.includes(path.join('AppData', 'Local', 'Electron', 'cache')), true);
+        });
+
+        (process.platform === 'win32' ? it : it.skip)('applicationUserDataDirectory', () => {
+            // NOTE: This fails, because the temporary mocha path is detected
+            assert.equal(testee.applicationUserDataDirectory.includes(path.join('AppData', 'Roaming', 'Electron')), true);
+        });
+    });
+
+    describe('must accept supported options', function() {
+
+        let expected = {
+            publicKey: 'publicKey',
+            applicationProtocol: 'applicationProtocol',
+            applicationUpdateURL: 'applicationUpdateURL',
+            connectorProtocol: 'connectorProtocol',
+            applicationStartupURL: 'protocol://applicationStartupURL',
+            applicationCacheDirectory: 'applicationCacheDirectory',
+            applicationUserDataDirectory: 'applicationUserDataDirectory'
+        };
+        let testee = new ConfigurationWindows(expected);
+
+        it('publicKey', () => {
+            assert.notEqual(testee.publicKey, expected.publicKey);
+        });
+
+        it('applicationProtocol', () => {
+            assert.equal(testee.applicationProtocol, expected.applicationStartupURL.split(':')[0]);
+        });
+
+        it('applicationUpdateURL', () => {
+            assert.equal(testee.applicationUpdateURL, expected.applicationUpdateURL);
+        });
+
+        it('connectorProtocol', () => {
+            assert.notEqual(testee.connectorProtocol, expected.connectorProtocol);
+        });
+
+        it('applicationStartupURL', () => {
+            assert.equal(testee.applicationStartupURL, expected.applicationStartupURL);
+        });
+
+        it('applicationCacheDirectory', () => {
+            assert.equal(testee.applicationCacheDirectory.endsWith(expected.applicationCacheDirectory), true);
+        });
+
+        it('applicationUserDataDirectory', () => {
+            assert.equal(testee.applicationUserDataDirectory.endsWith(expected.applicationUserDataDirectory), true);
         });
     });
 });
