d72ea7f99ad437f62ac7a10a661081e6d2969ff7
Ronny Wegener <wegener.ronny@gmail.com>
2019-08-27 17:30:48 +0200

    extract assertion method for e2e connector test

--- src/__tests__/Connectors.e2e.js
+++ src/__tests__/Connectors.e2e.js
@@ -23,6 +23,47 @@ const connection = {
     defaultViewport: null
 };
 
+async function assertConnector(browserPage, parameters, expectations) {
+
+    // or use an all in one method that runs fully in renderer scope?
+
+    // get connector for website as reference to the remote instance
+    let remoteConnector = await browserPage.evaluateHandle(connectorID => {
+        return Engine.Connectors.find(connector => connector.id === connectorID);
+    }, parameters.connectorID);
+    expect(remoteConnector._remoteObject.className).toEqual(expectations.connectorClass);
+
+    // get manga for website as reference to the remote instance
+    let remoteManga = await browserPage.evaluateHandle((connector, mangaURL) => {
+        return connector.getMangaFromURI(new URL(mangaURL));
+    }, remoteConnector, parameters.mangaURL);
+    expect(remoteManga._remoteObject.className).toEqual('Manga');
+
+    // get chapters for manga as reference to the remote instance
+    let remoteChapters = await browserPage.evaluateHandle(manga => {
+        return new Promise( resolve => {
+            manga.getChapters((_, chapters) => resolve(chapters));
+        });
+    }, remoteManga);
+    expect(remoteChapters._remoteObject.className).toEqual('Array');
+
+    // get first chapter for chapters as reference to the remote instance
+    let remoteChapter = await browserPage.evaluateHandle((chapters, chaptersMethod) => {
+        // first => shift, last => pop, INT => getAtIndex
+        return chapters[chaptersMethod]();
+    }, remoteChapters, parameters.chaptersMethod);
+    expect(remoteChapter._remoteObject.className).toEqual('Chapter');
+
+    // get pages for chapter as reference to the remote instance
+    let pages = await browserPage.evaluate(chapter => {
+        return new Promise(resolve => {
+            chapter.getPages((_, pages) => resolve(pages));
+        });
+    }, remoteChapter);
+    expect(pages.length).toEqual(expectations.pageCount);
+    pages.forEach(page => expect(page).toMatch(expectations.pageMatcher));
+}
+
 describe("HakuNeko Engine", () => {
 
     jest.setTimeout(25000);
@@ -60,34 +101,17 @@ describe("HakuNeko Engine", () => {
     describe('Connectors', function() {
 
         it('should support MangaDex', async () => {
-            // get connector for website as reference to the remote instance
-            let remoteConnector = await page.evaluateHandle(() => Engine.Connectors.find(connector => connector.id === 'mangadex'));
-            expect(remoteConnector._remoteObject.className).toEqual('MangaDex');
-            // get manga for website as reference to the remote instance
-            let remoteManga = await page.evaluateHandle(connector => {
-                return connector.getMangaFromURI(new URL('https://mangadex.org/title/20164/they-say-i-was-born-a-king-s-daughter'));
-            }, remoteConnector);
-            expect(remoteManga._remoteObject.className).toEqual('Manga');
-            // get chapters for manga as reference to the remote instance
-            let remoteChapters = await page.evaluateHandle(manga => {
-                return new Promise( resolve => {
-                    manga.getChapters((_, chapters) => resolve(chapters));
-                });
-            }, remoteManga);
-            expect(remoteChapters._remoteObject.className).toEqual('Array');
-            // get first chapter for chapters as reference to the remote instance
-            let remoteChapter = await page.evaluateHandle(chapters => {
-                return chapters.shift();
-            }, remoteChapters);
-            expect(remoteChapter._remoteObject.className).toEqual('Chapter');
-            // get pages for chapter as reference to the remote instance
-            let pages = await page.evaluate(chapter => {
-                return new Promise( resolve => {
-                    chapter.getPages((_, pages) => resolve(pages));
-                });
-            }, remoteChapter);
-            expect(pages.length).toEqual(51);
-            pages.forEach(page => expect(page).toMatch(/^https:\/\/s\d+.mangadex.org\/data\/[0-9a-f]{32}\/R\d+\.jpg$/));
+           await assertConnector(page, {
+                connectorID: 'mangadex',
+                mangaURL: 'https://mangadex.org/title/20164/they-say-i-was-born-a-king-s-daughter',
+                chaptersMethod: 'shift' // first => shift, last => pop
+           }, {
+                connectorClass: 'MangaDex',
+                mangaTitle: '',
+                chapterTitle: '',
+                pageCount: 51,
+                pageMatcher: /^https:\/\/s\d+.mangadex.org\/data\/[0-9a-f]{32}\/R\d+\.jpg$/
+           });
         });
     });
 });
