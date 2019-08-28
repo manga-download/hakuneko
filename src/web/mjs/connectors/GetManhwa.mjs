648def1cee34063b510e62d03e5050ed7b357cf5
Ronny Wegener <wegener.ronny@gmail.com>
2019-08-28 21:58:26 +0200

    [getmanhwa] fixed page layout changes references #384

--- src/web/mjs/connectors/GetManhwa.mjs
+++ src/web/mjs/connectors/GetManhwa.mjs
@@ -1,14 +1,8 @@
 import Connector from '../engine/Connector.mjs';
 import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class GetManhwa extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'getmanhwa';
@@ -22,7 +16,7 @@ export default class GetManhwa extends Connector {
      */
     _getMangaFromURI( uri ) {
         let request = new Request( uri.href, this.requestOptions );
-        return this.fetchDOM( request, 'div.elementor-widget-container h2.elementor-heading-title' )
+        return this.fetchDOM( request, 'div#content div.elementor-widget-wrap div.elementor-widget-container > h2.elementor-heading-title' )
             .then( data => {
                 let id = uri.pathname + uri.search;
                 let title = data[0].innerText.trim();
@@ -36,12 +30,15 @@ export default class GetManhwa extends Connector {
     _getMangaListFromPages( mangaPageLinks, index ) {
         index = index || 0;
         let request = new Request( mangaPageLinks[ index ], this.requestOptions );
-        return this.fetchDOM( request, 'section.elementor-hidden-tablet.elementor-hidden-phone div.make-column-clickable-elementor h2.elementor-heading-title', 5 )
+        return fetch( request )
+            .then( response => response.text() )
             .then( data => {
+                data = [...this.createDOM( data, false )
+                    .querySelectorAll( 'div#content div.elementor-widget-container > div.elementor-image > a[data-elementor-open-lightbox]' )];
                 let mangaList = data.map( element => {
                     return {
-                        id: this.getRootRelativeOrAbsoluteLink( element.closest( 'div.make-column-clickable-elementor' ).dataset.columnClickable, request.url ),
-                        title: element.innerText.replace( /[\r\n\t]/g, '' ).trim()
+                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
+                        title: element.querySelector('img').alt.replace( '2018_05_23_15270545846574_result', 'her time' ).trim()
                     };
                 } );
                 if( index < mangaPageLinks.length - 1 ) {
@@ -57,7 +54,7 @@ export default class GetManhwa extends Connector {
      *
      */
     _getMangaList( callback ) {
-        let pageLinks = [ 'romance', 'drama', 'comedy', 'fantasy', 'action', 'bl', 'school-life' ].map( genre => this.url + '/g-' + genre + '/' );
+        let pageLinks = [ 'romance', 'drama', 'comedy', 'fantasy', 'action', 'bl', 'gl', 'horror', 'school-life', 'scifi' ].map( genre => this.url + '/genre-' + genre + '/' );
         this._getMangaListFromPages( pageLinks )
             .then( data => {
                 callback( null, data );
@@ -77,12 +74,13 @@ export default class GetManhwa extends Connector {
             .then( data => {
                 let chapterList = data.map( element => {
                     return {
-                        id: this.getRootRelativeOrAbsoluteLink( element.closest( 'div.make-column-clickable-elementor' ).dataset.columnClickable, request.url ),
+                        // chapter links may still point to the old domain 'https://getmanhwa.co'
+                        id: this.getAbsolutePath( element.closest( 'div.make-column-clickable-elementor' ).dataset.columnClickable, request.url ),
                         title: element.innerText.replace( manga.title, '' ).trim(),
                         language: ''
                     };
                 } )
-                    .filter( chapter => chapter.title.toLowerCase() !== 'read' );
+                    .filter( chapter => chapter.title.toLowerCase() !== 'start reading' );
                 callback( null, chapterList );
             } )
             .catch( error => {
@@ -95,8 +93,9 @@ export default class GetManhwa extends Connector {
      *
      */
     _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'article.single-page-article div.elementor-image source' )
+        let uri = new URL( chapter.id, this.url );
+        let request = new Request( uri.href, this.requestOptions );
+        this.fetchDOM( request, 'div.reading-content div.page-break source.wp-manga-chapter-img' )
             .then( data => {
                 let pageList = data.map( element => this.getAbsolutePath( element, request.url ) );
                 callback( null, pageList );
