55e97a919d7e704338369392902a446759ba466c
Ronny Wegener <wegener.ronny@gmail.com>
2019-08-30 21:17:50 +0200

    [mangawow] added website support

---
+++ src/web/mjs/connectors/MangaWOW.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MangaWOW extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mangawow';
+        super.label = 'MangaWOW';
+        this.tags = [ 'webtoon', 'turkish' ];
+        this.url = 'https://manga-wow.com';
+    }
+}
