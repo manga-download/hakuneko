49bef72fffa0791f6154abaed0990c7caf9b1fcb
Ronny Wegener <wegener.ronny@gmail.com>
2019-08-31 17:07:24 +0200

    [holymnga] updated domain references #394

--- src/web/mjs/connectors/HolyManga.mjs
+++ src/web/mjs/connectors/HolyManga.mjs
@@ -1,18 +1,12 @@
 import HeavenManga from './HeavenManga.mjs';
 
-/**
- *
- */
 export default class HolyManga extends HeavenManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'holymanga';
         super.label = 'HolyManga';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'http://ww3.holymanga.net';
+        this.url = 'http://ww4.holymanga.net';
     }
 }
