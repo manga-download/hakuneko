b548721e8f5e62fedefeb2e1cfcc1c4ad99b5e4e
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-05 01:13:55 +0200

    [mangakomi] added website support seems to be successor of mangalike

---
+++ src/web/mjs/connectors/MangaKomi.mjs
@@ -0,0 +1,14 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MangaKomi extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mangakomi';
+        super.label = 'Manga Komi';
+        this.tags = [ 'manga', 'english' ];
+        this.url = 'https://mangakomi.com';
+
+        this.queryMangas = 'div.post-title h3 a';
+    }
+}
