c3fec8181990dc304c845cd6f22a8caf56150601
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-05 08:20:27 +0200

    [readwebtoons] added website support

---
+++ src/web/mjs/connectors/ReadWebtoons.mjs
@@ -0,0 +1,14 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class ReadWebtoons extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'readwebtoons';
+        super.label = 'ReadWebtoons'
+        this.tags = [ 'webtoon', 'english' ];
+        this.url = 'https://readwebtoons.com';
+
+        this.queryPages = 'div.reading-content p source';
+    }
+}
