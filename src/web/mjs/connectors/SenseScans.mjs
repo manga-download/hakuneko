7aaa9eb593cd3d2fd5d0f778b0527eb3b7e8371c
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-06 20:40:59 +0200

    [sensescans] fixed domain and path

--- src/web/mjs/connectors/SenseScans.mjs
+++ src/web/mjs/connectors/SenseScans.mjs
@@ -1,20 +1,14 @@
 import FoolSlide from './templates/FoolSlide.mjs';
 
-/**
- *
- */
 export default class SenseScans extends FoolSlide {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'sensescans';
         super.label = 'SenseScans';
         this.tags = [ 'manga', 'high-quality', 'english', 'scanlation' ];
-        this.url = 'http://reader.sensescans.com';
-        //this.path        = '/directory/';
+        this.url = 'http://sensescans.com';
+        this.path = '/reader/directory/';
         this.language = 'english';
     }
 }
