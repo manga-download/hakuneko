56a80fb7532f124649da5e21ee7864e466047b89
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-07 16:32:26 +0200

    [mangakakalot] updated tags

--- src/web/mjs/connectors/MangaKakalot.mjs
+++ src/web/mjs/connectors/MangaKakalot.mjs
@@ -1,20 +1,12 @@
 import MangaNel from './MangaNel.mjs';
 
-/**
- *
- */
 export default class MangaKakalot extends MangaNel {
 
-    /**
-     *
-     */
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = 'mangakakalot';
         super.label = 'MangaKakalot';
-        this.tags = [ 'manga', 'english' ];
-        // Private members for internal usage only (convenience)
+        this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://mangakakalot.com';
     }
 }
