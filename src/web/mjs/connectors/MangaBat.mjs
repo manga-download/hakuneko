029fbdfe4d28e1da537d417e3f13f6647da2e0e6
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-07 16:32:12 +0200

    [mangabar] updated tags

--- src/web/mjs/connectors/MangaBat.mjs
+++ src/web/mjs/connectors/MangaBat.mjs
@@ -1,22 +1,15 @@
 import MangaNel from './MangaNel.mjs';
 
-/**
- * Previously MangaSupa
- */
 export default class MangaBat extends MangaNel {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangabat';
         super.label = 'MangaBat';
-        this.tags = [ 'manga', 'english' ];
+        this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://mangabat.com';
 
         this.queryMangasPageCount = 'div.group-page a.page:last-of-type';
         this.queryMangas = 'div.list_category h3 a';
-
     }
 }
