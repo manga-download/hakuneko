92a3c82bf55ca7aa524a8376078a44a0cac73663
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-13 02:18:51 +0200

    updated e2e test suite structure

--- src/__tests__/Connectors.e2e.js
+++ src/__tests__/Connectors.e2e.js
@@ -113,69 +113,81 @@ describe("HakuNeko Engine", () => {
         await page.waitForSelector('hakuneko-app');
     });
 
-    describe('Connectors', function() {
+    describe('Connectors', () => {
 
-        it('should support MangaDex', async () => {
-            await assertConnector(page, {
-                connectorID: 'mangadex',
-                mangaURL: 'https://mangadex.org/title/20164/they-say-i-was-born-a-king-s-daughter',
-                chaptersAccessor: 0 // first => shift, last => pop, index => Integer
-            }, {
-                connectorClass: 'MangaDex',
-                mangaID: '20164',
-                mangaTitle: 'They Say I Was Born a King\'s Daughter',
-                chapterID: '534370',
-                chapterTitle: 'Ch.0001 (br) [Usagi Scan]',
-                pageCount: 51,
-                pageMatcher: /^https:\/\/s\d+.mangadex.org\/data\/[0-9a-f]{32}\/R\d+\.jpg$/
+        describe('Clipboard', () => {
+            //
+        });
+
+        describe('MangaDex', () => {
+            it('should get manga, chapters and page links', async () => {
+                await assertConnector(page, {
+                    connectorID: 'mangadex',
+                    mangaURL: 'https://mangadex.org/title/20164/they-say-i-was-born-a-king-s-daughter',
+                    chaptersAccessor: 0 // first => shift, last => pop, index => Integer
+                }, {
+                    connectorClass: 'MangaDex',
+                    mangaID: '20164',
+                    mangaTitle: 'They Say I Was Born a King\'s Daughter',
+                    chapterID: '534370',
+                    chapterTitle: 'Ch.0001 (br) [Usagi Scan]',
+                    pageCount: 51,
+                    pageMatcher: /^https:\/\/s\d+.mangadex.org\/data\/[0-9a-f]{32}\/R\d+\.jpg$/
+                });
             });
         });
 
-        it('should support KissManga', async () => {
-            await assertConnector(page, {
-                connectorID: 'kissmanga',
-                mangaURL: 'https://kissmanga.com/Manga/Black-Clover',
-                chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
-            }, {
-                connectorClass: 'KissManga',
-                mangaID: '/Manga/Black-Clover',
-                mangaTitle: 'Black Clover',
-                chapterID: '/Manga/Black-Clover/Chapter-001?id=277498',
-                chapterTitle: '001',
-                pageCount: 50,
-                pageMatcher: /^http:\/\/2\.bp\.blogspot\.com\/-[a-zA-Z0-9_-]{11}\/Vjv[a-zA-Z0-9_-]{7}I\/AAAAAAAE[a-zA-Z0-9]{3}\/[a-zA-Z0-9_-]{11}\/s16000\/0001-0\d{2}.jpg$/
+        describe('KissManga', () => {
+            it('should get manga, chapters and page links', async () => {
+                await assertConnector(page, {
+                    connectorID: 'kissmanga',
+                    mangaURL: 'https://kissmanga.com/Manga/Black-Clover',
+                    chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
+                }, {
+                    connectorClass: 'KissManga',
+                    mangaID: '/Manga/Black-Clover',
+                    mangaTitle: 'Black Clover',
+                    chapterID: '/Manga/Black-Clover/Chapter-001?id=277498',
+                    chapterTitle: '001',
+                    pageCount: 50,
+                    pageMatcher: /^http:\/\/2\.bp\.blogspot\.com\/-[a-zA-Z0-9_-]{11}\/Vjv[a-zA-Z0-9_-]{7}I\/AAAAAAAE[a-zA-Z0-9]{3}\/[a-zA-Z0-9_-]{11}\/s16000\/0001-0\d{2}.jpg$/
+                });
             });
         });
 
-        it('should support MangaGo', async () => {
-            await assertConnector(page, {
-                connectorID: 'mangago',
-                mangaURL: 'http://www.mangago.me/read-manga/black_clover/',
-                chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
-            }, {
-                connectorClass: 'MangaGo',
-                mangaID: '/read-manga/black_clover/',
-                mangaTitle: 'Black Clover',
-                chapterID: '/read-manga/black_clover/bt/314637/Ch1/',
-                chapterTitle: 'Ch.1  : The Boy\'s Vow',
-                pageCount: 51,
-                pageMatcher: /^http:\/\/iweb\d.mangapicgallery.com\/r\/newpiclink\/black_clover\/1\/[a-z0-9]{32}\.(?:png|jpg|jpeg)$/
+        describe('MangaGo', () => {
+            it('should get manga, chapters and page links', async () => {
+                await assertConnector(page, {
+                    connectorID: 'mangago',
+                    mangaURL: 'http://www.mangago.me/read-manga/black_clover/',
+                    chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
+                }, {
+                    connectorClass: 'MangaGo',
+                    mangaID: '/read-manga/black_clover/',
+                    mangaTitle: 'Black Clover',
+                    chapterID: '/read-manga/black_clover/bt/314637/Ch1/',
+                    chapterTitle: 'Ch.1  : The Boy\'s Vow',
+                    pageCount: 51,
+                    pageMatcher: /^http:\/\/iweb\d.mangapicgallery.com\/r\/newpiclink\/black_clover\/1\/[a-z0-9]{32}\.(?:png|jpg|jpeg)$/
+                });
             });
         });
 
-        it('should support EpikManga', async () => {
-            await assertConnector(page, {
-                connectorID: 'epikmanga',
-                mangaURL: 'https://www.epikmanga.com/seri/battle-through-the-heavens',
-                chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
-            }, {
-                connectorClass: 'EpikManga',
-                mangaID: '/seri/battle-through-the-heavens',
-                mangaTitle: 'Battle Through the Heavens',
-                chapterID: '/seri/battle-through-the-heavens/bolum/6',
-                chapterTitle: '#1 Artık Dahi Değil',
-                pageCount: 20,
-                pageMatcher: /^https:\/\/www\.epikmanga.com\/upload\/manga\/battle-through-the-heavens\/1\/\d+.jpg$/
+        describe('EpikManga', () => {
+            it('should get manga, chapters and page links', async () => {
+                await assertConnector(page, {
+                    connectorID: 'epikmanga',
+                    mangaURL: 'https://www.epikmanga.com/seri/battle-through-the-heavens',
+                    chaptersAccessor: 'pop' // first => shift, last => pop, index => Integer
+                }, {
+                    connectorClass: 'EpikManga',
+                    mangaID: '/seri/battle-through-the-heavens',
+                    mangaTitle: 'Battle Through the Heavens',
+                    chapterID: '/seri/battle-through-the-heavens/bolum/6',
+                    chapterTitle: '#1 Artık Dahi Değil',
+                    pageCount: 20,
+                    pageMatcher: /^https:\/\/www\.epikmanga.com\/upload\/manga\/battle-through-the-heavens\/1\/\d+.jpg$/
+                });
             });
         });
     });
