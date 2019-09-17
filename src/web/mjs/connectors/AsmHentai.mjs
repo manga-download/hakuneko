9261077bcf553f06673030ceefbab7376d49717d
Ronny Wegener <wegener.ronny@gmail.com>
2019-09-17 22:29:08 +0200

    [asmhentai] fixed website support

--- src/web/mjs/connectors/AsmHentai.mjs
+++ src/web/mjs/connectors/AsmHentai.mjs
@@ -1,28 +1,56 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class AsmHentai extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'asmhentai';
         super.label = 'AsmHentai';
-        this.tags = [];
+        this.tags = ['hentai', 'multi-lingual'];
         this.url = 'https://asmhentai.com';
     }
 
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+    /**
+     * Overwrite base function to get manga from clipboard link.
+     */
+    async _getMangaFromURI(uri) {
+        try {
+            let data = await this.fetchDOM(uri.href, 'div.book_page div.info h1', 3);
+            let id = uri.pathname;
+            let element = data[0];
+            this.cfMailDecrypt(element);
+            let title = element.textContent.trim();
+            return new Manga(this, id, title);
+        } catch(error) {
+            return null;
+        }
     }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getMangaList(callback) {
+        let msg = 'This website does not provide a manga list, please copy and paste the URL containing the images directly from your browser into HakuNeko.';
+        callback( new Error( msg ), undefined );
     }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getChapterList(manga, callback) {
+        try {
+            let chapterList = [ Object.assign({ language: '' }, manga) ];
+            callback(null, chapterList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
+    }
+
+    async _getPageList(manga, chapter, callback) {
+        try {
+            let request = new Request(this.url + chapter.id, this.requestOptions);
+            let data = await this.fetchDOM(request, 'div.gallery source.lazy');
+            let pageList = data.map(element => this.getAbsolutePath(element.dataset.src.replace('t.jpg', '.jpg'), request.url));
+            callback(null, pageList);
+        } catch(error) {
+            console.error(error, chapter);
+            callback(error, undefined);
+        }
     }
 }
