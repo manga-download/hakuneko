d01a1a61431eb06c8aa73ec24cf1b2f773ff224b
Ronny Wegener <wegener.ronny@gmail.com>
2019-10-01 07:51:39 +0200

    changed update URL and deployment directory

--- src/app/Configuration.js
+++ src/app/Configuration.js
@@ -23,7 +23,7 @@ module.exports = class Configuration {
     constructor(configuration) {
         let options = configuration || {};
         let applicationExecutableDirectory = path.dirname(electron.app.getPath('exe'));
-        this._applicationUpdateURL = options['applicationUpdateURL'] || 'http://static.hakuneko.download/6.0/latest';
+        this._applicationUpdateURL = options['applicationUpdateURL'] || 'https://manga-download.gihub.io/hakuneko/master/latest';
         this._applicationStartupURL = options['applicationStartupURL'] || 'hakuneko://cache/index.html';
         this._applicationCacheDirectory = options['applicationCacheDirectory'] || path.join(applicationExecutableDirectory, 'cache');
         this._applicationUserDataDirectory = options['applicationUserDataDirectory'] || path.join(applicationExecutableDirectory, 'userdata');
