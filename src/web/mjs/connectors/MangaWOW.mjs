d06c7295fab42acb0d2200cd21ec38abcdc11d1f
ronny1982 <wegener.ronny@gmail.com>
2019-10-05 15:58:30 +0200

    [mangawow] removed anonymous analytics, which was needed to access the website removal request (#494)

--- src/web/mjs/connectors/MangaWOW.mjs
+++ src/web/mjs/connectors/MangaWOW.mjs
@@ -9,24 +9,4 @@ export default class MangaWOW extends WordPressMadara {
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://manga-wow.com';
     }
-
-    async _getMangaList(callback) {
-        fetch('https://www.w3counter.com/track/pv?id=128203&userAgent=HakuNeko&rand=' + parseInt(1000*Math.random()), this.requestOptions);
-        super._getMangaList(callback);
-    }
-
-    async _getChapterList(manga, callback) {
-        fetch('https://www.w3counter.com/track/pv?id=128204&userAgent=HakuNeko&rand=' + parseInt(1000*Math.random()), this.requestOptions);
-        super._getChapterList(manga, callback);
-    }
-
-    async _getPageList(manga, chapter, callback) {
-        fetch('https://www.w3counter.com/track/pv?id=128208&userAgent=HakuNeko&rand=' + parseInt(1000*Math.random()), this.requestOptions);
-        super._getPageList(manga, chapter, callback);
-    }
-
-    async _handleConnectorURI(payload) {
-        fetch('https://www.w3counter.com/track/pv?id=128207&userAgent=HakuNeko&rand=' + parseInt(1000*Math.random()), this.requestOptions);
-        return super._handleConnectorURI(payload);
-    }
 }
