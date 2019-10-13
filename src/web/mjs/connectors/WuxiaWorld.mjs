0694146207ffbfbe35cf32eacc896a9e9b07f369
Ronny Wegener <wegener.ronny@gmail.com>
2019-10-13 14:49:44 +0200

    [wuxiaworld] added support for novels (text)

--- src/web/mjs/connectors/WuxiaWorld.mjs
+++ src/web/mjs/connectors/WuxiaWorld.mjs
@@ -10,5 +10,41 @@ export default class WuxiaWorld extends WordPressMadara {
         this.url = 'https://wuxiaworld.site';
 
         //this.formManga.append('vars[wp-manga-tag]', 'webcomics');
+        this.novelFormat = 'image/png';
+    }
+
+    async _getPageList(manga, chapter, callback) {
+        try {
+            let uri = new URL(chapter.id, this.url);
+            uri.searchParams.set('style', 'list');
+            let request = new Request(uri.href, this.requestOptions);
+            let data = await this.fetchDOM(request, 'div.reading-content div[class^="text-"');
+            if(data.length > 0) {
+                let pageLinks = await this._getPageListNovel(request);
+                callback(null, pageLinks);
+            } else {
+                super._getPageList(manga, chapter, callback);
+            }
+        } catch(error) {
+            console.error(error, chapter);
+            callback(error, undefined);
+        }
+    }
+
+    async _getPageListNovel(request) {
+        let script = `
+            new Promise(resolve => {
+                let novel = document.querySelector('div.entry-content');
+                novel.style.padding = '1.5em';
+                let script = document.createElement('script');
+                script.onload = async function() {
+                    let canvas = await html2canvas(novel);
+                    resolve(canvas.toDataURL('${this.novelFormat}'));
+                }
+                script.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js';
+                document.body.appendChild(script);
+            });
+        `;
+        return [ await Engine.Request.fetchUI(request, script) ];
     }
 }
