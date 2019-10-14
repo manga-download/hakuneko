bb00085854ff234c7e0de50dee8cbef5dd243a68
ronny1982 <wegener.ronny@gmail.com>
2019-10-14 22:21:09 +0200

    [mangaku] fixed website support (#525)

--- src/web/mjs/connectors/MangaKu.mjs
+++ src/web/mjs/connectors/MangaKu.mjs
@@ -1,28 +1,59 @@
 import Connector from '../engine/Connector.mjs';
 
-/**
- *
- */
 export default class MangaKu extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangaku';
         super.label = 'MangaKu';
-        this.tags = [];
-        this.url = 'http://mangaku.in';
+        this.tags = [ 'manga', 'indonesian' ];
+        this.url = 'https://mangaku.in';
     }
 
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+    async _getMangaList(callback) {
+        try {
+            let request = new Request(this.url + '/daftar-komik-bahasa-indonesia', this.requestOptions);
+            let data = await this.fetchDOM(request, 'div.series_col ul li a.screenshot');
+            let mangaList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.trim()
+                };
+            });
+            callback(null, mangaList);
+        } catch(error) {
+            console.error(error, this);
+            callback(error, undefined);
+        }
     }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getChapterList(manga, callback) {
+        try {
+            let request = new Request(this.url + manga.id, this.requestOptions);
+            let data = await this.fetchDOM(request, 'small div[style*="border-radius"] a');
+            let chapterList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.replace(manga.title, '').replace('–', '').trim(),
+                    language: ''
+                };
+            });
+            callback(null, chapterList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getPageList(manga, chapter, callback) {
+        try {
+            let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+            let data = await this.fetchDOM(request, 'div.entry div.separator source, div.entry-content source[alt*="Mirror"]');
+            let pageList = data.map(element => this.getAbsolutePath(element, request.url));
+            callback(null, pageList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
 }
