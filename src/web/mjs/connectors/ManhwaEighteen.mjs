85a954c0301196bd04c5e5583c5bd80a9951fa4c
Ronny Wegener <wegener.ronny@gmail.com>
2019-10-15 20:37:44 +0200

    [manhwa18-int] added website support

--- src/web/mjs/connectors/ManhwaEighteen.mjs
+++ src/web/mjs/connectors/ManhwaEighteen.mjs
@@ -4,11 +4,11 @@ export default class ManhwaEighteen extends WordPressMadara {
 
     constructor() {
         super();
-        super.id = 'manhwa18-new';
+        super.id = 'manhwa18-int';
         super.label = 'Manhwa 18 (.net)';
         this.tags = [ 'hentai', 'english' ];
         this.url = 'https://manhwa18.net';
-    }
 
-    // TODO: images not working
+        this.queryPages = 'div.read-container div.reading-content source';
+    }
 }
