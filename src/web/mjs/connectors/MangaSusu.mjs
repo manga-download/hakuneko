e4e3b21926c471a69e4f43616bd89ea19659ed79
ronny1982 <wegener.ronny@gmail.com>
2019-10-15 08:26:29 +0200

    [mangasusu] added website support (#526)

---
+++ src/web/mjs/connectors/MangaSusu.mjs
@@ -0,0 +1,14 @@
+import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
+
+export default class MangaSusu extends MangaReaderCMS {
+
+    constructor() {
+        super();
+        super.id = 'mangasusu';
+        super.label = 'MangaSusu';
+        this.tags = [ 'webtoon', 'hentai', 'indonesian' ];
+        this.url = 'https://www.mangasusu.me';
+
+        this.language = 'id';
+    }
+}
