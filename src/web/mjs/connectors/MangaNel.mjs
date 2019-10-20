719c775f392b9bbeaf403304a6c1eb8744d5ef6b
Ronny Wegener <wegener.ronny@gmail.com>
2019-10-20 15:42:25 +0200

    [manganel] improved ternary condition

--- src/web/mjs/connectors/MangaNel.mjs
+++ src/web/mjs/connectors/MangaNel.mjs
@@ -80,7 +80,7 @@ export default class MangaNel extends Connector {
             } )
             .then( data => {
                 let redirect = data.match( /window.location.assign\(\s*['"]([^'"]+)['"]\s*\)/ );
-                return this.fetchDOM( redirect && !this.id === 'manganel' ? redirect[1] : uri, this.queryChapters );
+                return this.fetchDOM( this.id !== 'manganel' && redirect ? redirect[1] : uri, this.queryChapters );
             } )
             .then( data => {
                 let chapterList = data.map( element => {
