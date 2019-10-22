4e1ac9feb4bbb9533c7a980b40647cf509012ac0
ronny1982 <wegener.ronny@gmail.com>
2019-10-22 19:09:01 +0200

    [mangashiro] updated domain (#570)
    * [wordpressemanga] improved/fixed image link extraction
    
    * [wordpressemanga] changed to async/await pattern
    
    * [mangashiro] updated domain

--- src/web/mjs/connectors/templates/WordPressEManga.mjs
+++ src/web/mjs/connectors/templates/WordPressEManga.mjs
@@ -15,63 +15,51 @@ export default class WordPressEManga extends Connector {
         this.queryPages = 'div#readerarea source[src]:not([src=""])';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        this.fetchDOM( this.url + this.path, this.queryMangas )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRelativeLink( element ),
-                        title: element.text.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangaList(callback) {
+        try {
+            let request = new Request(this.url + this.path, this.requestOptions);
+            let data = await this.fetchDOM(request, this.queryMangas);
+            let mangaList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.trim()
+                };
+            });
+            callback(null, mangaList);
+        } catch(error) {
+            console.error(error, this);
+            callback(error, undefined);
+        }
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        return this.fetchDOM( this.url + manga.id, this.queryChapters )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRelativeLink( element ),
-                        title: element.text.replace( manga.title, '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapterList(manga, callback) {
+        try {
+            let request = new Request(this.url + manga.id, this.requestOptions);
+            let data = await this.fetchDOM(request, this.queryChapters);
+            let chapterList = data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                    title: element.text.replace(manga.title, '').trim(),
+                    language: ''
+                };
+            });
+            callback(null, chapterList);
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        return this.fetchDOM( this.url + chapter.id, this.queryPages )
-            .then( data => {
-                let pageLinks = data.map( element => {
-                    let link = element.dataset['lazySrc'] || element.src;
-                    return ( link.startsWith( 'http' ) ? '' : 'http:' ) + link;
-                } )
-                    .filter( link => !link.includes( 'histats.com' ) );
-                callback( null, pageLinks );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getPageList(manga, chapter, callback) {
+        try {
+            let request = new Request(this.url + chapter.id, this.requestOptions);
+            let data = await this.fetchDOM(request, this.queryPages);
+            let pageLinks = data.map(element => this.getAbsolutePath(element.dataset['lazySrc'] || element, request.url));
+            pageLinks = pageLinks.filter(link => !link.includes('histats.com'));
+            callback(null, pageLinks);
+        } catch(error) {
+            console.error(error, chapter);
+            callback(error, undefined);
+        }
     }
 }
