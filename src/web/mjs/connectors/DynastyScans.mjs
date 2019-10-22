240bdb6ee23236914a4e98206631c2471e015ada
Neogeek <https://github.com/medmig>
2019-10-22 23:21:40 +0200

    [DynastyScans] Minor corrections and improvements  - Correct an issue where a "chapter" could be a header with value "null" (e.g. /anthologies/cider_and_crybaby)

--- src/web/mjs/connectors/DynastyScans.mjs
+++ src/web/mjs/connectors/DynastyScans.mjs
@@ -24,76 +24,70 @@ export default class DynastyScans extends Connector {
     /**
      *
      */
-    _getMangaList( callback ) {
-        let promises = ['/series', '/anthologies', '/issues', '/doujins'].map( page => {
-            return this.fetchJSON( this.url + `${page}.json` )
-                .then( json => {
-                    let mangaList = [];
-                    for( let letter in json ) {
-                        mangaList = mangaList.concat( ... json[letter].map( manga => {
-                            return {
-                                id: `${page}/${manga.permalink}`,
-                                title: manga.name.trim()
-                            };
-                        }) );
-                    }
-                    return Promise.resolve( mangaList );
-                } );
-        } );
-
-        Promise.all( promises )
-            .then( mangas => {
-                callback( null, [].concat( ... mangas ) );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
-    }
-
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        this.fetchJSON( this.url + manga.id + '.json' )
-            .then( json => {
-                // filter removes the "header" elements from the list
-                let chapterList = json['taggings'].filter( chapter => !chapter.header ).map( chapter => {
-                    let title = chapter.title.replace( manga.title, '' ).trim();
-                    return {
-                        id: `/chapters/${chapter.permalink}`,
-                        title:  title === '' ? manga.title : title ,
-                        language: 'en'
-                    };
-                } );
-                // rename duplicate chapters, using a stack to count preceding chapters with the same name
-                let titleStack = [];
-                for( let chapter of chapterList ) {
-                    // do not change the command order!
-                    let duplicateCount = titleStack.filter( t => t === chapter.title ).length;
-                    titleStack.push( chapter.title );
-                    chapter.title += duplicateCount > 0 ? ' #' + duplicateCount : '' ;
+    async _getMangaList( callback ) {
+        try {
+            let promises = ['/series', '/anthologies', '/issues', '/doujins'].map( async page => {
+                let json = await this.fetchJSON( this.url + `${page}.json` );
+                let mangaList = [];
+                for( let letter in json ) {
+                    mangaList = mangaList.concat( ... json[letter].map( manga => {
+                        return {
+                            id: `${page}/${manga.permalink}`,
+                            title: manga.name.trim()
+                        };
+                    }) );
                 }
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
+                return mangaList;
             } );
+            let mangas = await Promise.all( promises );
+            callback( null, [].concat( ... mangas ) );
+        } catch(error) {
+            console.error(error, this);
+            callback(error, undefined);
+        }
+    }
+
+
+    /**
+     *
+     */
+    async _getChapterList( manga, callback ) {
+        try {
+            let json = await this.fetchJSON( new URL(this.url + manga.id + '.json', this.url).href );
+            // filter removes the "header" elements from the list
+            let chapterList = json['taggings'].filter( chapter => !('header' in chapter) ).map( chapter => {
+                return {
+                    id: `/chapters/${chapter.permalink}`,
+                    title: chapter.title.trim(),
+                    language: 'en'
+                };
+            } );
+            // rename duplicate chapters, using a stack to count preceding chapters with the same name
+            let titleStack = [];
+            for( let chapter of chapterList ) {
+                // do not change the command order!
+                let duplicateCount = titleStack.filter( t => t === chapter.title ).length;
+                titleStack.push( chapter.title );
+                chapter.title += duplicateCount > 0 ? ' #' + duplicateCount : '' ;
+            }
+            callback( null, chapterList );
+        } catch(error) {
+            console.error(error, manga);
+            callback(error, undefined);
+        }
     }
 
     /**
      *
      */
-    _getPageList( manga, chapter, callback ) {
-        this.fetchJSON( this.url + chapter.id + '.json' )
-            .then( json => {
-                let pageList = json['pages'].map( page => this.url + page.url );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getPageList( manga, chapter, callback ) {
+        try {
+            let json = await this.fetchJSON( new URL(this.url + chapter.id + '.json', this.url).href );
+            let pageList = json['pages'].map( page => this.url + page.url );
+            callback( null, pageList );
+        } catch(error) {
+            console.error(error, chapter);
+            callback(error, undefined);
+        }
     }
 }
