8cab4f33920c50f841e118cb89bdd9cdb6d17222
ronny1982 <wegener.ronny@gmail.com>
2019-10-29 21:47:23 +0100

    [wordpresszbulu] added support for new template (#585)

--- src/web/mjs/connectors/HolyManga.mjs
+++ src/web/mjs/connectors/HolyManga.mjs
@@ -1,12 +1,27 @@
-import HeavenManga from './HeavenManga.mjs';
+import WordPressZbulu from './templates/WordPressZbulu.mjs';
 
-export default class HolyManga extends HeavenManga {
+export default class HolyManga extends WordPressZbulu {
 
     constructor() {
         super();
         super.id = 'holymanga';
-        super.label = 'HolyManga';
+        super.label = 'Holy Manga';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'https://ww5.holymanga.net';
+        // TODO: set URL so it always matches clipboard paste (e.g. http://ww7.holymanga.net), but also supports redirect ...
+        this.url = 'http://ww7.holymanga.net';
+    }
+
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri, this.requestOptions);
+        await Engine.Request.fetchUI(request, '');
+        let response = await fetch(request);
+        this.url = new URL(response.url).origin;
     }
 }
