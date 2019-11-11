c5fa94e4fb5f0abe922e8667be3305a764f1ef16
ronny1982 <wegener.ronny@gmail.com>
2019-11-11 21:13:58 +0100

    [bunnyscans] changed template (#632)

--- src/web/mjs/connectors/BunnysScans.mjs
+++ src/web/mjs/connectors/BunnysScans.mjs
@@ -1,20 +1,12 @@
-import FoolSlide from './templates/FoolSlide.mjs';
+import WordPressMadara from './templates/WordPressMadara.mjs';
 
-/**
- *
- */
-export default class BunnysScans extends FoolSlide {
+export default class BunnysScans extends WordPressMadara {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'bunnysscans';
         super.label = 'BunnysScans';
         this.tags = [ 'manga', 'high-quality', 'english', 'scanlation' ];
-        this.url = 'http://bns.shounen-ai.net';
-        this.path = '/read/directory/';
-        this.language = 'english';
+        this.url = 'http://bns.shounen-ai.net/read/';
     }
 }
