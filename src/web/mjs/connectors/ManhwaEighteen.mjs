446e06be58a32955cb544f1884d4719832597cac
ronny1982 <wegener.ronny@gmail.com>
2019-11-13 22:32:51 +0100

    [manwha18-int] updated website template (#637)

--- src/web/mjs/connectors/ManhwaEighteen.mjs
+++ src/web/mjs/connectors/ManhwaEighteen.mjs
@@ -1,14 +1,15 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import FlatManga from './templates/FlatManga.mjs';
 
-export default class ManhwaEighteen extends WordPressMadara {
+export default class ManhwaEighteen extends FlatManga {
 
     constructor() {
         super();
         super.id = 'manhwa18-int';
         super.label = 'Manhwa 18 (.net)';
-        this.tags = [ 'hentai', 'english' ];
+        this.tags = [ 'hentai', 'multi-lingual' ];
         this.url = 'https://manhwa18.net';
+        this.requestOptions.headers.set('x-referer', this.url);
 
-        this.queryPages = 'div.read-container div.reading-content source';
+        this.language = '';
     }
 }
