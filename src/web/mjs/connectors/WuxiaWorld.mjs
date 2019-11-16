031311ff3b8ede79cbea77e0e028a75c36b99747
ronny1982 <wegener.ronny@gmail.com>
2019-11-16 19:38:07 +0100

    [wordpressmadaranovel] introduced new specialized template (#654)
    * [wordpressmadaranovel] introduced new specialized template
    
    * [wuxiaworld] changed template
    
    * [wordpressmadaranovel] added ability to hide elements
    
    * [lindovel] added website support
    
    * [mynovel] added website support

--- src/web/mjs/connectors/WuxiaWorld.mjs
+++ src/web/mjs/connectors/WuxiaWorld.mjs
@@ -1,6 +1,6 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressMadaraNovel from './templates/WordPressMadaraNovel.mjs';
 
-export default class WuxiaWorld extends WordPressMadara {
+export default class WuxiaWorld extends WordPressMadaraNovel {
 
     constructor() {
         super();
@@ -10,38 +10,5 @@ export default class WuxiaWorld extends WordPressMadara {
         this.url = 'https://wuxiaworld.site';
 
         //this.formManga.append('vars[wp-manga-tag]', 'webcomics');
-        this.novelFormat = 'image/png';
-        this.novelWidth = '56em'; // parseInt(1200 / window.devicePixelRatio) + 'px';
-        this.novelPadding = '1.5em';
-    }
-
-    async _getPages(chapter) {
-        let uri = new URL(chapter.id, this.url);
-        uri.searchParams.set('style', 'list');
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.reading-content div[class^="text-"');
-        return data.length > 0 ? this._getPagesNovel(request) : super._getPages(chapter);
-    }
-
-    async _getPagesNovel(request) {
-        let script = `
-            new Promise(resolve => {
-                document.body.style.width = '${this.novelWidth}';
-                let container = document.querySelector('div.content-area > div.container');
-                container.style.maxWidth = '${this.novelWidth}';
-                container.style.padding = '0';
-                container.style.margin = '0';
-                let novel = document.querySelector('div.entry-content');
-                novel.style.padding = '${this.novelPadding}';
-                let script = document.createElement('script');
-                script.onload = async function() {
-                    let canvas = await html2canvas(novel);
-                    resolve(canvas.toDataURL('${this.novelFormat}'));
-                }
-                script.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js';
-                document.body.appendChild(script);
-            });
-        `;
-        return [ await Engine.Request.fetchUI(request, script) ];
     }
 }
