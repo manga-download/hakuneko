9f10cf72813f83d44ba670e9c4bb9cbdfe315487
ronny1982 <wegener.ronny@gmail.com>
2019-11-16 21:58:32 +0100

    [bacamanga] fixed cloudflare bypass (#656)
    updated variable name for image links

--- src/web/mjs/connectors/BacaManga.mjs
+++ src/web/mjs/connectors/BacaManga.mjs
@@ -1,13 +1,8 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
+// limited access for indonesian (and surounding) regions only
 export default class BacaManga extends WordPressEManga {
 
-    /**
-     * limited access for indonesian (and surounding) regions only
-     */
     constructor() {
         super();
         super.id = 'bacamanga';
@@ -20,15 +15,23 @@ export default class BacaManga extends WordPressEManga {
         this.queryChapters = 'div.bxcl ul li span.lchx a';
     }
 
-    /**
-     *
-     */
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(this.path, this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri, this.requestOptions);
+        await Engine.Request.fetchUI(request, '', 25000);
+    }
+
     _getPageList( manga, chapter, callback ) {
         let script = `
             new Promise(resolve => {
+                //This script does no longer work when window is not shown ...
                 /*
-                This script does no longer work when window is not shown ...
-                // "tratshcuot nwodyek nwodesuom evomesuom tratsllorcs unemtxetnoc sserpyek leehwno".split("").reverse().join("")
                 [
                     'onwheel', 'keypress', 'contextmenu', 'scrollstart', 'mousemove', 'mousedown', 'keydown', 'touchstart'
                 ].forEach(evt => document.dispatchEvent(new Event(evt)));
@@ -36,7 +39,7 @@ export default class BacaManga extends WordPressEManga {
                     resolve([...document.querySelectorAll('div.maincontent div#readerarea p img')].map(img => img.src));
                 }, 2500);
                 */
-                resolve([...jQuery('img', jQuery.parseHTML(window[telekleinchung.split('').reverse().join('')]))].map(img => img.src));
+                resolve([...jQuery('img', jQuery.parseHTML(window[araarararararara.split('').reverse().join('')]))].map(img => img.src));
             });
         `;
         let request = new Request( this.url + chapter.id, this.requestOptions );
