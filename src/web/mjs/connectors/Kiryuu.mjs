36dc4ac66d91b4e275c7184fb7b60c947d50afbf
Ronny Wegener <wegener.ronny@gmail.com>
2019-11-16 20:16:17 +0100

    [kiryuu] filter non-manga images

--- src/web/mjs/connectors/Kiryuu.mjs
+++ src/web/mjs/connectors/Kiryuu.mjs
@@ -1,13 +1,7 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class Kiryuu extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'kiryuu';
@@ -18,5 +12,14 @@ export default class Kiryuu extends WordPressEManga {
 
         this.queryChapters = 'div.bxcl ul li span.lchx a';
         this.queryPages = 'div#readerarea > :not(.kln) source[src]:not([src=""])';
+    }
+
+    async _getPages(chapter) {
+        let pageList = await super._getPages(chapter);
+        return pageList.filter(link => {
+            return !link.includes('.filerun.thumbnails')
+                && !link.endsWith(',5.jpg')
+                && !link.endsWith('ZZ.jpg');
+        });
     }
 }
