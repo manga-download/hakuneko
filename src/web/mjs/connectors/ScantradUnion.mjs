28432f4032aa1c1b9a2cd85782d0a6246cfffc7a
Ronny Wegener <ronny@MacBook-2.local>
2019-11-20 00:13:30 +0100

    [scantradunion] adapted new API

--- src/web/mjs/connectors/ScantradUnion.mjs
+++ src/web/mjs/connectors/ScantradUnion.mjs
@@ -10,91 +10,60 @@ export default class ScantradUnion extends Connector {
         this.url = 'https://scantrad-union.com';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/projets', this.requestOptions );
-        this.fetchDOM( request, 'section.page-single div.accordionItem div.accordionItemContent > a' )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.querySelector( 'h2.index-top3-title' ).innerText.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let request = new Request(this.url + '/projets', this.requestOptions);
+        let data = await this.fetchDOM(request, 'section.page-single div.accordionItem div.accordionItemContent > a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.querySelector('h2.index-top3-title').innerText.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'div.chapter-list div.accordionItem div.name-chapter' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    let number = element.querySelector( 'span.chapter-number' ).innerText.trim();
-                    let title = element.querySelector( 'span.chapter-name' ).innerText.trim();
-                    element = element.querySelector( 'div.buttons a.btnlel:not([onclick])' );
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: number + ' - ' + title,
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(this.url + manga.id, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.chapter-list div.accordionItem div.name-chapter');
+        return data.map(element => {
+            let number = element.querySelector('span.chapter-number').innerText.trim();
+            let title = element.querySelector('span.chapter-name').innerText.trim();
+            element = element.querySelector('div.buttons a.btnlel:not([onclick])');
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: number + ' - ' + title,
+                language: ''
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
+    async _getPages(chapter) {
         let script = `
+            new Promise(resolve => {
                 let result = [];
-                if( window.obj ) {
-                    result = obj.images.map( page => [obj.site_url, obj.all_manga_dir, obj.title + '_' + page.manga_id, 'ch_' + page.chapter_number, page.image_name].join( '/' ) );
+                if(window.obj) {
+                    result = obj.images.map(page => [obj.site_url, obj.all_manga_dir, obj.title + '_' + page.manga_id, 'ch_' + page.chapter_number, page.image_name].join('/'));
                 } else {
-                    result = [...document.querySelectorAll( 'div.current-image img.manga-image' )].map( img => img.src );
+                    result = [...document.querySelectorAll('div.current-image img.manga-image')].map(img => img.src);
                 }
-                result;
-            `;
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        Engine.Request.fetchUI( request, script )
-            .then( data => {
-                let pageList = data.map( link => this.createConnectorURI( {
-                    url: encodeURI( link ),
-                    referer: request.url
-                } ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+                resolve(result);
+            });
+        `;
+        let request = new Request(this.url + chapter.id, this.requestOptions);
+        let data = await Engine.Request.fetchUI(request, script);
+        return data.map(link => this.createConnectorURI({
+            url: encodeURI(link),
+            referer: request.url
+        }));
     }
 
-    /**
-     *
-     */
     _handleConnectorURI( payload ) {
         /*
          * TODO: only perform requests when from download manager
          * or when from browser for preview and selected chapter matches
          */
-        this.requestOptions.headers.set( 'x-referer', payload.referer );
-        let promise = super._handleConnectorURI( payload.url );
-        this.requestOptions.headers.delete( 'x-referer' );
+        this.requestOptions.headers.set('x-referer', payload.referer);
+        let promise = super._handleConnectorURI(payload.url);
+        this.requestOptions.headers.delete('x-referer');
         return promise;
     }
 }
