1156c78c1d148f10c831f0d240304fc60fcac6ca
Ronny Wegener <wegener.ronny@gmail.com>
2019-11-24 20:24:19 +0100

    [hiperdex] added website support

---
+++ src/web/mjs/connectors/Hiperdex.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class Hiperdex extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'hiperdex';
+        super.label = 'Hiperdex';
+        this.tags = [ 'hentai', 'webtoon', 'english' ];
+        this.url = 'https://hiperdex.com/';
+    }
+}
