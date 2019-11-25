4fc8fb1bb64503510b92241609942c461efa7e82
ronny1982 <wegener.ronny@gmail.com>
2019-11-25 22:19:42 +0100

    [mangawow] updated domain (#699)

--- src/web/mjs/connectors/MangaWOW.mjs
+++ src/web/mjs/connectors/MangaWOW.mjs
@@ -7,6 +7,6 @@ export default class MangaWOW extends WordPressMadara {
         super.id = 'mangawow';
         super.label = 'MangaWOW';
         this.tags = [ 'webtoon', 'turkish' ];
-        this.url = 'https://manga-wow.com';
+        this.url = 'https://mangawow.com';
     }
 }
