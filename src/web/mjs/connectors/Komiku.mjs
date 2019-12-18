ff8bf159dd906e8d32733a71b139df4c925be4d4
dodieboy <dodieboy@users.noreply.github.com>
2019-12-18 14:39:26 +0800

    [Komiku] update connector (#749) (#776)

--- src/web/mjs/connectors/Komiku.mjs
+++ src/web/mjs/connectors/Komiku.mjs
@@ -1,24 +1,23 @@
-import WordPressEManga from './templates/WordPressEManga.mjs';
+import WordPressEManga from "./templates/WordPressEManga.mjs";
 
 /**
  * Is this really a WordPressEManga theme?
  * Same as OtakuIndo
  */
 export default class Komiku extends WordPressEManga {
-
     /**
      *
      */
     constructor() {
         super();
-        super.id = 'komiku';
-        super.label = 'Komiku';
-        this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://komiku.co';
-        this.path = '/daftar-komik/?list';
+        super.id = "komiku";
+        super.label = "Komiku";
+        this.tags = ["manga", "indonesian"];
+        this.url = "https://komiku.co";
+        this.path = "/daftar-komik/?list";
 
-        this.queryMangas = 'div#animelist ol li h4 a';
-        this.queryChapters = 'div#list table.chapter tr td.judulseries a';
-        this.queryPages = 'div#readerareaimg source[src]:not([src=""])';
+        this.queryMangas = "div#a-z ol li.ranking1 h4 a";
+        this.queryChapters = "section#chapter table.chapter tbody._3Rsjq tr td.judulseries a.iklan";
+        this.queryPages = "div#readerareaimg.konten source[src]:not([src=''])";
     }
 }
