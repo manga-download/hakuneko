548392b4d495d0f729f49925e172f70b41aa9048
ronny1982 <wegener.ronny@gmail.com>
2019-12-20 20:15:00 +0100

    [manganel] fixed website support (#786)
    * [manganel] fixed website support
    migrated to new API
    
    * [mangabat] updated template configuration
    
    * [mangakakalot] updated template configuration
    
    * [manganel] hide cross-linked mangas/chapters to for manganel affiliates
    
    * [manganel] fixed format

--- src/web/mjs/connectors/MangaKakalot.mjs
+++ src/web/mjs/connectors/MangaKakalot.mjs
@@ -8,5 +8,11 @@ export default class MangaKakalot extends MangaNel {
         super.label = 'MangaKakalot';
         this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://mangakakalot.com';
+
+        this.path = '/manga_list?type=new&category=all&alpha=all&state=all&group=all&page=';
+        this.queryMangasPageCount = 'div.group_page a.page_last:last-of-type';
+        this.queryMangas = 'div.truyen-list h3 a';
+        this.queryChapters = 'div.chapter-list div.row span a';
+        this.queryPages = 'div#vungdoc source, div.vung-doc source, div.vung_doc source';
     }
 }
