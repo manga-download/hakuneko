fc78e14ce621b6af729367798432a0d3423b6b30
Ronny Wegener <wegener.ronny@gmail.com>
2020-01-01 15:27:44 +0100

    [pecintakomik] code cleanup

--- src/web/mjs/connectors/PecintaKomik.mjs
+++ src/web/mjs/connectors/PecintaKomik.mjs
@@ -1,20 +1,12 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class PecintaKomik extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = 'pecintakomik';
         super.label = 'PecintaKomik';
         this.tags = [ 'manga', 'indonesian' ];
-        // Private members for internal usage only (convenience)
         this.url = 'https://www.pecintakomik.net';
         this.path = '/daftar-manga/?list';
 
