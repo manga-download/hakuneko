4802e8f1ff348b4159b8a6aa498740a4387d8d86
ronny1982 <wegener.ronny@gmail.com>
2020-01-01 16:21:27 +0100

    [mangacanblog] updated domain (#832)
    * [mangacanblog] migrated to new API
    
    * [mangacanblog] updated domain

--- src/web/mjs/connectors/MangaCanBlog.mjs
+++ src/web/mjs/connectors/MangaCanBlog.mjs
@@ -1,19 +1,13 @@
 import Connector from '../engine/Connector.mjs';
 
-/**
- *
- */
 export default class MangaCanBlog extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangacanblog';
         super.label = 'MangaCan Blog';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'http://www.mangacanblog.com';
+        this.url = 'https://mangacanblog.com';
     }
 
     /**
@@ -23,63 +17,33 @@ export default class MangaCanBlog extends Connector {
         return Promise.resolve();
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/daftar-komik-manga-bahasa-indonesia.html', this.requestOptions );
-        this.fetchDOM( request, 'div.blix ul li a.series' )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let request = new Request(this.url + '/daftar-komik-manga-bahasa-indonesia.html', this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.blix ul li a.series');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'div#latestchapters table tr td a.chaptersrec' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.replace( manga.title, '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(this.url + manga.id, this.requestOptions);
+        let data = await this.fetchDOM( request, 'div#latestchapters table tr td a.chaptersrec' );
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.replace(manga.title, '').trim(),
+                language: ''
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
+    async _getPages(chapter) {
         let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'div.navigation div.pager div.pagers a' )
-            .then( data => this.fetchDOM( this.getAbsolutePath( data[0], request.url ), 'div#manga source.picture' ) )
-            .then( data => {
-                let pageList = data.map( element => this.getAbsolutePath( element, request.url ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+        let data = await this.fetchDOM(request, 'div.navigation div.pager div.pagers a');
+        data = await this.fetchDOM(this.getAbsolutePath(data[0], request.url), 'div#manga source.picture');
+        return data.map(element => this.getAbsolutePath(element, request.url));
     }
 }
