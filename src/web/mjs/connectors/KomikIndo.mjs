ab4787a7969ac5cf51fe044891ff8b1582311609
ronny1982 <wegener.ronny@gmail.com>
2020-01-01 16:12:20 +0100

    [komikindo] fixed chapter query (#829)

--- src/web/mjs/connectors/KomikIndo.mjs
+++ src/web/mjs/connectors/KomikIndo.mjs
@@ -1,23 +1,15 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class KomikIndo extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = 'komikindo';
         super.label = 'KomikIndo';
         this.tags = [ 'manga', 'indonesian' ];
-        // Private members for internal usage only (convenience)
         this.url = 'https://komikindo.co';
         this.path = '/manga-list/?list';
 
-        this.queryChapters = 'div.bxcl ul li div.lch a';
+        this.queryChapters = 'div.bxcl ul li span.lchx a';
     }
 }
