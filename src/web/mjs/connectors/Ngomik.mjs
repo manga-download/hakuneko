d40208ff4b9359f93a35b8f3e1c382246ac43966
ronny1982 <wegener.ronny@gmail.com>
2020-01-01 16:29:33 +0100

    [ngomik] fixed manga list path (#835)

--- src/web/mjs/connectors/Ngomik.mjs
+++ src/web/mjs/connectors/Ngomik.mjs
@@ -1,20 +1,14 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class Ngomik extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'ngomik';
         super.label = 'Ngomik';
         this.tags = [ 'manga', 'indonesian' ];
         this.url = 'https://ngomik.in';
-        this.path = '/daftar-manga/?list';
+        this.path = '/daftar-komik/?list';
 
         this.queryChapters = 'div.bxcl ul li div.lch a';
     }
