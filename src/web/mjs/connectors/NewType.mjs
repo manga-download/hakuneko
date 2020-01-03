ee16b6fc4881cfe4ae9cb4d1a1578644f60056d3
ronny1982 <wegener.ronny@gmail.com>
2020-01-03 13:21:55 +0100

    [newtype] migrated to new API (#848)
    * [newtype] migrated to new API
    
    * [newtype] add support for paginated mangas and chapters

--- src/web/mjs/connectors/NewType.mjs
+++ src/web/mjs/connectors/NewType.mjs
@@ -1,13 +1,8 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class NewType extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'newtype';
@@ -16,71 +11,72 @@ export default class NewType extends Connector {
         this.url = 'https://comic.webnewtype.com';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        /*
-         * Cookie: contents_list_pg=1000
-         * https://comic.webnewtype.com/contents/all/
-         * https://comic.webnewtype.com/contents/all/more/1/
-         */
-        let request = new Request( this.url + '/contents/all/', this.requestOptions );
-        this.fetchDOM( request, 'section.BorderSection ul#worksPanel li.OblongCard--border > a' )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.querySelector( 'div.OblongCard-content h3.OblongCard-title' ).innerText.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.Breadcrumb ul li:last-of-type');
+        let id = uri.pathname + uri.search;
+        let title = data[0].textContent.trim();
+        return new Manga(this, id, title);
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'section.BorderSection ul#episodeList li.ListCard > a' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.querySelector( 'div.ListCard-content h3.ListCard-title' ).innerText.replace( manga.title, '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _fetchJsonDOM(request, page, query) {
+        let data = await this.fetchJSON(request);
+        if(data.next !== page) {
+            let blobURL = URL.createObjectURL(new Blob([data.html], { type: 'text/html' }));
+            data = await this.fetchDOM(new Request(blobURL, this.requestOptions), query);
+            URL.revokeObjectURL(blobURL);
+        } else {
+            data = [];
+        }
+        return data;
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'div#viewerContainer' )
-            .then( data => {
-                let uri = this.getAbsolutePath( data[0].dataset.url, request.url );
-                return this.fetchJSON( new Request( uri, this.requestOptions ) );
-            } )
-            .then( data => {
-                let pageList = data.map( page => this.getAbsolutePath( page, request.url ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let mangaList = [];
+        for(let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let request = new Request(new URL(`/contents/all/more/${page}/`, this.url), this.requestOptions);
+        let data = await this._fetchJsonDOM(request, page, 'li a div.OblongCard-content h3.OblongCard-title');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element.closest('a'), request.url),
+                title: element.textContent.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        let chapterList = [];
+        for(let page = 1, run = true; run; page++) {
+            let chapters = await this._getChaptersFromPage(manga, page);
+            chapters.length > 0 ? chapterList.push(...chapters) : run = false;
+        }
+        return chapterList;
+    }
+
+    async _getChaptersFromPage(manga, page) {
+        let request = new Request(new URL(`${manga.id}/more/${page}/`, this.url), this.requestOptions);
+        let data = await this._fetchJsonDOM(request, page, 'li a div.description');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element.closest('a'), request.url),
+                title: element.textContent.replace(manga.title, '').trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'div#viewerContainer');
+        let link = this.getAbsolutePath(data[0].dataset.url, request.url);
+        data = await this.fetchJSON(new Request(link, this.requestOptions));
+        return data.map(image => this.getAbsolutePath(image, request.url));
     }
 }
