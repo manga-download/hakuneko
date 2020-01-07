e95237cdc2a5e713fcdab3898b6fc6366c2ea036
Ronny Wegener <wegener.ronny@gmail.com>
2020-01-07 20:40:35 +0100

    [mangalife] updated domain

--- src/web/mjs/connectors/MangaLife.mjs
+++ src/web/mjs/connectors/MangaLife.mjs
@@ -1,18 +1,12 @@
 import MangaSee from './MangaSee.mjs';
 
-/**
- *
- */
 export default class MangaLife extends MangaSee {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangalife';
         super.label = 'MangaLife';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'https://mangalife.us';
+        this.url = 'https://manga4life.com';
     }
 }
