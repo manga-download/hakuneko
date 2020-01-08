0acdd2f75896f6210e364d45a1c53b0b1c14f42c
Ronny Wegener <wegener.ronny@gmail.com>
2020-01-08 21:03:17 +0100

    [mangakid] updated domain

--- src/web/mjs/connectors/MangaKid.mjs
+++ src/web/mjs/connectors/MangaKid.mjs
@@ -1,19 +1,13 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class MangaKid extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangakid';
         super.label = 'MangaKid';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://mgku.me';
+        this.url = 'https://mangakid.me';
         this.path = '/manga-lists/';
     }
 }
