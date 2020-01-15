9494272e5654e2d64cf93b59ea2e5f0748c0b059
ronny1982 <wegener.ronny@gmail.com>
2020-01-15 22:33:37 +0100

    fixed deployment to get correct branch name (#938)
    * fixed deployment to get correct branch name
    
    * changed update url in tests

--- src/app/__tests__/Configuration.test.js
+++ src/app/__tests__/Configuration.test.js
@@ -85,7 +85,7 @@ describe('Configuration', function() {
     describe('applicationUpdateURL', function() {
         it('should have default after initialization', () => {
             let testee = new Configuration(undefined);
-            expect(testee.applicationUpdateURL).toEqual('http://manga-download.github.io/hakuneko/6.0/latest');
+            expect(testee.applicationUpdateURL).toEqual('https://manga-download.github.io/hakuneko/master/latest');
         });
         it('should be overwritten by update URL from options', () => {
             let testee = new Configuration(expected);
