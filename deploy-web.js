acab1581f7fdb67982326792da563352168051fa
Ronny Wegener <ronny@macbook.local>
2020-01-18 06:17:15 +0100

    [github-actions] debug remote list

--- deploy-web.js
+++ deploy-web.js
@@ -78,6 +78,7 @@ async function main() {
     await fs.move(path.resolve(config.build, meta), path.resolve(config.directory, meta));
     await fs.move(path.resolve(config.build, archive), path.resolve(config.directory, archive));
     let stashID = await gitStashPush();
+    await execute(`git remote -v`);
     await execute(`git branch`);
     await execute(`git checkout --track origin/${config.branch}`);
     await execute(`git rm -r ${config.directory} || true`);
