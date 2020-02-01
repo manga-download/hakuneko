91647dcdf0e3619df9ce9e80b5e0d00405977cbc
Ronny Wegener <wegener.ronny@gmail.com>
2020-02-01 12:08:47 +0100

    [knightnofansub] updated domain

--- src/web/mjs/connectors/KnightNoFansub.mjs
+++ src/web/mjs/connectors/KnightNoFansub.mjs
@@ -1,18 +1,12 @@
 import WordPressMadara from './templates/WordPressMadara.mjs';
 
-/**
- *
- */
 export default class KnightNoFansub extends WordPressMadara {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'knightnofansub';
         super.label = 'Knight no Fansub';
         this.tags = [ 'webtoon', 'high-quality', 'spanish', 'scanlation' ];
-        this.url = 'https://knightnofansub.site';
+        this.url = 'https://knightnoscanlation.com';
     }
 }
