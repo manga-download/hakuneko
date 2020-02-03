df4e3430bfedf07b29f8e8e550e77b6b83ac6d26
Ronny Wegener <wegener.ronny@gmail.com>
2020-02-03 18:32:55 +0100

    [seinagiadulto] updated domain and tag

--- src/web/mjs/connectors/SeinagiAdultoFansub.mjs
+++ src/web/mjs/connectors/SeinagiAdultoFansub.mjs
@@ -1,19 +1,13 @@
 import FoolSlide from './templates/FoolSlide.mjs';
 
-/**
- *
- */
 export default class SeinagiAdultoFansub extends FoolSlide {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'seinagiadultofansub';
         super.label = 'SeinagiAdultoFansub';
-        this.tags = [ 'manga', 'high-quality', 'spanish', 'scanlation' ];
-        this.url = 'https://adulto.seinagi.org';
+        this.tags = [ 'hentai', 'high-quality', 'spanish', 'scanlation' ];
+        this.url = 'https://adulto.seinagi.org.es';
         //this.path        = '/directory/';
         this.language = 'spanish';
     }
