00533c84d4f768cf19ca012a2e88e40130575bf2
ronny1982 <wegener.ronny@gmail.com>
2020-02-03 18:37:51 +0100

    [seinagifansub] updated domain (#1048) (#1049)
    * [seinagifansub] updated domain
    
    * [seinagiadulto] updated domain and tag

--- src/web/mjs/connectors/SeinagiFansubES.mjs
+++ src/web/mjs/connectors/SeinagiFansubES.mjs
@@ -1,19 +1,13 @@
 import FoolSlide from './templates/FoolSlide.mjs';
 
-/**
- *
- */
 export default class SeinagiFansubES extends FoolSlide {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'seinagifansub-es';
         super.label = 'SeinagiFansub (ES)';
         this.tags = [ 'manga', 'high-quality', 'spanish', 'scanlation' ];
-        this.url = 'https://seinagi.org';
+        this.url = 'https://seinagi.org.es';
         this.path = '/online/directory/';
         this.language = 'spanish';
     }
