7901a177dc815714ca8b00cc32aa43c643256f4d
ronny1982 <wegener.ronny@gmail.com>
2020-02-05 20:02:40 +0100

    [mangatx] added website support (#1054)

---
+++ src/web/mjs/connectors/MangaTX.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MangaTX extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mangatx';
+        super.label = 'Mangatx';
+        this.tags = [ 'webtoon', 'english' ];
+        this.url = 'https://mangatx.com';
+    }
+}
