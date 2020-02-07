a49b08cd1b3cd08b614b3454652a233bbf5d0e0d
ronny1982 <wegener.ronny@gmail.com>
2020-02-07 22:48:22 +0100

    [nightcomic] added website support (#1067)

---
+++ src/web/mjs/connectors/NightComic.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class NightComic extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'nightcomic';
+        super.label = 'NIGHT COMIC';
+        this.tags = [ 'webtoon', 'english' ];
+        this.url = 'http://nightcomic.com';
+    }
+}
