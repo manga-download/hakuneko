8861c9204d2ed8597813bca2da0812e85f54bbd5
Ronny Wegener <wegener.ronny@gmail.com>
2020-02-08 10:44:21 +0100

    add ability to exclude cookies with value 'EXPIRED'

--- src/web/mjs/engine/Cookie.mjs
+++ src/web/mjs/engine/Cookie.mjs
@@ -1,53 +1,39 @@
 export default class Cookie {
 
-    constructor( cookies ) {
+    constructor(cookies) {
         this.list = {};
-        ( cookies || '' ).split( ';' )
-            .filter( cookie => cookie.trim() )
-            .forEach( cookie => {
-                let pair = cookie.split( '=' );
-                this.set( pair.shift(), pair.join( '=' ) );
-            } );
+        (cookies || '').split(';')
+            .filter(cookie => cookie.trim())
+            .forEach(cookie => {
+                let pair = cookie.split('=');
+                this.set(pair.shift(), pair.join('='));
+            });
     }
 
-    /**
-     *
-     */
     toString() {
-        return Object.keys( this.list ).map( key => key + '=' + this.list[key] ).join( '; ' );
+        return Object.keys(this.list)
+            .filter(key => this.list[key] !== 'EXPIRED')
+            .map(key => key + '=' + this.list[key])
+            .join('; ');
     }
 
-    /**
-     *
-     */
-    get( key ) {
+    get(key) {
         return this.list[key];
     }
 
-    /**
-     *
-     */
-    set( key, value ) {
-        // NOTE: decodeURIcomponent( value... ) has been removed, the reason for its introduction is unknown
+    set(key, value) {
         this.list[key.toString().trim()] = value.toString().trim();
     }
 
-    /**
-     *
-     */
-    delete( key ) {
+    delete(key) {
         delete this.list[key];
-        //this.list = this.list.filter( cookie => cookie.key !== key );
     }
 
-    /**
-     *
-     */
-    merge( cookie ) {
+    merge(cookie) {
         let result = new Cookie();
-        Object.keys( this.list ).forEach( key => result.set( key, this.list[key] ) );
-        if( cookie instanceof Cookie ) {
-            Object.keys( cookie.list ).forEach( key => result.set( key, cookie.list[key] ) );
+        Object.keys(this.list).forEach(key => result.set(key, this.list[key]));
+        if(cookie instanceof Cookie) {
+            Object.keys(cookie.list).forEach(key => result.set(key, cookie.list[key]));
         }
         return result;
     }
