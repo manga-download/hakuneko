2d752f6bc3bfae1828399926c73a66a7fc8afacf
Ronny Wegener <wegener.ronny@gmail.com>
2020-02-18 19:00:55 +0100

    [mangawindow] updated domain

--- src/web/mjs/connectors/MangaWindow.mjs
+++ src/web/mjs/connectors/MangaWindow.mjs
@@ -1,18 +1,12 @@
 import AnyACG from './templates/AnyACG.mjs';
 
-/**
- *
- */
 export default class MangaWindow extends AnyACG {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangawindow';
         super.label = 'MangaWindow';
         this.tags = [ 'manga', 'multi-lingual' ];
-        this.url = 'https://mangawindow.net';
+        this.url = 'https://mangawindow.club';
     }
 }
