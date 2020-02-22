22c69a48140c8e1c966ac0b03f176f7f60e4e0b1
ronny1982 <wegener.ronny@gmail.com>
2020-02-22 19:28:49 +0100

    [scanmanga] fixed image extraction (#1174)
    migrated to new API

--- src/web/mjs/connectors/ScanManga.mjs
+++ src/web/mjs/connectors/ScanManga.mjs
@@ -1,110 +1,83 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class ScanManga extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'scanmanga';
         super.label = 'ScanManga';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'http://www.scan-manga.com';
+        this.url = 'https://www.scan-manga.com';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        this.requestOptions.headers.set( 'X-Requested-With', 'XMLHttpRequest' );
-        fetch( this.url + '/scanlation/scan.data.json', this.requestOptions )
-            .then( response => response.json() )
-            .then( data => {
-                let mangaList = [];
-                for(let title in data) {
-                    let id = data[title][0];
-                    let slug = data[title][1] || title;
-                    mangaList.push( {
-                        id: `/${id}/${slug}.html`,
-                        title: title
-                    } );
-                }
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
-        this.requestOptions.headers.delete( 'X-Requested-With' );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'meta[property="og:title"]');
+        let id = uri.pathname + uri.search;
+        let title = data[0].content.trim();
+        return new Manga(this, id, title);
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        this.fetchDOM( this.url + manga.id, 'div.texte_volume_manga ul li.chapitre div.chapitre_nom' )
-            .then( data => {
-                let chapterList = data
-                    .filter( element => element.querySelector( 'a:first-of-type[href]' ) )
-                    .map( element => {
-                        let anchor = element.querySelector( 'a:first-of-type' );
-                        return {
-                            id: this.getRelativeLink( anchor ),
-                            title: element.textContent.trim(),
-                            language: 'fr'
-                        };
-                    } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let request = new Request(new URL('/scanlation/scan.data.json', this.url), this.requestOptions);
+        request.headers.set('X-Requested-With', 'XMLHttpRequest');
+        let data = await this.fetchJSON(request);
+        let mangaList = [];
+        for(let title in data) {
+            let id = data[title][0];
+            let slug = data[title][1] || title;
+            let element = document.createElement('div');
+            element.innerHTML = title;
+            mangaList.push({
+                id: `/${id}/${slug}.html`,
+                title: element.textContent.trim()
+            });
+        }
+        return mangaList;
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        fetch( this.url + chapter.id, this.requestOptions )
-            .then( response => response.text() )
-            .then( data => {
-                let baseURL = data.match( /['"](https?:\/\/lel\.scan-manga\.com:8080\/.*?\/\d+\/\d+\/.*?)['"]/ )[1];
-                let pageList = [];
-                let match = undefined;
-                let regex = new RegExp( /\[\s*\d+\s*\]\s*=\s*['"](.*?zoneID.*?pageID.*?siteID.*?)['"]\s*;/g );
-                // eslint-disable-next-line no-cond-assign
-                while( match = regex.exec( data ) ) {
-                    pageList.push( match[1] );
-                }
-                pageList = pageList.map( link => this.createConnectorURI( {
-                    url: baseURL + link,
-                    referer: request.url
-                } ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.texte_volume_manga ul li.chapitre div.chapitre_nom');
+        return data
+            .filter(element => element.querySelector('a:first-of-type[href]'))
+            .map(element => {
+                let anchor = element.querySelector('a:first-of-type');
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(anchor, this.url),
+                    title: element.textContent.trim(),
+                    language: ''
+                };
+            });
     }
 
-    /**
-     *
-     */
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let response = await fetch(request);
+        let data = await response.text();
+        let baseURL = data.match(/['"](https?:\/\/lel\.scan-manga\.com:\d+\/.*?\/\d+\/\d+\/.*?)['"]/)[1];
+        let regex = /['"](.*?zoneID.*?pageID.*?siteID.*?)['"]/g;
+        let pageList = [];
+        let match = undefined;
+        // eslint-disable-next-line no-cond-assign
+        while(match = regex.exec(data)) {
+            pageList.push(match[1]);
+        }
+        return pageList.map(link => this.createConnectorURI({
+            url: baseURL + link,
+            referer: request.url
+        }));
+    }
+
     _handleConnectorURI( payload ) {
         /*
          * TODO: only perform requests when from download manager
          * or when from browser for preview and selected chapter matches
          */
-        this.requestOptions.headers.set( 'x-referer', payload.referer );
-        let promise = super._handleConnectorURI( payload.url );
-        this.requestOptions.headers.delete( 'x-referer' );
+        this.requestOptions.headers.set('x-referer', payload.referer);
+        let promise = super._handleConnectorURI(payload.url);
+        this.requestOptions.headers.delete('x-referer');
         return promise;
     }
 }
