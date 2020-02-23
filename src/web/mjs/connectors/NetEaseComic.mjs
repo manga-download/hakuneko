48f19a87700ef5240e40b05b6fc80e1094344f00
Ronny Wegener <wegener.ronny@gmail.com>
2020-02-23 18:18:23 +0100

    [netease] decompres and extract image index

--- src/web/mjs/connectors/NetEaseComic.mjs
+++ src/web/mjs/connectors/NetEaseComic.mjs
@@ -103,20 +103,23 @@ export default class NetEaseComic extends Connector {
 
         uri = new URL(data.data.path, data.data.host);
         let images = await this._getImageIndex(uri);
-        return images.map(image => this.createConnectorURI(image));
-    }
+        images = images.map(image => image + '@1100w.jpg');
 
-    async _handleConnectorURI(payload) {
-        /*
-         * TODO: only perform requests when from download manager
-         * or when from browser for preview and selected chapter matches
-         */
-        let request = new Request(new URL(payload), this.requestOptions);
-        let data = await this.fetchJSON(request);
-        let uri = new URL(data.data[0].url);
-        uri.searchParams.set('token', data.data[0].token);
-        uri.searchParams.set('ts', parseInt(Date.now()/1000).toString(16));
-        return super._handleConnectorURI(uri.href);
+        uri = new URL('/twirp/comic.v1.Comic/ImageToken', this.url);
+        uri.searchParams.set('device', 'pc');
+        uri.searchParams.set('platform', 'web');
+        request = new Request(uri, {
+            method: 'POST',
+            body: JSON.stringify({
+                urls: JSON.stringify(images)
+            }),
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        });
+        data = await this.fetchJSON(request);
+
+        return data.data.map(image => image.url + '?token=' + image.token);
     }
 
     async _getImageIndex(uri) {
@@ -128,9 +131,9 @@ export default class NetEaseComic extends Connector {
         let chapterID = parseInt(match[2]);
 
         let decrypted = this._decrypt(encrypted, mangaID, chapterID);
-        let inflated = this._inflate(decrypted);
-
-        return [];
+        let zip = await JSZip.loadAsync(decrypted, {});
+        let index = await zip.file('index.dat').async('string');
+        return JSON.parse(index).pics;
     }
 
     _decrypt(buffer, mangaID, chapterID) {
@@ -146,75 +149,9 @@ export default class NetEaseComic extends Connector {
         ];
         // create a view for the buffer
         let decrypted = new Uint8Array(buffer, 9);
-        //console.log('Encrypted:', new Uint8Array(buffer, 9));
         for(let index in decrypted) {
             decrypted[index] ^= key[index % key.length];
         }
-        console.log('Decrypted (Head):', new Uint8Array(buffer, 9, 64));
-        console.log('Decrypted (Tail):', new Uint8Array(buffer, buffer.byteLength - 48));
-        return buffer;
-
-        /*
-        function decryptBiliComicIndex($data, $mangaid, $epid) {
-            if (substr($data, 0, 9) !== 'BILICOMIC') {
-                return false;
-            }
-            $body = substr($data, 9);
-            $size = strlen($body);
-            $out = str_repeat("\0", $size);
-            $key = [
-                $epid & 0xff,
-                $epid >> 8 & 0xff,
-                $epid >> 16 & 0xff,
-                $epid >> 24 & 0xff,
-                $mangaid & 0xff,
-                $mangaid >> 8 & 0xff,
-                $mangaid >> 16 & 0xff,
-                $mangaid >> 24 & 0xff
-            ];
-
-            for ($i = 0; $i < $size; $i++) {
-                $byte = ord($body[$i]);
-                $byte ^= ($key[($i) % 8]);
-                $out[$i] = chr($byte);
-            }
-        }
-        */
-    }
-
-    _inflate(deflated) {
-        return [];
-        /*
-        function readBiliComicIndexZip(MemoryStream $out) {
-            $out->littleEndian = true;
-            $out->position = $out->size - 22;
-            $signature = $out->readData(4);
-            $num = $out->short;
-            $start = $out->short;
-            $numRecords = $out->short;
-            $total = $out->short;
-            $cdsize = $out->long;
-            $cdoffset = $out->long;
-            $commentlen = $out->short;
-
-            $out->position = $cdoffset + 4 + 2 * 6;
-            $crc = $out->ulong;
-            $compressedSize = $out->long;
-            $uncompressedSize = $out->long;
-
-            $out->position = 26;
-            $fnameLen = $out->short;
-            $extraLen = $out->short;
-            $out->position += $fnameLen + $extraLen;
-
-            $compressedData = $out->readData($compressedSize);
-            $data = gzinflate($compressedData);
-
-            if (crc32($data) !== $crc) {
-                return false;
-            }
-            return $data;
-        }
-        */
+        return decrypted;
     }
 }
