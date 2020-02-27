86dc069e2a971d5a0f9b68fd82022f493c3eae79
ronny1982 <wegener.ronny@gmail.com>
2020-02-27 19:13:44 +0100

    [ichirinnohanayuri] added website support (#1198)

---
+++ src/web/mjs/connectors/IchirinNoHanaYuri.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class IchirinNoHanaYuri extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'ichirinnohanayuri';
+        super.label = 'Ichirin No Hana Yuri';
+        this.tags = [ 'hentai', 'high-quality', 'portuguese', 'scanlation' ];
+        this.url = 'https://ichirinnohanayuri.com.br';
+    }
+}
