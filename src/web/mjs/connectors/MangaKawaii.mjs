adffeda6c03c1df5ab6d93a99d47abfcf7af0fef
ronny1982 <wegener.ronny@gmail.com>
2020-02-28 22:51:47 +0100

    merge from master (#1207)
    * [lilymanga] added website support (#1204)
    
    * [mangakawaii] updated domain (#1205)

--- src/web/mjs/connectors/MangaKawaii.mjs
+++ src/web/mjs/connectors/MangaKawaii.mjs
@@ -1,22 +1,22 @@
 import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
 
-/**
- *
- */
 export default class MangaKawaii extends MangaReaderCMS {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangakawaii';
         super.label = 'MangaKawaii';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'https://www.mangakawaii.to';
+        this.url = 'https://www.mangakawaii.com';
 
         this.queryMangas = 'ul.manga-list-text li a.alpha-link';
         this.queryChapters = 'div.chapters-list div.chapter-item div.chapter-item__name a.list-item__title';
         this.language = 'fr';
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let data = await this.fetchRegex(request, /attr\s*\(\s*['"]data-src['"]\s*,\s*['"]\s*([^'"]+)\s*['"]\s*\)/g);
+        return data;
     }
 }
--- src/web/mjs/connectors/templates/MangaReaderCMS.mjs
+++ src/web/mjs/connectors/templates/MangaReaderCMS.mjs
@@ -4,16 +4,11 @@ export default class MangaReaderCMS extends Connector {
 
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = undefined;
         super.label = undefined;
         this.tags = [ 'manga' ];
-        super.isLocked = false;
-        // Private members for internal usage only (convenience)
         this.url = undefined;
         this.path = '/';
-        // Private members for internal use that can be configured by the user through settings menu (set to undefined or false to hide from settings menu!)
-        this.config = undefined;
 
         this.queryMangas = 'ul.manga-list li a';
         this.queryChapters = 'ul.chapters li h5.chapter-title-rtl';
@@ -21,66 +16,39 @@ export default class MangaReaderCMS extends Connector {
         this.language = '';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        this.fetchDOM( this.url + this.path + 'changeMangaList?type=text', this.queryMangas )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRelativeLink( element ),
-                        title: element.text.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let request = new Request(new URL(this.path + 'changeMangaList?type=text', this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRelativeLink(element),
+                title: element.text.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        this.fetchDOM( this.url + manga.id, this.queryChapters )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    let anchor = element.nodeName.toLowerCase() === 'a' ? element : element.querySelector( 'a' );
-                    return {
-                        id: this.getRelativeLink( anchor ),
-                        title: element.innerText.replace( /\s*:\s*$/, '' ).replace( manga.title, '' ).trim(),
-                        language: this.language
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            let anchor = element.nodeName.toLowerCase() === 'a' ? element : element.querySelector('a');
+            return {
+                id: this.getRelativeLink(anchor),
+                title: element.innerText.replace(/\s*:\s*$/, '').replace(manga.title, '').trim(),
+                language: this.language
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        this.fetchDOM( this.url + chapter.id, this.queryPages )
-            .then( data => {
-                let pageList = data.map( element => {
-                    let src = ( element.dataset['src'] || element.src ).trim();
-                    if( src.startsWith( '//' ) ) {
-                        src = new URL( this.url ).protocol + src;
-                    }
-                    return src;
-                } );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryPages);
+        return data.map(element => {
+            let src = (element.dataset['src'] || element.src).trim();
+            if(src.startsWith('//')) {
+                src = new URL(this.url).protocol + src;
+            }
+            return src;
+        });
     }
 }
