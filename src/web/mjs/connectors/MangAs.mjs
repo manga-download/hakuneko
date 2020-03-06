d10af3a27bc274f415f6b020c8931e0514c766e1
ronny1982 <wegener.ronny@gmail.com>
2020-03-06 21:14:04 +0100

    merge from master (#1260)
    * [mintmanga] updated domain (#1258)
    
    * [mangas] updated domain (#1259)

--- src/web/mjs/connectors/MangAs.mjs
+++ src/web/mjs/connectors/MangAs.mjs
@@ -1,19 +1,13 @@
 import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
 
-/**
- *
- */
 export default class MangAs extends MangaReaderCMS {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangas';
         super.label = 'MangAs';
         this.tags = [ 'manga', 'spanish' ];
-        this.url = 'https://mangas.pw';
+        this.url = 'https://mangas.in';
 
         this.language = 'es';
     }
