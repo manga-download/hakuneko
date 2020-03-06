5831a00ad076f97dcec9e7bb8e9b18bc647ad225
ronny1982 <wegener.ronny@gmail.com>
2020-03-06 21:07:17 +0100

    [mintmanga] updated domain (#1258)

--- src/web/mjs/connectors/SelfManga.mjs
+++ src/web/mjs/connectors/SelfManga.mjs
@@ -4,10 +4,9 @@ export default class SelfManga extends ReadManga {
 
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = 'selfmanga';
         super.label = 'SelfManga';
-        // Private members for internal usage only (convenience)
+        this.tags = [ 'manga', 'russian' ];
         this.url = 'https://selfmanga.ru';
 
         this.preferSubtitleAsMangaTitle = false;
