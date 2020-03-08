9e2b664ff409adff675cbca9895212a5f583e21b
ronny1982 <wegener.ronny@gmail.com>
2020-03-08 19:10:50 +0100

    merge from master (#1271)
    * [imgur] fixed website support (#1265)
    
    * [8musesxxx] added website support (#1266)
    
    * [asmhentai] migrated to new API (#1267)
    
    * [comicboost] added website support (#1269)
    
    * [japscan] add support for single page with scrambled images (#1270)

--- src/web/mjs/connectors/Imgur.mjs
+++ src/web/mjs/connectors/Imgur.mjs
@@ -1,28 +1,42 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class Imgur extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'imgur';
-        super.label = 'imgur';
-        this.tags = [];
+        super.label = 'Imgur';
+        this.tags = [ 'hosting' ];
         this.url = 'https://imgur.com';
+        this.apiURL = 'https://api.imgur.com/3';
+        this.requestOptions.headers.set('Authorization', 'Client-ID 546c25a59c58ad7');
     }
 
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.post-header h1.post-title');
+        let id = uri.pathname.match(/gallery\/(\w+)/)[1];
+        let title = data[0].textContent.trim();
+        return new Manga(this, id, title);
     }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getMangas() {
+        let msg = 'This website does not provide a manga list, please copy and paste the URL containing the images directly from your browser into HakuNeko.';
+        throw new Error(msg);
     }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getChapters(manga) {
+        return [ {
+            id: manga.id,
+            title: manga.title,
+            language: ''
+        } ];
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(`${this.apiURL}/album/${chapter.id}/images`, this.requestOptions);
+        let data = await this.fetchJSON(request);
+        return data.data.map(image => this.getAbsolutePath(image.link, this.url));
     }
 }
