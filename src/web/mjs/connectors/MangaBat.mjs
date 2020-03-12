9f56fc27eae1424b95dd8f804e6380095d285490
Ronny Wegener <wegener.ronny@gmail.com>
2020-03-12 21:31:00 +0100

    [mangabat] updated queries

--- src/web/mjs/connectors/MangaBat.mjs
+++ src/web/mjs/connectors/MangaBat.mjs
@@ -9,10 +9,7 @@ export default class MangaBat extends MangaNel {
         this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://mangabat.com';
 
-        this.path = '/manga_list?type=latest&category=all&state=all&page=';
-        this.queryMangasPageCount = 'div.group-page a.page:last-of-type';
-        this.queryMangas = 'div.list_category h3 a';
-        this.queryChapters = 'div.chapter-list div.row span a';
-        this.queryPages = 'div#vungdoc source, div.vung-doc source, div.vung_doc source';
+        this.path = '/manga-list-all/';
+        this.queryMangas = 'div.panel-list-story div.list-story-item h3 a.item-title';
     }
 }
