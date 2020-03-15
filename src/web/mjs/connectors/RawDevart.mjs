2211f641aebf029716ce630c2ab75dc71bdcbf5a
ronny1982 <wegener.ronny@gmail.com>
2020-03-15 11:18:26 +0100

    [rawdevart] fixed pagination (#1321) (#1322)

--- src/web/mjs/connectors/RawDevart.mjs
+++ src/web/mjs/connectors/RawDevart.mjs
@@ -14,7 +14,7 @@ export default class RawDevart extends Connector {
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchDOM(request, 'div.manga-top-info h1.title');
-        let id = uri.pathname + uri.search;
+        let id = uri.pathname;
         let title = data[0].textContent.trim();
         return new Manga(this, id, title);
     }
@@ -23,7 +23,7 @@ export default class RawDevart extends Connector {
         let mangaList = [];
         let request = new Request(this.url, this.requestOptions);
         let data = await this.fetchDOM(request, 'ul.pagination li:last-of-type a.page-link');
-        let pageCount = parseInt(data[0].href.match(/\d+$/)[0]);
+        let pageCount = parseInt(data[0].href.match(/page=(\d+)/)[1]);
         for(let page = 1; page <= pageCount; page++) {
             let mangas = await this._getMangasFromPage(page);
             mangaList.push(...mangas);
@@ -43,7 +43,20 @@ export default class RawDevart extends Connector {
     }
 
     async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions);
+        // https://rawdevart.com/api/chapters.json?comic=666&ordering=volume%2Cnum%2Csnum&n=1000
+        let chapterList = [];
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'ul.pagination li:last-of-type a.page-link');
+        let pageCount = data.length === 0 ? 1 : parseInt(data[0].href.match(/page=(\d+)/)[1]);
+        for(let page = 1; page <= pageCount; page++) {
+            let chapters = await this._getChaptersFromPage(manga, page);
+            chapterList.push(...chapters);
+        }
+        return chapterList;
+    }
+
+    async _getChaptersFromPage(manga, page) {
+        let request = new Request(new URL(`${manga.id}?page=${page}`, this.url), this.requestOptions);
         let data = await this.fetchDOM(request, 'div.manga-body-right div.card-body div.list-group-item a');
         return data.map(element => {
             return {
