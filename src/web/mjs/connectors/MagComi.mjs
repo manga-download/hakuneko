3819167413bd55ff02d03acb7263a11c9f589c07
epsere <cookiebakaz@gmail.com>
2020-03-20 14:37:56 -0400

    magcomi getmangabyuri

--- src/web/mjs/connectors/MagComi.mjs
+++ src/web/mjs/connectors/MagComi.mjs
@@ -1,4 +1,5 @@
 import CoreView from './templates/CoreView.mjs';
+import Manga from '../engine/Manga.mjs';
 
 export default class MagComi extends CoreView {
 
@@ -12,5 +13,13 @@ export default class MagComi extends CoreView {
         this.path = [ '/series' ];
         this.queryManga = 'ul.magcomi-series-list > li.series-item > a';
         this.queryMangaTitle = 'h3.series-title';
+    }
+
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'head title');
+        let id = uri.pathname;
+        let title = data[0].text.split('|')[0].trim();
+        return new Manga(this, id, title);
     }
 }
