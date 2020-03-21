f64fbcae9674ab8944d5dd8210e4ecb630efb45e
Ronny Wegener <wegener.ronny@gmail.com>
2020-03-21 11:28:19 +0100

    [disasterscans] fixed manga query

--- src/web/mjs/connectors/DisasterScans.mjs
+++ src/web/mjs/connectors/DisasterScans.mjs
@@ -8,5 +8,7 @@ export default class DisasterScans extends WordPressMadara {
         super.label = 'Disaster Scans';
         this.tags = [ 'webtoon', 'high-quality', 'english', 'scanlation' ];
         this.url = 'https://disasterscans.com';
+
+        this.queryMangas = 'div.post-title h3 a:last-of-type, div.post-title h5 a:last-of-type';
     }
 }
