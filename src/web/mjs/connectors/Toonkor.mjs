91a0a3054c8c225dc8f58e53949b523459455a33
ronny1982 <wegener.ronny@gmail.com>
2020-03-21 18:51:54 +0100

    merge from master (#1374)
    * [toonkor] migrated to new API, updated domain (#1368)
    
    * [tapas] fixed website layout (#1369)
    
    * [disasterscans] fixed manga query (#1371)
    
    * [crunchyroll] fixed login (#1372)

--- src/web/mjs/connectors/Toonkor.mjs
+++ src/web/mjs/connectors/Toonkor.mjs
@@ -1,92 +1,63 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class Toonkor extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'toonkor';
         super.label = 'Toonkor';
         this.tags = [ 'webtoon', 'korean' ];
-        this.url = 'https://toonkor.gdn';
+        this.url = 'https://toonkor.loan';
     }
 
-    /**
-     *
-     */
-    _getMangaListFromPages( mangaPageLinks, index ) {
-        index = index || 0;
-        let request = new Request( mangaPageLinks[ index ], this.requestOptions );
-        return this.fetchDOM( request, 'div#wt_list .section-item div.section-item-title a#title', 5 )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim()
-                    };
-                } );
-                if( index < mangaPageLinks.length - 1 ) {
-                    return this._getMangaListFromPages( mangaPageLinks, index + 1 )
-                        .then( mangas => mangaList.concat( mangas ) );
-                } else {
-                    return Promise.resolve( mangaList );
-                }
-            } );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'meta[property="og:title"]');
+        let id = uri.pathname + uri.search;
+        let title = data[0].content.trim();
+        return new Manga(this, id, title);
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        this._getMangaListFromPages( [ this.url + '/웹툰/연재?fil=제목', this.url + '/웹툰/완결?fil=제목' ] )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangasFromPage(page) {
+        let request = new Request(new URL(page + '?fil=제목', this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'div#wt_list .section-item div.section-item-title a#title');
+        return data.map( element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
+                title: element.text.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'div#bo_list table.web_list tbody tr td.content__title' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element.dataset.role, request.url ),
-                        title: element.innerText.replace( manga.title, '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let mangaList = [];
+        for(let page of [ '/웹툰/연재', '/웹툰/완결' ]) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        Engine.Request.fetchUI( request, `[...document.querySelectorAll( 'div#toon_img img' )].map( img => img.src );` )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'div#bo_list table.web_list tbody tr td.content__title');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element.dataset.role, request.url),
+                title: element.textContent.replace(manga.title, '').trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        let script = `
+            new Promise(resolve => {
+                resolve([...document.querySelectorAll('div#toon_img img')].map(img => img.src));
+            });
+        `;
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        return Engine.Request.fetchUI(request, script);
     }
 }
