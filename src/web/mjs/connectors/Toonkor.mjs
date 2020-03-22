217c235c6a9c97752ad1807ef30f1252033770ac
ronny1982 <wegener.ronny@gmail.com>
2020-03-22 21:45:44 +0100

    [toonkor] changed domain again (#1381)

--- src/web/mjs/connectors/Toonkor.mjs
+++ src/web/mjs/connectors/Toonkor.mjs
@@ -8,7 +8,7 @@ export default class Toonkor extends Connector {
         super.id = 'toonkor';
         super.label = 'Toonkor';
         this.tags = [ 'webtoon', 'korean' ];
-        this.url = 'https://toonkor.loan';
+        this.url = 'https://toonkor.yoga';
     }
 
     async _getMangaFromURI(uri) {
