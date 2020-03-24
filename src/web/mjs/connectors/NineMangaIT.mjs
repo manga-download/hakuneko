4adbe91c22f546313299dadc93869a14a0541a7f
Ronny Wegener <wegener.ronny@gmail.com>
2020-03-24 22:46:36 +0100

    [ninemanga] adapted to changed template

--- src/web/mjs/connectors/NineMangaIT.mjs
+++ src/web/mjs/connectors/NineMangaIT.mjs
@@ -1,20 +1,12 @@
 import NineManga from './templates/NineManga.mjs';
 
-/**
- *
- */
 export default class NineMangaIT extends NineManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'ninemanga-it';
         super.label = 'NineMangaIT';
         this.tags = [ 'manga', 'italian' ];
         this.url = 'http://it.ninemanga.com';
-
-        this.pageCount = 150;
     }
 }
