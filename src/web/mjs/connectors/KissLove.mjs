d99db055b837c18b6c5cd2b9c44ad4458f3b044a
dodieboy <dodieboy@users.noreply.github.com>
2020-05-07 09:32:59 +0800

    [KissLove] Minor Changes

--- src/web/mjs/connectors/KissLove.mjs
+++ src/web/mjs/connectors/KissLove.mjs
@@ -1,13 +1,13 @@
 import FlatManga from './templates/FlatManga.mjs';
 
-export default class LHScan extends FlatManga {
+export default class KissLove extends FlatManga {
 
     constructor() {
         super();
         super.id = 'kisslove';
         super.label = 'KissLove';
         this.tags = [ 'manga', 'raw', 'japanese' ];
-        this.url = 'https://kisslove.net/';
+        this.url = 'https://kisslove.net';
         this.requestOptions.headers.set( 'x-referer', this.url );
     }
 }
