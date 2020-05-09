c22d29ced7f284b45804fd6207ba10d2f6d16ae7
XenonR <service.github@nocer.net>
2020-05-09 09:48:41 +0200

    [argosscan] add website support

---
+++ src/web/mjs/connectors/ArgosScan.mjs
@@ -0,0 +1,13 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+
+export default class ArgosScan extends WordPressMadara {
+    constructor() {
+        super();
+        super.id = 'argosscan';
+        super.label = 'Argos Scan';
+        this.tags = [ 'manga', 'high-quality', 'portuguese', 'scanlation' ];
+        this.url = 'https://argosscan.com';
+        this.language = 'pt';
+    }
+}
