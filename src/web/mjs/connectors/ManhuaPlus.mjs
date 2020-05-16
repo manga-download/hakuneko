1c104a0d2aa753aa8239d83a04413bb741b32ef5
Ronny Wegener <wegener.ronny@gmail.com>
2020-05-16 22:07:30 +0200

    [manhuaplus] fixed image link extraction

--- src/web/mjs/connectors/ManhuaPlus.mjs
+++ src/web/mjs/connectors/ManhuaPlus.mjs
@@ -9,4 +9,14 @@ export default class ManhuaPlus extends TruyenChon {
         this.tags = [ 'webtoon', 'vietnamese' ];
         this.url = 'https://manhuaplus.com';
     }
+
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'ul.blocks-gallery-grid li.blocks-gallery-item figure noscript');
+        return data.map(element => {
+            element = this.createDOM(element.innerText).querySelector('source');
+            let uri = this.getAbsolutePath(element.dataset.fullUrl || element.dataset.src || element, request.url);
+            return uri.includes('proxy.') ? this.createConnectorURI( { url: uri, referer: request.url } ) : uri;
+        });
+    }
 }
