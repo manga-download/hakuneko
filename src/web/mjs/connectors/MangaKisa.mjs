d033cfe2d9ef46282444d5c1ca3f0e87a80ca976
dodieboy <dodieboy@users.noreply.github.com>
2020-05-17 21:15:35 +0800

    [MangaKisa] remove debug msg

--- src/web/mjs/connectors/MangaKisa.mjs
+++ src/web/mjs/connectors/MangaKisa.mjs
@@ -38,7 +38,6 @@ export default class MangaKisa extends Connector {
     async _getChapters(manga) {
         let request = new Request(new URL(manga.id, this.url), this.requestOptions);
         let data = await this.fetchDOM(request, this.queryChapter);
-        console.log(data);
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
