bab20ba4971534a0620ef08ab64f040fc99127e9
ronny1982 <wegener.ronny@gmail.com>
2020-05-20 07:51:26 +0200

    [wordpresszbulu] normalize pathname (remove double slash) (#1732)
    * [wordpresszbulu] fixed chapter page template to prevent redirection to first chapter page

--- src/web/mjs/connectors/KooManga.mjs
+++ src/web/mjs/connectors/KooManga.mjs
@@ -7,6 +7,22 @@ export default class KooManga extends WordPressZbulu {
         super.id = 'koomanga';
         super.label = 'KooManga';
         this.tags = [ 'manga', 'webtoon', 'english' ];
-        this.url = 'http://ww1.koomanga.com';
+        this.url = 'http://www.koomanga.com';
+
+        this._initializeConnector();
+    }
+
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri, this.requestOptions);
+        await Engine.Request.fetchUI(request, '');
+        let response = await fetch(request);
+        this.url = new URL(response.url).origin;
     }
 }
