618f27eeb7faba00ceccb1bdcd3a1aafe0c7e1c7
Ronny Wegener <wegener.ronny@gmail.com>
2020-05-30 18:08:13 +0200

    [comic-novel] fixed line breaks

--- src/web/mjs/connectors/ComicoNovel.mjs
+++ src/web/mjs/connectors/ComicoNovel.mjs
@@ -28,7 +28,7 @@ export default class ComicoNovel extends Connector {
                                 images.push(...[...element.querySelectorAll('img.cn-picture__img')].map(img => img.src));
                                 // TODO: include figcaption.cn-picture__caption
                             } else {
-                                // convert to image
+                                element.parentElement.style.margin = '0';
                                 let canvas = await html2canvas(element);
                                 images.push(canvas.toDataURL('image/png'));
                             }
