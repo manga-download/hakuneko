d0d4a4091df9899a9c3ded3776347ef21381f56c
XenonR <service.github@nocer.net>
2020-05-31 10:26:07 +0200

    [sukima] Improvements #2

--- src/web/mjs/connectors/Sukima.mjs
+++ src/web/mjs/connectors/Sukima.mjs
@@ -14,14 +14,7 @@ export default class Sukima extends Connector {
     }
 
     async _getMangas() {
-        let request = new Request(new URL('/api/book/v1/free/', this.url), {
-            method: 'POST',
-            body: '{"store":false,"genre":"0"}',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        });
-        let categorys = await this.fetchJSON(request);
+        let categorys = await this._fetchPOST('/api/book/v1/free/','{"store":false,"genre":"0"}');
 
         let uri;
         let pages;
@@ -31,22 +24,16 @@ export default class Sukima extends Connector {
                 uri = new URL(category.more_btn.link, this.url);
                 pages = 1;
                 for (let page = 1; page <= pages; page++) {
-                    request = new Request(new URL('/api/v1/search/', this.url), {
-                        method: 'POST',
-                        body: JSON.stringify(
-                            {
-                                'free': [1, 2],
-                                'page': page,
-                                'sort_by': 0,
-                                'tag': [uri.searchParams.get('tag')]
-                            }
-                        ),
-                        headers: {
-                            'Content-Type': 'application/json;charset=utf-8',
+                    let body = JSON.stringify(
+                        {
+                            'free': [1, 2],
+                            'page': page,
+                            'sort_by': 0,
+                            'tag': [uri.searchParams.get('tag')]
                         }
-                    });
+                    );
 
-                    let pageContent = await this.fetchJSON(request);
+                    let pageContent = await this._fetchPOST('/api/v1/search/', body);
                     pages = pageContent.max_page;
 
                     for (const manga of pageContent.items) {
@@ -65,22 +52,15 @@ export default class Sukima extends Connector {
     }
 
     async _getChapters(manga) {
-        let request = new Request(new URL('/api/book/v1/title/'+manga.id+'/', this.url), {
-            method: 'POST',
-            body: '{}',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            }
-        });
+        let books = await this._fetchPOST('/api/book/v1/title/'+manga.id+'/','{}');
 
         let chapters = [];
-        let books = await this.fetchJSON(request);
         for (const book of books.contents) {
             for (const chapter of book.stories) {
                 chapters.push(
                     {
-                        id: '/bv/t/' + chapter.title_code + '/v/' + chapter.volume + '/s/' + chapter.story + '/p/0',
-                        title: chapter.volume.toString().padStart(3, '0') + '-' + chapter.story.toString().padStart(3, '0') + ' - ' + chapter.info.text.trim()
+                        id: '/bv/t/' + chapter.title_code + '/v/' + chapter.volume + '/s/' + chapter.story + '/p/1',
+                        title: '(' + chapter.volume.toString().padStart(3, '0') + '-' + chapter.story.toString().padStart(3, '0') + ')' + chapter.info.text.trim().replace(manga.title, '')
                     }
                 );
             }
@@ -88,25 +68,29 @@ export default class Sukima extends Connector {
         return chapters;
     }
 
-    async _getPages(manga) {
+    async _getPages(chapter) {
         let script = `
             new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    let shuffle_data = [];
-                    for (let page = 0; page < PAGES_INFO.length; page++) {
-                        shuffle_data.push(
-                            {
-                                id: PAGES_INFO[page].page_url,
-                                blocklen: BLOCKLEN,
-                                shuffle_map: JSON.parse(PAGES_INFO[page].shuffle_map)
-                            }
-                        )
+                    try {
+                        let shuffle_data = [];
+                        for (let page = 0; page < PAGES_INFO.length; page++) {
+                            shuffle_data.push(
+                                {
+                                    id: PAGES_INFO[page].page_url,
+                                    blocklen: BLOCKLEN,
+                                    shuffle_map: JSON.parse(PAGES_INFO[page].shuffle_map)
+                                }
+                            )
+                        }
+                        resolve(shuffle_data);
+                    } catch(error) {
+                        reject(error);
                     }
-                    resolve(shuffle_data);
                 }, 2500);
             });
         `;
-        let request = new Request(new URL(manga.id, this.url));
+        let request = new Request(new URL(chapter.id, this.url));
         let data = await Engine.Request.fetchUI(request, script);
 
         return data.map(page => this.createConnectorURI(
@@ -114,17 +98,15 @@ export default class Sukima extends Connector {
                 id: page.id,
                 blocklen: page.blocklen,
                 shuffle_map: page.shuffle_map,
+                referer: chapter.id
             }
         ));
     }
 
     async _handleConnectorURI(payload) {
-        while (this.lock) {
-            await this.wait(Math.floor(Math.random() * (2000 - 1000)) + 1000);
-        }
-        this.lock = true;
-
+        console.log(payload);
         let request = new Request(new URL(payload.id), this.requestOptions);
+        request.headers.set('x-referer', this.getRootRelativeOrAbsoluteLink(payload.referer, this.url));
         let response = await fetch(request);
         let blob = await response.blob();
         let bitmap = await createImageBitmap(blob);
@@ -146,13 +128,11 @@ export default class Sukima extends Connector {
                 let sx = col * payload.blocklen;
                 let sy = row * payload.blocklen;
                 ctx.drawImage(bitmap, sx, sy, payload.blocklen, payload.blocklen, dx, dy, payload.blocklen, payload.blocklen);
-                count += 1;
+                count++;
             }
         }
 
         let data = await this._canvasToBlob(canvas);
-
-        this.lock = false;
         return this._blobToBuffer(data);
     }
 
@@ -166,15 +146,18 @@ export default class Sukima extends Connector {
 
     async _getMangaFromURI(uri) {
         let id = uri.pathname.match(/\/(\w+)\/?$/)[1];
+        let data = await this._fetchPOST('/api/book/v1/title/'+id+'/','{}');
+        return new Manga(this, id, data.title);
+    }
 
-        let request = new Request(new URL('/api/book/v1/title/'+id+'/', this.url), {
+    async _fetchPOST(uri, body) {
+        let request = new Request(new URL(uri, this.url), {
             method: 'POST',
-            body: '{}',
+            body: body,
             headers: {
-                'Content-Type': 'application/json;charset=utf-8',
+                'Content-Type': 'application/json;charset=utf-8'
             }
         });
-        let data = await this.fetchJSON(request);
-        return new Manga(this, id, data.title);
+        return await this.fetchJSON(request);
     }
 }
