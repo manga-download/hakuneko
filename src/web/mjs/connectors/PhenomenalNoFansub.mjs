693f70f3d9185129f85ea486a85e8ffe77aa103a
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-04 12:53:59 +0200

    [phenomenalnofansub] added website support

---
+++ src/web/mjs/connectors/PhenomenalNoFansub.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class PhenomenalNoFansub extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'phenomenalnofansub';
+        super.label = 'Phenomenal No Fansub';
+        this.tags = [ 'webtoon', 'spanish' ];
+        this.url = 'https://www.phenomenalnofansub.site';
+    }
+}
