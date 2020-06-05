efbc764530dcc1e68484e334ae824bf6feb964cb
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-05 12:03:11 +0200

    [heavenmanga] updated URL initialization

--- src/web/mjs/connectors/HeavenManga.mjs
+++ src/web/mjs/connectors/HeavenManga.mjs
@@ -7,22 +7,14 @@ export default class HeavenManga extends WordPressZbulu {
         super.id = 'heavenmanga';
         super.label = 'Heaven Manga';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'http://www.heavenmanga.org';
+        this.url = 'http://heaventoon.com';
 
-        this._initializeConnector();
+        this._initializeURL();
     }
 
-    async _initializeConnector() {
-        /*
-         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
-         * => append random search parameter to avoid caching
-         */
-        let uri = new URL(this.url);
-        uri.searchParams.set('ts', Date.now());
-        uri.searchParams.set('rd', Math.random());
-        let request = new Request(uri, this.requestOptions);
-        await Engine.Request.fetchUI(request, '');
-        let response = await fetch(request);
+    async _initializeURL() {
+        let response = await fetch(this.url);
         this.url = new URL(response.url).origin;
+        console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
