8f13072276ad84e00e0c7c5b2d3c8b2f6e459c03
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-05 12:03:36 +0200

    [koomanga] updated URL initialization

--- src/web/mjs/connectors/KooManga.mjs
+++ src/web/mjs/connectors/KooManga.mjs
@@ -7,22 +7,14 @@ export default class KooManga extends WordPressZbulu {
         super.id = 'koomanga';
         super.label = 'KooManga';
         this.tags = [ 'manga', 'webtoon', 'english' ];
-        this.url = 'http://www.koomanga.com';
+        this.url = 'http://koomanga.com';
 
-        this._initializeConnector();
+        this._initializeURL();
     }
 
-    async _initializeConnector() {
-        /*
-         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
-         * => append random search parameter to avoid caching
-         */
-        let uri = new URL(this.url);
-        uri.searchParams.set('ts', Date.now());
-        uri.searchParams.set('rd', Math.random());
-        let request = new Request(uri, this.requestOptions);
-        await Engine.Request.fetchUI(request, '');
-        let response = await fetch(request);
+    async _initializeURL() {
+        let response = await fetch(this.url);
         this.url = new URL(response.url).origin;
+        console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
