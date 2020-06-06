0704494a6469ecd856e7af50bffe8d03dc5ecf24
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-06 21:11:41 +0200

    [toonkor] updated clipboard predicate, use lazy URL assignment (on-demand)

--- src/web/mjs/connectors/Toonkor.mjs
+++ src/web/mjs/connectors/Toonkor.mjs
@@ -7,8 +7,23 @@ export default class Toonkor extends GnuBoard5BootstrapBasic2 {
         super.id = 'toonkor';
         super.label = 'Toonkor';
         this.tags = [ 'webtoon', 'korean' ];
-        this.url = 'https://tkor.buzz';
+        this.url = 'https://tkor.one';
+    }
 
-        this._initializeTwitterURL('1202224761771741184');
+    canHandleURI(uri) {
+        return /https?:\/\/(?:toonkor|tkor)\.[a-z]+/.test(uri.origin);
+    }
+
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(await this._twitter.getProfileURL('1202224761771741184') || this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri.href, this.requestOptions);
+        this.url = await Engine.Request.fetchUI(request, `window.location.origin`);
+        console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
