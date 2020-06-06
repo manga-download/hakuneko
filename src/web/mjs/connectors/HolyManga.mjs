8d929cb595c6ffef6b514012d002859b0e8d42c0
ronny1982 <wegener.ronny@gmail.com>
2020-06-06 20:53:44 +0200

    show captcha when website is protected (#1844)
    * show captcha when website is protected
    
    * [copytoon] updated URL initialization
    
    * [heavenmanga] updated URL initialization
    
    * [holymanga] updated URL initialization
    
    * [koomanga] updated URL initialization
    
    * [mangafreak] updated URL initialization
    
    * [newtoki] updated URL initialization
    
    * [clipboard] introduced regex to find matching connector
    
    * [vrv] explicit set url to prevent errors when using regex based clipboard matching
    
    * [holymanga] updated clipboard regex, use lazy URL assignment (on-demand)
    
    * [clipboard] use method instead of regex to determine supported connector
    
    * [holymanga] convert clipboard regex to predicate function
    
    * [heavenmanga] updated clipboard predicate, use lazy URL assignment (on-demand)
    
    * [copytoon] updated clipboard predicate, use lazy URL assignment (on-demand)
    
    * [koomanga] updated clipboard predicate, use lazy URL assignment (on-demand)
    
    * [mangafreak] updated clipboard predicate, use lazy URL assignment (on-demand)
    
    * [newtoki] updated clipboard predicate, use lazy URL assignment (on-demand)
    
    * fixed typo
    
    * added setting for hCaptcha accessibility UUID (improve captcha usability experience)
    
    * initialize hcaptcha accessibility cookie during application start
    
    * improved hcaptcha accessibility signup logging

--- src/web/mjs/connectors/HolyManga.mjs
+++ src/web/mjs/connectors/HolyManga.mjs
@@ -7,9 +7,11 @@ export default class HolyManga extends WordPressZbulu {
         super.id = 'holymanga';
         super.label = 'Holy Manga';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'http://www.holymanga.net';
+        this.url = 'http://holymanga.net';
+    }
 
-        this._initializeConnector();
+    canHandleURI(uri) {
+        return /https?:\/\/w+\d*.holymanga.net/.test(uri.origin);
     }
 
     async _initializeConnector() {
@@ -20,9 +22,8 @@ export default class HolyManga extends WordPressZbulu {
         let uri = new URL(this.url);
         uri.searchParams.set('ts', Date.now());
         uri.searchParams.set('rd', Math.random());
-        let request = new Request(uri, this.requestOptions);
-        await Engine.Request.fetchUI(request, '');
-        let response = await fetch(request);
-        this.url = new URL(response.url).origin;
+        let request = new Request(uri.href, this.requestOptions);
+        this.url = await Engine.Request.fetchUI(request, `window.location.origin`);
+        console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
