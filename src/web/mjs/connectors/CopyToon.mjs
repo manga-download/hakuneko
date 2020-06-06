43531c922a2228bfb9eee0c57a2d0fd3bcac37fc
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-06 17:16:55 +0200

    [copytoon] updated clipboard predicate, use lazy URL assignment (on-demand)

--- src/web/mjs/connectors/CopyToon.mjs
+++ src/web/mjs/connectors/CopyToon.mjs
@@ -15,13 +15,22 @@ export default class CopyToon extends GnuBoard5BootstrapBasic2 {
                 resolve([...document.querySelectorAll('div#bo_v_con img')].map(img => img.src));
             });
         `;
-
-        this._initializeURL();
     }
 
-    async _initializeURL() {
-        let response = await fetch(this.url);
-        this.url = new URL(response.url).origin;
+    canHandleURI(uri) {
+        return /https:\/\/copytoon\d+.com/.test(uri.origin);
+    }
+
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri.href, this.requestOptions);
+        this.url = await Engine.Request.fetchUI(request, `window.location.origin`);
         console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 
