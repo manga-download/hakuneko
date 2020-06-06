a0fc4d7308c0649dcf6a73a3deb8e122eea588bc
Sheepux <sheepux@live.com>
2020-06-06 16:51:55 +0200

    MangaSusuReborn Domain Change (#1853)

--- src/web/mjs/connectors/MangaSusuReborn.mjs
+++ src/web/mjs/connectors/MangaSusuReborn.mjs
@@ -7,7 +7,7 @@ export default class MangaSusuReborn extends WordPressEManga {
         super.id = 'mangasusureborn';
         super.label = 'MangaSusu Reborn';
         this.tags = [ 'manga', 'webtoon', 'indonesian' ];
-        this.url = 'https://mangasusu.us';
+        this.url = 'https://mangasusu.fun';
         this.path = '/manga/?list';
 
         this.queryChapters = 'div.bxcl ul li span.lchx a';
