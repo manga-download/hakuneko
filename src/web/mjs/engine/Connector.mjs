da1bdae0d7970d4977b03946f385d32f01c8c579
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-06 16:42:33 +0200

    [clipboard] introduced regex to find matching connector

--- src/web/mjs/engine/Connector.mjs
+++ src/web/mjs/engine/Connector.mjs
@@ -38,6 +38,10 @@ export default class Connector {
         this.requestOptions.headers.set('accept', 'image/webp,image/apng,image/*,*/*');
     }
 
+    get urlPattern() {
+        return new RegExp(this.url);
+    }
+
     /**
      *
      */
