5b9f18238786ac430927f79e68954e48a6b63403
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-06 21:11:51 +0200

    [funbe] updated clipboard predicate, use lazy URL assignment (on-demand)

--- src/web/mjs/connectors/Funbe.mjs
+++ src/web/mjs/connectors/Funbe.mjs
@@ -7,8 +7,23 @@ export default class Funbe extends GnuBoard5BootstrapBasic2 {
         super.id = 'funbe';
         super.label = 'Funbe';
         this.tags = [ 'webtoon', 'hentai', 'korean' ];
-        this.url = 'https://funbe.me';
+        this.url = 'https://funbe.page';
+    }
 
-        this._initializeTwitterURL('1087681227832754182');
+    canHandleURI(uri) {
+        return /https?:\/\/funbe\.[a-z]+/.test(uri.origin);
+    }
+
+    async _initializeConnector() {
+        /*
+         * sometimes cloudflare bypass will fail, because chrome successfully loads the page from its cache
+         * => append random search parameter to avoid caching
+         */
+        let uri = new URL(await this._twitter.getProfileURL('1087681227832754182') || this.url);
+        uri.searchParams.set('ts', Date.now());
+        uri.searchParams.set('rd', Math.random());
+        let request = new Request(uri.href, this.requestOptions);
+        this.url = await Engine.Request.fetchUI(request, `window.location.origin`);
+        console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
