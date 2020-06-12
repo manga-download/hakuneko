11622059e98b4ada2ebc27fbce5fbb2105a53d1c
XenonR <service.github@nocer.net>
2020-06-12 10:58:14 +0200

    [vizshonenjump] add image decryption

--- src/web/mjs/connectors/VizShonenJump.mjs
+++ src/web/mjs/connectors/VizShonenJump.mjs
@@ -1,5 +1,5 @@
 import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+const EXIF = require('exif-js');
 
 export default class VizShonenJump extends Connector {
 
@@ -20,7 +20,7 @@ export default class VizShonenJump extends Connector {
             return {
                 id: manga.pathname,
                 title: manga.querySelector(':nth-child(2)').innerText.trim()
-            }
+            };
         });
     }
 
@@ -34,13 +34,13 @@ export default class VizShonenJump extends Connector {
                 return {
                     id: chapter.href,
                     title: chapter.querySelector('.disp-id').innerText.trim()
-                }
+                };
             } catch(error) {
                 try {
                     return {
                         id: chapter.href,
-                        title: 'Ch. ' + chapter.href.match(/chapter\-(\d+)\//)[1]
-                    }
+                        title: 'Ch. ' + chapter.href.match(/chapter-(\d+)\//)[1]
+                    };
                 } catch(error) {
                     throw new Error('Unknown chapter format. Please report at https://github.com/manga-download/hakuneko/issues');
                 }
@@ -49,8 +49,7 @@ export default class VizShonenJump extends Connector {
     }
 
     async _getPages(chapter) {
-        chapter.id = chapter.id.replace('hakuneko://cache/shonenjump', this.url)
-        console.log(chapter);
+        chapter.id = chapter.id.replace('hakuneko://cache/shonenjump', this.url);
         let script = `
             new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -85,14 +84,79 @@ export default class VizShonenJump extends Connector {
         `;
         let request = new Request(new URL(chapter.id, this.url));
         let data = await Engine.Request.fetchUI(request, script, 60000, false);
-        
-        console.log(data);
-        return data;
+
+        return data.map(page => this.createConnectorURI(
+            {
+                id: page,
+                referer: chapter.id
+            }
+        ));
     }
 
-    // async _handleConnectorURI(payload) {
+    async _handleConnectorURI(payload) {
+        let request = new Request(new URL(payload.id), this.requestOptions);
+        request.headers.set('x-referer', this.getRootRelativeOrAbsoluteLink(payload.referer, this.url));
+        request.headers.set('crossOrigin', 'Anonymous');
+        request.headers.set('Origin', new URL(this.url).origin);
 
-    // }
+        const response = await fetch(request);
+        const blob = await response.blob();
+        const bitmap = await createImageBitmap(blob);
+        const exif = EXIF.readFromBinaryFile(await blob.arrayBuffer());
+
+        let canvas = document.createElement('canvas');
+        canvas.width = exif.ImageWidth;
+        canvas.height = exif.ImageHeight;
+        let ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, exif.ImageWidth, exif.ImageHeight);
+
+        const x_split = Math.floor(exif.ImageWidth / 10);
+        const y_split = Math.floor(exif.ImageHeight / 15);
+        const shuffle_map = exif.ImageUniqueID.split(":");
+
+        // Draw border pieces
+        ctx.drawImage(
+            bitmap,
+            0, 0,
+            exif.ImageWidth, y_split,
+            0, 0,
+            exif.ImageWidth, y_split
+        );
+        ctx.drawImage(
+            bitmap,
+            0, y_split + 10,
+            x_split, exif.ImageHeight - 2 * y_split,
+            0, y_split, x_split,
+            exif.ImageHeight - 2 * y_split
+        );
+        ctx.drawImage(
+            bitmap, 0, 14 * (y_split + 10),
+            exif.ImageWidth, bitmap.height - 14 * (y_split + 10),
+            0, 14 * y_split,
+            exif.ImageWidth, bitmap.height - 14 * (y_split + 10)
+        );
+        ctx.drawImage(
+            bitmap,
+            9 * (x_split + 10), y_split + 10,
+            x_split + (exif.ImageWidth - 10 * x_split), exif.ImageHeight - 2 * y_split,
+            9 * x_split, y_split,
+            x_split + (exif.ImageWidth - 10 * x_split), exif.ImageHeight - 2 * y_split
+        );
+
+        // Draw inside pieces
+        for (let piece = 0; piece < shuffle_map.length; piece++) {
+            shuffle_map[piece] = parseInt(shuffle_map[piece], 16);
+            ctx.drawImage(
+                bitmap,
+                Math.floor((piece % 8 + 1) * (x_split + 10)), Math.floor((Math.floor(piece / 8) + 1) * (y_split + 10)), // sx, sy
+                Math.floor(x_split), Math.floor(y_split), // sWidth, sHeight
+                Math.floor((shuffle_map[piece] % 8 + 1) * x_split), Math.floor((Math.floor(shuffle_map[piece] / 8) + 1) * y_split), // dx, dy
+                Math.floor(x_split), Math.floor(y_split)); // dWidth, dHeight
+        }
+
+        let data = await this._canvasToBlob(canvas);
+        return this._blobToBuffer(data);
+    }
 
     async _canvasToBlob(canvas) {
         return new Promise(resolve => {
@@ -102,7 +166,9 @@ export default class VizShonenJump extends Connector {
         });
     }
 
-    async _getMangaFromURI(uri) {
-
+    throttle_connections(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
     }
 }
