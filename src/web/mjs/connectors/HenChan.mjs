60db582a581f58c6af554f746fb22ab947477b9f
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-19 11:57:10 +0200

    [henchan] updated domain

--- src/web/mjs/connectors/HenChan.mjs
+++ src/web/mjs/connectors/HenChan.mjs
@@ -1,19 +1,13 @@
 import MangaChan from './MangaChan.mjs';
 
-/**
- * Template: MangaChan
- */
 export default class HenChan extends MangaChan {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'henchan';
         super.label = 'Хентай-тян! (Hentai-chan)';
         this.tags = [ 'hentai', 'russian' ];
-        this.url = 'http://h-chan.me';
+        this.url = 'https://henchan.pro';
 
         this.path = '/manga/new';
         this.queryChapters = 'div.extaraNavi p.extra_off:last-of-type a';
