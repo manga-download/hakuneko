fbffe4aea8c12f455a127043f708d421174e0e98
ronny1982 <wegener.ronny@gmail.com>
2020-06-27 22:18:22 +0200

    [wordpressmadara] added/updated websites (#1956)
    * [phenomenalnofansub] updated domain
    
    * [ahstudios] added website support
    
    * [angelofresistance] updated protocol

--- src/web/mjs/connectors/AngelOfResistance.mjs
+++ src/web/mjs/connectors/AngelOfResistance.mjs
@@ -7,7 +7,7 @@ export default class AngelOfResistance extends WordPressMadara {
         super.id = 'angelofresistance';
         super.label = 'Angel Of Resistance (AOR-TR)';
         this.tags = [ 'manga', 'webtoon', 'turkish'];
-        this.url = 'http://angelofresistance-tr.com';
+        this.url = 'https://angelofresistance-tr.com';
         this.requestOptions.headers.set('x-referer', this.url);
     }
 }
