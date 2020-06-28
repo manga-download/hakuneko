4b7947602267f71eff003031e17c44b86e2549f7
XenonR <service.github@nocer.net>
2020-06-28 18:11:50 +0200

    DiscordPresence: emit events

--- src/web/mjs/engine/Enums.mjs
+++ src/web/mjs/engine/Enums.mjs
@@ -7,6 +7,9 @@ export default class Enums {
         return {
             onMangaStatusChanged:     'onMangaStatusChanged', // ...
             onChapterStatusChanged:   'onChapterStatusChanged', // ...
+            onChapterGetPages:        'onChapterGetPages',
+            onSelectConnector:        'onSelectConnector',
+            onMangaClicked:           'onMangaClicked'
         };
     }
 }
