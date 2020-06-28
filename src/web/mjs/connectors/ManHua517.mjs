6b1beb4f4088484b15a7f23532db918a5c3eca4b
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-28 04:36:38 +0200

    [517mh] updated path

--- src/web/mjs/connectors/ManHua517.mjs
+++ src/web/mjs/connectors/ManHua517.mjs
@@ -8,5 +8,7 @@ export default class ManHua517 extends ZYMK {
         super.label = '517漫画网 (517 ManhuaWeb)';
         this.tags = [ 'webtoon', 'chinese' ];
         this.url = 'http://www.517mh.net';
+
+        this.path = '/sort/index_';
     }
 }
