a772128d54636d164951907510ae815a05d788f6
Ronny Wegener <wegener.ronny@gmail.com>
2020-06-28 04:36:52 +0200

    [1359mh] updated path

--- src/web/mjs/connectors/ManHua1359.mjs
+++ src/web/mjs/connectors/ManHua1359.mjs
@@ -8,5 +8,7 @@ export default class ManHua1359 extends ZYMK {
         super.label = '1359漫画网 (1359 ManhuaWeb)';
         this.tags = [ 'webtoon', 'chinese' ];
         this.url = 'https://1359mh.com';
+
+        this.path = '/sort/';
     }
 }
