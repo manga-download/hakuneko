7c8acc9de1c9fd9a074fd14264e4a4170ca99b57
Steffen Schmidt <service.github@nocer.net>
2020-07-11 20:13:24 +0200

    [furyosquad] Add website support (#2024)

---
+++ src/web/mjs/connectors/FuryoSquad.mjs
@@ -0,0 +1,49 @@
+import Connector from '../engine/Connector.mjs';
+
+export default class FuryoSquad extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'furyosquad';
+        super.label = 'Furyo Squad';
+        this.tags = [ 'manga', 'french', 'high-quality' ];
+        this.url = 'http://www.furyosquad.com';
+    }
+
+    async _getMangas() {
+        const categories = ['/', '/en-cours', '/termines', '/stoppes'];
+
+        let mangas = [];
+        for ( const category of categories) {
+            let request = new Request(new URL(category, this.url), this.requestOptions);
+            let data = await this.fetchDOM(request, 'div.fs-chap-container div.grid-item-container div.media-body a');
+            mangas.push( ...data.map(manga => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(manga, this.url),
+                    title: manga.title.trim()
+                };
+            }));
+        }
+
+        return mangas;
+    }
+
+    async _getChapters(manga) {
+        const request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.list.fs-chapter-list div.title a');
+
+        return data.map(chapter => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(chapter, this.url),
+                title: chapter.title.trim()
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.fs-reader-page source');
+
+        return data.map(page => this.getAbsolutePath(page.src, this.url));
+    }
+}
