c3fae0f91fb1a194bfc19d8cb010ad6bfb5eee87
Steffen Schmidt <service.github@nocer.net>
2020-07-12 10:15:01 +0200

    [SmackJeeves] fix website

--- src/web/mjs/connectors/SmackJeeves.mjs
+++ src/web/mjs/connectors/SmackJeeves.mjs
@@ -1,130 +1,89 @@
 import Connector from '../engine/Connector.mjs';
 
-/**
- *
- */
 export default class SmackJeeves extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'smackjeeves';
         super.label = 'Smack Jeeves';
         this.tags = [ 'webtoon', 'english' ];
-        this.url = 'http://www.smackjeeves.com';
+        this.url = 'https://www.smackjeeves.com';
     }
 
-    /**
-     *
-     */
-    _getMangaListFromPages( mangaPageLinks, index ) {
-        index = index || 0;
-        return this.fetchDOM( mangaPageLinks[ index ], 'div.comic-container a.card', 5 )
-            .then( data => {
-                let mangaList = data.map( element => {
+    async _getCategories( url) {
+        const script = `new Promise(resolve => resolve(cmnData));`;
+        const request = new Request(new URL(url), this.requestOptions);
+        const data = await Engine.Request.fetchUI(request, script);
+
+        return data.navigation.items.map(item => item.val);
+    }
+
+    async _getMangas() {
+        const categories = await this._getCategories( new URL('/discover?type=genre', this.url));
+
+        let manga_list = [];
+        for (const category of categories) {
+            let form = new URLSearchParams();
+            form.set('genre', category);
+            form.set('order', 'newarriva');
+
+            let pages = 1;
+            for (let page = 1; page <= pages; page++) {
+                form.set('page', page);
+                const data = await this._fetchPOST(new URL('/api/getTitleListByGenreDiscover', this.url), form);
+                pages = data.result.totalPageCnt;
+
+                manga_list.push(...data.result.list.map(item => {
+                    // decode html entities
+                    let title = document.createElement('div');
+                    title.innerHTML = item.title;
                     return {
-                        id: this.getRelativeLink( element ),
-                        title: element.querySelector( 'div.title' ).textContent.trim()
+                        id: this.getRootRelativeOrAbsoluteLink(item.titleUrl, this.url),
+                        title: title.textContent.trim()
                     };
-                } );
-                if( index < mangaPageLinks.length - 1 ) {
-                    return this._getMangaListFromPages( mangaPageLinks, index + 1 )
-                        .then( mangas => mangas.concat( mangaList ) );
-                } else {
-                    return Promise.resolve( mangaList );
-                }
-            } );
+                }));
+            }
+        }
+
+        return manga_list;
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        let path = '/search.php?last_update=6&sort_by=4';
-        this.fetchDOM( this.url + path, 'div.comic-nav div#browse-page-select select option' )
-            .then( data => {
-                let pageLinks = data.map( option => this.url + path + '&start=' + option.value );
-                return this._getMangaListFromPages( pageLinks );
-            } )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        const url = new URL('/api'+manga.id, this.url);
+        const data = await this._fetchPOST(url);
+
+        return data.result.list.map(item => {
+            // decode html entities
+            let title = document.createElement('div');
+            title.innerHTML = item.articleTitle;
+            return {
+                id: item.articleUrl,
+                title: title.textContent.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let comicURL;
-        this.fetchDOM( this.url + manga.id, 'div.middle div.title div.buttons a.button:last-of-type' )
-            .then( data => {
-                comicURL = data[0].href;
-                this.requestOptions.headers.set( 'x-cookie', '_sj_view_m=' + Math.floor( Date.now() / 1000 ) );
-                return this.fetchDOM( comicURL, 'select.jumpbox' );
-            } )
-            .then( data => {
-                if( !data.length ) {
-                    throw new Error( 'Page template not supported, no chapter/page selection box found!' );
-                }
-                let chapterList = [...data[0].querySelectorAll( '.jumpbox_chapter' )];
-                if( !chapterList.length ) {
-                    chapterList = [
-                        {
-                            id: comicURL,
-                            title: manga.title,
-                            language: 'en'
-                        }
-                    ];
-                } else {
-                    chapterList = chapterList.map( element => {
-                        return {
-                            id:  new URL( element.value || comicURL, comicURL ).href,
-                            title: element.label.trim() || element.textContent.trim(),
-                            language: 'en'
-                        };
-                    } );
-
-                }
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getPages(chapter) {
+        const script = `
+            new Promise(resolve => resolve(
+                cmnData.comicData.map(page => {
+                    return page.url;
+                })
+            ));
+        `;
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        return await Engine.Request.fetchUI(request, script);
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'select.jumpbox option.jumpbox_page' )
-            .then( data => {
-                let pageLinks = data.map( element => this.createConnectorURI( this.getAbsolutePath( element.value, request.url ) ) );
-                callback( null, pageLinks );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
-    }
-
-    /**
-     *
-     */
-    _handleConnectorURI( payload ) {
-        let request = new Request( payload, this.requestOptions );
-        /*
-         * TODO: only perform requests when from download manager
-         * or when from browser for preview and selected chapter matches
-         */
-        return this.fetchDOM( request, 'source#comic_image' )
-            .then( data => super._handleConnectorURI( this.getAbsolutePath( data[0], request.url ) ) );
+    async _fetchPOST( uri, form) {
+        const request = new Request(new URL(uri, this.url), {
+            method: 'POST',
+            body: form.toString(),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        const data = await fetch(request);
+        return data.json();
     }
 }
