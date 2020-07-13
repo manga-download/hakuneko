20f9c651eed9b12e3624559ded7f3bfd690aad08
AthK <51260976+newaccforhc@users.noreply.github.com>
2020-07-13 17:13:09 +0530

    [JapanRead] Implimented new method and Fixed the URI naming error (#2039)
    * [JapanRead] Implimented new method and Fixed #2036

--- src/web/mjs/connectors/JapanRead.mjs
+++ src/web/mjs/connectors/JapanRead.mjs
@@ -1,13 +1,8 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class JapanRead extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'japanread';
@@ -16,93 +11,49 @@ export default class JapanRead extends Connector {
         this.url = 'https://www.japanread.cc';
     }
 
-    /**
-     *
-     */
-    async _initializeConnector() {
-        let request = new Request( this.url + '/manga-list/switch-view/3', this.requestOptions );
-        return Engine.Request.fetchUI( request, '' );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'h1.card-header');
+        let id = uri.pathname;
+        let title = data[0].textContent.trim();
+        return new Manga(this, id, title);
     }
 
-    /**
-     *
-     */
-    _getMangaListFromPages( mangaPageLinks, index ) {
-        index = index || 0;
-        let request = new Request( mangaPageLinks[ index ], this.requestOptions );
-        return this.fetchDOM( request, 'div.container div.row a.manga_title', 5 )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim()
-                    };
-                } );
-                if( index < mangaPageLinks.length - 1 ) {
-                    return this._getMangaListFromPages( mangaPageLinks, index + 1 )
-                        .then( mangas => mangaList.concat( mangas ) );
-                } else {
-                    return Promise.resolve( mangaList );
-                }
-            } );
+    async _getMangas() {
+        let request = new Request(new URL('/manga-list', this.url), this.requestOptions);
+        let pages = await this.fetchDOM(request, 'ul.pagination li.page-item:nth-last-child(2) > a');
+        pages = Number(pages[0].text);
+
+        let mangas = [];
+        for (let page = 0; page <= pages; page++) {
+            request = new Request(new URL('/manga-list?page=' + page, this.url), this.requestOptions);
+            let data = await this.fetchDOM(request, 'a.text-truncate', 5);
+            mangas.push( ...data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                    title: element.innerText.trim()
+                };
+            }));
+        }
+
+        return mangas;
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/manga-list', this.requestOptions );
-        this.fetchDOM( request, 'nav ul.pagination li.page-item:nth-last-of-type(2) a.page-link' )
-            .then( data => {
-                let pageCount = parseInt( data[0].text.trim() );
-                let pageLinks = [... new Array( pageCount ).keys()].map( page => request.url + '?page=' + ( page + 1 ) );
-                return this._getMangaListFromPages( pageLinks );
-            } )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'a.text-truncate');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.textContent.trim(),
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'div.chapter-container div.chapter-row div.order-lg-2 a' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.replace( manga.title, '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
-    }
-
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'head meta[data-chapter-id]' )
-            .then( data => this.fetchJSON( this.url + '/api/?type=chapter&id=' + data[0].dataset.chapterId ) )
-            .then( data => {
-                let pageList = data.page_array.map( page => this.getAbsolutePath( data.baseImagesUrl + '/' + page, request.url ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let id = await this.fetchDOM(request, 'head meta[data-chapter-id]');
+        let data = await this.fetchJSON(this.url + '/api/?type=chapter&id=' + id[0].dataset.chapterId);
+        return data.page_array.map( page => this.getAbsolutePath( data.baseImagesUrl + '/' + page, request.url ) );
     }
 }
