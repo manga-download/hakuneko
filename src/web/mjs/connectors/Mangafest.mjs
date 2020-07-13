56b7488f0ac0ca943c142c5d56199c1e2f00bf70
XenonR <service.github@nocer.net>
2020-07-13 22:23:30 +0200

    mangafest: minor Fixes & Optimization

--- src/web/mjs/connectors/Mangafest.mjs
+++ src/web/mjs/connectors/Mangafest.mjs
@@ -18,11 +18,11 @@ export default class Mangafest extends Connector {
 
         let mangas = [];
         for (let page = 1; page <= pages; page++) {
-            request = new Request(new URL('/category/manga/page/'+page+'/', this.url), this.requestOptions);
+            request = new Request(new URL(`/category/manga/page/${page}/`, this.url), this.requestOptions);
             data = await this.fetchDOM(request, 'div.post_image a');
             mangas.push(...data.map(manga => {
                 return {
-                    id: this.getRootRelativeOrAbsoluteLink(manga.pathname, request.url),
+                    id: this.getRootRelativeOrAbsoluteLink(manga.pathname, this.url),
                     title: manga.getAttribute('aria-label').trim()
                 };
             }));
@@ -33,12 +33,7 @@ export default class Mangafest extends Connector {
 
     async _getChapters(manga) {
         // website provides mangas as whole
-        return [
-            {
-                id: manga.id,
-                title: manga.title
-            }
-        ];
+        return [ manga ];
     }
 
     async _getPages(manga) {
@@ -46,28 +41,36 @@ export default class Mangafest extends Connector {
         let data = await this.fetchDOM(request, 'div#ngy2p');
         const flickr_data = JSON.parse(data[0].getAttribute('data-nanogallery2-portable'));
 
-        let url = new URL('https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=2f0e634b471fdb47446abcb9c5afebdc&format=json');
+        let url = new URL('https://api.flickr.com/services/rest/');
+        url.searchParams.set('method', 'flickr.photosets.getPhotos');
+        url.searchParams.set('api_key', '2f0e634b471fdb47446abcb9c5afebdc');
+        url.searchParams.set('format', 'json');
         url.searchParams.set('user_id', flickr_data.userID);
         url.searchParams.set('photoset_id', flickr_data.photoset);
         url.searchParams.set('page', 1); // Returns 500 pictures per page. Since these are hobbyist mangas I assume none will be over 500 pages.
 
+        const flickr_regex = /^jsonFlickrApi\((.*)\)$/g;
         request = new Request(url);
-        data = await this.fetchRegex(request, /^jsonFlickrApi\((.*)\)/g);
+        data = await this.fetchRegex(request, flickr_regex);
         data = JSON.parse(data);
 
         if (data.stat != 'ok') throw new Error('Flickr API - Gallery query failed.');
 
+        url.searchParams.delete('user_id');
+        url.searchParams.delete('photoset_id');
+        url.searchParams.delete('page');
+
         let pages = [];
-        url = new URL('https://api.flickr.com/services/rest/?&method=flickr.photos.getSizes&format=json&api_key=2f0e634b471fdb47446abcb9c5afebdc');
+        url.searchParams.set('method', 'flickr.photos.getSizes');
         for (const page of data.photoset.photo) {
             url.searchParams.set('photo_id', page.id);
             request = new Request(url);
-            data = await this.fetchRegex(request, /^jsonFlickrApi\((.*)\)/g);
+            data = await this.fetchRegex(request, flickr_regex);
             data = JSON.parse(data);
 
             if (data.stat != 'ok') throw new Error('Flickr API - Image query failed.');
 
-            data = data.sizes.size.sort(this.sortSizes)[0];
+            data = data.sizes.size.sort((a, b) => b.height - a.height)[0];
             pages.push(data.source);
         }
 
@@ -80,15 +83,5 @@ export default class Mangafest extends Connector {
         let id = uri.pathname;
         let title = data[0].text.split(' – ')[0].trim();
         return new Manga(this, id, title);
-    }
-
-    sortSizes( first, second ) {
-        if ( first.height > second.height ){
-            return -1;
-        }
-        if ( first.height < second.height ){
-            return 1;
-        }
-        return 0;
     }
 }
