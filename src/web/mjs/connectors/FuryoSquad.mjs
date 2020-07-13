4d5d56c47403a805bc72303275ccb2d411b47cae
Steffen Schmidt <service.github@nocer.net>
2020-07-13 18:42:56 +0200

    [furyosquad] add age check (#2033)

--- src/web/mjs/connectors/FuryoSquad.mjs
+++ src/web/mjs/connectors/FuryoSquad.mjs
@@ -29,10 +29,10 @@ export default class FuryoSquad extends Connector {
     }
 
     async _getChapters(manga) {
-        const request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.list.fs-chapter-list div.title a');
+        const url = new URL(manga.id, this.url);
+        const data = await this._fetchPOST(url, 'adult=true', 'div.list.fs-chapter-list div.title a');
 
-        return data.map(chapter => {
+        return [...data].map(chapter => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(chapter, this.url),
                 title: chapter.title.trim()
@@ -45,5 +45,18 @@ export default class FuryoSquad extends Connector {
         const data = await this.fetchDOM(request, 'div.fs-reader-page source');
 
         return data.map(page => this.getAbsolutePath(page.src, this.url));
+    }
+
+    async _fetchPOST(uri, body, selector) {
+        const request = new Request(new URL(uri, this.url), {
+            method: 'POST',
+            body: body,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        let data = await fetch(request);
+        data = this.createDOM(await data.text());
+        return data.querySelectorAll(selector);
     }
 }
