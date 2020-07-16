ef431cd30b27b875c5132d8f5516ead9ea4f42d0
Ronny Wegener <wegener.ronny@gmail.com>
2020-07-16 19:52:34 +0200

    [maid] update queries and chapter list extraction

--- src/web/mjs/connectors/MaID.mjs
+++ src/web/mjs/connectors/MaID.mjs
@@ -1,13 +1,7 @@
 import WordPressEManga from './templates/WordPressEManga.mjs';
 
-/**
- *
- */
 export default class MaID extends WordPressEManga {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'maid';
@@ -16,8 +10,20 @@ export default class MaID extends WordPressEManga {
         this.url = 'https://www.maid.my.id';
         this.path = '/manga-list/?list';
 
-        this.queryMangas = 'div.contentpost div.soralist ul li a.series';
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
-        this.queryPages = 'div#readerarea :not(.kln) > source';
+        this.queryMangas = 'div.mangalist-blc ul li.Manga a.series';
+        this.queryChapters = 'div.series-chapter ul.series-chapterlist li div.flexch-infoz a';
+        this.queryPages = 'div.reader-area source[src]:not([src=""])';
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title.replace(manga.title, '').trim(),
+                language: ''
+            };
+        });
     }
 }
