5834ed0f23a2540c1ef5b3d32fbab1aeb788ab59
Ronny Wegener <wegener.ronny@gmail.com>
2020-07-22 00:10:17 +0200

    [magairo] get title in correct casing (instead of upper case)

--- src/web/mjs/connectors/MangaIro.mjs
+++ src/web/mjs/connectors/MangaIro.mjs
@@ -1,4 +1,5 @@
 import MangaNel from './MangaNel.mjs';
+import Manga from '../engine/Manga.mjs';
 
 export default class MangaIro extends MangaNel {
 
@@ -10,7 +11,7 @@ export default class MangaIro extends MangaNel {
         this.url = 'https://m.mangairo.com';
 
         this.path = '/manga-list/type-latest/ctg-all/state-all/page-';
-        this.queryMangaTitle = 'div.story_content ul.story_info_right h1';
+        this.queryMangaTitle = /name_title\s*=\s*['"](.+)['"]\s*;/g;
         this.queryMangasPageCount = 'div.group-page a.go-p-end';
         this.queryMangas = 'div.story-list div.story-item h3.story-name a';
         // NOTE: a corresponding entry for chapter/page queries must be defined in the base class (required for cross-domain-support)
@@ -19,5 +20,13 @@ export default class MangaIro extends MangaNel {
     canHandleURI(uri) {
         // Verification: https://regex101.com/r/pZFHj3/2/tests
         return /^(m\.|chap\.)?mangairo\.com$/.test(uri.hostname);
+    }
+
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchRegex(request, this.queryMangaTitle);
+        let id = uri.href;
+        let title = data[0].trim();
+        return new Manga(this, id, title);
     }
 }
