78ee778a3875d43226acc21535a83ce6da14e6ec
ronny1982 <wegener.ronny@gmail.com>
2020-07-29 00:17:36 +0200

    [manhwatime] changed template (#2112)

--- src/web/mjs/connectors/ManhwaTime.mjs
+++ src/web/mjs/connectors/ManhwaTime.mjs
@@ -1,6 +1,6 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressEManga from './templates/WordPressEManga.mjs';
 
-export default class ManhwaTime extends WordPressMadara {
+export default class ManhwaTime extends WordPressEManga {
 
     constructor() {
         super();
@@ -8,5 +8,21 @@ export default class ManhwaTime extends WordPressMadara {
         super.label = 'Manhwa Time';
         this.tags = [ 'webtoon', 'hentai', 'english' ];
         this.url = 'https://manhwatime.xyz';
+        this.path = '/manhwa/?list';
+
+        this.queryMangas = 'div.animepost div.animposx > a';
+        this.queryChapters = 'div#chapter_list span.lchx a';
+        this.queryPages = 'div.reader-area source[src]:not([src=""])';
+    }
+
+    async _getMangas() {
+        let request = new Request(new URL(this.path, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title.trim()
+            };
+        });
     }
 }
