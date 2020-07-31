b526095cc233403d3c49c75ca5264032c12ec193
Ronny Wegener <wegener.ronny@gmail.com>
2020-07-31 13:00:09 +0200

    [manga1001] added website support

---
+++ src/web/mjs/connectors/Manga1001.mjs
@@ -0,0 +1,70 @@
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+
+export default class Manga1001 extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'manga1001';
+        super.label = 'Manga 1001';
+        this.tags = [ 'manga', 'japanese' ];
+        this.url = 'https://manga1001.com';
+    }
+
+    canHandleURI(uri) {
+        return /^manga100[01]\.com$/.test(uri.hostname);
+    }
+
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'header.entry-header h1.entry-title');
+        let id = decodeURI(uri.pathname);
+        let title = data[0].textContent.trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        let uri = new URL(this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'nav.pagination div.nav-links a.page-numbers:nth-last-of-type(2)');
+        let pageCount = parseInt(data[0].text.trim());
+        for(let page = 1; page <= pageCount; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let uri = new URL(`/page/${page}/`, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'main#main header.entry-header h3.entry-title a');
+        return data.map(element => {
+            return {
+                id: decodeURI(element.pathname),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        let uri = new URL(manga.id, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.chaplist table.table tbody tr td:first-of-type a');
+        return data.map(element => {
+            return {
+                id: decodeURI(element.pathname),
+                title: element.text.trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        let uri = new URL(chapter.id, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.entry-content figure.wp-block-image source');
+        return data.map(element => this.getAbsolutePath(element.dataset.src || element, request.url));
+    }
+}
