8f3184563054865a6f87242d045f76ee643f164e
ronny1982 <wegener.ronny@gmail.com>
2020-08-06 23:24:48 +0200

    merge from master (#2151)
    * [mangakik] added website support (#2143)
    
    * [whatstatus] added website support (#2143)
    
    * [tumangaonline] updated image extraction (#2148)
    
    * [kissmanga] moved to closed source, bypass fake images (#2150)

---
+++ src/web/mjs/connectors/MangaKik.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MangaKik extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mangakik';
+        super.label = 'MangaKik';
+        this.tags = [ 'manga', 'webtoon', 'english' ];
+        this.url = 'https://mangakik.com';
+    }
+}
