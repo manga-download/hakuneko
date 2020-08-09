5536808e6e962c957997e00d8f3a9a0d721ffd7d
Ronny Wegener <wegener.ronny@gmail.com>
2020-08-09 05:17:51 +0200

    [animepahe] use permanent manga IDs instead of temporary sessions

--- src/web/mjs/connectors/AnimePahe.mjs
+++ src/web/mjs/connectors/AnimePahe.mjs
@@ -29,46 +29,56 @@ export default class AnimePahe extends Connector {
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchDOM(request, 'div#modalBookmark div.modal-body div a[href*="pahe.win/a"]');
-        let id = uri.pathname;
-        let title = data[0].text.trim();
+        let title = data[0].title.trim();
+        let id = await this._getMangaID(uri.pathname.split('/').pop(), title);
         return new Manga(this, id, title);
     }
 
     async _getMangas() {
+        let mangaList = [];
         let uri = new URL('/anime', this.url);
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchDOM(request, 'div.tab-content div.row a');
-        return data.map(element => {
-            return {
-                id: element.pathname,
-                title: element.text.trim()
-            };
-        });
+        for(let element of data) {
+            this.cfMailDecrypt(element);
+            let title = element.text.trim();
+            mangaList.push({
+                id: await this._getMangaID(element.pathname.split('/').pop(), title),
+                title: title
+            });
+        }
+        return mangaList;
+    }
+
+    async _getMangaID(guid, title) {
+        let uri = new URL('/api', this.url);
+        uri.searchParams.set('m', 'search');
+        uri.searchParams.set('l', 8);
+        uri.searchParams.set('q', title);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchJSON(request);
+        return data.data.find(manga => manga.session === guid).id;
     }
 
     async _getChapters(manga) {
         let chapterList = [];
-        let uri = new URL(manga.id, this.url);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div#modalBookmark div.modal-body div a[href*="pahe.win/a"]');
-        let id = parseInt(data[0].pathname.split('/').pop());
         for(let page = 1, run = true; run; page++) {
-            let chapters = await this._getChaptersFromPage(manga, id, page);
+            let chapters = await this._getChaptersFromPage(manga, page);
             chapters.length > 0 ? chapterList.push(...chapters) : run = false;
         }
         return chapterList;
     }
 
-    async _getChaptersFromPage(manga, id, page) {
+    async _getChaptersFromPage(manga, page) {
         let uri = new URL('/api', this.url);
         uri.searchParams.set('m', 'release');
-        uri.searchParams.set('id', id);
+        uri.searchParams.set('id', manga.id);
         uri.searchParams.set('page', page);
         //uri.searchParams.set('l', 30); // limit
         //uri.searchParams.set('sort', 'episode_desc');
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchJSON(request);
-        if(data.current_page > data.last_page) {
+        if(!data.data || data.current_page > data.last_page) {
             return [];
         }
         return data.data.map(item => {
@@ -76,7 +86,7 @@ export default class AnimePahe extends Connector {
             title += item.episode2 > 0 ? '.' + item.episode2 : '';
             title += item.title ? ' - ' + item.title : '';
             return {
-                id: `/api?m=links&id=${id}&session=${item.session}&p=kwik`,
+                id: item.session, // item.id
                 title: title,
                 language: ''
             };
@@ -84,8 +94,11 @@ export default class AnimePahe extends Connector {
     }
 
     async _getPages(chapter) {
-        let resolution = parseInt(this.config.resolution.value || 0);
-        let uri = new URL(chapter.id, this.url);
+        let uri = new URL('/api', this.url);
+        uri.searchParams.set('m', 'links');
+        uri.searchParams.set('id', chapter.manga.id);
+        uri.searchParams.set('session', chapter.id);
+        uri.searchParams.set('p', 'kwik');
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchJSON(request);
         let streams = data.data.reduce((accumulator, entry) => {
@@ -97,14 +110,20 @@ export default class AnimePahe extends Connector {
             });
             return accumulator.concat(links);
         }, []);
+        let resolution = parseInt(this.config.resolution.value || 0);
         let stream = streams.find(stream => stream.resolution >= resolution) || streams.shift();
         let kwik = new Kwik(stream.url, this.url);
-        let playlist = await kwik.getPlaylist();
         return {
             hash: 'id,language,resolution',
-            mirrors: [ playlist ],
+            mirrors: [ await kwik.getPlaylist() ],
             referer: stream.url,
             subtitles: []
         };
+        /*
+        return {
+            video: await kwik.getStream(),
+            subtitles: []
+        };
+        */
     }
 }
