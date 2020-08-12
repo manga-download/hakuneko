4544fa500bf2107c1f68c42b0c25adf05b19af3e
ronny1982 <wegener.ronny@gmail.com>
2020-08-12 21:16:31 +0200

    merge from master (#2170)
    * increase video stream chunk size to prevent amount of requests (hydrax => block) (#2152)
    
    * [ravensscans] introduced image CDN (#2159)
    
    * [animepahe] added website support (#2153)
    
    * [mangas] fixed chapter list (#2169)

--- src/web/mjs/engine/DownloadJob.mjs
+++ src/web/mjs/engine/DownloadJob.mjs
@@ -26,7 +26,7 @@ export default class DownloadJob extends EventTarget {
         };
         this.requestOptions = chapter.manga.connector.requestOptions || {};
         // TODO: initialize requestOptions.headers = new Headers() if not set
-        this.chunkSize = 1048576; // 1 MB
+        this.chunkSize = 8388608; // 8 MB
         this.throttle = chapter.manga.connector.config && chapter.manga.connector.config['throttle'] ? chapter.manga.connector.config['throttle'].value : 50 ;
         this.status = undefined;
         this.progress = 0;
@@ -161,7 +161,11 @@ export default class DownloadJob extends EventTarget {
         };
         let playlistURL = undefined;
         let promises = episode.mirrors.map( mirror => {
-            return fetch( mirror, this.requestOptions )
+            let request = new Request(mirror, this.requestOptions);
+            if(episode.referer) {
+                request.headers.set('x-referer', episode.referer);
+            }
+            return fetch(request)
                 .then( response => {
                     if( response.status !== 200 ) {
                         throw new Error( 'Playlist "' + mirror + '" returned status: ' + response.status + ' - ' + response.statusText );
@@ -212,7 +216,11 @@ export default class DownloadJob extends EventTarget {
                 let promises = packets.map( ( packet, index ) => {
                     return this._wait( index * this.throttle )
                         .then( () => {
-                            return fetch( packet.source, this.requestOptions );
+                            let request = new Request(packet.source, this.requestOptions);
+                            if(episode.referer) {
+                                request.headers.set('x-referer', episode.referer);
+                            }
+                            return fetch(request);
                         } )
                         .then( response => {
                             if( response.status !== 200 ) {
@@ -242,7 +250,11 @@ export default class DownloadJob extends EventTarget {
                     }
                     return this._wait( index * 50 )
                         .then( () => {
-                            return fetch( subtitle.url, this.requestOptions );
+                            let request = new Request(subtitle.url, this.requestOptions);
+                            if(episode.referer) {
+                                request.headers.set('x-referer', episode.referer);
+                            }
+                            return fetch(request);
                         } )
                         .then( response => {
                             if( response.status !== 200 ) {
@@ -287,9 +299,8 @@ export default class DownloadJob extends EventTarget {
             } );
     }
 
-    /**
-     *
-     */
+    // TODO: read from the stream directly to the file instead of processing chunks
+    // => https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream | response.body.pipe(...)
     _downloadVideoStream( episode, directory, callback ) {
         let basename = Date.now(); // episode.video.split( '/' ).pop();
         this.requestOptions['method'] = 'HEAD';
