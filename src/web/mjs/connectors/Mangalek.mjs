96e7976a92a4b8bb385bbb44ebee753b99c11f6d
ronny1982 <wegener.ronny@gmail.com>
2020-08-15 15:39:26 +0200

    [mangalek] fixed manga list retrieval (#2175)

--- src/web/mjs/connectors/Mangalek.mjs
+++ src/web/mjs/connectors/Mangalek.mjs
@@ -8,5 +8,28 @@ export default class Mangalek extends WordPressMadara {
         super.label = 'مانجا ليك (Mangalek)';
         this.tags = [ 'manga', 'webtoon', 'arabic' ];
         this.url = 'https://mangalek.com';
+
+        this.queryMangas = 'div.page-content-listing:not(#loop-content) div.post-title h3 a';
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        for(let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let uri = new URL(`/manga/page/${page}/`, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.trim()
+            };
+        });
     }
 }
