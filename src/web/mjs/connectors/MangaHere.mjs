8c752954a74c8126755677501c602d2747ff0c8d
ronny1982 <wegener.ronny@gmail.com>
2020-08-21 20:58:57 +0200

    [mangahere] use text based manga list (#2208)

--- src/web/mjs/connectors/MangaHere.mjs
+++ src/web/mjs/connectors/MangaHere.mjs
@@ -1,22 +1,23 @@
 import MangaFox from './MangaFox.mjs';
 
-/**
- *
- */
 export default class MangaHere extends MangaFox {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangahere';
         super.label = 'MangaHere';
         this.url = 'https://www.mangahere.cc';
+    }
 
-        /*
-         * this script uses chapter_bar.js instead of chapterfun.ashx as in MangaFox
-         *this.scriptSource = 'chapter_bar.js';
-         */
+    async _getMangas() {
+        let uri = new URL(`/mangalist/`, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.browse-new-block-list div.browse-new-block p.browse-new-block-content a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title.trim()
+            };
+        });
     }
 }
