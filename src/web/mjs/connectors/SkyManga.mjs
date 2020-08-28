351716084a89f30944eb3e392dec81da88143ba5
ronny1982 <wegener.ronny@gmail.com>
2020-08-28 16:08:59 +0200

    merge from maste (#2228)
    * [MangaHere][MangaFox] Remove ad on last page (#2214)
    
    * [9anime] prevent IP ban, updated mcloud protection (#2223)
    
    * [skymanga] added website support (#2224)
    
    * [heroscan] disable referer to prevent blocking of cross-domain linked images (e.g. isekaiscan) (#2219)
    
    Co-authored-by: Sheepux <sheepux@live.com>

---
+++ src/web/mjs/connectors/SkyManga.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class SkyManga extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'skymanga';
+        super.label = 'Sky Manga';
+        this.tags = [ 'webtoon', 'hentai', 'english' ];
+        this.url = 'https://skymanga.co';
+    }
+}
