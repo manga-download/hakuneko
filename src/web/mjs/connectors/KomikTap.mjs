23eccf835e28584b98630c8cdceb9615d90ffc62
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-08 20:08:23 +0200

    [komiktap] added website support

---
+++ src/web/mjs/connectors/KomikTap.mjs
@@ -0,0 +1,16 @@
+import WordPressEManga from './templates/WordPressEManga.mjs';
+
+export default class KomikTap extends WordPressEManga {
+
+    constructor() {
+        super();
+        super.id = 'komiktap';
+        super.label = 'KomikTap';
+        this.tags = [ 'manga', 'webtoon', 'hentai', 'indonesian' ];
+        this.url = 'https://komiktap.in';
+        this.path = '/manga/?list';
+
+        this.queryMangas = 'div#content div.soralist ul li a.series';
+        this.queryChapters = 'div.bxcl ul li span.lchx a';
+    }
+}
