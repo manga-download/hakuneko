d364ca77489d04f199d1b48380263e8ee30f7359
ronny1982 <wegener.ronny@gmail.com>
2020-09-08 19:50:09 +0200

    [foolslide] added and updated websites (#2244)
    * [mabushimajo] added website support
    
    * [kirishimafansub] updated domain

--- src/web/mjs/connectors/KirishimaFansub.mjs
+++ src/web/mjs/connectors/KirishimaFansub.mjs
@@ -1,20 +1,14 @@
 import FoolSlide from './templates/FoolSlide.mjs';
 
-/**
- *
- */
 export default class KirishimaFansub extends FoolSlide {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'kirishimafansub';
         super.label = 'KirishimaFansub';
         this.tags = [ 'manga', 'high-quality', 'spanish', 'scanlation' ];
-        this.url = 'http://lector.kirishimafansub.net';
-        //this.path        = '/directory/';
+        this.url = 'https://www.kirishimafansub.net';
+        this.path = '/lector/directory/';
         this.language = 'spanish';
     }
 }
