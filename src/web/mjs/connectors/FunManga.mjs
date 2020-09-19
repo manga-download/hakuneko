399ebc01ab475685f2e085de060dae74c74cc61e
Steffen Schmidt <service.github@nocer.net>
2020-09-19 08:36:31 +0200

    [funmanga] Add website support (#2315)
    * use MangaInn as template
    
    * Replace generic logo
    
    * Reimagine icon

--- src/web/mjs/connectors/FunManga.mjs
+++ src/web/mjs/connectors/FunManga.mjs
@@ -1,28 +1,11 @@
-import Connector from '../engine/Connector.mjs';
+import MangaInn from './MangaInn.mjs';
 
-/**
- *
- */
-export default class FunManga extends Connector {
-
-    /**
-     *
-     */
+export default class FunManga extends MangaInn {
     constructor() {
         super();
         super.id = 'funmanga';
         super.label = 'FunManga';
-        this.tags = [];
+        this.tags = ['manga', 'english'];
         this.url = 'http://www.funmanga.com';
-    }
-
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
-    }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
-    }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
     }
 }
