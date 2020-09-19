3d0f0c99238a44c29c906515355c25421cfc9949
XenonR <service.github@nocer.net>
2020-09-19 09:39:59 +0200

    [nhattruyen] Remove URL trailing /

--- src/web/mjs/connectors/NhatTruyen.mjs
+++ src/web/mjs/connectors/NhatTruyen.mjs
@@ -7,6 +7,6 @@ export default class NhatTruyen extends MojoPortalComic {
         super.id = 'nhattruyen';
         super.label = 'NhatTruyen';
         this.tags = [ 'manga', 'vietnamese' ];
-        this.url = 'http://nhattruyen.com/';
+        this.url = 'http://nhattruyen.com';
     }
 }
