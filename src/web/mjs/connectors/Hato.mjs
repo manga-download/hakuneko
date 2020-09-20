15f923df51d0ada5e9cac7dbe6504b7f465e6133
dodieboy <dodieboy@users.noreply.github.com>
2020-09-20 12:52:00 +0800

    [Hato] Update label

--- src/web/mjs/connectors/Hato.mjs
+++ src/web/mjs/connectors/Hato.mjs
@@ -5,7 +5,7 @@ export default class Hato extends FlatManga {
     constructor() {
         super();
         super.id = 'hato';
-        super.label = 'HATO';
+        super.label = 'KissLove';
         this.tags = [ 'manga', 'raw', 'japanese' ];
         this.url = 'https://kisslove.net';
         this.requestOptions.headers.set( 'x-referer', this.url );
