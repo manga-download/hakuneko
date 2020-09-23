1b93ad7f3d71351fa144d2067a89335871cd6d8a
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-23 22:48:52 +0200

    [mangakita] fixed manga list path

--- src/web/mjs/connectors/MangaKita.mjs
+++ src/web/mjs/connectors/MangaKita.mjs
@@ -1,22 +1,14 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-/**
- *
- */
 export default class MangaKita extends WordPressMangastream {
 
-    /**
-     *
-     */
     constructor() {
         super();
-        // Public members for usage in UI (mandatory)
         super.id = 'mangakita';
         super.label = 'MangaKita';
         this.tags = [ 'manga', 'indonesian' ];
-        // Private members for internal usage only (convenience)
         this.url = 'https://mangakita.net';
-        this.path = '/manga-list/';
+        this.path = '/daftar-manga/?list';
 
         this.queryMangas = 'div.cpp div.daftarkartun div.jdlbar ul li a.series';
         this.queryChapters = 'div.chapter-list span.chapterLabel:first-of-type a';
