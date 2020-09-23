466a2a7159721a3e45e484fa0fa1880c4753dcce
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-23 23:14:13 +0200

    [komikav] fixed chapter list and image extraction

--- src/web/mjs/connectors/KomikAV.mjs
+++ src/web/mjs/connectors/KomikAV.mjs
@@ -1,21 +1,29 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-/**
- *
- */
 export default class KomikAV extends WordPressMangastream {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'komikav';
         super.label = 'KomikAV';
         this.tags = [ 'manga', 'indonesian' ];
         this.url = 'https://komikav.com';
-        this.path = '/manga/?list';
+        this.path = '/manga/list-mode/';
 
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.querMangaTitleFromURI = 'div#content div.postbody article h1.entry-title';
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            this.adLinkDecrypt(element);
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.querySelector('span.chapternum').textContent.trim(),
+                language: ''
+            };
+        });
     }
 }
