21161fc5e972cb5979a276af5d01dadd5761d71e
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-23 23:59:27 +0200

    [kiryuu] use chapter title query

--- src/web/mjs/connectors/Kiryuu.mjs
+++ src/web/mjs/connectors/Kiryuu.mjs
@@ -10,21 +10,9 @@ export default class Kiryuu extends WordPressMangastream {
         this.url = 'https://kiryuu.co';
         this.path = '/manga/list-mode/';
 
-        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
         this.querMangaTitleFromURI = 'div#content div.postbody article h1.entry-title';
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryChapters);
-        return data.map(element => {
-            this.adLinkDecrypt(element);
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.querySelector('span.chapternum').textContent.trim(),
-                language: ''
-            };
-        });
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.queryChaptersTitle = 'span.chapternum';
     }
 
     async _getPages(chapter) {
