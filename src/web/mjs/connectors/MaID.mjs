c55b5a8ff7093d9b05a3fa2328ee8e95e9b1f818
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-24 00:25:38 +0200

    [maid] use chapter title query

--- src/web/mjs/connectors/MaID.mjs
+++ src/web/mjs/connectors/MaID.mjs
@@ -10,21 +10,10 @@ export default class MaID extends WordPressMangastream {
         this.url = 'https://www.maid.my.id';
         this.path = '/manga-list/?list';
 
+        this.querMangaTitleFromURI = 'div.series-title h2';
         this.queryMangas = 'div.mangalist-blc ul li.Manga a.series';
         this.queryChapters = 'div.series-chapter ul.series-chapterlist li div.flexch-infoz a';
+        this.queryChaptersTitle = 'span.ch';
         this.queryPages = 'div.reader-area source[src]:not([src=""])';
-        this.querMangaTitleFromURI = 'div.series-title h2';
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryChapters);
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.querySelector('span.ch').textContent.trim(),
-                language: ''
-            };
-        });
     }
 }
