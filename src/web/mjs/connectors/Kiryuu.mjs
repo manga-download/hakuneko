4bda504b93dd8ff1c9b27b01481934eb7eb0bc00
Ronny Wegener <wegener.ronny@gmail.com>
2020-09-23 23:07:16 +0200

    [kiryuu] fixed chapter list and image extraction

--- src/web/mjs/connectors/Kiryuu.mjs
+++ src/web/mjs/connectors/Kiryuu.mjs
@@ -8,10 +8,23 @@ export default class Kiryuu extends WordPressMangastream {
         super.label = 'Kiryuu';
         this.tags = [ 'manga', 'indonesian' ];
         this.url = 'https://kiryuu.co';
-        this.path = '/manga/?list';
+        this.path = '/manga/list-mode/';
 
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
-        this.queryPages = 'div#readerarea > :not(.kln) source[src]:not([src=""])';
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.querMangaTitleFromURI = 'div#content div.postbody article h1.entry-title';
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            this.adLinkDecrypt(element);
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.querySelector('span.chapternum').textContent.trim(),
+                language: ''
+            };
+        });
     }
 
     async _getPages(chapter) {
