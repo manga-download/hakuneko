6fc84cec1b14db58cba86800c6f8d24db2d265a1
ronny1982 <wegener.ronny@gmail.com>
2020-09-27 17:03:23 +0200

    [gufengmh8] added website support (#2368)

---
+++ src/web/mjs/connectors/GuFengMH8.mjs
@@ -0,0 +1,13 @@
+import SinMH from './templates/SinMH.mjs';
+
+export default class GuFengMH8 extends SinMH {
+
+    constructor() {
+        super();
+        super.id = 'gufengmh8';
+        super.label = '古风漫画网 (GuFengMH8)';
+        this.tags = [ 'manga', 'webtoon', 'chinese' ];
+        this.url = 'https://www.gufengmh8.com';
+        // this.requestOptions.headers.set('x-referer', this.url);
+    }
+}
