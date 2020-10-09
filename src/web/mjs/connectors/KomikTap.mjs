60c51d7d57a6892f162e1ad2036e4282eb5cd9ea
Naufal Hakim <muhaqilanaufalhakim@gmail.com>
2020-10-09 14:25:53 +0700

    [KomikTap] add lazyload handler
    add lazyload handler
    add chapters title query

--- src/web/mjs/connectors/KomikTap.mjs
+++ src/web/mjs/connectors/KomikTap.mjs
@@ -8,9 +8,21 @@ export default class KomikTap extends WordPressMangastream {
         super.label = 'KomikTap';
         this.tags = [ 'manga', 'webtoon', 'hentai', 'indonesian' ];
         this.url = 'https://komiktap.in';
-        this.path = '/manga/?list';
+        this.path = '/manga/list-mode/';
 
-        this.queryMangas = 'div#content div.soralist ul li a.series';
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.queryChaptersTitle = 'span.chapternum';
+        this.queryPages = 'div#content div.wrapper script';
+
+    }
+
+    // NOTE: This web use costum lazyload methid
+    async _getPages(chapter) {
+        let uri = new URL(chapter.id, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let nodelist = await this.fetchDOM(request, this.queryPages);
+        // NOTE: faster then find method
+        let data = JSON.parse(nodelist[nodelist.length-2].innerText.slice(14,-2))['sources'][0]['images'];
+        return data;
     }
 }
