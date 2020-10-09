4e306b70c20bac27077bc139a18c5142fc10686d
Naufal Hakim <muhaqilanaufalhakim@gmail.com>
2020-10-09 19:04:50 +0700

    [MangaIndoWeb] Update Connector

--- src/web/mjs/connectors/MangaIndoWeb.mjs
+++ src/web/mjs/connectors/MangaIndoWeb.mjs
@@ -1,77 +1,63 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class MangaIndoWeb extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangaindoweb';
         super.label = 'MangaIndoWeb';
-        this.tags = [ 'manga', 'indonesian' ];
+        this.tags = [ '!test', 'manga', 'indonesian' ];
         this.url = 'https://mangaindo.web.id';
+        this.path = '/manga-list/';
+
+        this.queryMangas = 'div#az-slider div.letter-section ul li.manga-list a';
+        this.queryChapters = 'ul.lcp_catlist li a';
+        this.queryPages = 'div.entry-content source[src]:not([src=""])';
+        this.querMangaTitleFromURI = 'div#main article div.title h2';
     }
 
-    /**
-     *
-     */
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/manga-list-201902-v052/', this.requestOptions );
-        this.fetchDOM( request, 'div#az-slider div.letter-section ul li.manga-list a' )
-            .then( data => {
-                let mangaList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.trim()
-                    };
-                } );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangas(){
+        const URI = new URL(this.path, this.url);
+        const request = new Request(URI, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
+                title: element.text.trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        let request = new Request( this.url + manga.id, this.requestOptions );
-        this.fetchDOM( request, 'ul.lcp_catlist li a' )
-            .then( data => {
-                let chapterList = data.map( element => {
-                    return {
-                        id: this.getRootRelativeOrAbsoluteLink( element, request.url ),
-                        title: element.text.replace( manga.title, '' ).replace( '–', '' ).trim(),
-                        language: ''
-                    };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getChapters(manga){
+        const URI = new URL(manga.id, this.url);
+        const request = new Request( URI, this.requestOptions );
+        let data = await this.fetchDOM(request, this.queryChapters);
+
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.replace( manga.title, '' ).replace( '–', '' ).trim()
+            };
+        });
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        this.fetchDOM( request, 'div.entry-content p source' )
-            .then( data => {
-                let pageList = data.map( element => this.getAbsolutePath( element, request.url ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getPages(chapter){
+        const URI = new URL(chapter.id, this.url);
+        const request = new Request(URI, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryPages);
+
+        return data.map(element => this.getAbsolutePath( element, request.url ));
     }
+
+    async _getMangaFromURI(uri){
+        const URI = new URL(uri);
+        const request = new Request(URI, this.requestOptions);
+        const data = await this.fetchDOM(request, this.querMangaTitleFromURI);
+        const title = data[0].textContent.trim();
+
+        return new Manga(this, uri, title);
+    }
+
 }
