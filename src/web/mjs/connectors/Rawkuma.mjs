11ab38afe0d0aec7ee06ef70cad71edd4e689bd0
Ronny Wegener <wegener.ronny@gmail.com>
2020-10-13 16:53:38 +0200

    [rawkuma] fixed chapter query and image extraction

--- src/web/mjs/connectors/Rawkuma.mjs
+++ src/web/mjs/connectors/Rawkuma.mjs
@@ -9,7 +9,19 @@ export default class Rawkuma extends WordPressMangastream {
         this.tags = [ 'manga', 'raw', 'japanese' ];
         this.url = 'https://rawkuma.com';
 
-        this.path = '/manga/?list';
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
+        this.path = '/manga/list-mode/';
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.queryChaptersTitle = 'span.chapternum';
+    }
+
+    async _getPages(chapter) {
+        let script = `
+            new Promise(resolve => {
+                resolve(ts_reader.params.sources.pop().images);
+            });
+        `;
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        return await Engine.Request.fetchUI(request, script);
     }
 }
