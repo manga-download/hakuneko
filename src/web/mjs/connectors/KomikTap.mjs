6004de307f36ec473b714dcc04a0172ce0360d3f
Ronny Wegener <wegener.ronny@gmail.com>
2020-10-13 15:11:42 +0200

    [komiktap] fixed chapter query and image extraction

--- src/web/mjs/connectors/KomikTap.mjs
+++ src/web/mjs/connectors/KomikTap.mjs
@@ -8,9 +8,21 @@ export default class KomikTap extends WordPressMangastream {
         super.label = 'KomikTap';
         this.tags = [ 'manga', 'webtoon', 'hentai', 'indonesian' ];
         this.url = 'https://komiktap.in';
-        this.path = '/manga/?list';
 
+        this.path = '/manga/list-mode/';
         this.queryMangas = 'div#content div.soralist ul li a.series';
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a';
+        this.queryChaptersTitle = 'span.chapternum';
+    }
+
+    async _getPages(chapter) {
+        let script = `
+            new Promise(resolve => {
+                resolve(ts_reader.params.sources.pop().images);
+            });
+        `;
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        return await Engine.Request.fetchUI(request, script);
     }
 }
