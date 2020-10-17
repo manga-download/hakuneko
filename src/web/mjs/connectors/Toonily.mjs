3fa79c2b359c05424ca08c721e89049ca0542881
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-17 15:35:05 +0100

    toonily fix (#2435)
    content = content.replace(/<\/?noscript>/g, '');
    was replacing something it shouldn't have

--- src/web/mjs/connectors/Toonily.mjs
+++ src/web/mjs/connectors/Toonily.mjs
@@ -9,4 +9,20 @@ export default class Toonily extends WordPressMadara {
         this.tags = [ 'webtoon', 'hentai', 'english' ];
         this.url = 'https://toonily.com';
     }
+    createDOM( content, replaceImageTags, clearIframettributes ) {
+        replaceImageTags = replaceImageTags !== undefined ? replaceImageTags : true ;
+        clearIframettributes = clearIframettributes !== undefined ? clearIframettributes : true ;
+        if( replaceImageTags ) {
+            content = content.replace( /<img/g, '<source');
+            content = content.replace( /<\/img/g, '</source');
+            content = content.replace( /<use/g, '<source');
+            content = content.replace( /<\/use/g, '</source');
+        }
+        if( clearIframettributes ) {
+            content = content.replace( /<iframe[^<]*?>/g, '<iframe>');
+        }
+        let dom = document.createElement( 'html' );
+        dom.innerHTML = content;
+        return dom;
+    }
 }
