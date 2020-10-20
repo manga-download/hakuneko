8ac10afbb039abd951c66413b93e14bc49e423df
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-20 12:22:02 +0100

    [Site Request] JPMANGAS #2453
    resolves #2453

---
+++ src/web/mjs/connectors/JpMangas.mjs
@@ -0,0 +1,14 @@
+import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
+
+export default class JpMangas extends MangaReaderCMS {
+
+    constructor() {
+        super();
+        super.id = 'jpmangas';
+        super.label = 'JpMangas';
+        this.tags = [ 'manga', 'webtoon', 'french' ];
+        this.url = 'https://www.jpmangas.com/';
+
+        this.language = 'fr';
+    }
+}
