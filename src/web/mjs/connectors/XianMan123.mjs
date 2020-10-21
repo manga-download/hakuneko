bd0d51b062694229e3546fd11c41446cd218f6dc
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-21 13:27:17 +0100

    Update XianMan123.mjs
    console.log

--- src/web/mjs/connectors/XianMan123.mjs
+++ src/web/mjs/connectors/XianMan123.mjs
@@ -20,9 +20,7 @@ async _getPages(chapter) {
     let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
     let data = await this.fetchRegex(request, this.queryPages);
     let domain = await this.fetchRegex(request, /imgDomain\s*=\s*'([^\']+)\s*'/g);
-    console.log(domain[0])
     data = Object.values(JSON.parse(data));
-    console.log(data[0])
     return data.map(element => new URL(element, domain[0]).href);
 }
 
