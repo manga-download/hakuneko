33b0e80529f326a0b07ba26c62c2a48683628db4
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-23 16:51:04 +0100

    probably useless

---
+++ src/web/mjs/connectors/rawmangatop.mjs
@@ -0,0 +1,59 @@
+import Connector from '../engine/Connector.mjs';
+
+export default class RawMangatop extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'rawmangatop';
+        super.label = 'Raw Manga (生漫画)';
+        this.tags = [ 'manga', 'raw', 'japanese' ];
+        this.url = 'https://rawmanga.top';
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        let request = new Request(this.url + '/directory', this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.content ul.pagination li:nth-last-of-type(2) a.page-link' , 3);
+        let pageCount = parseInt(data[0].textContent);
+        for(let page = 1; page <= pageCount; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let request = new Request(this.url + '/directory?page=' + page, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.content ul.directory li.series div.details p.title a' , 3);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(this.url + manga.id, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.tab-pane ul.list-group li.list-group-item a' , 3);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.text.trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        let pagesList = [];
+        let request = new Request(this.url + chapter.id, this.requestOptions);
+        let data = (await this.fetchDOM(request, 'select.custom-select option:last-of-type' , 3))[1];
+        for(let page = 1; page <= data.value; page++) {
+            let pages = `${this.url}/viewer${chapter.id}/${page}`;
+            pagesList.push(pages);
+        }
+        return pagesList;
+    }
+
+}
