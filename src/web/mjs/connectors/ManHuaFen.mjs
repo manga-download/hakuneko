c0c7aad3eeebb554ea9db88be909d067ac844ddf
vinh2201 <potter.harry0909@yahoo.com>
2020-10-24 21:37:53 +0700

    [ManHuaFen] add website support

---
+++ src/web/mjs/connectors/ManHuaFen.mjs
@@ -0,0 +1,16 @@
+import SinMH from './templates/SinMH.mjs';
+
+export default class ManHuaBei extends SinMH {
+
+    constructor() {
+        super();
+        super.id = 'manhuafen';
+        super.label = '漫画呗 (ManHuaFen)';
+        this.tags = [ 'manga', 'webtoon', 'chinese' ];
+        this.url = 'https://www.manhuafen.com';
+
+        this.queryMangasPageCount = 'div.list_head_mid ul.pagination li.last a';
+        this.queryMangas = 'ul.list_con_li li span.comic_list_det h3 a';
+        this.queryChapters = 'div.zj_list_con ul li a';
+    }
+}
