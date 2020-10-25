ce5c3250f105dafc46c56528baa3e7c7882e4c55
robo <30987265+Robonau@users.noreply.github.com>
2020-10-25 07:41:08 +0000

    add jpm1234 & manhuagui TLC (#2495)
    * add jpm1234 connector
    
    resolves #2490
    wont work till #2486 goes through
    
    * fix jpm1234 & manhuagui TLC
    
    + some minor changes to SinMH
    
    * lint
    
    +removed unnessary variables from manhuagui
    
    * fixes
    
    Co-authored-by: 09morbab <30987265+09morbab@users.noreply.github.com>

---
+++ src/web/mjs/connectors/jpm1234.mjs
@@ -0,0 +1,31 @@
+import SinMH from './templates/SinMH.mjs';
+
+export default class jpm1234 extends SinMH {
+    constructor() {
+        super();
+        super.id = 'jpm1234';
+        super.label = 'jpm1234';
+        this.tags = [ 'manga','webtoon','chinese' ];
+        this.url = 'http://jpm1234.com';
+        this.requestOptions.headers.set('x-referer', this.url);
+
+        this.path = '/All/';
+        this.pathMatch = '/All/0/0/0/0/0/lastpost/p/(\\d+)/';
+        this.queryMangasPageCount = '#last_page';
+        this.queryMangas = 'ul#contList li p.ell a:not([href="//"])';
+        this.queryChapters = 'div.chapter-list ul li a';
+        this.config = {
+            throttle: {
+                label: 'Throttle Requests [ms]',
+                description: 'Enter the timespan in [ms] to delay consecuitive HTTP requests.\nThe website may reject to many consecuitive requests.\nChapters with lots of pages may require a longer delay',
+                input: 'numeric',
+                min: 0,
+                max: 5000,
+                value: 1000
+            }
+        };
+        this.scriptPages = `
+            new Promise(resolve => resolve(cInfo.fs.map(img => 'http://' + (/\\d$/.test(img) ? pageConfig.host.auto[0].replace('uploads','') : pageConfig.host.auto) + img)));
+        `;
+    }
+}
