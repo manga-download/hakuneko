bd75d0be3eeeaa918ff92a4257832f62450d12cf
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-26 21:33:48 +0000

    added bx117 (mobile)
    for some reason the mobile version has pages when the desktop ver doesn't
    resolves #2509
    no manga list cause i couldn't find one

---
+++ src/web/mjs/connectors/Bx117.mjs
@@ -0,0 +1,42 @@
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+
+export default class Bx117 extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'bx117';
+        super.label = '117漫画网 (bx117)';
+        this.tags = [ 'manga', 'webtoon', 'spanish' ];
+        this.url = 'http://m.bx117.com/';
+    }
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'p.txtItme.h1');
+        let id = uri.pathname + uri.search;
+        let title = data[0].textContent.trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(this.url + manga.id, this.requestOptions);
+        let data = await this.fetchDOM(request, 'a.d-nowrap',3);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        let script = `
+            new Promise(resolve => {
+                resolve(new Array(qTcms_page.total).fill().map((pages,ind) => new URL(getPicUrlP(qTcms_S_m_murl,ind),qTcms_m_weburl).href));
+            });
+        `;
+        let request = new Request(this.url + chapter.id, this.requestOptions);
+        let data = await Engine.Request.fetchUI(request, script);
+        return data;
+    }
+}
