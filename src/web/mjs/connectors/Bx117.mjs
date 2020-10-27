0f47ef4f1ad1f393d4d028d5b4d0f17803e13adc
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-27 05:01:41 +0000

    forgot 2 const

--- src/web/mjs/connectors/Bx117.mjs
+++ src/web/mjs/connectors/Bx117.mjs
@@ -19,8 +19,8 @@ export default class Bx117 extends Connector {
     }
 
     async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions);
-        let data = await this.fetchDOM(request, 'a.d-nowrap',3);
+        const request = new Request(this.url + manga.id, this.requestOptions);
+        const data = await this.fetchDOM(request, 'a.d-nowrap',3);
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
