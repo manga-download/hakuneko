9984de81a1794099905879e60872ba24cc86758f
robo <30987265+Robonau@users.noreply.github.com>
2020-10-29 10:37:29 +0000

    add mangaspark (#2542)
    resolves #2537
    
    Co-authored-by: 09morbab <30987265+09morbab@users.noreply.github.com>

---
+++ src/web/mjs/connectors/MangaSpark.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MangaSpark extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mangaspark';
+        super.label = 'مانجا سبارك (MangaSpark)';
+        this.tags = [ 'manga', 'webtoon', 'arabic' ];
+        this.url = 'https://mangaspark.com';
+    }
+}
