50d5e6680c1bb15383487ee551f6326f56db7000
09morbab <30987265+09morbab@users.noreply.github.com>
2020-10-29 05:16:41 +0000

    add mangalink
    resolves #2538

---
+++ src/web/mjs/connectors/MangaProZ.mjs
@@ -0,0 +1,16 @@
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+
+export default class MangaProZ extends WordPressMangastream {
+
+    constructor() {
+        super();
+        super.id = 'mangaproz';
+        super.label = 'Manga Pro Z';
+        this.tags = [ 'manga', 'webtoon', 'arabic' ];
+        this.url = 'https://mangaproz.com';
+
+        this.path = '/manga/list-mode/';
+    }
+
+
+}
