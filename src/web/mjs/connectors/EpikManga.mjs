a547d420f34c4714f94d9dd76d59dcc44027f444
ronny1982 <wegener.ronny@gmail.com>
2020-10-31 16:59:22 +0100

    [flatmanga] updated template websites (#2499)
    * [flatmanga] introduce fields to hold configurable data
    
    * [epikmanga] changed template to flatmanga
    
    * [mangatr] changed template to flatmanga
    
    * [flatmanga] extend generic logic
    
    * [truyentranhlh] changed template to flatmanga

--- src/web/mjs/connectors/EpikManga.mjs
+++ src/web/mjs/connectors/EpikManga.mjs
@@ -1,11 +1,6 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import FlatManga from './templates/FlatManga.mjs';
 
-/**
- * Seems to be customized FlatManga CMS
- * Very similar to Puzzmos
- */
-export default class EpikManga extends Connector {
+export default class EpikManga extends FlatManga {
 
     constructor() {
         super();
@@ -13,25 +8,9 @@ export default class EpikManga extends Connector {
         super.label = 'Epik Manga';
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://www.epikmanga.com';
-    }
+        this.path = '/seri-listesi?ltype=text&type=text';
 
-    async _getMangaFromURI(uri) {
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'head title');
-        let id = uri.pathname;
-        let title = data[0].text.split(' | ')[0].trim();
-        return new Manga(this, id, title);
-    }
-
-    async _getMangas() {
-        let request = new Request(this.url + '/seri-listesi?type=text', this.requestOptions);
-        let data = await this.fetchDOM(request, 'div#pop-href div[id^=char-] a');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.text.trim()
-            };
-        });
+        this.queryMangas = 'div#pop-href div[id^=char-] a';
     }
 
     async _getChapters(manga) {
@@ -44,11 +23,5 @@ export default class EpikManga extends Connector {
                 language: ''
             };
         });
-    }
-
-    async _getPages(chapter) {
-        let request = new Request(this.url + chapter.id, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div#reader div.chapter-content source.chapter-img');
-        return data.map(element => this.getAbsolutePath(element, request.url));
     }
 }
