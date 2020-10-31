9a91a98582531b35782e01b9ba490cc195f6e7fd
Ronny Wegener <wegener.ronny@gmail.com>
2020-10-31 15:06:09 +0100

    [lelscanvf] updated domain

--- src/web/mjs/connectors/LELScanVF.mjs
+++ src/web/mjs/connectors/LELScanVF.mjs
@@ -1,18 +1,12 @@
 import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
 
-/**
- *
- */
 export default class LELScanVF extends MangaReaderCMS {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'lelscanvf';
         super.label = 'LELSCAN-VF';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'https://www.lelscan-vf.com';
+        this.url = 'https://www.lelscan-vf.me';
     }
 }
