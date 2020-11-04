6576fc6458efbc36033fdda87ffef97741db7ff0
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2020-11-05 06:55:59 +0700

    [Queensmanga] fix query (#2579)

--- src/web/mjs/connectors/QueensManga.mjs
+++ src/web/mjs/connectors/QueensManga.mjs
@@ -8,20 +8,5 @@ export default class QueensManga extends WordPressMadara {
         super.label = 'ملكات المانجا (Queens Manga)';
         this.tags = [ 'webtoon', 'arabic' ];
         this.url = 'https://queensmanga.com';
-
-        this.queryPages = 'div.page-break picture source[srcset]';
-    }
-
-    async _getChapters(manga) {
-        let uri = new URL(manga.id, this.url);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'ul li.wp-manga-chapter');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element.closest('a'), request.url),
-                title: [...element.childNodes].map(node => node.textContent.trim()).find(text => text.length > 0),
-                language: ''
-            };
-        });
     }
 }
