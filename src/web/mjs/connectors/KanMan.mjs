519d7e83bc0de723445da5addaa3ae18f9150ee8
robo <30987265+Robonau@users.noreply.github.com>
2020-11-05 08:39:42 +0000

    lint

--- src/web/mjs/connectors/KanMan.mjs
+++ src/web/mjs/connectors/KanMan.mjs
@@ -6,7 +6,7 @@ export default class KanMan extends MHXK {
         super();
         super.id = 'kanman';
         super.label = '看漫画 (KanMan)';
-        this.tags = [ 'manga','webtoon', 'Chinese'];
+        this.tags = [ 'manga', 'webtoon', 'Chinese'];
         this.url = 'https://www.kanman.com';
 
         this.product = {
