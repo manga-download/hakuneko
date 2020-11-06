5a6383c5c0046f7b1159159720dedad5f0a162bc
09morbab <30987265+09morbab@users.noreply.github.com>
2020-11-06 15:51:52 +0000

    const

--- src/web/mjs/connectors/HentaiShark.mjs
+++ src/web/mjs/connectors/HentaiShark.mjs
@@ -13,9 +13,9 @@ export default class HentaiShark extends Connector {
 
     async _getMangas() {
         let mangaList = [];
-        let request = new Request(this.url, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.type-content li:nth-last-of-type(2) > a');
-        let pageCount = parseInt(data[0].href.match(/(\d+)$/)[1]);
+        const request = new Request(this.url, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.type-content li:nth-last-of-type(2) > a');
+        const pageCount = parseInt(data[0].href.match(/(\d+)$/)[1]);
         for(let page = 1; page <= pageCount; page++) {
             let mangas = await this._getMangasFromPage(page);
             mangaList.push(...mangas);
@@ -24,8 +24,8 @@ export default class HentaiShark extends Connector {
     }
 
     async _getMangasFromPage(page) {
-        let request = new Request(new URL(`/manga-list?page=${page}`, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.item > a');
+        const request = new Request(new URL(`/manga-list?page=${page}`, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.item > a');
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
@@ -35,8 +35,8 @@ export default class HentaiShark extends Connector {
     }
 
     async _getMangaFromURI(uri) {
-        let request = new Request(new URL(uri.href), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.col-sm-7 > h2');
+        const request = new Request(new URL(uri.href), this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.col-sm-7 > h2');
         return new Manga(this, uri.pathname, data[0].textContent);
     }
 
