86d116dc7c7c4c468be155c742bc26b882669a8c
09morbab <30987265+09morbab@users.noreply.github.com>
2020-11-06 13:01:05 +0000

    add 78te
    resolves #666

---
+++ src/web/mjs/connectors/Seven8te.mjs
@@ -0,0 +1,12 @@
+import MH from './templates/MH.mjs';
+
+export default class Seven8te extends MH {
+
+    constructor() {
+        super();
+        super.id = 'seven8te';
+        super.label = '78te (特漫网)';
+        this.tags = [ 'manga', 'webtoon', 'chinese', 'hentai' ];
+        this.url = 'https://www.78te.com';
+    }
+}
