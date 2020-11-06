b380115b690f62a788c4f08e9be335446b7d537b
09morbab <30987265+09morbab@users.noreply.github.com>
2020-11-06 15:57:15 +0000

    7 = seven

--- src/web/mjs/connectors/Seven8te.mjs
+++ src/web/mjs/connectors/Seven8te.mjs
@@ -4,7 +4,7 @@ export default class Seven8te extends MH {
 
     constructor() {
         super();
-        super.id = 'seven8te';
+        super.id = '78te';
         super.label = '78te (特漫网)';
         this.tags = [ 'manga', 'webtoon', 'chinese', 'hentai' ];
         this.url = 'https://www.78te.com';
