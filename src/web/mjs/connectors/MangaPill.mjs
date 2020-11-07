47609b0fbf83dc40b3c14566747048bba807ac0c
Ronny Wegener <wegener.ronny@gmail.com>
2020-11-07 23:26:39 +0100

    [mangapill] fixed manga list

--- src/web/mjs/connectors/MangaPill.mjs
+++ src/web/mjs/connectors/MangaPill.mjs
@@ -18,7 +18,23 @@ export default class MangaPill extends Connector {
     }
 
     async _getMangas() {
-        const uri = new URL('/search', this.url);
+        let mangaList = [];
+        for(let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let uri = new URL('/search', this.url);
+        uri.searchParams.set('page', page);
+        //uri.searchParams.set('title', '');
+        //uri.searchParams.set('type', '');
+        //uri.searchParams.set('status', '');
+        for(let i = 1; i < 100; i++) {
+            uri.searchParams.append('genre', i);
+        }
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, 'div.container div.grid div.mt-2 a');
         return data.map(element => {
