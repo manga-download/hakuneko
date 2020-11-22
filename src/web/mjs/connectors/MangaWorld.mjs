5d5a638222428bae9ebd617479996cf026720605
ronny1982 <wegener.ronny@gmail.com>
2020-11-22 13:21:18 +0100

    [mangaworld] updated to support new website layout (#2656)
    * [mangaworld] updated to support new website layout
    
    * [mangaworld] updated icon

--- src/web/mjs/connectors/MangaWorld.mjs
+++ src/web/mjs/connectors/MangaWorld.mjs
@@ -1,13 +1,62 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-export default class MangaWorld extends WordPressMadara {
+export default class MangaWorld extends Connector {
 
     constructor() {
         super();
         super.id = 'mangaworld';
         super.label = 'Manga World';
         this.tags = [ 'manga', 'webtoon', 'italian'];
-        this.url = 'https://mangaworld.tv';
+        this.url = 'https://www.mangaworld.cc';
     }
 
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.single-comic div.info h1.name');
+        const id = uri.pathname + uri.search;
+        const title = data[0].textContent.trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        for(let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/archive?page=' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.comics-grid div.content a.manga-title');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.chapters-wrapper div.chapter a.chap');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.querySelector('span').textContent.trim()
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(chapter.id, this.url);
+        uri.searchParams.set('style', 'list');
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#reader div#page source.page-image');
+        return data.map(image => this.getAbsolutePath(image, request.url));
+    }
 }
