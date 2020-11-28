43f0f750da88a8d4c2d9ce121cfbc9c7f2fb4b9e
Ronny Wegener <wegener.ronny@gmail.com>
2020-11-28 15:58:45 +0100

    [takeshobo] added copy & paste support

--- src/web/mjs/connectors/templates/TakeShobo.mjs
+++ src/web/mjs/connectors/templates/TakeShobo.mjs
@@ -1,4 +1,5 @@
 import SpeedBinb from './SpeedBinb.mjs';
+import Manga from '../../engine/Manga.mjs';
 
 export default class TakeShobo extends SpeedBinb {
 
@@ -8,13 +9,25 @@ export default class TakeShobo extends SpeedBinb {
         super.label = undefined;
         this.tags = [];
         this.url = undefined;
+
+        this.queryMangaTitle = 'section.work_main div.col_work_name h1';
+        this.queryMangas = 'section.lineup ul li a';
+        this.queryMangasTitle = 'source';
+        this.queryChapters = 'section.episode div.box_episode div.box_episode_text a:first-of-type';
+        this.queryChaptersTitle = 'div.episode_title';
+    }
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(new URL(uri), this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryMangaTitle);
+        return new Manga(this, uri.pathname, data[0].textContent.trim());
     }
 
     async _getMangas() {
         let request = new Request(new URL(this.url + '/'), this.requestOptions);
-        let data = await this.fetchDOM(request, 'section.lineup ul li a');
+        let data = await this.fetchDOM(request, this.queryMangas);
         return data.map(element => {
-            let title = element.querySelector('source').getAttribute('alt');
+            let title = element.querySelector(this.queryMangasTitle).getAttribute('alt');
             let match = title.match(/『(.*)』/);
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
@@ -25,9 +38,9 @@ export default class TakeShobo extends SpeedBinb {
 
     async _getChapters(manga) {
         let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'section.episode div.box_episode div.box_episode_text a:first-of-type');
+        let data = await this.fetchDOM(request, this.queryChapters);
         return data.map(element => {
-            let title = element.parentElement.querySelector('div.episode_title');
+            let title = element.parentElement.querySelector(this.queryChaptersTitle);
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, request.url),
                 title: title.innerText.replace(manga.title, '').trim(),
