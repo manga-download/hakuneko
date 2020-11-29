27352070bc5c7c8af5f918e22555b34127b458a1
ronny1982 <wegener.ronny@gmail.com>
2020-11-29 11:59:20 +0100

    [scanfr] updated domain (#2684)

--- src/web/mjs/connectors/ScanFR.mjs
+++ src/web/mjs/connectors/ScanFR.mjs
@@ -7,7 +7,7 @@ export default class ScanFR extends MangaReaderCMS {
         super.id = 'scanfr';
         super.label = 'Scan FR';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'https://www.scan-fr.co';
+        this.url = 'https://www.scan-fr.cc';
 
         this.queryChapters = 'ul.chapters888 li h5.chapter-title-rtlrr';
         this.language = 'fr';
