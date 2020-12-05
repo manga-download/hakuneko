9a91f688edeb31b83f1cff587ca6bd460cf61f69
Ronny Wegener <wegener.ronny@gmail.com>
2020-12-05 12:55:05 +0100

    [hato, kisslove] repaced by KissAway

--- src/web/mjs/connectors/KissAway.mjs
+++ src/web/mjs/connectors/KissAway.mjs
@@ -4,10 +4,10 @@ export default class KissAway extends FlatManga {
 
     constructor() {
         super();
-        super.id = 'hato';
+        super.id = 'kissaway';
         super.label = 'KissAway';
         this.tags = [ 'manga', 'raw', 'japanese' ];
         this.url = 'https://kissaway.net';
-        this.requestOptions.headers.set( 'x-referer', this.url );
+        this.requestOptions.headers.set('x-referer', this.url);
     }
 }
