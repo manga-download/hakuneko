94a24a55020d1fd840c9a8993663680389a2af49
09morbab <30987265+09morbab@users.noreply.github.com>
2020-12-09 23:17:55 +0000

    working + fixes

--- src/web/mjs/connectors/AstralLibrary.mjs
+++ src/web/mjs/connectors/AstralLibrary.mjs
@@ -7,31 +7,35 @@ export default class AstralLibrary extends WordPressMadaraNovel {
         super.id = 'astrallibrary';
         super.label = 'Astral Library';
         this.tags = [ 'webtoon', 'novel', 'english' ];
-        this.url = 'https://astrallibrary.net';
+        this.url = 'https://www.astrallibrary.net';
 
         this.novelObstaclesQuery = 'div.ad';
-        this.queryMangas = 'div.item-summary';
+        this.queryMangas = 'h3.h5 a';
     }
 
     async _getMangasFromPage(page) {
-        let request = new Request(new URL('page/'+page+1,'https://www.astrallibrary.net/manga-tag/manga/'));
-        let data = await this.fetchDOM(request, this.queryMangas);
+        const request = new Request(new URL('/manga-tag/manga/page/'+(page+1), this.url));
+        const data = await this.fetchDOM(request, this.queryMangas);
         return data.map(element => {
             return {
-                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('.list-chapter a'), request.url),
-                title: element.querySelector('a').textContent.trim()
+                id:  this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.textContent.trim()
             };
         });
     }
 
     async _getChapters(manga) {
-        let chap = manga.id.match(/(\d+)\/$/)[1]
-        let arr = new Array(Number(chap)+1).fill(0)
-        return arr.map((_,ind) => {
+        const uri = new URL(this.url);
+        uri.searchParams.set('post_type', 'wp-manga');
+        uri.searchParams.set('s', manga.title);
+        const request = new Request(uri, this.requestOption);
+        const data = await this.fetchDOM(request, 'div.latest-chap span.chapter a');
+        const chap = data[0].text.match(/(\d+)$/)[1];
+        return new Array(Number(chap)+1).fill(0).map((_, ind) => {
             return {
-                id: manga.id.replace(/(\d+)\/$/,ind),
+                id: new URL(manga.id+'/chapter-'+ind, this.url),
                 title: 'chapter '+ind
-            }
+            };
         });
     }
 }
