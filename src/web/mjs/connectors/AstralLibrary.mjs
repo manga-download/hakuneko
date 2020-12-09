083198ea6c51bd4f8b0ef260621bc2d53c3171d8
09morbab <30987265+09morbab@users.noreply.github.com>
2020-12-09 18:24:06 +0000

    AstralLibrary fix
    dumb fix for dumb problem
    resolves #2741

--- src/web/mjs/connectors/AstralLibrary.mjs
+++ src/web/mjs/connectors/AstralLibrary.mjs
@@ -10,5 +10,28 @@ export default class AstralLibrary extends WordPressMadaraNovel {
         this.url = 'https://astrallibrary.net';
 
         this.novelObstaclesQuery = 'div.ad';
+        this.queryMangas = 'div.item-summary';
+    }
+
+    async _getMangasFromPage(page) {
+        let request = new Request(new URL('page/'+page+1,'https://www.astrallibrary.net/manga-tag/manga/'));
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('.list-chapter a'), request.url),
+                title: element.querySelector('a').textContent.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        let chap = manga.id.match(/(\d+)\/$/)[1]
+        let arr = new Array(Number(chap)+1).fill(0)
+        return arr.map((_,ind) => {
+            return {
+                id: manga.id.replace(/(\d+)\/$/,ind),
+                title: 'chapter '+ind
+            }
+        });
     }
 }
