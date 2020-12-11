ea477f3e4f7d8b7d24b5b0f2aa383ba5bc7fed6b
09morbab <30987265+09morbab@users.noreply.github.com>
2020-12-11 07:24:34 +0000

    Update MangaNeloToday.mjs
    new url and fixed uri method, get manga, get chapters

--- src/web/mjs/connectors/MangaNeloToday.mjs
+++ src/web/mjs/connectors/MangaNeloToday.mjs
@@ -7,8 +7,13 @@ export default class MangaNeloToday extends AnyACG {
         super.id = 'manganelotoday';
         super.label = 'MangaNelo (by AnyACG)';
         this.tags = [ 'manga', 'webtoon', 'english' ];
-        this.url = 'http://manganelo.today';
+        this.url = 'http://manganelos.com';
 
         this.queryPages = 'div.chapter-content-inner p#arraydata';
+        this.queryMangaTitle = 'ol > li:nth-child(3)';
+        this.queryMangaTitleText = 'a';
+        this.queryChapters = '#examples a';
+        this.queryMangaLink = 'a';
+        this.queryMangas ='div.media-body';
     }
 }
