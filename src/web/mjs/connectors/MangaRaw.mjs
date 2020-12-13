240cb44a43d25b5ec30fa48b31d7f92a5cd0a4da
Ronny Wegener <wegener.ronny@gmail.com>
2020-12-13 10:24:39 +0100

    [mangaraw] adapted website layout changes

--- src/web/mjs/connectors/MangaRaw.mjs
+++ src/web/mjs/connectors/MangaRaw.mjs
@@ -14,8 +14,8 @@ export default class MangaRaw extends Connector {
     async _getMangas() {
         let mangaList = [];
         let request = new Request(this.url +'/browse/', this.requestOptions);
-        let data = await this.fetchDOM(request, '#explore > nav.paging a:nth-last-of-type(2)');
-        let pageCount = parseInt(data[0].href.match(/results=(\d+)&/)[1]);
+        let data = await this.fetchDOM(request, 'div.catalog ul.paginator li:nth-last-of-type(2) a');
+        let pageCount = parseInt(data[0].href.match(/results=(\d+)/)[1]);
         for(let page = 1; page <= pageCount; page++) {
             await this.wait(250);
             let mangas = await this._getMangasFromPage(page);
@@ -26,52 +26,40 @@ export default class MangaRaw extends Connector {
 
     async _getMangasFromPage(page) {
         let request = new Request(`${this.url}/browse/?results=${page}`, this.requestOptions);
-        let data = await this.fetchDOM(request, '.novel-item a', 3);
+        let data = await this.fetchDOM(request, 'div.catalog div.card h3.card__title a', 3);
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.title.trim()
+                title: element.text.trim()
             };
         });
     }
 
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, '#novel');
+        let data = await this.fetchDOM(request, 'h1.section__title');
         let id = uri.pathname + uri.search;
-        let title = data[0].dataset.novelstr.trim();
+        let title = data[0].textContent.trim();
         return new Manga(this, id, title);
     }
 
     async _getPages(chapter) {
         let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'source.loading');
-        return data.map(element => this.getAbsolutePath(element.src, request.url));
+        let data = await this.fetchDOM(request, 'div.tab-pane source');
+        return data.map(element => this.getAbsolutePath(element, request.url));
     }
 
     async _getChapters(manga) {
-        let script = `
-        new Promise(resolve => {
-            showDivEn();
-            const chaptersEn = [...document.querySelectorAll('ul#raw > li > a')].map(data => {
-                return{
-                    id: data.pathname,
-                    title: data.title.trim().replace('-eng-li',' [en]'),
-                    language: 'en'
-                }
-            })
-            showDivRaw();
-            const chaptersRaw = [...document.querySelectorAll('ul#raw > li > a')].map(data => {
-                return{
-                    id: data.pathname,
-                    title: data.title.trim(),
-                    language: 'raw'
-                }
-            })
-            resolve([ ...chaptersEn, ...chaptersRaw ]);
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.card__meta ul li a');
+        return data.map(element => {
+            const language = element.text.match(/-([a-z]+)-li/);
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.replace(/-([a-z]+)-li/, ' ($1)').trim(),
+                language: language ? language.pop() : 'raw'
+            };
         });
-        `;
-        let request = new Request(this.url + manga.id, this.requestOptions);
-        return Engine.Request.fetchUI(request, script);
     }
 }
