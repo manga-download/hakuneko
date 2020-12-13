06b9bde330e2b0b576136963a6ee72b9bb6c1bca
Naufal Hakim <muhaqilanaufalhakim@gmail.com>
2020-12-13 16:09:42 +0700

    cleanup

--- src/web/mjs/connectors/Boosei.mjs
+++ src/web/mjs/connectors/Boosei.mjs
@@ -13,25 +13,13 @@ export default class Boosei extends WordPressMangastream {
 
     async _getPages(chapter) {
         const script = `
-            new Promise((resolve, reject) => {
-                if(window.ts_reader) {
-                    // resolve(ts_reader.params.source.pop().images);
-                    resolve(ts_reader_control.getImages());
-                } else {
-                    setTimeout(() => {
-                        try {
-                            const images = [...document.querySelectorAll('${this.queryPages}')];
-                            resolve(images.map(image => image.dataset['lazySrc'] || image.dataset['src'] || image.src));
-                        } catch(error) {
-                            reject(error);
-                        }
-                    }, 500);
-                }
+            new Promise((resolve) => {
+                resolve(ts_reader_control.getImages());
             });
         `;
         const uri = new URL(chapter.id, this.url);
         let request = new Request(uri, this.requestOptions);
         let data = await Engine.Request.fetchUI(request, script);
-        return data.map(link => this.getAbsolutePath(link, request.url)).filter(link => !link.includes('histats.com'));
+        return data.map(link => this.getAbsolutePath(link, request.url));
     }
 }
