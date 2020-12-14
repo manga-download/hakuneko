c5e94dd227393fea3e122ee25220c302ae0c428e
ronny1982 <wegener.ronny@gmail.com>
2020-12-14 09:46:00 +0100

    [gntai] removed blogger template (#2751)
    * [gntai] removed blogger template
    
    * [gntai] updated icon

--- src/web/mjs/connectors/Gntai.mjs
+++ src/web/mjs/connectors/Gntai.mjs
@@ -1,57 +1,51 @@
-import BloggerManga from './templates/BloggerManga.mjs';
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
-export default class Gntai extends BloggerManga {
+export default class Gntai extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'gntai';
         super.label = 'GNTAI';
         this.tags = [ 'hentai', 'spanish' ];
-        this.url = 'http://www.gntai.xyz';
-
-        this.path = '/page';
-        this.feed = 'default';
-        this.queryMangasPerPage = 50;
-        this.queryMangasPageCount = 'div#nav-wrapper select#select-nav-pag option:last-of-type';
+        this.url = 'http://www.gntai.net';
     }
 
-    /**
-     *
-     */
-    _getChapterList( manga, callback ) {
-        Promise.resolve()
-            .then( () => {
-                let chapterList = [ {
-                    id: manga.id,
-                    title: manga.title,
-                    language: ''
-                } ];
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'main header.entry-header h1');
+        return new Manga(this, uri.pathname, data[0].textContent.trim());
     }
 
-    /**
-     *
-     */
-    _getPageList( manga, chapter, callback ) {
-        let request = new Request( this.url + chapter.id, this.requestOptions );
-        Engine.Request.fetchUI( request, `pages` )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        let mangaList = [];
+        let request = new Request(new URL(this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.navigation ul li:last-of-type a');
+        let pageCount = parseInt(data[0].text.trim());
+        for(let page = 1; page <= pageCount; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        let request = new Request(new URL(`/page/${page}/`, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, 'main article div.chapter-thumb > a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        return [ Object.assign({ language: '' }, manga) ];
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions );
+        return await this.fetchRegex(request, /['"]page_image['"]\s*:\s*['"]([^'"]+)['"]/g);
     }
 }
