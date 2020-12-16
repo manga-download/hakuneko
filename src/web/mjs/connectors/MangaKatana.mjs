7fe2fa89204f96425c23bc788d155b35ff2d4022
Ronny Wegener <wegener.ronny@gmail.com>
2020-12-16 23:41:50 +0100

    [mangakatana] force image mime type detection, migrate to async API

--- src/web/mjs/connectors/MangaKatana.mjs
+++ src/web/mjs/connectors/MangaKatana.mjs
@@ -1,4 +1,5 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
 export default class MangaKatana extends Connector {
 
@@ -10,67 +11,58 @@ export default class MangaKatana extends Connector {
         this.url = 'https://mangakatana.com';
     }
 
-    async _getMangaListPage(pageNumber) {
-        let request = new Request(this.url + '/manga/page/' + pageNumber, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div#book_list div.item div.text h3.title a');
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'meta[property="og:title"]');
+        return new Manga(this, uri.pathname, data[0].content.trim());
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        const uri = new URL('/manga', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#book_list ul.uk-pagination li:nth-last-of-type(2) a');
+        let pageCount = parseInt(data[0].href.match(/\/(\d+)$/)[1]);
+        for(let page = 1; page <= pageCount; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/manga/page/' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#book_list div.item div.text h3.title a');
         return data.map(element => {
             return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
                 title: element.text.trim()
             };
         });
     }
 
-    async _getMangaList(callback) {
-        try {
-            let mangaList = [];
-            let request = new Request(this.url + '/manga', this.requestOptions);
-            let data = await this.fetchDOM(request, 'div#book_list ul.uk-pagination li:nth-last-of-type(2) a');
-            let pageCount = parseInt(data[0].href.match(/\/(\d+)$/)[1]);
-            for(let page = 1; page <= pageCount; page++) {
-                let mangas = await this._getMangaListPage(page);
-                mangaList.push(...mangas);
-            }
-            callback(null, mangaList);
-        } catch(error) {
-            console.error(error, this);
-            callback(error, undefined);
-        }
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.chapters table tbody tr td div.chapter a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
     }
 
-    async _getChapterList(manga, callback) {
-        try {
-            let request = new Request(this.url + manga.id, this.requestOptions);
-            let data = await this.fetchDOM(request, 'div.chapters table tbody tr td div.chapter a');
-            let chapterList = data.map(element => {
-                return {
-                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                    title: element.text.trim(),
-                    language: ''
-                };
+    async _getPages(chapter) {
+        const script = `
+            new Promise(resolve => {
+                resolve(ytaw);
             });
-            callback(null, chapterList);
-        } catch(error) {
-            console.error(error, manga);
-            callback(error, undefined);
-        }
-    }
-
-    async _getPageList(manga, chapter, callback) {
-        try {
-            let script = `
-                new Promise(resolve => {
-                    //let images = [...document.querySelectorAll('div#imgs div.wrap_img img')].map(img => img.src);
-                    //resolve(images);
-                    resolve(ytaw);
-                });
-            `;
-            let request = new Request(this.url + chapter.id, this.requestOptions);
-            let data = await Engine.Request.fetchUI(request, script);
-            callback(null, data);
-        } catch(error) {
-            console.error(error, manga);
-            callback(error, undefined);
-        }
+        `;
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await Engine.Request.fetchUI(request, script);
+        return data.map(link => this.createConnectorURI(link));
     }
 }
