369a786496de5ab6bf1be3f284a3a6040a93b1be
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2020-12-16 18:03:44 +0700

    [Mgkomik] add website support (#2767)

---
+++ src/web/mjs/connectors/Mgkomik.mjs
@@ -0,0 +1,13 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class Mgkomik extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mgkomik';
+        super.label = 'MGKOMIK';
+        this.tags = [ 'webtoon', 'indonesian' ];
+        this.url = 'https://mgkomik.my.id';
+    }
+
+}
