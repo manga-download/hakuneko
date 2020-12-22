06eee164e916bc0328843e9cac4dcb100117aa1e
ronny1982 <wegener.ronny@gmail.com>
2020-12-22 14:51:44 +0100

    [team1x1] fixed page query (#2804)

--- src/web/mjs/connectors/Team1x1.mjs
+++ src/web/mjs/connectors/Team1x1.mjs
@@ -74,7 +74,7 @@ export default class Team1x1 extends Connector {
 
     async _getPages(chapter) {
         let request = new Request( this.url + chapter.id, this.requestOptions );
-        let data = await this.fetchDOM( request, 'div#translationPageall picture embed');
+        let data = await this.fetchDOM( request, 'div[id^="translationPageall"] picture embed');
         return data.map(dat=> dat.src);
     }
 }
