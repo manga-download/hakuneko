6dee15f844770b74a8802b8182250fe8d64677f7
devemloop <devemloop@gmail.com>
2020-12-23 21:06:41 -0500

    Add Connector
    ### Added
    - Connector vermangasporno

---
+++ src/web/mjs/connectors/VerMangasPorno.mjs
@@ -0,0 +1,69 @@
+import Connector from "../engine/Connector.mjs";
+
+// base for VerMangasPorno
+export default class VerMangasPorno extends Connector {
+    constructor() {
+        super();
+        super.id = "vermangasporno";
+        super.label = "VerMangasPorno";
+        this.tags = ["porn", "spanish"];
+        this.url = "https://vermangasporno.com";
+
+        this.path = "/page/";
+        this.queryMangas = "div.gallery > a.cover";
+        this.pager = "ul.pagination li:last-of-type a";
+        this.listPages =
+            'div.comicimg source:not([src*="descargavcp.png"]):not([data-lazy-src*="banner.png"])';
+    }
+
+    async _getMangasFromPage(page) {
+        let request = new Request(
+            this.url + this.path + page,
+            this.requestOptions
+        );
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map((element) => {
+            this.cfMailDecrypt(element);
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.querySelector("div.caption").textContent.trim(),
+            };
+        });
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        let request = new Request(
+            this.url + this.path + "1",
+            this.requestOptions
+        );
+        let data = await this.fetchDOM(request, this.pager);
+        let pageCount = parseInt(data[0].href.match(/\d+$/));
+        for (let page = 1; page <= pageCount; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getChapters(manga) {
+        return [
+            {
+                id: manga.id,
+                title: manga.title,
+                language: "",
+            },
+        ];
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(this.url + chapter.id, this.requestOptions);
+        let data = await this.fetchDOM(request, this.listPages);
+        return data.map((element) =>
+            this.getAbsolutePath(
+                element.dataset["lazySrc"] || element.dataset["src"] || element,
+                request.url
+            )
+        );
+    }
+}
