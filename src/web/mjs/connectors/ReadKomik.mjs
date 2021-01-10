1688c83fcce8f76906ecd4b71484e38a79a35e69
ronny1982 <wegener.ronny@gmail.com>
2021-01-10 17:16:02 +0100

    Various template based websites added/fixed (#2896)
    * [readkomik] changed template to wordpressmangastream
    
    * [wordpressmangastream] added mechanic to optionally remove unwanted child elements from chapter element
    
    * [mangakane] added website support
    
    * [randomtranslations] added website support
    
    * [imhentai] fixed gif image link extraction
    
    * [manga33] added website support
    
    * [centraldemangas] added website support

--- src/web/mjs/connectors/ReadKomik.mjs
+++ src/web/mjs/connectors/ReadKomik.mjs
@@ -1,7 +1,6 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class ReadKomik extends Connector {
+export default class ReadKomik extends WordPressMangastream {
 
     constructor() {
         super();
@@ -9,42 +8,6 @@ export default class ReadKomik extends Connector {
         super.label = 'ReadKomik';
         this.tags = [ 'webtoon', 'english' ];
         this.url = 'https://www.readkomik.com';
-    }
-
-    async _getMangaFromURI(uri) {
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'article h1.entry-title');
-        let id = uri.pathname;
-        let title = data[0].textContent.trim();
-        return new Manga(this, id, title);
-    }
-
-    async _getMangas() {
-        let request = new Request(new URL('/p/blog-page.html', this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.entry-content a');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.text.trim()
-            };
-        });
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.entry-content div[dir="ltr"] > a');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.text.trim(),
-                language: ''
-            };
-        });
-    }
-
-    async _getPages(chapter) {
-        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.entry-content div.separator source');
-        return data.map(element => this.getAbsolutePath(element, request.url));
+        this.path = '/manga/list-mode/';
     }
 }
