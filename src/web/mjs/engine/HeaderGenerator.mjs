dbbe2f80994592e78b0521290fed3b18e311380f
robo <30987265+Robonau@users.noreply.github.com>
2021-01-14 17:13:32 +0000

    Update ScanManga.mjs & HeaderGenerator (#2922)
    * Update ScanManga.mjs
    
    made the accept-language header a random string
    resolves #2921
    
    * languageHeaderGenerator
    
    * merged them
    
    still don't know if i should put it in request.mjs nor whare to put it if i should
    
    Co-authored-by: 09morbab <30987265+09morbab@users.noreply.github.com>

--- src/web/mjs/engine/HeaderGenerator.mjs
+++ src/web/mjs/engine/HeaderGenerator.mjs
@@ -1,4 +1,4 @@
-export default class UserAgent {
+export default class HeaderGenerator {
 
     static _rn( min, max ) {
         return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
@@ -9,14 +9,14 @@ export default class UserAgent {
     }
 
     static get _osUnix() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let arch = [' i386', ' i686', ' amd64', ' x86_64'];
         let unix = ['X11; Linux', 'X11; OpenBSD', 'X11; Ubuntu; Linux'];
         return $._rd( unix ) + $._rd( arch );
     }
 
     static get _osMacOSX() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let sep = ['_', '.'];
         let major = ['10'];
         let minor = ['8', '9', '10', '11', '12', '13'];
@@ -26,32 +26,32 @@ export default class UserAgent {
     }
 
     static get _osWindows() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let arch = ['', '; WOW64', '; Win64; x64'];
         let version = $._rd( ['5.0', '5.1', '5.2', '6.0', '6.1', '6.2', '6.3', '10.0', '10.1'] );
         return 'Windows NT ' + version + $._rd( arch );
     }
 
     static get _os() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let os = [$._osUnix, $._osMacOSX, $._osWindows];
         return $._rd( os );
     }
 
     static get _browserChrome() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let version = $._rn( 50, 66 ) + '.' + $._rn( 0, 99 ) + '.' + $._rn( 0, 9999 ) + '.' + $._rn( 0, 999 );
         return 'Mozilla/5.0 (' + $._os + ') AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' + version + ' Safari/537.36';
     }
 
     static get _browserFirefox() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let version = $._rn( 45, 60 );
         return 'Mozilla/5.0 (' + $._os + '; rv:' + version + '.0) Gecko/20100101 Firefox/' + version + '.0';
     }
 
     static get _browserIE() {
-        let $ = UserAgent;
+        let $ = HeaderGenerator;
         let version = $._rn( 8, 10 ) + '.0';
         let trident = $._rn( 5, 8 ) + '.0';
         let rv = $._rd( ['', '; rv:7.0', '; rv:8.0', '; rv:9.0', '; rv:10.0', '; rv:11.0'] );
@@ -64,11 +64,22 @@ export default class UserAgent {
         return 'Mozilla/5.0 (' + prefix + $._osWindows + '; Trident/' + trident + rv + ')' + suffix;
     }
 
-    /**
-     *
-     */
-    static random() {
-        let $ = UserAgent;
+    static get lang() {
+        let $ = HeaderGenerator;
+
+        let langCodes = ['af', 'sq', 'ar', 'ar-dz', 'ar-bh', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb', 'ar-ly', 'ar-ma', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sy', 'ar-tn', 'ar-ae', 'ar-ye', 'ar', 'hy', 'as', 'ast', 'az', 'eu', 'bg', 'be', 'bn', 'bs', 'br', 'bg', 'my', 'ca', 'ch', 'ce', 'zh', 'zh-hk', 'zh-cn', 'zh-sg', 'zh-tw', 'cv', 'co', 'cr', 'hr', 'cs', 'da', 'nl', 'nl-be', 'en', 'en-au', 'en-bz', 'en-ca', 'en-ie', 'en-jm', 'en-nz', 'en-ph', 'en-za', 'en-tt', 'en-gb', 'en-us', 'en-zw', 'eo', 'et', 'fo', 'fa', 'fj', 'fi', 'fr', 'fr-be', 'fr-ca', 'fr-fr', 'fr-lu', 'fr-mc', 'fr-ch', 'fy', 'fur', 'gd', 'gd-ie', 'gl', 'ka', 'de', 'de-at', 'de-de', 'de-li', 'de-lu', 'de-ch', 'el', 'gu', 'ht', 'he', 'hi', 'hu', 'is', 'id', 'iu', 'ga', 'it', 'it-ch', 'ja', 'kn', 'ks', 'kk', 'km', 'ky', 'tlh', 'ko', 'ko-kp', 'ko-kr', 'la', 'lv', 'lt', 'lb', 'mk', 'ms', 'ml', 'mt', 'mi', 'mr', 'mo', 'nv', 'ng', 'ne', 'no', 'nb', 'nn', 'oc', 'or', 'om', 'fa', 'fa-ir', 'pl', 'pt', 'pt-br', 'pa', 'pa-in', 'pa-pk', 'qu', 'rm', 'ro', 'ro-mo', 'ru', 'ru-mo', 'sz', 'sg', 'sa', 'sc', 'gd', 'sd', 'si', 'sr', 'sk', 'sl', 'so', 'sb', 'es', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ec', 'es-sv', 'es-gt', 'es-hn', 'es-mx', 'es-ni', 'es-pa', 'es-py', 'es-pe', 'es-pr', 'es-es', 'es-uy', 'es-ve', 'sx', 'sw', 'sv', 'sv-fi', 'sv-sv', 'ta', 'tt', 'te', 'th', 'tig', 'ts', 'tn', 'tr', 'tk', 'uk', 'hsb', 'ur', 've', 'vi', 'vo', 'wa', 'cy', 'xh', 'ji', 'zu'];
+        return new Array($._rn(1, 3)).fill().map(function () {
+            return $._rd(langCodes) + ($._rn(0, 1) ? ';q=' + Math.random().toString().substr(0, 3) : '');
+        }).join(' ');
+    }
+
+    static randomLang() {
+        let $ = HeaderGenerator;
+        return $.lang;
+    }
+
+    static randomUA() {
+        let $ = HeaderGenerator;
         /*
          *return $._rd( [$._browserChrome, $._browserFirefox, $._browserIE] );
          * only return chrome because it has over 7E+014 signature possibilites
