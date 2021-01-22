82139f2c64aaf99197aae1b4289ea47974190c8e
09morbab <30987265+09morbab@users.noreply.github.com>
2021-01-22 20:02:22 +0000

    Update Manga347.mjs
    not sure why this fixes it

--- src/web/mjs/connectors/Manga347.mjs
+++ src/web/mjs/connectors/Manga347.mjs
@@ -6,9 +6,29 @@ export default class Manga347 extends WordPressMadara {
         super();
         super.id = 'manga347';
         super.label = 'Manga 347';
-        this.tags = [ 'webtoon', 'english' ];
+        this.tags = ['webtoon', 'english'];
         this.url = 'https://manga347.com';
+    }
 
-        this.queryPages = 'ul.blocks-gallery-grid li.blocks-gallery-item figure source';
+    async _getPages(chapter) {
+        let uri = new URL(chapter.id, this.url);
+        uri.searchParams.set('style', 'list');
+        let request = new Request(uri, this.requestOptions);
+        let data1 = await this.fetchDOM(request, 'body');
+        let data = [...data1[0].querySelectorAll('figure source').length > 0 ? data1[0].querySelectorAll('figure source') : data1[0].querySelectorAll('div.page-break source')];
+        return data.map(element => this.createConnectorURI(this.getAbsolutePath(element.dataset['src'] || element['srcset'] || element, request.url).replace(/\/i\d+\.wp\.com/, ''),));
+    }
+
+    async _handleConnectorURI(payload) {
+        let request = new Request(payload, {
+            "headers": {
+                "x-referer": "https://manga347.com/"
+            },
+        });
+        let response = await fetch(request);
+        let data = await response.blob();
+        data = await this._blobToBuffer(data);
+        this._applyRealMime(data);
+        return data;
     }
 }
