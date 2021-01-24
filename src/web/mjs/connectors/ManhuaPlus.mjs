b94c511ec6620b2686375f29af85012eca4b24f6
09morbab <30987265+09morbab@users.noreply.github.com>
2021-01-24 00:46:10 +0000

    ManhuaPlus Issue on 1 title
    use two different selectors for pages
    copied from https://github.com/manga-download/hakuneko/pull/2973/files#diff-dff5663c78c5561172e44666bb1624f6c9e7016c57df415bae5a2eba09810b51

--- src/web/mjs/connectors/ManhuaPlus.mjs
+++ src/web/mjs/connectors/ManhuaPlus.mjs
@@ -11,8 +11,16 @@ export default class ManhuaPlus extends WordPressMadara {
     }
 
     async _getPages(chapter) {
-        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'li.blocks-gallery-item source');
-        return data.map(element => this.getAbsolutePath(element, this.url));
+        let uri = new URL(chapter.id, this.url);
+        // TODO: setting this parameter seems to be problematic for various website (e.g. ChibiManga, AniMangaES server will crash)
+        uri.searchParams.set('style', 'list');
+        let request = new Request(uri, this.requestOptions);
+        let data1 = await this.fetchDOM(request, 'body');
+        let data = [...data1[0].querySelectorAll('figure source').length > 0 ? data1[0].querySelectorAll('figure source') : data1[0].querySelectorAll('div.page-break source')];
+        return data.map(element => this.createConnectorURI({
+            // HACK: bypass 'i0.wp.com' image CDN to ensure original images are loaded directly from host
+            url: this.getAbsolutePath(element.dataset['src'] || element['srcset'] || element, request.url).replace(/\/i\d+\.wp\.com/, ''),
+            referer: request.url
+        }));
     }
 }
