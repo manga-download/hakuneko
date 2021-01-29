7fc984e2f6bc740f1945e018319afc7d6dca8b88
ronny1982 <wegener.ronny@gmail.com>
2021-01-29 11:27:46 +0100

    Added various templated websites (#3002)
    * [fenixscanlator] added website support
    
    * [finalscans] added website support
    
    * [prismascans] added website support
    
    * [scanlatorhunters] added website support
    
    * [vaposcan] added website support
    
    * [warqueenscan] added website support
    
    * [arthurscan] added website support
    
    * [okamiyomi] added website support
    
    * [hentaidexy] added website support
    
    * [imperioscans] added website support
    
    * [limascans] added website support
    
    * [mmscans] added website support
    
    * [nekobreakerscan] added website support

---
+++ src/web/mjs/connectors/MMScans.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class MMScans extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'mmscans';
+        super.label = 'MMSCANS';
+        this.tags = [ 'webtoon', 'english' ];
+        this.url = 'https://mm-scans.com';
+    }
+}
