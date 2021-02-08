7bb97d6d34e70b85f0b4bf66f9ff8ce40d8b0848
09morbab <30987265+09morbab@users.noreply.github.com>
2021-02-08 14:36:56 +0000

    Update kakaopage.mjs

--- src/web/mjs/connectors/kakaopage.mjs
+++ src/web/mjs/connectors/kakaopage.mjs
@@ -66,23 +66,6 @@ export default class kakaopage extends Connector {
         this.requestOptions.method = 'GET';
         delete this.requestOptions.body;
         let data = await this.fetchJSON(request);
-        return data.downloadData.members.files.map(element => this.createConnectorURI({url:data.downloadData.members.sAtsServerUrl + element.secureUrl, referer:this.url}));
-    }
-
-    async _handleConnectorURI(payload) {
-        /*
-         * TODO: only perform requests when from download manager
-         * or when from browser for preview and selected chapter matches
-         */
-        let request = new Request(payload.url, this.requestOptions);
-        request.headers.set('x-referer', payload.referer);
-        request.headers.set('x-origin', payload.referer);
-        request.headers.set('accept-language', 'en-US');
-        request.headers.set('accept', 'image/webp,image/apng,image/*,*/*');
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
+        return data.downloadData.members.files.map(element => data.downloadData.members.sAtsServerUrl + element.secureUrl);
     }
 }
