28c7b32223ed3f1d9a7e15bb33d5c61b76d8e032
Sheepux <sheepux@live.com>
2021-02-14 02:01:45 +0100

    template WordPressMangastream

--- src/web/mjs/connectors/MangaCanBlog.mjs
+++ src/web/mjs/connectors/MangaCanBlog.mjs
@@ -1,6 +1,6 @@
-import Connector from '../engine/Connector.mjs';
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class MangaCanBlog extends Connector {
+export default class MangaCanBlog extends WordPressMangastream {
 
     constructor() {
         super();
@@ -8,47 +8,8 @@ export default class MangaCanBlog extends Connector {
         super.label = 'MangaCan Blog';
         this.tags = [ 'manga', 'indonesian' ];
         this.url = 'https://mangacanblog.com';
+        this.path = '/daftar-komik-manga-bahasa-indonesia.html';
+        this.queryMangas = 'div.blix ul li a.series';
     }
 
-    /**
-     * TODO: for some reason fetchUI did not return successfully
-     */
-    async _initializeConnector() {
-        return Promise.resolve();
-    }
-
-    async _getMangas() {
-        let request = new Request(this.url + '/daftar-komik-manga-bahasa-indonesia.html', this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.blix ul li a.series');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.text.trim()
-            };
-        });
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions);
-        let data = await this.fetchDOM( request, 'div#chapterlist ul li div.eph-num a' );
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.text.replace(manga.title, '').trim(),
-                language: ''
-            };
-        });
-    }
-
-    async _getPages(chapter) {
-        let script = `
-            new Promise(resolve => {
-                var div = document.createElement('div');
-                div.innerHTML = ffff;
-                return resolve(Array.from(div.querySelectorAll('img')).map(item => item.src));
-            });
-        `;
-        let request = new Request(chapter.id, this.requestOptions);
-        return await Engine.Request.fetchUI(request, script);
-    }
 }
