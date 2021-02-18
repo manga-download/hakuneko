bc11fb09600e4457b8192d24b05c47c34617aa3b
robo <pokemon097@hotmail.co.uk>
2021-02-18 09:31:12 +0000

    GoddessManga specific

--- src/web/mjs/connectors/GoddessManga.mjs
+++ src/web/mjs/connectors/GoddessManga.mjs
@@ -9,4 +9,12 @@ export default class GoddessManga extends WordPressMadara {
         this.tags = [ 'manga', 'webtoon', 'turkish' ];
         this.url = 'https://goddessmanga.com';
     }
+
+    getFormatRegex() {
+        return {
+            chapterRegex: /\s*(?:^|ch\.?|ep\.?|chapter|Bölüm|chapitre|episode|#)?\s*([\d.?\-?v?,?]+)\s*(?:^|ch\.?|ep\.?|chapter|Bölüm|chapitre|episode|#)?\s*(?:\s|:|$)+/i, // $ not working in character groups => [\s\:$]+ does not work
+            volumeRegex: /\s*(?:vol\.?|volume|tome)\s*(\d+)/i
+        };
+
+    }
 }
