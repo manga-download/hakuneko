33120e20642cf3e56740457850200828072d6fd5
robo <30987265+Robonau@users.noreply.github.com>
2021-02-19 20:59:37 +0000

    Format chapter title regex (#3091)
    * koomanga getFormatRegex
    
    * turkish getFormatRegex
    
    * vietnamese
    
    * GoddessManga specific
    
    * turkish volume
    
    * lovablesubs
    
    * thai spanish
    
    * russian
    
    * portuguese
    
    the :? is for toomics
    
    * polish
    
    * italian & german
    
    * all the languages in one

--- src/web/mjs/connectors/lovablesubs.mjs
+++ src/web/mjs/connectors/lovablesubs.mjs
@@ -9,4 +9,11 @@ export default class LovableSubs extends WordPressMadara {
         this.tags = [ 'manga', 'turkish' ];
         this.url = 'https://lovablesubs.com';
     }
+
+    getFormatRegex() {
+        return {
+            chapterRegex: /\s*(?:^|ch\.?|ep\.?|chapter|Bölüm|chapitre|episode|#)?\s*([\d.?\-?v?,?]+)\s*(?:^|ch\.?|ep\.?|chapter|\.?Bölüm|chapitre|episode|#)?\s*(?:\s|:|$)+/i, // $ not working in character groups => [\s\:$]+ does not work
+            volumeRegex: super.getFormatRegex().volumeRegex
+        };
+    }
 }
