0bec30383a6b30909cb4f80ec6f1dc61c48df837
Ronny Wegener <wegener.ronny@gmail.com>
2021-02-24 21:30:30 +0100

    [tonarinoyoungjump] decrease resource usage

--- src/web/mjs/connectors/TonariNoYoungJump.mjs
+++ src/web/mjs/connectors/TonariNoYoungJump.mjs
@@ -8,6 +8,7 @@ export default class TonariNoYoungJump extends CoreView {
         super.label = 'となりのヤングジャンプ (Tonari no Young Jump)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://tonarinoyj.jp';
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
 
         this.queryManga = 'div.series-items ul.daily-series > li.daily-series-item > a';
         this.queryMangaTitle = 'h4.daily-series-title';
