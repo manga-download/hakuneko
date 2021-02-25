0e2db2b1f1fbddaa8faa8a50df8e9f6a758ac9c6
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:12:32 +0100

    [ComicZenon] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ComicZenon.mjs
+++ src/web/mjs/connectors/ComicZenon.mjs
@@ -8,7 +8,8 @@ export default class ComicZenon extends CoreView {
         super.label = 'ゼノン編集部 (Comic Zenon)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://comic-zenon.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series/zenon', '/series/zenyon', '/series/tatan', '/series/oneshot' ];
         this.queryManga = 'div.serial-contents section div.series-item h4 > a';
         this.queryMangaTitle = undefined;
