bf94a936d592ef4cea2646c806a5c5e57530e39f
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:42:16 +0100

    [ShonenMagazine] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ShonenMagazine.mjs
+++ src/web/mjs/connectors/ShonenMagazine.mjs
@@ -8,7 +8,8 @@ export default class ShonenMagazine extends CoreView {
         super.label = '週刊少年マガジ (Weekly Shonen Magazine)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://shonenmagazine.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series/smaga', '/series/bmaga', '/series/others' ];
         this.queryManga = 'article.serial-series-contents ul.serial-series-list > li.serial-series-item > a';
         this.queryMangaTitle = 'h4.series-title';
