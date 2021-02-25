e1e2a2bb761c7d48215b6d484d1c0f500cfc443f
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:52:27 +0100

    [ShonenJumpPlus] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ShonenJumpPlus.mjs
+++ src/web/mjs/connectors/ShonenJumpPlus.mjs
@@ -8,5 +8,6 @@ export default class ShonenJumpPlus extends CoreView {
         super.label = '少年ジャンプ＋ (Shonen Jump +)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://shonenjumpplus.com';
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
     }
 }
