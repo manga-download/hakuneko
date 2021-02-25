236624c3462b28dca4bcab3d17efe078b5e3cb02
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:47:53 +0100

    [ShonenMagazinePocket] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ShonenMagazinePocket.mjs
+++ src/web/mjs/connectors/ShonenMagazinePocket.mjs
@@ -8,7 +8,8 @@ export default class ShonenMagazinePocket extends CoreView {
         super.label = 'マガジンポケット (Shonen Magazine Pocket)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://pocket.shonenmagazine.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series' ];
         this.queryManga = 'div.series-items ul.daily-series > li.daily-series-item > a';
         this.queryMangaTitle = 'h4.daily-series-title';
