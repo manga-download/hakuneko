96d51bf8dd3e87bfdf2d3d1fccbb022f3701ef0e
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 10:46:55 +0100

    [ComicAction] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ComicAction.mjs
+++ src/web/mjs/connectors/ComicAction.mjs
@@ -8,7 +8,8 @@ export default class ComicAction extends CoreView {
         super.label = 'webアクション (Comic Action)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://comic-action.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.queryManga = 'section.series ul.series-series-list > li.series-series-item';
         this.queryMangaURI = 'a';
         this.queryMangaTitle = 'h3.series-title';
