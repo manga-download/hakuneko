4ee30f656e1e195875ac2f57bcfb1f1bc3436011
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:09:08 +0100

    [ComicTrail] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ComicTrail.mjs
+++ src/web/mjs/connectors/ComicTrail.mjs
@@ -8,7 +8,8 @@ export default class ComicTrail extends CoreView {
         super.label = 'Comic Trail (コミックトレイル)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://comic-trail.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series' ];
         this.queryManga = '#page-comicTrail-serial-serial > div div a';
         this.queryMangaTitle = 'h4';
