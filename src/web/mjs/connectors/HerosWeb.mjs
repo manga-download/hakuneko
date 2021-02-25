9d6061fef2615fe57e13ed02e62d2684d228ed8d
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:26:36 +0100

    [HeroesWeb] decrease resource usage
    [HeroesWeb] decrease resource usage

--- src/web/mjs/connectors/HerosWeb.mjs
+++ src/web/mjs/connectors/HerosWeb.mjs
@@ -8,7 +8,8 @@ export default class HerosWeb extends CoreView {
         super.label = "Hero's (ヒーローズ)";
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://viewer.heros-web.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series/heros', '/series/flat', '/series/wild' ];
         this.queryManga = 'section.series-section ul.series-items > li.series-item > a';
         this.queryMangaTitle = 'h4.item-series-title';
