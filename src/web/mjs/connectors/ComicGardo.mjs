c28885d048e372749554a41eb1455991fa663cfa
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:02:56 +0100

    [ComicGardo] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/ComicGardo.mjs
+++ src/web/mjs/connectors/ComicGardo.mjs
@@ -8,7 +8,8 @@ export default class ComicGardo extends CoreView {
         super.label = 'コミックガルド (Comic Gardo)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://comic-gardo.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series' ];
         this.queryManga = 'div.series ul.series-section-list li.series-section-item a.series-section-item-link';
         this.queryMangaTitle = 'h5.series-title';
