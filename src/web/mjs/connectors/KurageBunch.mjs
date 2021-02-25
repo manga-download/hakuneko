47a8c8f94321534b4554599cb58ef4d8f1775e93
Ever4Engel <47645041+Ever4engel@users.noreply.github.com>
2021-02-25 11:32:11 +0100

    [KurageBunch] decrease resource usage
    decrease resource usage

--- src/web/mjs/connectors/KurageBunch.mjs
+++ src/web/mjs/connectors/KurageBunch.mjs
@@ -8,7 +8,8 @@ export default class KurageBunch extends CoreView {
         super.label = 'くらげバンチ (KurageBunch)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://kuragebunch.com';
-
+        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        
         this.path = [ '/series/kuragebunch', '/series/comicbunch', '/series/bbunch', '/series/ututu', '/series/oneshot' ];
         this.queryManga = 'ul.page-series-list li.page-series-list-item div.series-data a.series-data-container';
         this.queryMangaTitle = 'h4';
