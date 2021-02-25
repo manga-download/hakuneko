e6743034899c093ca710253acd741f6a2da4b1f9
Ronny Wegener <wegener.ronny@gmail.com>
2021-02-25 11:14:01 +0100

    [manhwaid] added website support

---
+++ src/web/mjs/connectors/ManhwaID.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class ManhwaID extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'manhwaid';
+        super.label = 'Manhwaid';
+        this.tags = [ 'manga', 'webtoon', 'hentai', 'indonesian' ];
+        this.url = 'https://manhwaid.co';
+    }
+}
