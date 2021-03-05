7d8ff5aee5cfb712e05303a1cace1964325d25e0
Ronny Wegener <wegener.ronny@gmail.com>
2021-03-05 22:58:11 +0100

    [hunterscan] added website support

---
+++ src/web/mjs/connectors/HunterScan.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class HunterScan extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'hunterscan';
+        super.label = 'Hunters Scan';
+        this.tags = [ 'manga', 'webtoon', 'portuguese' ];
+        this.url = 'https://www.hunterscan.tk';
+    }
+}
