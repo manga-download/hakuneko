c085854f9b3cdfcd394d8ff7a1f303599563001f
Ronny Wegener <wegener.ronny@gmail.com>
2021-03-05 22:57:55 +0100

    [otakusekaiscan] added website support

---
+++ src/web/mjs/connectors/OtakuSekaiScan.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class OtakuSekaiScan extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'otakusekaiscan';
+        super.label = 'OtakuSekai Scan';
+        this.tags = [ 'manga', 'webtoon', 'portuguese' ];
+        this.url = 'https://www.sekaiscan.com';
+    }
+}
