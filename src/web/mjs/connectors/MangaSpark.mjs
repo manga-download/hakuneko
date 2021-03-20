9ba2b050ce4dddba04d3fc5dbe7a5a4f86908cf7
Ronny Wegener <wegener.ronny@gmail.com>
2021-03-20 17:24:15 +0100

    [mangaspark] removed obsolete cloudflare init (solved by hack in wordpressmadara)

--- src/web/mjs/connectors/MangaSpark.mjs
+++ src/web/mjs/connectors/MangaSpark.mjs
@@ -9,13 +9,4 @@ export default class MangaSpark extends WordPressMadara {
         this.tags = [ 'manga', 'webtoon', 'arabic' ];
         this.url = 'https://mangaspark.com';
     }
-
-    async _initializeConnector() {
-        const paths = [ '/', '/manga/_/_/?style=list' ];
-        for(let path of paths) {
-            const uri = new URL(path, this.url);
-            const request = new Request(uri, this.requestOptions);
-            await Engine.Request.fetchUI(request, '', 60000, true);
-        }
-    }
 }
