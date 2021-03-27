90c7b581326097ab7b4f147d432049182f327db5
Ronny Wegener <wegener.ronny@gmail.com>
2021-03-27 18:52:34 +0100

    [shieldmanga] fixed chapter and image CSS query

--- src/web/mjs/connectors/ShieldManga.mjs
+++ src/web/mjs/connectors/ShieldManga.mjs
@@ -8,5 +8,8 @@ export default class ShieldManga extends WordPressMadara {
         super.label = 'Shield Manga';
         this.tags = [ 'webtoon', 'english' ];
         this.url = 'https://shieldmanga.club';
+
+        this.queryChapters = 'li.wp-manga-chapter > a, li.wp-manga-hapter > a';
+        this.queryPages = 'div.page-break source, div.page-beak source';
     }
 }
