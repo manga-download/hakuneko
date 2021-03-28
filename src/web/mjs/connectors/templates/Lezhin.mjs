e67ba8b06313c55e687b9473821b31334d2b7eac
ronny1982 <wegener.ronny@gmail.com>
2021-03-28 15:54:14 +0200

    website maintenance (#3241)
    * [manga1001] bypass image referer protection
    
    * [lezhin] use fetchUI to extract images (ensure highest quality for purchased chapters)
    
    * [turktoon] fixed manga list extraction
    
    * [manga18-club] fixed manga, chapter and image extraction
    
    * [kisscomic] changed to new API, ensure multi page for image extraction
    
    * [japanread] fixed manga list and image extraction
    
    * [sekaikomik] updated domain
    
    * [mangafast] fixed CSS queries
    
    * [mangaraw] fixed image and manga title CSS query
    
    * [mangaraw] fix manga title
    
    * [shieldmanga] fixed chapter and image CSS query
    
    * [topcomicporno] added website support
    
    * [komikcast] fixed manga CSS query

--- src/web/mjs/connectors/templates/Lezhin.mjs
+++ src/web/mjs/connectors/templates/Lezhin.mjs
@@ -185,42 +185,39 @@ export default class Lezhin extends Connector {
 
     async _getPages(chapter) {
         await this._initializeAccount();
-        let uri = new URL(this.apiURL + '/inventory_groups/comic_viewer');
-        uri.searchParams.set('type', 'comic_episode');
-        uri.searchParams.set('alias', chapter.manga.id);
-        uri.searchParams.set('name', chapter.id);
         /*
-         *uri.searchParams.set('platform', 'web');
-         *uri.searchParams.set('store', 'web');
-         */
-        let data = await this.fetchJSON(uri.href, this.requestOptions);
-        let comicID = data.data.extra.comic.id;
-        let episodeID = data.data.extra.episode.id;
-        /*
-         *let top = data.data.extra.episode.topInfo !== undefined;
-         *let bottom = data.data.extra.episode.bottomInfo !== undefined;
-         *let pageType = data.data.extra.episode.display.type; // 'g'
-         */
-        let pages = 0;
-        let path = '';
-        if(data.data.extra.episode.scroll) {
-            pages = data.data.extra.episode.scroll;
-            path = 'scroll';
-        }
-        return [... new Array(pages).keys()].map(page => {
-            let uri = new URL([this.cdnURL, 'v2/comics', comicID, 'episodes', episodeID, 'contents', path + 's', page + 1].join('/'));
-            uri.searchParams.set('access_token', this.accessToken);
-            /*
-             * q  | Free  | Purchased
-             * ----------------------
-             * 10 |  480w |  640w
-             * 20 |  640w |  720w
-             * 30 |  720w | 1080w
-             * 40 | 1080w | 1280w
-             */
-            uri.searchParams.set('q', 40);
-            //uri.searchParams.set( 'updated', 1539846001617 /* Date.now() */ );
-            return uri.href;
-        });
+            q  | Free  | Purchased
+            ----------------------
+            10 |  480w |  640w
+            20 |  640w |  720w
+            30 |  720w | 1080w
+            40 | 1080w | 1280w
+        */
+        const script = `
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        if(!__LZ_DATA__) {
+                            throw new Error('No data found, this may happen if the chapter requires to be logged in!');
+                        }
+                        const purchased = __LZ_DATA__.product.subscribed || __LZ_DATA__.purchased.includes(__LZ_DATA__.episode.id);
+                        const images = __LZ_DATA__.episode.scrollsInfo.map(x => {
+                            const uri = new URL('/v2' + x.path, __LZ_CONFIG__.cdnUrl);
+                            uri.searchParams.set('access_token', '${this.accessToken}');
+                            uri.searchParams.set('purchased', purchased);
+                            uri.searchParams.set('q', 40);
+                            return uri.href;
+                        });
+                        resolve(images);
+                    } catch(error) {
+                        reject(error);
+                    }
+                }, 2500);
+            });
+        `;
+
+        const uri = new URL(`${this.url}/comic/${chapter.manga.id}/${chapter.id}`);
+        const request = new Request(uri, this.requestOptions);
+        return Engine.Request.fetchUI(request, script);
     }
 }
