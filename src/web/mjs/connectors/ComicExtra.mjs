5765a6dca6a0c1fcae4a0a45fd3a026e5cef136c
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-03-29 23:53:34 +0545

    [comicextra] update website
    Fixes #3247

--- src/web/mjs/connectors/ComicExtra.mjs
+++ src/web/mjs/connectors/ComicExtra.mjs
@@ -1,28 +1,45 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class ComicExtra extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'comicextra';
         super.label = 'ComicExtra';
-        this.tags = [];
+        this.tags = ['comic', 'english'];
         this.url = 'http://www.comicextra.com';
     }
 
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+    _getMangas() {
+        let msg = 'This website provides a manga list that is to large to scrape, please copy and paste the URL containing the images directly from your browser into HakuNeko.';
+        throw new Error(msg);
     }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '.title-1');
+        const id = uri.pathname;
+        const title = data[0].textContent.trim();
+        return new Manga(this, id, title);
     }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#list tr td a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.textContent.trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(this.url + chapter.id + '/full', this.requestOptions);
+        const data = await this.fetchDOM(request, '.chapter_img');
+        return data.map(element => element.src);
     }
 }
