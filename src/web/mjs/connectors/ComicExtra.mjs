a7277b67285146a49c79df9432a2db8cc6eba108
x4nk1t <47915404+x4nk1t@users.noreply.github.com>
2021-04-01 23:11:39 +0545

    [comicextra] update website (#3250)
    * [comicextra] update website
    
    Fixes #3247
    
    * manga list and some improvements
    
    * lint fix

--- src/web/mjs/connectors/ComicExtra.mjs
+++ src/web/mjs/connectors/ComicExtra.mjs
@@ -1,28 +1,69 @@
 import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-/**
- *
- */
 export default class ComicExtra extends Connector {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'comicextra';
         super.label = 'ComicExtra';
-        this.tags = [];
-        this.url = 'http://www.comicextra.com';
+        this.tags = ['comic', 'english'];
+        this.url = 'https://www.comicextra.com';
+        this.path = '/comic-list/';
     }
 
-    _getMangaList( callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+    async _getMangas() {
+        let mangaList = [];
+        const uri = new URL(this.path + 'others', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '.general-nav a');
+        const pages = data.map(element => {
+            return this.getRootRelativeOrAbsoluteLink(element, request.url);
+        });
+
+        for(let page of pages) {
+            const mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
     }
-    _getChapterList( manga, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getMangasFromPage(page) {
+        const uri = new URL(page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '.home-list .hl-box .hlb-t a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.textContent.trim()
+            };
+        });
     }
-    _getPageList( manga, chapter, callback ) {
-        callback( new Error( 'Please report this broken website on HakuNeko\'s GitHub project page.' ), undefined );
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '.title-1');
+        const id = uri.pathname;
+        const title = data[0].textContent.trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#list tr td a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.textContent.trim(),
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(this.url + chapter.id + '/full', this.requestOptions);
+        const data = await this.fetchDOM(request, '.chapter_img');
+        return data.map(element => this.getAbsolutePath(element, request.url));
     }
 }
