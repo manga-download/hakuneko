b0645cb825ebcef4daeeae5e647e20148e120d07
Francisco J. Solis <siscomagma@gmail.com>
2021-04-02 11:40:53 -0600

    Updated KumaManga as advised by @ronny1982 in order to get a shorter implementation by the use of queryChaptersTitleBloat to supress date

--- src/web/mjs/connectors/KumaManga.mjs
+++ src/web/mjs/connectors/KumaManga.mjs
@@ -14,26 +14,7 @@ export default class KumaManga extends WordPressMangastream {
         this.queryMangas = 'div.mangalist-blc ul li a';
         this.queryChapters = 'div.series-chapter ul.series-chapterlist li div.flexch-infoz a';
         this.queryChaptersTitle = 'span';
+        this.queryChaptersTitleBloat = 'span > span.date';
         this.queryPages = 'div.reader-area img[src]:not([src=""])';
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryChapters);
-        return data.map(element => {
-            this.adLinkDecrypt(element);
-            if(this.queryChaptersTitleBloat) {
-                [...element.querySelectorAll(this.queryChaptersTitleBloat)].forEach(bloat => {
-                    if(bloat.parentElement) {
-                        bloat.parentElement.removeChild(bloat);
-                    }
-                });
-            }
-            const title = this.queryChaptersTitle ? element.querySelector(this.queryChaptersTitle).childNodes[0].nodeValue : element.text;
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: title.replace(manga.title, '').trim()
-            };
-        });
     }
 }
