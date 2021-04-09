e8020b9bba0240a840fce83a452d68c069e9dad9
ronny1982 <wegener.ronny@gmail.com>
2021-04-09 07:43:21 +0200

    Website Maintenance (#3307)
    * [penlab] added website support
    
    * [ngomik] fix mangalist extraction
    
    * [kumotran] add support for canvas images
    
    * [manhwaland] updated domain
    
    * [manhwaworld] added website support
    
    * [gdscans] added website support
    
    * [hmanhwa] added website support

---
+++ src/web/mjs/connectors/GDScans.mjs
@@ -0,0 +1,11 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class GDScans extends WordPressMadara {
+    constructor() {
+        super();
+        super.id = 'gdscans';
+        super.label = 'GD Scans';
+        this.tags = [ 'manga', 'scanlation', 'english' ];
+        this.url = 'https://gdegenscans.xyz';
+    }
+}
