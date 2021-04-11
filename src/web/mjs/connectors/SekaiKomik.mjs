efcf75a94cd0a6ce4683dd6d74f7bde0505f5bfa
ronny1982 <wegener.ronny@gmail.com>
2021-04-12 00:12:36 +0200

    [sekaikomik] updated domain (#3326)

--- src/web/mjs/connectors/SekaiKomik.mjs
+++ src/web/mjs/connectors/SekaiKomik.mjs
@@ -7,7 +7,7 @@ export default class SekaiKomik extends WordPressMangastream {
         super.id = 'sekaikomik';
         super.label = 'SekaiKomik';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://www.sekaikomik.net';
+        this.url = 'https://www.sekaikomik.club';
         this.path = '/daftar-komik/?list';
     }
 }
