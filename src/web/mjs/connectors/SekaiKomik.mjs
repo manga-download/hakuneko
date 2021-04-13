416b68252e6ae3d0d5973f2a1b5b7bab9dabd300
Ronny <wegener.ronny@gmail.com>
2021-04-13 22:27:50 +0200

    [sekaikomik] initialize all CloudFlare protected paths (#3331)

--- src/web/mjs/connectors/SekaiKomik.mjs
+++ src/web/mjs/connectors/SekaiKomik.mjs
@@ -10,4 +10,13 @@ export default class SekaiKomik extends WordPressMangastream {
         this.url = 'https://www.sekaikomik.club';
         this.path = '/daftar-komik/?list';
     }
+
+    async _initializeConnector() {
+        const paths = [ '/', '/manga/' ];
+        for(let path of paths) {
+            const uri = new URL(path, this.url);
+            const request = new Request(uri, this.requestOptions);
+            await Engine.Request.fetchUI(request, '', 25000, true);
+        }
+    }
 }
