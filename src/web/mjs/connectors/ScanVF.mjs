1416ee3b33af5c99cdc9796ee6662985b3c32b46
Ronny <wegener.ronny@gmail.com>
2021-04-14 23:33:27 +0200

    updated domains (#3345)
    * [scanvf] updated domain
    
    * [kisscomic] updated domain

--- src/web/mjs/connectors/ScanVF.mjs
+++ src/web/mjs/connectors/ScanVF.mjs
@@ -1,18 +1,12 @@
 import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
-//dead?
-/**
- *
- */
+
 export default class ScanVF extends MangaReaderCMS {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'scanvf';
         super.label = 'Scan VF';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'https://www.scan-vf.co';
+        this.url = 'https://www.scan-vf.net';
     }
 }
