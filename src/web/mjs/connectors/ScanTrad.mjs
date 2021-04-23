7134a386d8159e9647d9814284d39a1d1d563a93
dodieboy <dodieboy@users.noreply.github.com>
2021-04-23 16:55:54 +0800

    [ScanTrad] Fix Connector

--- src/web/mjs/connectors/ScanTrad.mjs
+++ src/web/mjs/connectors/ScanTrad.mjs
@@ -15,29 +15,24 @@ export default class ScanTrad extends Connector {
 
     async _getMangas() {
         let request = new Request(this.url + '/mangas', this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.main div.home a.home-manga');
+        let data = await this.fetchDOM(request, 'div.main div.home div.manga div.manga_right div.mr-info a');
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                title: element.querySelector('div.hmi-titre').textContent.trim()
+                title: element.text.trim()
             };
         });
     }
 
     async _getChapters(manga) {
         let request = new Request(this.url + manga.id, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.chapitre');
-        return data.filter(element => element.querySelector('span.chl-num'))
-            .map(element => {
-                let number = element.querySelector('div.chl-titre span.chl-num').textContent.trim();
-                let title = element.querySelector('div.chl-titre span.chl-titre').textContent.trim();
-                let link = element.querySelector('div.ch-right a');
-                return {
-                    id: this.getRootRelativeOrAbsoluteLink(link, request.url),
-                    title: number + ' - ' + title,
-                    language: ''
-                };
-            });
+        let data = await this.fetchDOM(request, 'div.chapitre a.ch-left');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.querySelector('div.chl-titre span.chl-num').textContent.trim()
+            };
+        });
     }
 
     async _getPages(chapter) {
