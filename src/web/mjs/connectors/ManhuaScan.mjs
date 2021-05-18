7f385846884078379fe90dd2d6eaa7224d01337c
Ronny Wegener <wegener.ronny@gmail.com>
2021-05-18 21:11:50 +0200

    [manhuascan] support for encrypted image links

--- src/web/mjs/connectors/ManhuaScan.mjs
+++ src/web/mjs/connectors/ManhuaScan.mjs
@@ -12,4 +12,22 @@ export default class ManhuaScan extends FlatManga {
 
         this.queryChapters = 'div#tab-chapper div#list-chapters span.title a.chapter';
     }
+
+    async _getPages(chapter) {
+        const script = `
+            new Promise(async resolve => {
+                const response = await fetch('/app/manga/controllers/cont.chapterServer1.php', {
+                    method: 'POST',
+                    body: 'id=' + chapter_id,
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                });
+                const data = await response.json();
+                const content = CryptoJSAesJson.decrypt(data.content, '4xje8fvkub2d3mb5cy9rv661zyjakbcn');
+                resolve(content.split('http').slice(1).map(link => 'http' + link));
+            });
+        `;
+        let uri = new URL(chapter.id, this.url);
+        let request = new Request(uri, this.requestOptions);
+        return Engine.Request.fetchUI(request, script);
+    }
 }
