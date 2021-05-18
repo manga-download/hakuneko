45c438896ee310b72457a9a8262f28c5793a043a
Sheepux <sheepux@live.com>
2021-05-18 20:43:42 +0200

    Websites Maintenance (#3458)
    * mangahost domain change #3449
    
    * toonkor domain change #3451
    
    * copytoon domain change #3452
    
    * TMO removed search page count  #3454

--- src/web/mjs/connectors/Toonkor.mjs
+++ src/web/mjs/connectors/Toonkor.mjs
@@ -6,8 +6,8 @@ export default class Toonkor extends GnuBoard5BootstrapBasic2 {
         super();
         super.id = 'toonkor';
         super.label = 'Toonkor';
-        this.tags = [ 'webtoon', 'korean' ];
-        this.url = 'https://tkor.fit';
+        this.tags = ['webtoon', 'korean'];
+        this.url = 'https://tkor.zone';
     }
 
     canHandleURI(uri) {
