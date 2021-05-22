fed4d706cdbb51d63c091f9c5794a29919f37588
Comid7 <70775237+Comid7@users.noreply.github.com>
2021-05-22 02:20:15 -0400

    [leviatanscans-xxx] added website support (#3486)
    Co-authored-by: Comid7 <comid7@protonmail.com>

---
+++ src/web/mjs/connectors/LeviatanScansXXX.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class LeviatanScans extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'leviatanscans-xxx';
+        super.label = 'LeviatanScans (XXX)';
+        this.tags = [ 'webtoon', 'high-quality', 'english', 'scanlation','hentai'];
+        this.url = 'https://xxx.leviatanscans.com';
+    }
+}
