08f8a6aadf5627fb33e8d1e8e7a254a1fc4ee61e
Ronny Wegener <wegener.ronny@gmail.com>
2021-05-22 10:08:46 +0200

    [manhuascan] updated decryption (partially stolen from mangago)

--- src/web/mjs/connectors/ManhuaScan.mjs
+++ src/web/mjs/connectors/ManhuaScan.mjs
@@ -21,9 +21,14 @@ export default class ManhuaScan extends FlatManga {
                     body: 'id=' + chapter_id,
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                 });
-                const data = await response.json();
-                const content = CryptoJSAesJson.decrypt(data.content, '4xje8fvkub2d3mb5cy9rv661zyjakbcn');
-                resolve(content.split('http').slice(1).map(link => 'http' + link));
+                const data = await response.text();
+                const key = CryptoJS.enc.Hex.parse('e11adc3949ba59abbe56e057f20f131e');
+                const options = {
+                    iv: CryptoJS.enc.Hex.parse('1234567890abcdef1234567890abcdef'),
+                    padding: CryptoJS.pad.ZeroPadding
+                };
+                const decrypted = CryptoJS.AES.decrypt(data, key, options).toString(CryptoJS.enc.Utf8).replace(/(^\u0002+)|(\u0003+$)/g, '').trim();
+                resolve(decrypted.split(','));
             });
         `;
         let uri = new URL(chapter.id, this.url);
