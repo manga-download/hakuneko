9805befc028321151d2bf19289e01b66fc4745c8
M4lho <enzovolkov@gmail.com>
2021-05-23 07:56:44 -0300

    Update TsundokuTraducoes.mjs

--- src/web/mjs/connectors/TsundokuTraducoes.mjs
+++ src/web/mjs/connectors/TsundokuTraducoes.mjs
@@ -8,7 +8,6 @@ export default class TsundokuTraducoes extends WordPressMadaraNovel {
         super.label = 'Tsundoku Traduções';
         this.tags = [ 'webtoon', 'novel', 'portuguese', 'scanlation' ];
         this.url = 'https://tsundokutraducoes.com.br';
-
-        this.novelContentQuery = 'div.reading-content div[class^="text-"';
+        this.novelObstaclesQuery = 'div#text-chapter-toolbar, div.ad, div.go-to-top';
     }
 }
