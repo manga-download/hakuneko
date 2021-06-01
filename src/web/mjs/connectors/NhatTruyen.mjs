4f715e2a9b0517cccd27f24ca8ed79590970c2e4
Ronny Wegener <wegener.ronny@gmail.com>
2021-06-01 13:09:26 +0200

    [nhattruyen] updated domain

--- src/web/mjs/connectors/NhatTruyen.mjs
+++ src/web/mjs/connectors/NhatTruyen.mjs
@@ -7,9 +7,9 @@ export default class NhatTruyen extends MojoPortalComic {
         super.id = 'nhattruyen';
         super.label = 'NhatTruyen';
         this.tags = [ 'manga', 'vietnamese' ];
-        this.url = 'http://nhattruyen.com';
+        this.url = 'http://nhattruyentranh.com';
         this.links = {
-            login: 'https://nhattruyen.com/Secure/Login.aspx'
+            login: 'http://nhattruyentranh.com/Secure/Login.aspx'
         };
     }
 }
