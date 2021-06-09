aeef65c926f5f7f6ab3fe75cd4cfe2f67d50fefd
Ronny Wegener <wegener.ronny@gmail.com>
2021-06-09 11:13:01 +0200

    caisemh] load images from desktop version (mobile seems no longer to work)

--- src/web/mjs/connectors/Caisemh.mjs
+++ src/web/mjs/connectors/Caisemh.mjs
@@ -9,16 +9,6 @@ export default class Caisemh extends MH {
         this.tags = [ 'manga', 'webtoon', 'chinese', 'hentai' ];
         this.url = 'https://www.caisemh.com';
 
-        this.queryPages = 'div#cp_img source';
-    }
-
-    async _getPages(chapter) {
-        const request = new Request(new URL(chapter.id, this.url), {
-            headers: {
-                'x-user-agent': 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36'
-            }
-        });
-        let data = await this.fetchDOM(request, this.queryPages);
-        return data.map(element => this.getAbsolutePath(element.dataset.original || element, request.url));
+        this.queryPages = 'div.comicpage source';
     }
 }
