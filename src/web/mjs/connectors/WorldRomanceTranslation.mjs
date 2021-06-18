e929c6fca33838ff6387f6ed08ba1f67504080f9
Kappamalone <uzman.zawahir1@gmail.com>
2021-06-18 09:17:33 +0530

    Fix chapter retrieval

--- src/web/mjs/connectors/WorldRomanceTranslation.mjs
+++ src/web/mjs/connectors/WorldRomanceTranslation.mjs
@@ -1,13 +1,26 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
 export default class WorldRomanceTranslation extends WordPressMangastream {
-
     constructor() {
         super();
         super.id = 'worldromancetranslation';
         super.label = 'World Romance Translation';
-        this.tags = [ 'manga', 'indonesian' ];
+        this.tags = ['manga', 'indonesian'];
         this.url = 'https://wrt.my.id';
         this.path = '/manga/list-mode/';
+
+        this.queryChapters = 'div#chapterlist ul li div.eph-num';
+    }
+
+    async _getChapters(manga) {
+        let request = new Request(new URL(manga.id, this.url),this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryChapters);
+        return data.map((element) => {
+            const title = element.querySelector(this.queryChaptersTitle).textContent;
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('a'), request.url),
+                title: title.replace(manga.title, '').trim(),
+            };
+        });
     }
 }
