771dfe18ba11a0c6164b9a19803d564149c67ec8
Kappamalone <uzman.zawahir1@gmail.com>
2021-06-18 17:28:27 +0530

    use better query to avoid overwriting method

--- src/web/mjs/connectors/WorldRomanceTranslation.mjs
+++ src/web/mjs/connectors/WorldRomanceTranslation.mjs
@@ -1,6 +1,7 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
 export default class WorldRomanceTranslation extends WordPressMangastream {
+
     constructor() {
         super();
         super.id = 'worldromancetranslation';
@@ -9,18 +10,6 @@ export default class WorldRomanceTranslation extends WordPressMangastream {
         this.url = 'https://wrt.my.id';
         this.path = '/manga/list-mode/';
 
-        this.queryChapters = 'div#chapterlist ul li div.eph-num';
-    }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryChapters);
-        return data.map((element) => {
-            const title = element.querySelector(this.queryChaptersTitle).textContent;
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('a'), request.url),
-                title: title.replace(manga.title, '').trim(),
-            };
-        });
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a:last-of-type';
     }
 }
