0f5ce52180731cd1c1ddebb1903b754badc8adbc
Kappamalone <uzman.zawahir1@gmail.com>
2021-06-18 09:20:39 +0530

    fix linting error

--- src/web/mjs/connectors/WorldRomanceTranslation.mjs
+++ src/web/mjs/connectors/WorldRomanceTranslation.mjs
@@ -13,7 +13,7 @@ export default class WorldRomanceTranslation extends WordPressMangastream {
     }
 
     async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url),this.requestOptions);
+        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
         let data = await this.fetchDOM(request, this.queryChapters);
         return data.map((element) => {
             const title = element.querySelector(this.queryChaptersTitle).textContent;
