c9ce35ebc335bb928b14aa894f357fc16329173d
XenonR <service.github@nocer.net>
2021-06-28 13:27:32 +0200

    update selectors

--- src/web/mjs/connectors/IchirinNoHanaYuri.mjs
+++ src/web/mjs/connectors/IchirinNoHanaYuri.mjs
@@ -8,5 +8,7 @@ export default class IchirinNoHanaYuri extends WordPressMadara {
         super.label = 'Ichirin No Hana Yuri';
         this.tags = [ 'hentai', 'high-quality', 'portuguese', 'scanlation' ];
         this.url = 'https://ichirinnohanayuri.com.br';
+        this.queryMangas = 'div.post-title.font-title h4 a';
+        this.queryChapters = 'li.wp-manga-chapter a';
     }
 }
