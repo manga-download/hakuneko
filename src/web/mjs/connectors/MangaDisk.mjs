981aeced0c5967da5448d56b0aaa0dcfe54a553d
XenonR <service.github@nocer.net>
2021-06-30 21:14:29 +0200

    optimize mangadisk

--- src/web/mjs/connectors/MangaDisk.mjs
+++ src/web/mjs/connectors/MangaDisk.mjs
@@ -9,6 +9,7 @@ export default class MangaDisk extends WordPressMangastream {
         this.url = 'https://mangadisk.web.id';
         this.path = '/manga/list-mode/';
         this.queryPages = 'div#readerarea canvas';
+        this.requestOptions.headers.set('x-referer', this.url);
     }
 
     async _getPages(chapter) {
@@ -17,21 +18,7 @@ export default class MangaDisk extends WordPressMangastream {
         let data = await this.fetchDOM(request, this.queryPages);
 
         return data.map(hash => {
-            const payload = {
-                url: 'https://img.nesia.my.id/image?id='+hash.attributes["file-id"].value,
-                referer: this.url
-            };
-            return this.createConnectorURI(payload);
+            return this.createConnectorURI('https://img.nesia.my.id/image?id='+hash.getAttribute('file-id'));
         });
-    }
-
-    async _handleConnectorURI(payload) {
-        let request = new Request(new URL(payload.url), this.requestOptions);
-        request.headers.set('x-referer', payload.referer, this.url);
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
     }
 }
