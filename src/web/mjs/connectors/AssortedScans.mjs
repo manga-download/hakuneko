3267d843c7e372aa7e8aceff2567e6bab9fd287e
Pyther <finngaertner2@gmx.de>
2021-07-09 19:47:39 +0200

    code refactor

--- src/web/mjs/connectors/AssortedScans.mjs
+++ src/web/mjs/connectors/AssortedScans.mjs
@@ -1,5 +1,5 @@
 import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs'
+import Manga from '../engine/Manga.mjs';
 
 export default class AssortedScans extends Connector {
     constructor() {
@@ -7,13 +7,13 @@ export default class AssortedScans extends Connector {
         super.id = 'assortedscans';
         super.label = 'assortedscans';
         this.tags = ['manga', 'english'];
-        this.url = 'https://assortedscans.com/';
+        this.url = 'https://assortedscans.com';
     }
 
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchDOM(request, '#series-title');
-        let title = data.text.trim();
+        let title = data['0'].text.trim();
         let id = uri.pathname + uri.search;
         return Manga(this, id, title);
     }
@@ -23,29 +23,27 @@ export default class AssortedScans extends Connector {
         let data = await this.fetchDOM(request, 'section.series h2.series-title a');
         return data.map(element => {
                 return {
-                    id: element.href,
-                    title: element.text.trim(),
-                };
-            }
-        )
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim(),
+            };
+        });
     }
 
     async _getChapters(manga) {
         let id = manga.id.match(/\/reader\/.*\/?/)[0];
         let request = new Request(new URL(id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.chapter a');
+        let data = await this.fetchDOM(request, 'div.chapter > a');
         return data
-            .filter((_, i) => (i + 1) % 2) // Every second chapter link is a link to the translator
             .map(element => {
                 return {
-                    id: element.href,
-                    title: element.title
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title
                 }
             });
     }
 
-    async _getMaxSite(chapter) {
-        let id = chapter.id.match(/\/reader\/.*\/?/)[0];
+    async _getMaxSite(chapterId) {
+        let id = chapterId.match(/\/reader\/.*\/?/)[0];
         let request = new Request(new URL(id + '1/', this.url), this.requestOptions);
         let data = await this.fetchDOM(request, 'li.dropdown-element.page-details:last-child a');
         let maxSite = data['0'].text.match(/Page (\d+)/)[1];
@@ -53,7 +51,7 @@ export default class AssortedScans extends Connector {
     }
 
     async _getPages(chapter) {
-        let [id, maxSiteInt] = await this._getMaxSite(chapter);
+        let [id, maxSiteInt] = await this._getMaxSite(chapter.id);
         return Promise.all(new Array(maxSiteInt)
             .fill()
             .map(async (_, index) => {
