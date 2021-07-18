49be34dab8528431ed6e494d32616b3b064f356c
Ronny <wegener.ronny@gmail.com>
2021-07-18 23:14:09 +0200

    [trwebtoon] chenged template (#3729)

--- src/web/mjs/connectors/TRWebtoon.mjs
+++ src/web/mjs/connectors/TRWebtoon.mjs
@@ -1,6 +1,7 @@
-import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/manga.mjs';
 
-export default class TRWebtoon extends MangaReaderCMS {
+export default class TRWebtoon extends Connector {
 
     constructor() {
         super();
@@ -9,10 +10,53 @@ export default class TRWebtoon extends MangaReaderCMS {
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://trwebtoon.com';
         this.links = {
-            login: 'https://trwebtoon.com/yonetici/login'
+            login: 'https://trwebtoon.com/panel/giris'
         };
+    }
 
-        this.queryChapters = 'ul.chapters li h5.chapter-title-rtl a';
-        this.language = 'tr';
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#kt_content div.card div.card-body div.mr-3 a.font-size-h5');
+        return new Manga(this, uri.pathname, data[0].text.trim());
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/webtoon-listesi?page=' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#kt_content div.card div.card-body div.table-responsive a.card-title');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'table#chapters tbody tr td:first-of-type a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div#images source');
+        return data.map(image => this.getAbsolutePath(image.dataset.src, request.url));
     }
 }
