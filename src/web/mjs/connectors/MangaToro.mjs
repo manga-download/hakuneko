b2bb69ca667df41eae4ea06ecbea5fb1b23b6f3e
Ronny <wegener.ronny@gmail.com>
2021-07-18 12:51:38 +0200

    [mangatoro] added japanese and vietnamese website support (#3720)
    * [mangatoro] updated tags
    
    * [mangatoro-ja] added website support
    
    * [mangatoro-vi] added website support

--- src/web/mjs/connectors/MangaToro.mjs
+++ src/web/mjs/connectors/MangaToro.mjs
@@ -6,7 +6,7 @@ export default class MangaToro extends MojoPortalComic {
         super();
         super.id = 'mangatoro';
         super.label = 'MangaToro';
-        this.tags = [ 'manga', 'webtoon', 'english', 'raw' ];
+        this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://www.mangatoro.com';
         this.links = {
             login: this.url + '/Secure/Login.aspx'
