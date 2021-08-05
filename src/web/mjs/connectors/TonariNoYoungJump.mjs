22888eaa8ddd71e37a8f83bb40bff1a47a7bac12
Sheepux <sheepux@live.com>
2021-08-05 18:41:56 +0200

    TonariNoYoungJump style change fixes #3765

--- src/web/mjs/connectors/TonariNoYoungJump.mjs
+++ src/web/mjs/connectors/TonariNoYoungJump.mjs
@@ -6,11 +6,12 @@ export default class TonariNoYoungJump extends CoreView {
         super();
         super.id = 'tonarinoyoungjump';
         super.label = 'となりのヤングジャンプ (Tonari no Young Jump)';
-        this.tags = [ 'manga', 'japanese' ];
+        this.tags = ['manga', 'japanese'];
         this.url = 'https://tonarinoyj.jp';
+        this.path = ['/series', '/series/oneshot', '/series/trial'];
 
-        this.queryManga = 'div.series-items ul.daily-series > li.daily-series-item > a';
-        this.queryMangaTitle = 'h4.daily-series-title';
+        this.queryManga = 'div.serial-contents ul.series-table-list > li.subpage-table-list-item > a';
+        this.queryMangaTitle = 'h4.title';
         this.queryPages = 'p.page-area[data-src]';
     }
 }
