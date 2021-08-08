5a648063798807d2e22c7d883770f3b88f7f18ac
Ronny <wegener.ronny@gmail.com>
2021-08-08 21:04:14 +0200

    [mangasehri] filter broken ad-image (#3788)

--- src/web/mjs/connectors/MangaSehri.mjs
+++ src/web/mjs/connectors/MangaSehri.mjs
@@ -5,8 +5,15 @@ export default class MangaSehri extends WordPressMadara {
     constructor() {
         super();
         super.id = 'mangasehri';
-        super.label = 'mangasehri';
-        this.tags = [ 'manga', 'turkish' ];
+        super.label = 'Manga Şehri';
+        this.tags = [ 'manga', 'webtoon', 'turkish' ];
         this.url = 'https://mangasehri.com';
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryPages);
+        return data.map(element => this.getAbsolutePath(element.dataset.src || element, request.url)).filter(image => !/grumpybumpers/.test(image));
     }
 }
