33510f637f699792d5cac0072fd0bcd92a8e8620
Ronny Wegener <wegener.ronny@gmail.com>
2021-08-18 07:44:11 +0200

    [isekaiscan] fix chapter list

--- src/web/mjs/connectors/IsekaiScan.mjs
+++ src/web/mjs/connectors/IsekaiScan.mjs
@@ -9,4 +9,16 @@ export default class IsekaiScan extends WordPressMadara {
         this.tags = [ 'manga', 'english' ];
         this.url = 'https://isekaiscan.com';
     }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id + 'ajax/chapters/', this.url);
+        const request = new Request(uri, { method: 'POST' });
+        const data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim(),
+            };
+        });
+    }
 }
