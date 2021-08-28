c86d92d9126a99b66b814de94a3ae9b8876e2217
Ronny <wegener.ronny@gmail.com>
2021-08-28 13:14:40 +0200

    merge from master (#3897)
    * [manhuaplus] updated image query (#3862)
    
    * [worldromancetranslation] add image referer (#3862)
    
    * [maid] updated chapter query (#3862)
    
    * [komikindo] fixed chapter query (#3862)
    
    * [sektekomik] fixed manga list extraction (#3862)
    
    * [mangatoon-fr] added website support (#3862)
    
    * [argosscan] updated domain and template (#3862)
    
    * [cocomanhua] updated domain (#3862)
    
    * [wordpressmadara] add support for new chapter ajax endpoint (backward compatible) (#3861)
    
    * [hentaiwebtoon] added website support (#3864)
    
    * [thundernovels] added website support (#3864)
    
    * [komikplay] added website support (#3864)
    
    * [linewebtoon] support for javascript chapter redirect (#3874)
    
    * [team1x1] introduce url auto-update and fix manga list extraction (#3863)
    
    * [jmana1] fixed all CSS queries (#3876)
    
    * [hocvientruyentranh] add referer for images (#3884)
    
    * [9anime] update link decoder and video hosts (#3888)

--- src/web/mjs/connectors/Jmana1.mjs
+++ src/web/mjs/connectors/Jmana1.mjs
@@ -5,17 +5,27 @@ export default class Jmana1 extends Connector {
     constructor() {
         super();
         super.id = 'jmana1';
-        super.label = '제이마나 {Jaymana)';
+        super.label = '제이마나 (Jaymana)';
         this.tags = [ 'manga', 'korean' ];
         this.url = 'https://jmana1.net';
-
-        this.path = '/comic_main';
-        this.queryMangas = 'div.section div div.conts ul.allList li a';
-        this.queryChapters = 'div.post-content-list h2 a';
-        this.queryPages = 'div ul.listType.view li#view_content2 source';
-        this.queryMangaTitle = 'div.media-body.col-9 div div h2 a';
-
         this.requestOptions.headers.set('x-referer', this.url);
+
+        this.config = {
+            throttle: {
+                label: 'Throttle Requests [ms]',
+                description: 'Enter the timespan in [ms] to delay consecuitive HTTP requests.\nThe website may reject to many consecuitive requests',
+                input: 'numeric',
+                min: 10000,
+                max: 25000,
+                value: 10000
+            }
+        };
+    }
+
+    async _getMangaFromURI(uri) {
+        const id = uri.pathname + uri.search;
+        const title = uri.searchParams.get('bookname').trim();
+        return new Manga(this, id, title);
     }
 
     async _getMangas() {
@@ -29,15 +39,15 @@ export default class Jmana1 extends Connector {
 
     async _getMangasFromPage(page) {
         // NOTE: Avoid 429 Too Many Request
-        await this.wait(1000);
-        const uri = new URL(this.path, this.url);
+        await this.wait(this.config.throttle.value);
+        const uri = new URL('/comic_list', this.url);
         uri.searchParams.set('page', page);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, this.queryMangas);
+        const data = await this.fetchDOM(request, 'ul li.etc div.txt-wrap a.tit');
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.querySelector('span.titBox span.price').textContent.trim()
+                title: element.text.trim()
             };
         });
     }
@@ -45,11 +55,11 @@ export default class Jmana1 extends Connector {
     async _getChapters(manga) {
         const uri = new URL(manga.id, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, this.queryChapters);
-        return data.map(el => {
+        const data = await this.fetchDOM(request, 'div.lst-wrap ul li div.inner a.tit');
+        return data.map(element => {
             return {
-                id: this.getRootRelativeOrAbsoluteLink(el, this.url),
-                title: el.textContent.replace(manga.title, '').trim(),
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.replace(manga.title, '').trim()
             };
         });
     }
@@ -57,16 +67,7 @@ export default class Jmana1 extends Connector {
     async _getPages(chapter) {
         const uri = new URL(chapter.id, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, this.queryPages);
+        const data = await this.fetchDOM(request, 'div.view-wrap source.comicdetail');
         return data.map(img => this.createConnectorURI(this.getAbsolutePath(img.dataset['src'] || img['srcset'] || img, request.url)));
-    }
-
-    async _getMangaFromURI(uri) {
-        const bookname = uri.searchParams.get('bookname');
-        const request = new Request(new URL(`/book?bookname=${bookname}`, this.url), this.requestOptions);
-        const data = await this.fetchDOM(request, this.queryMangaTitle);
-        const id = `/book?bookname=${bookname}`;
-        const title = data[0].textContent.replace('제목', '').replace(':', '').trim();
-        return new Manga(this, id, title);
     }
 }
--- src/web/mjs/connectors/KomikPlay.mjs
+++ src/web/mjs/connectors/KomikPlay.mjs
@@ -1,20 +1,20 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class MaID extends WordPressMangastream {
+export default class KomikPlay extends WordPressMangastream {
 
     constructor() {
         super();
-        super.id = 'maid';
-        super.label = 'MAID';
-        this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://www.maid.my.id';
-        this.path = '/manga-list/?list';
+        super.id = 'komikplay';
+        super.label = 'KomikPlay';
+        this.tags = [ 'manga', 'webtoon', 'indonesian' ];
+        this.url = 'https://komikplay.com';
+        this.path = '/daftar-komik/';
 
         this.querMangaTitleFromURI = 'div.series-title h2';
         this.queryMangas = 'div.mangalist-blc ul li.Manga a.series';
         this.queryChapters = 'div.series-chapter ul.series-chapterlist li div.flexch-infoz a';
-        this.queryChaptersTitle = undefined;
-        this.queryChaptersTitleBloat = 'span.date';
+        this.queryChaptersTitle = 'span';
+        this.queryChaptersTitleBloat = 'span > span.date';
         this.queryPages = 'div.reader-area img[src]:not([src=""])';
     }
 }
