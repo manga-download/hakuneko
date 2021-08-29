6c55ee932da1d18fcb9ac89064f495a743099e9d
Enzo <84632071+M4lho@users.noreply.github.com>
2021-08-29 08:08:29 -0300

    GekkouScans  CMS to Madara (#3901)

--- src/web/mjs/connectors/GekkouScans.mjs
+++ src/web/mjs/connectors/GekkouScans.mjs
@@ -1,36 +1,12 @@
-import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
+import WordPressMadara from './templates/WordPressMadara.mjs';
 
-export default class GekkouScans extends MangaReaderCMS {
+export default class GekkouScans extends WordPressMadara {
 
     constructor() {
         super();
         super.id = 'gekkouscans';
         super.label = 'Gekkou Scans';
-        this.tags = [ 'manga', 'webtoon', 'portuguese' ];
-        this.url = 'https://leitor.gekkouscans.com.br';
-
-        this.queryChapters = 'ul.grow li h5.chapter-title-rtl';
+        this.tags = [ 'manga', 'webtoon', 'portuguese', 'scanlation' ];
+        this.url = 'https://gekkouscans.com.br';
         this.language = 'portuguese';
-    }
-
-    async _getPages(chapter) {
-        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryPages);
-        return data.map(element => {
-            return this.createConnectorURI({
-                url: this.getAbsolutePath(element.dataset['src'] || element, request.url),
-                referer: request.url
-            });
-        });
-    }
-
-    async _handleConnectorURI(payload) {
-        let request = new Request(payload.url, this.requestOptions);
-        request.headers.set('x-referer', payload.referer);
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
-    }
-}
+        this.queryChapters = 'li.wp-manga-chapter a';
