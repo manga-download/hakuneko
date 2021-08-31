bffb33e8169bd654b459f385f016a8c2ee077020
Ronny Wegener <wegener.ronny@gmail.com>
2021-08-31 19:44:30 +0200

    [yaoifanclube] added website support

---
+++ src/web/mjs/connectors/YaoiFanClube.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class YaoiFanClube extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'yaoifanclube';
+        super.label = 'Yaoi Fan Clube';
+        this.tags = [ 'webtoon', 'hentai', 'portuguese' ];
+        this.url = 'https://yaoifanclube.com.br';
+    }
+}
