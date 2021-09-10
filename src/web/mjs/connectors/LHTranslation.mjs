67797ec0b48e5be93a9a34270a2db881719c554a
dodieboy <dodieboy@users.noreply.github.com>
2021-09-10 09:14:59 +0800

    [LHtranslation] Remove old code

--- src/web/mjs/connectors/LHTranslation.mjs
+++ src/web/mjs/connectors/LHTranslation.mjs
@@ -8,9 +8,5 @@ export default class LHTranslation extends WordPressMadara {
         super.label = 'LHTranslation';
         this.tags = [ 'manga', 'high-quality', 'english', 'scanlation' ];
         this.url = 'https://lhtranslation.net';
-        //this.requestOptions.headers.set('x-referer', this.url);
-
-        //this.queryChapters = 'div#tab-chapper div#list-chapters span.titleLink a.chapter';
-        //his.language = 'en';
     }
 }
