46f33894d97e50efef46f16ebbb16b1625c606dd
Ronny <wegener.ronny@gmail.com>
2021-10-11 07:57:11 +0200

    [ganma] manga and chapter list improvments (#4038)
    * [ganma] fix endpoint URL for copy & paste support
    
    * [ganma] remove chapter filter as it seems to only shows accessible chapters anyway (user based)
    
    * [ganma] improve manga list extraction

--- src/web/mjs/connectors/Ganma.mjs
+++ src/web/mjs/connectors/Ganma.mjs
@@ -9,36 +9,49 @@ export default class Ganma extends Connector {
         super.label = 'GANMA!';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://ganma.jp';
-        this.requestOptions.headers.set( 'x-from', this.url );
+        this.requestOptions.headers.set('x-from', this.url);
     }
 
     async _getMangaFromURI(uri) {
-        let request = new Request(this.url + '/api/2.0/magazines/' + uri.pathname.split('/').pop(), this.requestOptions);
+        let request = new Request(this.url + '/api/1.0/magazines/web/' + uri.pathname.split('/').pop(), this.requestOptions);
         let data = await this.fetchJSON(request);
-        let id = data.root.id; // panel.alias, panel.link
+        let id = data.root.id;
         let title = data.root.title.trim();
         return new Manga(this, id, title);
     }
 
-    _getMangaList( callback ) {
-        let request = new Request( this.url + '/api/2.2/top', this.requestOptions );
-        this.fetchJSON( request )
-            .then( data => {
-                let mangaList = data.root.boxes.reduce( ( accumulator, box ) => {
-                    let mangas = box.panels.map( panel => {
-                        return {
-                            id: panel.id, // panel.alias, panel.link
-                            title: panel.title.trim()
-                        };
-                    } );
-                    return accumulator.concat( mangas );
-                }, [] );
-                callback( null, mangaList );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    async _getMangas() {
+        return [
+            ...await this._getMangasTop(),
+            ...await this._getMangasCompleted()
+        ];
+    }
+
+    async _getMangasTop() {
+        const uri = new URL('/api/2.2/top', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.root.boxes.reduce((accumulator, box) => {
+            let mangas = box.panels.map(panel => {
+                return {
+                    id: panel.id,
+                    title: panel.title.trim()
+                };
+            });
+            return accumulator.concat(mangas);
+        }, []);
+    }
+
+    async _getMangasCompleted() {
+        const uri = new URL('/api/1.1/ranking?flag=Finish', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.root.map(manga => {
+            return {
+                id: manga.id,
+                title: manga.title.trim()
+            };
+        });
     }
 
     async _getChapters(manga) {
@@ -46,7 +59,6 @@ export default class Ganma extends Connector {
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchJSON(request);
         return data.root.items
-            .filter(chapter => chapter.kind === 'free')
             .map((chapter, index) => {
                 return {
                     id: chapter.id,
