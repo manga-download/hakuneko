7f5ce63f3806399ac4b9301ec6d7d008cafceefe
Enzo <84632071+M4lho@users.noreply.github.com>
2021-10-17 11:19:06 -0300

    XXXYaoi - New Website
    #3987, #3911

---
+++ src/web/mjs/connectors/XXXYaoi.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class XXXYaoi extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'xxxyaoi';
+        super.label = 'XXXYaoi';
+        this.tags = [ 'hentai', 'high-quality', 'portuguese', 'scanlation' ];
+        this.url = 'https://xxxyaoi.com';
+    }
+}
