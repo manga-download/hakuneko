95fd3105c7b210da82659d3bdc3cbfaa70703f42
Ronny Wegener <wegener.ronny@gmail.com>
2021-10-20 17:52:03 +0200

    [weloma] bypass new captcha protection

--- src/web/mjs/connectors/WeLoMa.mjs
+++ src/web/mjs/connectors/WeLoMa.mjs
@@ -21,7 +21,7 @@ export default class WeLoMa extends FlatManga {
     async _initializeConnector() {
         const uri = new URL('/0/', this.url);
         const request = new Request(uri, this.requestOptions);
-        return Engine.Request.fetchUI(request, '');
+        return Engine.Request.fetchUI(request, '', 30000, true);
     }
 
     async _getMangas() {
