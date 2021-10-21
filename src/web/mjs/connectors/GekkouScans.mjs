8fb1577700e9cdecaf1760b3f68835bcefe15f39
Enzo <84632071+M4lho@users.noreply.github.com>
2021-10-21 03:13:00 -0300

    Added Website (#4070)
    * MiradScanlator - New Website
    
    * XXXYaoi - New Website
    
    #3987, #3911
    
    * Comeback to CMS

--- src/web/mjs/connectors/GekkouScans.mjs
+++ src/web/mjs/connectors/GekkouScans.mjs
@@ -1,14 +1,36 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import MangaReaderCMS from './templates/MangaReaderCMS.mjs';
 
-export default class GekkouScans extends WordPressMadara {
+export default class GekkouScans extends MangaReaderCMS {
 
     constructor() {
         super();
         super.id = 'gekkouscans';
         super.label = 'Gekkou Scans';
-        this.tags = [ 'manga', 'webtoon', 'portuguese', 'scanlation' ];
-        this.url = 'https://gekkouscans.com.br';
+        this.tags = [ 'manga', 'webtoon', 'portuguese' ];
+        this.url = 'https://www.gekkouscans.com.br';
+
+        this.queryChapters = 'ul.grow li h5.chapter-title-rtl';
         this.language = 'portuguese';
-        this.queryChapters = 'li.wp-manga-chapter a';
+    }
+
+    async _getPages(chapter) {
+        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryPages);
+        return data.map(element => {
+            return this.createConnectorURI({
+                url: this.getAbsolutePath(element.dataset['src'] || element, request.url),
+                referer: request.url
+            });
+        });
+    }
+
+    async _handleConnectorURI(payload) {
+        let request = new Request(payload.url, this.requestOptions);
+        request.headers.set('x-referer', payload.referer);
+        let response = await fetch(request);
+        let data = await response.blob();
+        data = await this._blobToBuffer(data);
+        this._applyRealMime(data);
+        return data;
     }
 }
