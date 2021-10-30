af712b8a73d2adaac649bcae591b946045132676
amit34521 <amit34521@gmail.com>
2021-10-30 15:46:21 +0530

    FIxed all methods according to comments.

--- src/web/mjs/connectors/Comikey.mjs
+++ src/web/mjs/connectors/Comikey.mjs
@@ -11,80 +11,57 @@ export default class ComiKey extends Connector {
         this.chapterUrl = 'https://relay-us.epub.rocks/consumer/COMIKEY/series/';
     }
 
-    _getMangaListFromPages(page) {
-        page = page || 1;
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+            mangas.length == 25 ? run = true : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
         let uri = new URL('/sapi/comics', this.url);
         uri.searchParams.set('page', page);
-        let request = new Request(uri.href, this.requestOptions);
-        return this.fetchJSON(request)
-            .then(data => {
-                let mangaList = data.results.map(manga => {
-                    return {
-                        id: manga.id + ',' + manga.e4pid,
-                        title: manga.name
-                    };
-                });
-                if (mangaList.length == 25) {
-                    return this._getMangaListFromPages(page + 1)
-                        .then(mangas => mangaList.concat(mangas));
-                } else {
-                    return Promise.resolve(mangaList);
-                }
-            });
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.results.map(manga => {
+            return {
+                id: manga.id + ',' + manga.e4pid,
+                title: manga.name
+            };
+        });
     }
 
-    _getMangaList(callback) {
-        this._getMangaListFromPages()
-            .then(data => callback(null, data))
-            .catch(error => {
-                console.error(error, this);
-                callback(error, undefined);
-            });
+    async _getChapters(manga) {
+        const uri = new URL(manga.id.split(',')[1] + '/content?clientid=dylMNK5a32of', this.chapterUrl);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.data.episodes.map(chapter => {
+            let name = chapter.name;
+            let title = '';
+            if (name.length > 0) {
+                title = name.shift().name;
+                console.log(title);
+            }
+            title = title ? ' - ' + title : '';
+            return {
+                id: chapter.id,
+                title: chapter.number + title,
+                language: chapter.language
+            };
+        });
     }
 
-    _getChapterList(manga, callback) {
-        let request = new Request(this.chapterUrl + manga.id.split(',')[1] + '/content?clientid=dylMNK5a32of', this.requestOptions);
-        this.fetchJSON(request)
-            .then(data => {
-                let chapterList = data.data.episodes.map(chapter => {
-                    let name = chapter.name;
-                    let title = '';
-                    if (name.length > 0) {
-                        title = name.shift().name;
-                        console.log(title);
-                    }
-                    title = title ? ' - ' + title : '';
-                    return {
-                        id: chapter.id,
-                        title: chapter.number + title,
-                        language: chapter.language
-                    };
-                });
-                callback(null, chapterList);
-            })
-            .catch(error => {
-                console.error(error, manga);
-                callback(error, undefined);
-            });
-    }
-
-    _getPageList(manga, chapter, callback) {
-        let uri = new URL('sapi/comics/' + manga.id.split(',')[0] + '/read', this.url);
+    async _getPages(chapter) {
+        let uri = new URL('sapi/comics/' + chapter.manga.id.split(',')[0] + '/read', this.url);
         uri.searchParams.set('format', 'json');
         uri.searchParams.set('content', chapter.id);
         let chapterRequest = new Request(uri.href, this.requestOptions);
-        let data = this.fetchJSON(chapterRequest);
-        let request = new Request(data.href, this.requestOptions);
-        this.fetchJSON(request)
-            .then(data => {
-                let pageList = data.readingOrder.map(
-                    page => page.href
-                );
-                callback(null, pageList);
-            })
-            .catch(error => {
-                console.error(error, chapter);
-                callback(error, undefined);
-            });
+        let data = await this.fetchJSON(chapterRequest);
+        const request = new Request(data.href, this.requestOptions);
+        const pageData = await this.fetchJSON(request);
+        return data.map(image => image.readingOrder.href);
     }
 }
