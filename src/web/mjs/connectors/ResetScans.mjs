baa2003c8fb4fadd651f705e35788d7322059ed0
Ronny Wegener <wegener.ronny@gmail.com>
2021-11-04 22:24:37 +0100

    [resetscans] add support for bilibili cross links

--- src/web/mjs/connectors/ResetScans.mjs
+++ src/web/mjs/connectors/ResetScans.mjs
@@ -1,4 +1,5 @@
 import WordPressMadara from './templates/WordPressMadara.mjs';
+import BilibiliComics from './BilibiliComics.mjs';
 
 export default class ResetScans extends WordPressMadara {
 
@@ -8,5 +9,17 @@ export default class ResetScans extends WordPressMadara {
         super.label = 'Reset Scans';
         this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://reset-scans.com';
+
+        this.bilibili = new BilibiliComics();
+        this.queryChapters = 'li.wp-manga-chapter span.chapter-name-before a:last-of-type';
+    }
+
+    async _getPages(chapter) {
+        if(chapter.id.startsWith(this.bilibili.url)) {
+            const bilibiliChapter = { id: new URL(chapter.id).pathname.split('/').pop() };
+            return this.bilibili._getPages(bilibiliChapter);
+        } else {
+            return super._getPages(chapter);
+        }
     }
 }
