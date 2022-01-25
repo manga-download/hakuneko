ee89e169b3491ece61487922f62ff920362994e2
Naufal Hakim <31276236+manh21@users.noreply.github.com>
2022-01-25 23:38:17 +0700

    feat: improve code

--- src/web/mjs/connectors/MangaReaderTo.mjs
+++ src/web/mjs/connectors/MangaReaderTo.mjs
@@ -9,7 +9,7 @@ export default class MangaReaderTo extends Connector {
         super.label = 'MangaReader.to';
         this.tags = ['manga', 'english'];
         this.url = 'https://mangareader.to';
-        this.path = '/az-list?page=%PAGE%';
+        this.path = '/az-list?page=';
 
         this.querMangaTitleFromURI = 'div#ani_detail div.anisc-detail h2.manga-name';
         this.queryMangas = '#main-content div.manga-detail h3 a';
@@ -37,7 +37,7 @@ export default class MangaReaderTo extends Connector {
     }
 
     async _getMangasFromPage(page) {
-        const uri = new URL(this.path.replace('%PAGE%', page), this.url);
+        const uri = new URL(this.path + page, this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, this.queryMangas);
         return data.map(element => {
@@ -69,15 +69,12 @@ export default class MangaReaderTo extends Connector {
         const uri = new URL(chapter.id, this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, this.queryPages);
-        const readingId = data[0].getAttribute('data-reading-id');
+        const readingId = data[0].dataset.readingId;
 
-        const script = await this._getImages(readingId);
-        const images = await Engine.Request.fetchUI(request, script, 60000, true);
-
-        return images.map(page => this.createConnectorURI(page));
+        return this._getImages(request, readingId);
     }
 
-    async _getImages(readingId) {
+    async _getImages(requestChapter, readingId) {
         // https://mangareader.to/ajax/image/list/chap/545260?mode=vertical&quality=high&hozPageSize=1
         const uri = new URL(`ajax/image/list/chap/${readingId}`, this.url);
         uri.searchParams.set('quality', 'high');
@@ -85,37 +82,30 @@ export default class MangaReaderTo extends Connector {
         const data = await this.fetchJSON(request, 3);
 
         const dom = this.createDOM(data.html);
-        const images = Array.from(dom.querySelectorAll('.iv-card'));
+        const imagesArr = Array.from(dom.querySelectorAll('.iv-card'));
 
         // Example: https://c-1.mreadercdn.ru/_v2/1/0dcb8f9eaacfd940603bd75c7c152919c72e45517dcfb1087df215e3be94206cfdf45f64815888ea0749af4c0ae5636fabea0abab8c2e938ab3ad7367e9bfa52/9d/32/9d32bd84883afc41e54348e396c2f99a/9d32bd84883afc41e54348e396c2f99a_1200.jpeg?t=4b419e2c814268686ff05d2c25965be9&amp;ttl=1642926021
-        const imageData = JSON.stringify(images.map(image => image.getAttribute('data-url')));
+        const imageData = JSON.stringify(imagesArr.map(image => image.dataset.url));
 
-        return `
-            new Promise((resolve, reject) => {
-                (async () => {
-                    try {
-                        let images = [];
-                        const data = ${imageData};
-                        for(const d of data) {
-                            const canvas = await imgReverser(d);
-                            const uri = canvas.toDataURL('image/jpeg')
-                            images.push(uri);
-                        }
-                        resolve(images);
-                    } catch(error) {
-                        resolve(error);
+        const type = Engine.Settings.recompressionFormat.value;
+        const quality = parseFloat(Engine.Settings.recompressionQuality.value)/100;
+        const script = `
+            new Promise(async (resolve, reject) => {
+                try {
+                    let images = [];
+                    const data = ${imageData};
+                    for(const d of data) {
+                        const canvas = await imgReverser(d);
+                        const uri = canvas.toDataURL('${type}', ${quality})
+                        images.push(uri);
                     }
-                })()
+                    resolve(images);
+                } catch(error) {
+                    reject(error);
+                }
             });
         `;
-    }
 
-    async _handleConnectorURI(payload) {
-        const data = payload.replace(/^data:image\/\w+;base64,/, "");
-        const buf = Buffer.from(data, "base64");
-        return {
-            mimeType: 'image/jpeg',
-            data: buf
-        };
+        return Engine.Request.fetchUI(requestChapter, script, 60000, true);
     }
 }
