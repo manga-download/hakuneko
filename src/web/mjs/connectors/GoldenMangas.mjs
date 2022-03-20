80c265ba79b5d70b60d4279c6be3f2fdf865b3e9
Sheep <19878135+Sheepux@users.noreply.github.com>
2022-03-20 12:49:39 +0100

    Domains changes (#4504)
    * golden mangas domain change
    * MangaSwat domain change
    * AsuraScans domain change

--- src/web/mjs/connectors/GoldenMangas.mjs
+++ src/web/mjs/connectors/GoldenMangas.mjs
@@ -6,88 +6,88 @@ export default class GoldenMangas extends Connector {
         super();
         super.id = 'goldenmangas';
         super.label = 'GoldenMangas';
-        this.tags = [ 'manga', 'portuguese' ];
-        this.url = 'https://goldenmangas.top';
+        this.tags = ['manga', 'portuguese'];
+        this.url = 'https://goldenmanga.top';
     }
 
     /**
      *
      */
-    _getMangaListFromPages( mangaPageLinks, index ) {
-        if( index === undefined ) {
+    _getMangaListFromPages(mangaPageLinks, index) {
+        if (index === undefined) {
             index = 0;
         }
-        return this.wait( 0 )
-            .then ( () => this.fetchDOM( mangaPageLinks[ index ], 'div.mangas a', 5 ) )
-            .then( data => {
-                let mangaList = data.map( element => {
+        return this.wait(0)
+            .then(() => this.fetchDOM(mangaPageLinks[index], 'div.mangas a', 5))
+            .then(data => {
+                let mangaList = data.map(element => {
                     return {
-                        id: this.getRelativeLink( element ),
+                        id: this.getRelativeLink(element),
                         title: element.text.trim()
                     };
-                } );
-                if( index < mangaPageLinks.length - 1 ) {
-                    return this._getMangaListFromPages( mangaPageLinks, index + 1 )
-                        .then( mangas => mangas.concat( mangaList ) );
+                });
+                if (index < mangaPageLinks.length - 1) {
+                    return this._getMangaListFromPages(mangaPageLinks, index + 1)
+                        .then(mangas => mangas.concat(mangaList));
                 } else {
-                    return Promise.resolve( mangaList );
+                    return Promise.resolve(mangaList);
                 }
-            } );
+            });
     }
 
     /**
      *
      */
-    _getMangaList( callback ) {
-        this.fetchDOM( this.url + '/mangas', 'nav ul.pagination li:nth-last-child(2) a' )
-            .then( data => {
-                let pageCount = parseInt( data[0].text.trim() );
-                let pageLinks = [... new Array( pageCount ).keys()].map( page => this.url + '/mangas?pagina=' + ( page + 1 ) );
-                return this._getMangaListFromPages( pageLinks );
-            } )
-            .then( data => {
-                callback( null, data );
-            } )
-            .catch( error => {
-                console.error( error, this );
-                callback( error, undefined );
-            } );
+    _getMangaList(callback) {
+        this.fetchDOM(this.url + '/mangas', 'nav ul.pagination li:nth-last-child(2) a')
+            .then(data => {
+                let pageCount = parseInt(data[0].text.trim());
+                let pageLinks = [... new Array(pageCount).keys()].map(page => this.url + '/mangas?pagina=' + (page + 1));
+                return this._getMangaListFromPages(pageLinks);
+            })
+            .then(data => {
+                callback(null, data);
+            })
+            .catch(error => {
+                console.error(error, this);
+                callback(error, undefined);
+            });
     }
 
     /**
      *
      */
-    _getChapterList( manga, callback ) {
-        this.fetchDOM( this.url + manga.id, 'ul#capitulos li.row > a div:first-of-type' )
-            .then( data => {
-                console.log( data );
-                let chapterList = data.map( element => {
+    _getChapterList(manga, callback) {
+        this.fetchDOM(this.url + manga.id, 'ul#capitulos li.row > a div:first-of-type')
+            .then(data => {
+                console.log(data);
+                let chapterList = data.map(element => {
                     return {
-                        id: this.getRelativeLink( element.parentNode ),
+                        id: this.getRelativeLink(element.parentNode),
                         title: element.childNodes[2].data.trim(),
                         language: 'pt'
                     };
-                } );
-                callback( null, chapterList );
-            } )
-            .catch( error => {
-                console.error( error, manga );
-                callback( error, undefined );
-            } );
+                });
+                callback(null, chapterList);
+            })
+            .catch(error => {
+                console.error(error, manga);
+                callback(error, undefined);
+            });
     }
 
     /**
      *
      */
-    _getPageList( manga, chapter, callback ) {
-        this.fetchDOM( this.url + chapter.id, 'div#leitor_full div[id^="capitulos"]:not([style*="none"]) source.img-manga' )
-            .then( data => {
-                let pageList = data.map( element => this.url + this.getRelativeLink( element ) );
-                callback( null, pageList );
-            } )
-            .catch( error => {
-                console.error( error, chapter );
-                callback( error, undefined );
-            } );
+    _getPageList(manga, chapter, callback) {
+        this.fetchDOM(this.url + chapter.id, 'div#leitor_full div[id^="capitulos"]:not([style*="none"]) source.img-manga')
+            .then(data => {
+                let pageList = data.map(element => this.url + this.getRelativeLink(element));
+                callback(null, pageList);
+            })
+            .catch(error => {
+                console.error(error, chapter);
+                callback(error, undefined);
+            });
     }
 }
