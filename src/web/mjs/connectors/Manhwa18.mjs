9a06894ccdff3080d959e3aebc62e5ecc1d0e3cc
Jon Marco Reyes <rjonmarco@gmail.com>
2022-07-09 13:31:33 +0800

    Fix Lint Error

--- src/web/mjs/connectors/Manhwa18.mjs
+++ src/web/mjs/connectors/Manhwa18.mjs
@@ -18,9 +18,9 @@ export default class Manhwa18 extends Connector {
         }
         return mangaList;
     }
-    
+
     async _getMangasFromPage(page) {
-        let request = new Request(this.url + '/manga-list?page=' + page ,this.requestOptions);
+        let request = new Request(this.url + '/manga-list?page=' + page, this.requestOptions);
         let data = await this.fetchDOM(request, 'div.series-title a');
         return data.map(element => {
             return {
@@ -31,7 +31,7 @@ export default class Manhwa18 extends Connector {
     }
 
     async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions); 
+        let request = new Request(this.url + manga.id, this.requestOptions);
         let data = await this.fetchDOM(request, 'ul.list-chapters a');
         return data.map(element => {
             return {
@@ -43,7 +43,7 @@ export default class Manhwa18 extends Connector {
     }
 
     async _getPages(chapter) {
-        let request = new Request(this.url + chapter.id, this.requestOptions); 
+        let request = new Request(this.url + chapter.id, this.requestOptions);
         let data = await this.fetchDOM(request, 'div#chapter-content source.lazy');
         return data.map(element => this.getAbsolutePath(element.dataset.src || element, request.url));
     }
