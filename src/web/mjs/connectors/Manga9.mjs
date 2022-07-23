25a74b155b6bccea5144ebf324ec17296a0fa3d7
Druzai <70586473+Druzai@users.noreply.github.com>
2022-07-24 02:29:07 +0300

    [Manga9] Added Manga9 connector (#4849)
    * Added Manga9 connector
    
    * Added using regex when cleaning manga title
    
    * Simplified getting chapter title

---
+++ src/web/mjs/connectors/Manga9.mjs
@@ -0,0 +1,69 @@
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+
+export default class Manga9 extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'manga9';
+        super.label = 'Manga Raw (manga9.co)';
+        this.tags = [ 'manga', 'japanese' ];
+        this.url = 'https://manga9.co';
+
+        this.path = '/list/?page=';
+        this.queryMangaTitle = 'main#primary h1';
+        this.queryMangaPages = 'div.wp-pagenavi span.pages';
+        this.queryMangas = 'div.card-body div.rotate-img a';
+        this.queryChapters = 'div.list-scoll a';
+        this.queryPages = 'div.card-wrap source';
+    }
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryMangaTitle);
+        const id = uri.pathname;
+        const title = data[0].textContent.replace(/\(Raw( – Free)?\)/, '').trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        const request = new Request(new URL(this.path, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryMangaPages);
+        const pageCount = parseInt(data[0].textContent.split('/').pop());
+        for(let page = 1; page <= pageCount; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const request = new Request(new URL(this.path + page, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title.replace(/\(Raw( – Free)?\)/, '').trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        const request = new Request(new URL(manga.id, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryChapters);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.match(/【([^【】]+)】\s*$/)[1],
+                language: ''
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, this.queryPages);
+        return data.map(image => image.dataset.src);
+    }
+}
