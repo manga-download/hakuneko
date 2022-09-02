34706a2e47c9a90fc7fc20eb24e1f00cdeee7d8b
gammesB <gammes.bubble@gmail.com>
2022-09-02 22:01:46 +0200

    changed issue date as chap title to issue number(more correct)

--- src/web/mjs/connectors/ShonenJumpPlus.mjs
+++ src/web/mjs/connectors/ShonenJumpPlus.mjs
@@ -13,10 +13,10 @@ export default class ShonenJumpPlus extends CoreView {
     async _getChapters(manga) {
         if(/^\/magazine\/\d+$/.test(manga.id)) {
             let request = new Request(new URL(this.url+manga.id), this.requestOptions);
-            let data = await this.fetchDOM(request, '.episode-header-date');
+            let data = await this.fetchDOM(request, '.episode-header-title');
             return [{
                 id: manga.id,
-                title: data[0].textContent.trim()
+                title: data[0].textContent.replace(manga.title, "").trim()
             }];
         } else {
             return super._getChapters(manga);
