3d4d5e05930a3c0ded21c645085c48cf5acd078a
gammesB <96754537+gammesB@users.noreply.github.com>
2022-09-03 08:42:18 +0200

    requested change for fixing magazine support shonenjumpplus (#4944)
    * requested change for fixing magazine support
    
    * lint fix
    
    * corrected issue name
    
    * changed issue date as chap title to issue number(more correct)

--- src/web/mjs/connectors/ShonenJumpPlus.mjs
+++ src/web/mjs/connectors/ShonenJumpPlus.mjs
@@ -9,4 +9,17 @@ export default class ShonenJumpPlus extends CoreView {
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://shonenjumpplus.com';
     }
+
+    async _getChapters(manga) {
+        if(/^\/magazine\/\d+$/.test(manga.id)) {
+            let request = new Request(new URL(this.url+manga.id), this.requestOptions);
+            let data = await this.fetchDOM(request, '.episode-header-title');
+            return [{
+                id: manga.id,
+                title: data[0].textContent.replace(manga.title, "").trim()
+            }];
+        } else {
+            return super._getChapters(manga);
+        }
+    }
 }
