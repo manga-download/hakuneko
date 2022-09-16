2640a65d7837b1689f9323539ace79699b52d134
akn19 <akunhitam2019@gmail.com>
2022-09-16 19:29:58 +0700

    fix lianscans

--- src/web/mjs/connectors/LianScans.mjs
+++ src/web/mjs/connectors/LianScans.mjs
@@ -8,13 +8,6 @@ export default class LianScans extends WordPressMangastream {
         super.label = 'LIAN';
         this.tags = [ 'manga', 'indonesian', 'scanlation' ];
         this.url = 'https://www.lianscans.my.id';
-        this.path = '/manga-list/';
-
-        this.querMangaTitleFromURI = 'div.series-title h2';
-        this.queryMangas = 'div.mangalist-blc ul li a.series';
-        this.queryChapters = 'div.series-chapter ul.series-chapterlist li div.flexch-infoz a';
-        this.queryChaptersTitle = 'span';
-        this.queryChaptersTitleBloat = 'span > span.date';
-        this.queryPages = 'div.reader-area img[src]:not([src=""])';
+        this.path = '/manga/list-mode';
     }
 }
