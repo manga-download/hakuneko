a634b0d6e02c9c69a81b370f8e3ac0c3c5b0dc66
apt42541 <lionking20130@gmail.com>
2022-10-27 13:26:54 +0700

    fix chapter is null
    remove debugging code and add fix get chapter is null for some mangas

--- src/web/mjs/connectors/Baozimh.mjs
+++ src/web/mjs/connectors/Baozimh.mjs
@@ -27,7 +27,10 @@ export default class Baozimh extends Connector {
     async _getChapters(manga) {
         let request = new Request(new URL(manga.id, this.url), this.requestOptions);
         //where can it find the chapters
-        const data = await this.fetchDOM(request, ".l-box #chapter-items > div > a, .l-box #chapters_other_list > div > a");
+        let data = await this.fetchDOM(request, ".l-box #chapter-items > div > a, .l-box #chapters_other_list > div > a");
+        if(data.length == 0) {
+            data = await this.fetchDOM(request, ".l-box .pure-g > div > a, .l-box #chapters_other_list > div > a");
+        }
         let chapters = data.reverse(); //get first chapters on top
         return chapters.map((element) => {
             return {
@@ -37,10 +40,8 @@ export default class Baozimh extends Connector {
         });
     }
 
-    async _getPages(chapter) {
+      async _getPages(chapter) {
         let pagesList = [];
-        let pagesList2 = [];
-        let data2 = ''
         let uri = new URL(chapter.id, this.url);
         const sectionSlot = uri.searchParams.get('section_slot');
         const chapterSlot = uri.searchParams.get('chapter_slot');
@@ -49,6 +50,7 @@ export default class Baozimh extends Connector {
         while(run) {
             const request = new Request(uri, this.requestOptions);
             let data = await this.fetchDOM(request, '.comic-contain amp-img.comic-contain__item, div:not(.chapter-main) > div.next_chapter a');
+            console.log(data);
             if (data[data.length - 1].tagName.toLowerCase() === 'a') {
                 uri = this.getRootRelativeOrAbsoluteLink(data.pop(), this.url);
                 if (!uri || uri.match(linkRegex) == null) {
@@ -57,22 +59,12 @@ export default class Baozimh extends Connector {
             } else {
                 uri = null;
             }
-            data2 = data[data.length - 1].getAttribute('src');
-            pagesList2.push(data2);
+            pagesList.push( ...data.map(element => element.getAttribute('src')).filter(page => page) );
 
             run = uri != null;
         }
-        // let result = data2.substr(0, data2.lastIndexOf("/"));
-        const lastArray = pagesList2[pagesList2.length -1 ];
-        const ur2 = data2.substr(0, data2.lastIndexOf('/')+1);
-        const maxCount = data2.substr(data2.lastIndexOf('/') +1).split('.jpg')[0];
-        console.log(lastArray);
-        for (let i = 1; i < maxCount; i++) {
-            console.log(`${ur2}${i}.jpg`);
-            pagesList.push(`${ur2}${i}.jpg`);
-        }
         return pagesList.filter((page, index) => {
-            return index === pagesList.findIndex(item => page === item);
+            return index === pagesList.findIndex(item => item == page);
         });
     }
 }
