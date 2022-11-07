a05859ea79bba654319ff36d64d56730fa04c303
Druzai <70586473+Druzai@users.noreply.github.com>
2022-11-08 00:47:20 +0300

    [Tappytoon] Fix getting paid chapters #5066
    Improved filtering of free and paid chapters
    Added login links

--- src/web/mjs/connectors/TappytoonEN.mjs
+++ src/web/mjs/connectors/TappytoonEN.mjs
@@ -8,6 +8,11 @@ export default class TappytoonEN extends Tappytoon {
         super.label = 'Tappytoon (English)';
         this.tags = [ 'webtoon', 'english' ];
         this.url = 'https://www.tappytoon.com/en';
+        this.links = {
+            login: 'https://www.tappytoon.com/en/login'
+        };
+
         this.lang = 'en';
+        this.requestOptions.headers.set('Accept-Language', this.lang);
     }
 }
