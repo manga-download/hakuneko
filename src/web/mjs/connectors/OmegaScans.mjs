d54ca1b1320c6ac3e2cf3ab6395a2699daddb8c9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-11-12 20:06:21 +0100

    fix id case

--- src/web/mjs/connectors/OmegaScans.mjs
+++ src/web/mjs/connectors/OmegaScans.mjs
@@ -4,7 +4,7 @@ export default class OmegaScans extends WordPressMangastream {
 
     constructor() {
         super();
-        super.id = 'OmegaScans';
+        super.id = 'omegascans';
         super.label = 'OmegaScans';
         this.tags = [ 'webtoon', 'scanlation', 'english', 'hentai'];
         this.url = 'https://omegascans.org';
