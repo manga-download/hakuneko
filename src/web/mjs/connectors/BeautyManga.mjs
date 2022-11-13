409a8f6bf0e38d4997671b77b2d815247324f8ed
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-11-13 13:58:49 +0100

    Lint Fix attempt 1

--- src/web/mjs/connectors/BeautyManga.mjs
+++ src/web/mjs/connectors/BeautyManga.mjs
@@ -1,5 +1,4 @@
 import WordPressMadara from './templates/WordPressMadara.mjs';
-
 export default class BeautyManga extends WordPressMadara {
 	constructor() {
 		super();
@@ -14,48 +13,44 @@ export default class BeautyManga extends WordPressMadara {
 		this.queryPages = 'p#arraydata';
 		this.mangaTitleFilter = '';
 	}
-	
 	async _getMangasFromPage(page) {
-		let uri = new URL(this.path + this.pathMangas.replace('%PAGE%', page), this.url);
-		uri.pathname = uri.pathname.replace(/\/+/g, '/');
-		let request = new Request(uri, this.requestOptions);
-		let data = await this.fetchDOM(request, this.queryMangas);
-		return data.map(element => {
-			return {
-				id: new URL(element.href, request.url).pathname,
-				title: element.title.replace(this.mangaTitleFilter, '').trim()
-			};
-		});
-	}
-	
-	async _getChaptersAjaxOld(mangaID) {
-		const uri = new URL('/ajax-list-chapter?mangaID='+mangaID , this.url);
-		const request = new Request(uri, {
-			method: 'GET'
-		});
-		const data = await this.fetchDOM(request, this.queryChapters);
-		if (data.length) {
-			return data;
-		}
-		else {
-			throw new Error('No chapters found (new ajax endpoint)!');
-		}
-	}
-	
-	async _getPages(chapter)
-	{
-		let uri = new URL(chapter.id, this.url);
-		let request = new Request(uri, this.requestOptions);
-		let data = await this.fetchDOM(request, this.queryPages);
-		let el = data[0].innerText.split(',');
-		return el.map(element =>
-		{
-			const uri = new URL(this.getAbsolutePath(element, request.url));
-			return this.createConnectorURI({
-				// HACK: bypass 'i0.wp.com' image CDN to ensure original images are loaded directly from host
-				url: uri.href.replace(/\/i\d+\.wp\.com/, ''),
-				referer: uri.origin
-			})
-		});
-	}
+        let uri = new URL(this.path + this.pathMangas.replace('%PAGE%', page), this.url);
+        uri.pathname = uri.pathname.replace(/\/+/g, '/');
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: new URL(element.href, request.url).pathname,
+                title: element.title.replace(this.mangaTitleFilter, '').trim()
+            };
+        });
+    }
+    async _getChaptersAjaxOld(mangaID) {
+        const uri = new URL('/ajax-list-chapter?mangaID='+mangaID , this.url);
+        const request = new Request(uri, {
+            method: 'GET'
+        });
+        const data = await this.fetchDOM(request, this.queryChapters);
+        if (data.length) {
+            return data;
+        }
+        else {
+            throw new Error('No chapters found (new ajax endpoint)!');
+        }
+    }
+    async _getPages(chapter)
+    {
+        let uri = new URL(chapter.id, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryPages);
+        let el = data[0].innerText.split(',');
+        return el.map(element =>
+        {
+            const uri = new URL(this.getAbsolutePath(element, request.url));
+            return this.createConnectorURI({
+                url: uri.href,
+                referer: uri.origin
+            })
+        });
+    }
 }
