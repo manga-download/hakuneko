b0cf89502b94ad01300f03a7abf7962abba122f2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-02 21:01:01 +0000

    Add Nhentai.com
    * Fixes https://github.com/manga-download/hakuneko/issues/2308
    * Very similar to HentaiManhwa (HM is even used as a CDN)

---
+++ src/web/mjs/connectors/NHentaiCom.mjs
@@ -0,0 +1,67 @@
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+
+
+export default class NHentaiCom extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'nhentaicom';
+        super.label = 'NHentai.Com';
+        this.tags = [ 'hentai' ];
+        this.url = 'https://nhentai.com';
+    }
+    
+    async _getMangas() {
+        let mangaList = [];
+        for(let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/api/comics', this.url);
+        uri.searchParams.set('page', page);
+        uri.searchParams.set('sort', 'uploaded_at');
+        uri.searchParams.set('order', 'asc');
+        uri.searchParams.set('duration', 'day');
+        uri.searchParams.set('nsfw', true);
+        const request = new Request(uri, this.requestOptions);
+        const response = await this.fetchJSON(request);
+        const data = response.data;
+
+        if(!data) return [];
+
+        return data.map(el => {
+            return {
+                id: el.slug,
+                title: el.title
+            };
+        });
+    }
+    
+    async _getChapters(manga) {
+        return [ Object.assign({language: '' }, manga) ];
+    }
+    
+    async _getPages(chapter) {
+        const uri = new URL(`/api/comics/${chapter.id}/images`, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.images.map(el => {
+            return el.source_url;
+        });
+    }
+
+    async _getMangaFromURI(uri) {
+        const ch = uri.pathname.split('/');
+        const slug = ch[ch.length-1];
+        const request = new Request(new URL(`/api/comics/${slug}`, this.url), this.requestOptions);
+        const data = await this.fetchJSON(request);
+        const id = data.slug;
+        const title = data.title;
+        return new Manga(this, id, title);
+    }
+}
