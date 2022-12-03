56f3beafc2be2b3d667acf713f8db74b3ad2a7bc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-03 11:32:34 +0100

    Nhentai.com : dont fetch manga list

--- src/web/mjs/connectors/NHentaiCom.mjs
+++ src/web/mjs/connectors/NHentaiCom.mjs
@@ -13,33 +13,8 @@ export default class NHentaiCom extends Connector {
     }
     
     async _getMangas() {
-        let mangaList = [];
-        for(let page = 1, run = true; run; page++) {
-            let mangas = await this._getMangasFromPage(page);
-            mangas.length ? mangaList.push(...mangas) : run = false;
-        }
-        return mangaList;
-    }
-
-    async _getMangasFromPage(page) {
-        const uri = new URL('/api/comics', this.url);
-        uri.searchParams.set('page', page);
-        uri.searchParams.set('sort', 'uploaded_at');
-        uri.searchParams.set('order', 'asc');
-        uri.searchParams.set('duration', 'day');
-        uri.searchParams.set('nsfw', true);
-        const request = new Request(uri, this.requestOptions);
-        const response = await this.fetchJSON(request);
-        const data = response.data;
-
-        if(!data) return [];
-
-        return data.map(el => {
-            return {
-                id: el.slug,
-                title: el.title
-            };
-        });
+        let msg = 'This website does not provide a manga list, please copy and paste the URL containing the chapters directly from your browser into HakuNeko.';
+        throw new Error(msg);
     }
     
     async _getChapters(manga) {
