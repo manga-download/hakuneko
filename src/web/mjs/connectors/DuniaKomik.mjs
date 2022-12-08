7447a4ddee8a3af40ce26f1a6edacbc23421adb5
akn19 <akunhitam2019@gmail.com>
2022-12-08 11:50:03 +0700

    fixes DuniaKomik classname

--- src/web/mjs/connectors/DuniaKomik.mjs
+++ src/web/mjs/connectors/DuniaKomik.mjs
@@ -1,6 +1,6 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class GabutScans extends WordPressMangastream {
+export default class DuniaKomik extends WordPressMangastream {
 
     constructor() {
         super();
