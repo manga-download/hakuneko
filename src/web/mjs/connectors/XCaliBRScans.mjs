6d3715dd5f4a5182316b0550144200dd0ae3187d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-14 14:39:43 +0000

    Fix lint (1)

--- src/web/mjs/connectors/XCaliBRScans.mjs
+++ src/web/mjs/connectors/XCaliBRScans.mjs
@@ -67,43 +67,44 @@ export default class XCaliBRScans extends WordPressMangastream {
         let data = await Engine.Request.fetchUI(request, script);
         // HACK: bypass 'i0.wp.com' image CDN to ensure original images are loaded directly from host
         const piclist = data.imagz.map(link => this.getAbsolutePath(link, request.url).replace(/\/i\d+\.wp\.com/, '')).filter(link => !link.includes('histats.com'));
-        switch (data.scrambled){
+        switch (data.scrambled) {
             case 0: //do nothing
-            return piclist;
+                return piclist;
             case 1: //Flip each picture, no grouping
-            return piclist.map(element => {
-                return this.createConnectorURI({
-                    picture : element, scrambled : data.scrambled
+                return piclist.map(element => {
+                    return this.createConnectorURI({
+                        picture : element, scrambled : data.scrambled
+                    });
                 });
-            });
-            case 2: //Flip and group by 2
-            let puzzles = [];
-            let count = 0;
-            for(let i = 0; i < piclist.length-1; i+=2){
-                puzzles.push(this.createConnectorURI({
-                    picture1 : piclist[i],
-                    picture2 : piclist[i+1] ,
-                scrambled : data.scrambled })
-                );
-                count +=2;
+            case 2: {//Flip and group by 2
+                let puzzles = [];
+                let count = 0;
+                for(let i = 0; i < piclist.length-1; i+=2) {
+                    puzzles.push(this.createConnectorURI({
+                        picture1 : piclist[i],
+                        picture2 : piclist[i+1] ,
+                    scrambled : data.scrambled })
+                    );
+                    count +=2;
+                }
+                //get remaining picture if number was odd
+                if (count < piclist.length) {
+                    puzzles.push(piclist[count]);
+                }
+                return puzzles;
             }
-            //get remaining picture if number was odd
-            if (count < piclist.length) {
-                puzzles.push(piclist[count]);
-            }
-            return puzzles;
             default:
         }
     }
     async _handleConnectorURI(payload) {
         let pictures = [];
-        switch(payload.scrambled){
+        switch(payload.scrambled) {
             case 1:
-            pictures.push(payload.picture);
+                pictures.push(payload.picture);
             break;
             case 2:
-            pictures.push(payload.picture1);
-            pictures.push(payload.picture2);
+                pictures.push(payload.picture1);
+                pictures.push(payload.picture2);
             break;
         }
         let promises = pictures.map(async part => {
@@ -116,36 +117,36 @@ export default class XCaliBRScans extends WordPressMangastream {
         return this._blobToBuffer(data);
     }
     async composePuzzle(bitmaps, scramblemode) {
-        switch (scramblemode){
+        switch (scramblemode) {
             case 1: //reverse picture
-            return new Promise(resolve => {
-                let canvas = document.createElement('canvas');
-                canvas.width = bitmaps[0].width;
-                canvas.height = bitmaps[0].height;
-                let ctx = canvas.getContext('2d');
-                ctx.scale(-1, 1);
-                ctx.drawImage(bitmaps[0], 0, 0, -bitmaps[0].width, bitmaps[0].height);
-                canvas.toBlob(data => {
-                    resolve(data);
-                },
-                Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
+                return new Promise(resolve => {
+                    let canvas = document.createElement('canvas');
+                    canvas.width = bitmaps[0].width;
+                    canvas.height = bitmaps[0].height;
+                    let ctx = canvas.getContext('2d');
+                    ctx.scale(-1, 1);
+                    ctx.drawImage(bitmaps[0], 0, 0, -bitmaps[0].width, bitmaps[0].height);
+                    canvas.toBlob(data => {
+                        resolve(data);
+                    },
+                    Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
             });
             case 2:
-            return new Promise(resolve => {
-                let canvas = document.createElement('canvas');
-                //combine 2 pictures flipped
-                const b1 = bitmaps[0];
-                const b2 = bitmaps[1];
-                canvas.width = b1.width+b2.width;
-                canvas.height = b1.height;
-                let ctx = canvas.getContext('2d');
-                ctx.scale(-1, 1);
-                ctx.drawImage(b2, 0, 0, -b2.width, b2.height);
-                ctx.drawImage(b1, -b2.width, 0, -b1.width , b1.height);
-                canvas.toBlob(data => {
-                    resolve(data);
-                },
-                Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
+                return new Promise(resolve => {
+                    let canvas = document.createElement('canvas');
+                    //combine 2 pictures flipped
+                    const b1 = bitmaps[0];
+                    const b2 = bitmaps[1];
+                    canvas.width = b1.width+b2.width;
+                    canvas.height = b1.height;
+                    let ctx = canvas.getContext('2d');
+                    ctx.scale(-1, 1);
+                    ctx.drawImage(b2, 0, 0, -b2.width, b2.height);
+                    ctx.drawImage(b1, -b2.width, 0, -b1.width, b1.height);
+                    canvas.toBlob(data => {
+                        resolve(data);
+                    },
+                    Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
             });
         }
     }
