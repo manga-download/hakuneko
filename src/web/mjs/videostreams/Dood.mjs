5c465a0f407dcd2990b8b05bd1f47dc89b52a7eb
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-22 19:49:28 +0000

    Dood : requested changes

--- src/web/mjs/videostreams/Dood.mjs
+++ src/web/mjs/videostreams/Dood.mjs
@@ -13,16 +13,16 @@ export default class Dood {
         let request = new Request(this._uri);
         const data = await Engine.Request.fetchUI(request, script, 30000);
         //end of final video url
-        const videourlend = data.tok;
+        const videoUrlEnd = data.tok;
         //get the url for requestion the video url beginning
         let dom = (new DOMParser).parseFromString(data.doc, "text/html");
-        const requestthing = dom.documentElement.innerHTML.match(/(\/pass_md5\S+)'/)[1];
-        const url = new URL(requestthing, 'https://dood.wf');
+        const requestThing = dom.documentElement.innerHTML.match(/(\/pass_md5\S+)'/)[1];
+        const url = new URL(requestThing, 'https://dood.wf');
         request = new Request(url);
         request.headers.set('x-referer', this._uri);
         request.headers.set('X-Requested-With', 'XMLHttpRequest');
         const response = await fetch(request);
-        let videolink = await response.text();
-        return videolink + videourlend;
+        let videoLink = await response.text();
+        return videoLink + videoUrlEnd;
     }
 }
