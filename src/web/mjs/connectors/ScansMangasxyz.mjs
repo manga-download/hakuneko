30080944d5710fe336f5f501563f6564f0e2c0bd
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-31 12:09:46 +0000

    Update ScansMangasxyz (#5279)
    * Url change
    * New way to get pages
    * Need rerferer for pages
    * Fixes https://github.com/manga-download/hakuneko/issues/5277

--- src/web/mjs/connectors/ScansMangasxyz.mjs
+++ src/web/mjs/connectors/ScansMangasxyz.mjs
@@ -5,24 +5,33 @@ export default class ScansMangasxyz extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'scansmangasxyz';
-        super.label = 'ScansMangas (XYZ)';
+        super.label = 'ScansMangas (WS)';
         this.tags = [ 'manga', 'french' ];
-        this.url = 'https://scansmangas.xyz';
+        this.url = 'https://scansmangas.ws';
         this.path = '/tous-nos-mangas/';
-
         this.queryMangas = 'div.bigor > a';
         this.queryChapters = 'span.lchx.desktop > a';
         this.queryChaptersTitle = undefined;
     }
 
     async _getPages(chapter) {
-        let pagelist = [];
+        const script = `
+        new Promise((resolve ) => {
+            resolve(pages);
+        });
+        `;
         const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        const data = (await this.fetchDOM(request, 'body'))[0];
-        const lenloop = parseInt(data.querySelector('div.nav_apb > a:nth-last-of-type(2)').text);
-        for (let i = 1; i <= lenloop; i++) {
-            pagelist.push(data.querySelector('a[rel=nofollow] > source').src.replace(/(\d+)(\.)/, i+'$2'));
-        }
-        return pagelist;
+        const data = await Engine.Request.fetchUI(request, script);
+        return data.map(element => this.createConnectorURI(element.page_image ));
+    }
+
+    async _handleConnectorURI(payload) {
+        let request = new Request(payload, this.requestOptions);
+        request.headers.set('x-referer', this.url);
+        let response = await fetch(request);
+        let data = await response.blob();
+        data = await this._blobToBuffer(data);
+        this._applyRealMime(data);
+        return data;
     }
 }
