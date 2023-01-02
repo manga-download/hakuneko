8b8bbc39abbb1bbeabce0d32cec8d441b9be9bae
Smile.Id79 <56505683+Shiin-79@users.noreply.github.com>
2023-01-03 00:29:52 +0700

    Update ReaperScansID.mjs

--- src/web/mjs/connectors/ReaperScansID.mjs
+++ src/web/mjs/connectors/ReaperScansID.mjs
@@ -1,6 +1,6 @@
 import ReaperScans from './ReaperScans.mjs';
 
-export default class ReaperScansFR extends ReaperScans {
+export default class ReaperScansID extends ReaperScans {
     constructor() {
         super();
         super.id = 'reaperscansid';
