48b00fd69cc44f7669631497732d55b87470dce3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-06 15:07:02 +0000

    Reset Scans : fix getting chapters (#5330)
    * Changed CSS selector for chapters
    * No need to support Bilibili anymore. All chapters for 39 mangas are on the website.

--- src/web/mjs/connectors/ResetScans.mjs
+++ src/web/mjs/connectors/ResetScans.mjs
@@ -1,5 +1,4 @@
 import WordPressMadara from './templates/WordPressMadara.mjs';
-import BilibiliComics from './BilibiliComics.mjs';
 
 export default class ResetScans extends WordPressMadara {
 
@@ -9,17 +8,6 @@ export default class ResetScans extends WordPressMadara {
         super.label = 'Reset Scans';
         this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://reset-scans.com';
-
-        this.bilibili = new BilibiliComics();
-        this.queryChapters = 'li.wp-manga-chapter span.chapter-name-before a:last-of-type';
-    }
-
-    async _getPages(chapter) {
-        if(chapter.id.startsWith(this.bilibili.url)) {
-            const bilibiliChapter = { id: new URL(chapter.id).pathname.split('/').pop() };
-            return this.bilibili._getPages(bilibiliChapter);
-        } else {
-            return super._getPages(chapter);
-        }
+        this.queryChapters = 'li.wp-manga-chapter div.li__text > a';
     }
 }
