2b4edf7f75515de8ef147e5732ebcd5e29f0f17f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-06 15:05:17 +0000

    Required changes in Ainzscans

--- src/web/mjs/connectors/Ainzscans.mjs
+++ src/web/mjs/connectors/Ainzscans.mjs
@@ -8,21 +8,35 @@ export default class Ainzscans extends Connector {
         super.label = 'Ainz Scans';
         this.tags = [ 'webtoon', 'indonesian', 'scanlation' ];
         this.url = 'https://www.ainzscans.my.id';
+        this.resultsPerPages = 500;
     }
 
     async _getMangas() {
-        const uri = new URL('/feeds/posts/default/-/Series?alt=json&max-results=500', this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchJSON(request);
-        return data.feed.entry.map(entry => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink( entry.link.find( a => a.rel === 'alternate' ).href, request.url ),
-                title: entry.title['$t'].trim()
-            };
-        });
+        let mangalist = [];
+        let uri = new URL('/feeds/posts/default/-/Series?alt=json&max-results='+this.resultsPerPages, this.url);
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchJSON(request);
+        const totalentries = parseInt(data.feed.openSearch$totalResults['$t']);
+        let numpages = Math.floor(totalentries / this.resultsPerPages);
+        numpages == 0 ? numpages++ : numpages;
+        for (let page = 0; page < numpages; page++) {
+            uri = new URL('/feeds/posts/default/-/Series?alt=json&max-results=500&start-index='+ page * this.resultsPerPages+1, this.url);
+            request = new Request(uri, this.requestOptions);
+            let data = await this.fetchJSON(request);
+
+            data.feed.entry.forEach(entry => {
+                mangalist.push( {
+                    id: this.getRootRelativeOrAbsoluteLink( entry.link.find( a => a.rel === 'alternate' ).href, request.url ),
+                    title: entry.title['$t'].trim()
+                });
+            });
+
+        }
+        return mangalist;
     }
 
     async _getChapters(manga) {
+        let chapterlist = [];
         const uri = new URL(manga.id, this.url);
         const script = `
           new Promise(resolve => {
@@ -30,20 +44,35 @@ export default class Ainzscans extends Connector {
         });
         `;
         let request = new Request(uri);
-        const data = await Engine.Request.fetchUI(request, script, 5000);
+        const chapterSlang = await Engine.Request.fetchUI(request, script, 5000);
 
-        const apiuri = new URL('/feeds/posts/default/-/'+data+'?alt=json&max-results=500', this.url);
+        let apiuri = new URL('/feeds/posts/default/-/'+chapterSlang+'?alt=json&max-results='+this.resultsPerPages, this.url);
         request = new Request(apiuri, this.requestOptions);
-        const feeds = await this.fetchJSON(request);
-        return feeds.feed.entry
-            .filter(entry => entry.title['$t'].trim() != data )
-            .map(entry => {
-                return {
-                    id: entry.id['$t'],
-                    title: entry.title['$t'].trim()
-                };
+        let feeds = await this.fetchJSON(request);
+        const totalentries = parseInt(feeds.feed.openSearch$totalResults['$t']);
+        let numpages = Math.floor(totalentries / this.resultsPerPages);
+        numpages == 0 ? numpages++ : numpages;
+
+        for (let page = 0; page < numpages; page++) {
+            apiuri = new URL('/feeds/posts/default/-/'+chapterSlang+'?alt=json&max-results='+this.resultsPerPages+'&start-index='+ page * this.resultsPerPages+1, this.url);
+            request = new Request(apiuri, this.requestOptions);
+            let data = await this.fetchJSON(request);
+
+            data.feed.entry.forEach(entry => {
+                const entrylink = entry.link.find( a => a.rel === 'alternate' ).href;
+                if (entrylink != uri ) {
+                    chapterlist.push( {
+                        id: entry.id['$t'],
+                        title: entry.title['$t'].trim()
+                    });
+                }
+
             });
+
+        }
+        return chapterlist;
     }
+
     async _getPages(chapter) {
         let uri = new URL(chapter.manga.id, this.url);
         const script = `
@@ -52,9 +81,9 @@ export default class Ainzscans extends Connector {
         });
         `;
         let request = new Request(uri);
-        let data = await Engine.Request.fetchUI(request, script, 5000);
+        const chapterSlang = await Engine.Request.fetchUI(request, script, 5000);
 
-        uri = new URL('/feeds/posts/default/-/'+data+'?alt=json&max-results=500', this.url);
+        uri = new URL('/feeds/posts/default/-/'+chapterSlang+'?alt=json', this.url);
         request = new Request(uri, this.requestOptions);
         const feeds = await this.fetchJSON(request);
         const goodchap = feeds.feed.entry.find( a => a.id['$t'] == chapter.id);
