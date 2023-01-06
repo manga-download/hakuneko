4d21908dddb74af56470943898666afecd5cb17a
Abhishek <abhishek276533@gmail.com>
2023-01-07 01:52:32 +0530

    ManhwaList Connector Added(#5336)

---
+++ src/web/mjs/connectors/ManhwaList.mjs
@@ -0,0 +1,26 @@
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+
+export default class ManhwaList extends WordPressMangastream {
+
+    constructor() {
+        super();
+        super.id = 'manhwalist';
+        super.label = 'Manhwa List';
+        this.tags = [ 'manga', 'indonesian'];
+        this.url = 'https://manhwalist.in';
+        this.path = '/manga/?list';
+    }
+    async _getPages(chapter) {
+        const data = await super._getPages(chapter);
+        return data.map(element => this.createConnectorURI(element));
+    }
+    async _handleConnectorURI(payload) {
+        const request = new Request(payload, this.requestOptions);
+        request.headers.set('x-referer', this.url);
+        let response = await fetch(request);
+        let data = await response.blob();
+        data = await this._blobToBuffer(data);
+        this._applyRealMime(data);
+        return data;
+    }
+}
