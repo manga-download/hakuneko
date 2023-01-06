90fa15e4a152c904095839e0677b08ded738cc85
Abhishek <abhishek276533@gmail.com>
2023-01-06 14:43:18 +0530

    AniGliScans connector changes done

--- src/web/mjs/connectors/AniGliScans.mjs
+++ src/web/mjs/connectors/AniGliScans.mjs
@@ -5,8 +5,8 @@ export default class AniGliScans extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'anigliscans';
-        super.label = 'AniGliScans';
-        this.tags = [ 'manga', 'english' ];
+        super.label = 'Animated Glitched Scans';
+        this.tags = [ 'manga', 'english', 'scanlation' ];
         this.url = 'https://anigliscans.com';
         this.path = '/series/?list';
     }
@@ -15,7 +15,7 @@ export default class AniGliScans extends WordPressMangastream {
         return data.map(element => this.createConnectorURI(element));
     }
     async _handleConnectorURI(payload) {
-        let request = new Request(payload, this.requestOptions);
+        const request = new Request(payload, this.requestOptions);
         request.headers.set('x-referer', this.url);
         let response = await fetch(request);
         let data = await response.blob();
