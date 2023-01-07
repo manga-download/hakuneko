ce6f0a14b1edc2e473b59ff4f4fcca85e480e8d0
Abhishek <abhishek276533@gmail.com>
2023-01-07 18:29:40 +0530

    [Update] Aniglitch & ManhwaList connectors

--- src/web/mjs/connectors/AniGliScans.mjs
+++ src/web/mjs/connectors/AniGliScans.mjs
@@ -1,5 +1,4 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
-
 export default class AniGliScans extends WordPressMangastream {
 
     constructor() {
@@ -9,18 +8,5 @@ export default class AniGliScans extends WordPressMangastream {
         this.tags = [ 'manga', 'english', 'scanlation' ];
         this.url = 'https://anigliscans.com';
         this.path = '/series/?list';
-    }
-    async _getPages(chapter) {
-        const data = await super._getPages(chapter);
-        return data.map(element => this.createConnectorURI(element));
-    }
-    async _handleConnectorURI(payload) {
-        const request = new Request(payload, this.requestOptions);
-        request.headers.set('x-referer', this.url);
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
     }
 }
--- src/web/mjs/connectors/ManhwaList.mjs
+++ src/web/mjs/connectors/ManhwaList.mjs
@@ -1,5 +1,4 @@
 import WordPressMangastream from './templates/WordPressMangastream.mjs';
-
 export default class ManhwaList extends WordPressMangastream {
 
     constructor() {
@@ -9,18 +8,5 @@ export default class ManhwaList extends WordPressMangastream {
         this.tags = [ 'manga', 'indonesian'];
         this.url = 'https://manhwalist.in';
         this.path = '/manga/?list';
-    }
-    async _getPages(chapter) {
-        const data = await super._getPages(chapter);
-        return data.map(element => this.createConnectorURI(element));
-    }
-    async _handleConnectorURI(payload) {
-        const request = new Request(payload, this.requestOptions);
-        request.headers.set('x-referer', this.url);
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
     }
 }
