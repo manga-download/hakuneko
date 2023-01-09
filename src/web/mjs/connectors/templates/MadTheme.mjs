cd0544a6d25837ebe55837d9375925bcaf12d95b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-09 09:03:30 +0000

    Update MadTheme.mjs

--- src/web/mjs/connectors/templates/MadTheme.mjs
+++ src/web/mjs/connectors/templates/MadTheme.mjs
@@ -40,9 +40,8 @@ export default class MadTheme extends Connector {
         });
     }
     async _getChapters(manga) {
-        let mangaid = manga.id.split('/').pop();
-        mangaid = '/'+mangaid;//make sure to take last part of url for the api
-        const uri = new URL('/api/manga'+mangaid+'/chapters?source=detail', this.url);
+        const mangaid = manga.id.split('/').pop();
+        const uri = new URL('/api/manga/'+mangaid+'/chapters?source=detail', this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, 'a');
         return data.map(element => {
