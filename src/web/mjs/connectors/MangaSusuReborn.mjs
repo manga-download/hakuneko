99fc74d781c2f10201d236f05b08a2c92e0cde4c
akn19 <akunhitam2019@gmail.com>
2023-01-13 18:43:22 +0700

    fix MangaSusu Reborn

--- src/web/mjs/connectors/MangaSusuReborn.mjs
+++ src/web/mjs/connectors/MangaSusuReborn.mjs
@@ -7,7 +7,34 @@ export default class MangaSusuReborn extends WordPressMangastream {
         super.id = 'mangasusureborn';
         super.label = 'MangaSusu Reborn';
         this.tags = [ 'manga', 'webtoon', 'indonesian' ];
-        this.url = 'https://mangasusu.biz';
-        this.path = '/manga/list-mode/';
+        this.url = 'https://92.84.132.251';
+        this.path = '/az-list';
+
+        this.queryMangas = 'div.listo div.bs div.bsx > a';
+        this.queryChapters = 'div#chapterlist div.chbox div.eph-num a';
+        this.queryPages = 'div#readerarea img[src]:not([src=""])';
+        this.queryChaptersTitle = undefined;
+        this.querMangaTitleFromURI = 'div.seriestucon div.seriestuheader h1.entry-title';
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/az-list/page/' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title.trim()
+            };
+        });
     }
 }
