e5492eb60e848d9cbb24ddecdb06e31dd99433d6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-21 09:28:17 +0000

    Bilibili : Language, login support and clipboard fixes (#5399)
    * Only 2 connectors needed : Bibilicomics and BibiliManhua
    * Clipboard feature works again now (only one Bilibilicomics.com connector)
    * Bilibilicomics is the multilingual one and handle Bilibilicomics .com languages through a setting
    * Deletion of Bibili ES, FR, ID as they are handled by Bilibilicomics
    * Bilibilimanhua ,is Chinese only and url is manga.bilibili.com.
    * Bilibilicomics extends Bilibilimanhua, it only override its config so user can select language.
    * Login pages are different
    * Language setting affect fetched manga list.
    * Login & downloading unlocked/free chapter was tested for Bilibilicomics . Free download was tested for Bilibilimanhua
    *  May require testing for paid stuff on Bilibilimanhua

--- src/web/mjs/connectors/BilibiliComics.mjs
+++ src/web/mjs/connectors/BilibiliComics.mjs
@@ -5,9 +5,39 @@ export default class BilibiliComics extends BilibiliManhua {
     constructor() {
         super();
         super.id = 'bilibili-comics';
-        super.label = 'Bilibili Comics (English)';
-        this.tags = [ 'webtoon', 'english' ];
+        super.label = 'Bilibili Comics (Multi-lingual)';
+        this.tags = [ 'webtoon', 'multi-lingual' ];
         this.url = 'https://www.bilibilicomics.com';
-        this.lang = 'en';
+        this.links = {
+            login: 'https://www.bilibilicomics.com/account'
+        };
+        //override BilibiliManhua config as we need lanuage switch. Difference is handled in the main code with (this.config.language) ? : test
+        this.config = {
+            quality:  {
+                label: 'Preferred format',
+                description: 'format of images\nwebp (low)\njpg (medium)\npng (high))',
+                input: 'select',
+                options: [
+                    { value: 'webp', name: 'webp' },
+                    { value: 'jpg', name: 'jpg' },
+                    { value: 'png', name: 'png' },
+                ],
+                value: 'png'
+            },
+            language: {
+                label: 'Language Settings',
+                description: 'Choose the language to use. This will affect available manga lists',
+                input: 'select',
+                options: [
+                    { value: 'fr', name: 'French' },
+                    { value: 'en', name: 'English' },
+                    { value: 'cn', name: 'Chinese' },
+                    { value: 'id', name: 'Indonesian' },
+                    { value: 'es', name: 'Spanish' },
+                ],
+                value: 'en'
+            },
+        };
+
     }
 }
