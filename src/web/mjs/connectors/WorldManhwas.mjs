95d4085e1dd9fe70c0d830a751164e019fdfeb07
Xsamename <whosamename@gmail.com>
2023-01-29 13:20:39 +0700

    feat: add connector for worldmanhwas

---
+++ src/web/mjs/connectors/WorldManhwas.mjs
@@ -0,0 +1,12 @@
+import WordPressMadara from './templates/WordPressMadara.mjs';
+
+export default class WorldManhwas extends WordPressMadara {
+
+    constructor() {
+        super();
+        super.id = 'worldmanhwas';
+        super.label = 'WorldManhwas';
+        this.tags = ['hentai', 'indonesia', 'indonesian'];
+        this.url = 'https://worldmanhwas.info/';
+    }
+}
