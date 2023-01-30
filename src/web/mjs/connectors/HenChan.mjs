3a230600d3b31f2c35a56e7c949ab056633a1397
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-30 10:02:45 +0000

    Update Hentai-chan (#5422)
    * Fix url
    * Made Mangachan more like a template at its the template for Hentai-chan
    * Fixes https://github.com/manga-download/hakuneko/issues/5357

--- src/web/mjs/connectors/HenChan.mjs
+++ src/web/mjs/connectors/HenChan.mjs
@@ -7,9 +7,23 @@ export default class HenChan extends MangaChan {
         super.id = 'henchan';
         super.label = 'Хентай-тян! (Hentai-chan)';
         this.tags = [ 'hentai', 'russian' ];
-        this.url = 'https://henchan.pro';
-
+        this.url = 'https://y.hentaichan.live';
         this.path = '/manga/new';
-        this.queryChapters = 'div.extaraNavi p.extra_off:last-of-type a';
+        this.queryChapters = 'div#manga_images a';
+        this.queryPages = 'data.fullimg';
     }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions );
+        const data = await this.fetchDOM(request, this.queryChapters );
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title.replace(manga.title, '').trim().replace('Читать онлайн', manga.title),
+                language: 'ru'
+            };
+        });
+    }
+
 }
