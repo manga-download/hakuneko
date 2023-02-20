72d1f43aa43fa0eb563134d1dc89bfde0a529136
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-02-20 10:47:41 +0000

    KlManga : workadound for anti scraping
    This wont last long i think.

--- src/web/mjs/connectors/KissAway.mjs
+++ src/web/mjs/connectors/KissAway.mjs
@@ -10,4 +10,12 @@ export default class KissAway extends FlatManga {
         this.url = 'https://klmanga.com';
         this.requestOptions.headers.set('x-referer', this.url);
     }
+
+    async _getChapters(manga) {
+        manga.id += '?PageSpeed=noscript';
+        const chapters = await super._getChapters(manga);
+        chapters.forEach(chapter => chapter.id+='?PageSpeed=noscript');
+        return chapters;
+    }
+
 }
