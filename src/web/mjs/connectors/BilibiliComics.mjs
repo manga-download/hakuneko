bb8c62e3922aff87c6d620541bbd38105a6b2423
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-02-24 12:28:44 +0000

    Bilibilicomics : Add quality selection

--- src/web/mjs/connectors/BilibiliComics.mjs
+++ src/web/mjs/connectors/BilibiliComics.mjs
@@ -13,7 +13,7 @@ export default class BilibiliComics extends BilibiliManhua {
         };
         //override BilibiliManhua config as we need lanuage switch. Difference is handled in the main code with (this.config.language) ? : test
         this.config = {
-            quality:  {
+            format:  {
                 label: 'Preferred format',
                 description: 'format of images\nwebp (low)\njpg (medium)\npng (high))',
                 input: 'select',
@@ -37,7 +37,48 @@ export default class BilibiliComics extends BilibiliManhua {
                 ],
                 value: 'en'
             },
+            picquality: {
+                label: 'Quality Settings',
+                description: 'Choose the prefered quality',
+                input: 'select',
+                options: [
+                    { value: 'raw', name: 'Raw' },
+                    { value: 'hd', name: 'HD' },
+                    { value: 'sd', name: 'SD' },
+                ],
+                value: 'raw'
+            },
+            forcepicturesize : {
+                label: 'Force max quality (Experimental)',
+                description: 'Force server to send pictures with max size. Override "quality" settings',
+                input: 'checkbox',
+                value: false
+            }
         };
 
     }
+
+    //override quality formula cause its different for Bilibilicomics than for BilibiliManhua
+    getImageSizeByQuality(width) {
+
+        //q is an optional url parameter to put when user selectod lowest quality
+        let o = {
+            w : width,
+            q : undefined
+        };
+
+        if (width < 1) return o;
+
+        const n = this.config.picquality.value;
+        const r = 3000; //r = window.innerWidth, force it to max. Why are they adjusting picture to window width?
+
+        n === 'raw' ? o.w = r <= 1000 ? 1000 : r <= 1200 ? 1200 : r <= 1600 ? 1600 : r <= 2000 ? 2000 : width : n === 'hd' ? o.w = r <= 1200 ? 800 : 1000 : (o.w = r <= 1200 ? 600 : 800, o.q = 50);
+
+        if (this.config.forcepicturesize.value) {
+            o.w = Math.max(o.w, width);
+            o.q = undefined;
+        }
+        return o;
+    }
+
 }
