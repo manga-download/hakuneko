af4d389ed7493292a96719a15dcae4460dd7026f
Icyelut <panetghoul@protonmail.com>
2023-03-13 15:50:29 +0900

    [PeanuToon] Refactor _getMangas for better code reuse

--- src/web/mjs/connectors/PeanuToon.mjs
+++ src/web/mjs/connectors/PeanuToon.mjs
@@ -19,116 +19,69 @@ export default class PeanuToon extends Connector {
                 value: 2500
             }
         };
-		this.links = {
-			login : 'https://www.peanutoon.com/ko/login'
-			};
-    }
-
-    async _getDaysMangas() {
-        const script = `new Promise(async (resolve, reject) => {
-                            async function wait( time ) {
-                                return new Promise( resolve => {
-                                    setTimeout( resolve, time );
-                                } );
-                            };
-                            function getTitleAndUrl(element) {
-                                return {
-                                    url: element.href,
-                                    title: element.text.trim()
-                                };
-                            }; 
-                            function getMangasForMenu(element) {
-                                return ([...document.querySelectorAll("div.serialization_work_cnt a")].map(element => getTitleAndUrl(element)));
-                                
-                            };             
-
-
-                            let promise = Promise.resolve();
-                            [...document.querySelectorAll("ul#comic-nav-tabs a")].forEach((tab, index) => {
-                                promise = promise.then(async prom => {
-                                    tab.click();
-                                    await wait(1000);
-                                    return getMangasForMenu(tab);
-                                });
-                            });
-
-                            const result = await promise;
-                            resolve(result);
-                                            
-                        }, ${this.config.scrapeDelay.value});`;
-
-        const request = new Request(this.url + '/ko/days', this.requestOptions);
-
-
-		return Engine.Request.fetchUI(request, script).then((data) => {
-			const genre_manga = data.map(element => {
-				return {
-					id: this.getRootRelativeOrAbsoluteLink(element.url, this.url),
-					title: element.title
-				};
-			});
-
-			return genre_manga;
-		});
-
-
-    }
-
-    async _getGenreMangas() {
-
-        const script = `new Promise(async (resolve, reject) => {
-                            async function wait( time ) {
-                                return new Promise( resolve => {
-                                    setTimeout( resolve, time );
-                                } );
-                            };
-                            function getTitleAndUrl(element) {
-                                return {
-                                    url: element.href,
-                                    title: element.text.trim()
-                                };
-                            }; 
-                            function getMangasForMenu(element) {
-                                return ([...document.querySelectorAll("div#로맨스 article div.g-py-10 a")].map(element => getTitleAndUrl(element)));
-                                
-                            };             
-
-
-                            let promise = Promise.resolve();
-                            [...document.querySelectorAll("ul#comic-nav-tabs li.nav-item")].forEach((tab, index) => {
-                                promise = promise.then(async prom => {
-                                    tab.click();
-                                    await wait(1000);
-                                    return getMangasForMenu(tab);
-                                });
-                            });
-
-                            const result = await promise;
-                            resolve(result);
-                                            
-                        }, ${this.config.scrapeDelay.value});`;
-
-        let request = new Request(this.url + '/ko/genre?로맨스', this.requestOptions);
-
-		return Engine.Request.fetchUI(request, script).then((data) => {
-			const genre_manga = data.map(element => {
-				return {
-					id: this.getRootRelativeOrAbsoluteLink(element.url, this.url),
-					title: element.title
-				};
-			});
-
-			return genre_manga
-		});
+        this.links = {
+            login : 'https://www.peanutoon.com/ko/login'
+        };
     }
 
     async _getMangas() {
+        const mangalist = [];
 
-        const days_manga = await this._getDaysMangas();
-        const genre_manga = await this._getGenreMangas();
+        mangalist.push(... await this._getMangasJS ('div article div.g-py-10 a', 'ul#comic-nav-tabs li.nav-item a', '/ko/genre?성인'));
+        mangalist.push(... await this._getMangasJS ('div.serialization_work_cnt a', 'ul#comic-nav-tabs a', '/ko/days'));
+        mangalist.push(... await this._getMangasJS ('div.tab-content div.g-pt-8 a', 'div.lnb-tabs a.nav-link', '/ko/original'));
 
-        console.log(Array.prototype.concat(days_manga, genre_manga));
-        return Array.prototype.concat(days_manga, genre_manga);
+        return mangalist;
+    }
+
+    async _getMangasJS(menuCSSSelector, tabCSSselector, path) {
+        const script = `new Promise(async (resolve, reject) => {
+                            async function wait( time ) {
+                                return new Promise( resolve => {
+                                    setTimeout( resolve, time );
+                                } );
+                            };
+                            function getTitleAndUrl(element) {
+                                return {
+                                    url: element.href,
+                                    title: element.text.trim()
+                                };
+                            }; 
+                             function getMangasForMenu(element) {
+                                return ([...document.querySelectorAll('${menuCSSSelector}')].map(element => getTitleAndUrl(element)));
+                                };         
+
+
+                            let promise = Promise.resolve();
+                            [...document.querySelectorAll('${tabCSSselector}')].forEach((tab, index) => {
+                                promise = promise.then(async prom => {
+                                    // For the genre page, we can't click on the adult tab or it will take us to another page
+                                    // if the user cannot access adult material.
+                                    // But, if we load the page with /ko/genre?성인 then it will load the tab without content
+                                    // If the user can access adult material, then the tab will load normally
+                                    if(tab.innerText != "성인")
+                                        tab.click();
+                                    await wait(1000);
+                                    return getMangasForMenu(tab);
+                                });
+                            });
+
+                            const result = await promise;
+                            resolve(result);
+                                            
+                        }, ${this.config.scrapeDelay.value});`;
+
+        let request = new Request(this.url + path, this.requestOptions);
+
+        return Engine.Request.fetchUI(request, script).then((data) => {
+            return data.map(element => {
+                return {
+                    id: this.getRootRelativeOrAbsoluteLink(element.url, this.url),
+                    title: element.title
+                };
+            });
+
+        });
     }
 
     async _getChapters(manga) {
@@ -142,13 +95,13 @@ export default class PeanuToon extends Connector {
             };
         });
     }
-	
+
     async _getPages(chapter) {
         if(chapter.id.includes("/#payment"))
             throw new Error(`The paid chapter '${chapter.title}' has not been purchased!`);
         else if(chapter.id.includes("/login"))
             throw new Error(`The chapter '${chapter.title}' requires login!`);
-		
+
         const uri = new URL(chapter.id, this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, 'section#viewer-list source.lazyload');
