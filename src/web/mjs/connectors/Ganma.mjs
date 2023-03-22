976ff19a49589cb3716fb40300bc6f82e2023859
Yan <i@yinyan.fr>
2023-03-22 21:21:12 +0100

    fix(connector): Ganma - vacant chapter titles become undefined

--- src/web/mjs/connectors/Ganma.mjs
+++ src/web/mjs/connectors/Ganma.mjs
@@ -7,7 +7,7 @@ export default class Ganma extends Connector {
         super();
         super.id = 'ganma';
         super.label = 'GANMA!';
-        this.tags = [ 'manga', 'japanese' ];
+        this.tags = ['manga', 'japanese'];
         this.url = 'https://ganma.jp';
         this.requestOptions.headers.set('x-from', this.url);
     }
@@ -62,7 +62,7 @@ export default class Ganma extends Connector {
             .map((chapter, index) => {
                 return {
                     id: chapter.id,
-                    title: ((chapter.number || index + 1) + ': 【' + chapter.title + '】 ' + chapter.subtitle).trim()
+                    title: ((chapter.number || index + 1) + ': 【' + chapter.title + '】 ' + (chapter.subtitle || "")).trim()
                 };
             });
     }
@@ -71,7 +71,7 @@ export default class Ganma extends Connector {
         const uri = new URL('/api/1.0/magazines/web/' + chapter.manga.id, this.url);
         const request = new Request(uri, this.requestOptions);
         let data = await this.fetchJSON(request);
-        data = data.root.items.find(item => item.id === chapter.id ).page;
+        data = data.root.items.find(item => item.id === chapter.id).page;
         return data.files.map(image => this.getAbsolutePath(image + '?' + data.token, data.baseUrl));
     }
 }
