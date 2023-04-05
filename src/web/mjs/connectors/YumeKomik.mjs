15d24ade89d6b13df272b962112782f810710dcb
akn19 <akunhitam2019@gmail.com>
2023-04-05 16:14:18 +0700

    add queryMangaTitleRemove

--- src/web/mjs/connectors/YumeKomik.mjs
+++ src/web/mjs/connectors/YumeKomik.mjs
@@ -9,5 +9,17 @@ export default class YumeKomik extends WordPressMangastream {
         this.tags = ['manga', 'indonesian'];
         this.url = 'https://yumekomik.com';
         this.path = '/manga/list-mode/';
+        this.queryMangaTitleRemove = 'Bahasa Indonesia';
+    }
+
+    async _getMangas() {
+        let request = new Request(new URL(this.path, this.url), this.requestOptions);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title ? element.title.replace(this.queryMangaTitleRemove, '').trim() : element.text.replace(this.queryMangaTitleRemove, '').trim()
+            };
+        });
     }
 }
