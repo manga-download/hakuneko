0942aad8f792b7231884472ba2bdfe61cec49df2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-06 10:15:20 +0000

    CeriseScans : change URL & Fix getpages
    WordPress Manga Protector plugin. Fixes https://github.com/manga-download/hakuneko/issues/5643

--- src/web/mjs/connectors/CeriseScans.mjs
+++ src/web/mjs/connectors/CeriseScans.mjs
@@ -7,6 +7,20 @@ export default class CeriseScans extends WordPressMadara {
         super.id = 'cerisescans';
         super.label = 'Cerise Scans';
         this.tags = [ 'webtoon', 'portuguese', 'scanlation' ];
-        this.url = 'https://cerisescans.com';
+        this.url = 'https://cerisescan.com';
+    }
+    //WP MangaProtector
+    async _getPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const script = `
+            new Promise((resolve, reject) => {
+                var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
+                    format: CryptoJSAesJson
+                }).toString(CryptoJS.enc.Utf8));
+                resolve(JSON.parse(imgdata));
+            });
+        `;
+        return await Engine.Request.fetchUI(request, script);
     }
 }
