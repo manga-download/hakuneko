f6cd7f785f7d47f53953bbb8afa3913de21a6072
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-07 15:27:40 +0000

    CeriseScans & Sinenis Scan : change URL & Fix getpages (#5647)
    * CeriseScans : change URL & Fix getpages
    
    WordPress Manga Protector plugin. Fixes https://github.com/manga-download/hakuneko/issues/5643
    
    * CeriseScans: handling novels
    
    * SinensisScan : fixes
    
    * Change url
    * Changing template for MadaraNovel
    * Handle WpManga protector
    * Fixes https://github.com/manga-download/hakuneko/issues/5644

--- src/web/mjs/connectors/SinensisScan.mjs
+++ src/web/mjs/connectors/SinensisScan.mjs
@@ -1,12 +1,35 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressMadaraNovel from './templates/WordPressMadaraNovel.mjs';
 
-export default class SinensisScan extends WordPressMadara {
+export default class SinensisScan extends WordPressMadaraNovel {
 
     constructor() {
         super();
         super.id = 'sinensisscan';
-        super.label = 'Sinenis Scan';
+        super.label = 'SinenSis  Scan';
         this.tags = [ 'manga', 'webtoon', 'portuguese', 'scanlation' ];
-        this.url = 'https://sinensisscan.com';
+        this.url = 'https://sinensisscans.com';
     }
+
+    async _getPages(chapter) {
+        let uri = new URL(chapter.id, this.url);
+        uri.searchParams.set('style', 'list');
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, this.novelContentQuery);
+        return data.length > 0 ? this._getPagesNovel(request) : this._getWProtectedPages(chapter);
+    }
+
+    async _getWProtectedPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const script = `
+            new Promise((resolve, reject) => {
+                var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
+                    format: CryptoJSAesJson
+                }).toString(CryptoJS.enc.Utf8));
+                resolve(JSON.parse(imgdata));
+            });
+        `;
+        return await Engine.Request.fetchUI(request, script);
+    }
+
 }
