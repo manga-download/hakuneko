49ef8d6b8ee1e34cba6fc0f3fb9d2e8226a7a37b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-19 11:44:07 +0000

    Hentai3z url change (#5692)

--- src/web/mjs/connectors/Hentai3z.mjs
+++ src/web/mjs/connectors/Hentai3z.mjs
@@ -7,7 +7,7 @@ export default class Hentai3z extends WordPressMadara {
         super.id = 'hentai3z';
         super.label = 'Hentai3z';
         this.tags = [ 'manga', 'webtoon', 'hentai', 'english' ];
-        this.url = 'https://hentai3z.com';
+        this.url = 'https://hentai3z.xyz';
 
         this.requestOptions.headers.set('x-referer', this.url);
     }
