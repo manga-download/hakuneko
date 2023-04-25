dc73bdbe8bdb01c2b2ac09cff3ebfbb802904539
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-25 08:26:21 +0000

    MangaOkuTR : fix downloading pictures (#5720)
    Fixes https://github.com/manga-download/hakuneko/issues/5718

--- src/web/mjs/connectors/MangaOkuTr.mjs
+++ src/web/mjs/connectors/MangaOkuTr.mjs
@@ -10,4 +10,20 @@ export default class MangaOkuTr extends WordPressMangastream {
         this.url = 'https://mangaokutr.com';
         this.path = '/manga/list-mode/';
     }
+
+    async _getPages(chapter) {
+        let pages = await super._getPages(chapter);
+        return pages.map(page => this.createConnectorURI(page));
+    }
+
+    async _handleConnectorURI(payload) {
+        let request = new Request(payload, this.requestOptions);
+        request.headers.set('x-referer', this.url);
+        const response = await fetch(request);
+        let data = await response.blob();
+        data = await this._blobToBuffer(data);
+        this._applyRealMime(data);
+        return data;
+    }
+
 }
