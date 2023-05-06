f6cfbe09dca99518cced363dd38cad6c96e5b746
Mustofa <45929526+MIRVOUS@users.noreply.github.com>
2023-05-06 17:28:07 +0700

    SheaManga : Correct Domain
    Changes Url

--- src/web/mjs/connectors/SheaManga.mjs
+++ src/web/mjs/connectors/SheaManga.mjs
@@ -7,7 +7,7 @@ export default class SheaManga extends WordPressMangastream {
         super.id = 'sheamanga';
         super.label = 'Shea Manga';
         this.tags = [ 'webtoon', 'indonesian' ];
-        this.url = 'https://sheamanga.my.id';
+        this.url = 'http://62.171.176.78/';
         this.path = '/manga/list-mode/';
     }
 }
