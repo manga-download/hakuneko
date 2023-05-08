cfb453f6cd606e75a0b4378d0194a5be7e2fd7bc
akn19 <akunhitam2019@gmail.com>
2023-05-08 11:30:01 +0700

    add Noromax

---
+++ src/web/mjs/connectors/Noromax.mjs
@@ -0,0 +1,13 @@
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+
+export default class Noromax extends WordPressMangastream {
+
+    constructor() {
+        super();
+        super.id = 'noromax';
+        super.label = 'Noromax';
+        this.tags = ['manga', 'indonesian'];
+        this.url = 'https://noromax.my.id';
+        this.path = '/Komik/list-mode/';
+    }
+}
