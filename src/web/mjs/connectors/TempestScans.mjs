b586725306b68d26012e6a4bccd9163996256ea9
akn19 <86169232+akn19@users.noreply.github.com>
2023-05-09 14:03:49 +0700

    add tempestscans (#5774)

---
+++ src/web/mjs/connectors/TempestScans.mjs
@@ -0,0 +1,13 @@
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+
+export default class TempestScans extends WordPressMangastream {
+
+    constructor() {
+        super();
+        super.id = 'tempestscans';
+        super.label = 'Tempest Scans';
+        this.tags = ['manga', 'turkish'];
+        this.url = 'https://tempestscans.com';
+        this.path = '/manga/list-mode/';
+    }
+}
