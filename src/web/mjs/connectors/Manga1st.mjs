1a499efee6e5cdfdbb8d67ca02cd59a5ae9e7eb2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-05-27 12:38:21 +0000

    Tiny websites fixes (#5831)
    * Tiny websites fixes
    
    Fixes https://github.com/manga-download/hakuneko/issues/5824
    Fixes https://github.com/manga-download/hakuneko/issues/5827
    Fixes https://github.com/manga-download/hakuneko/issues/5830
    Fixes https://github.com/manga-download/hakuneko/issues/5829
    
    * Update Hentai3z
    
    Fix manga list

--- src/web/mjs/connectors/Manga1st.mjs
+++ src/web/mjs/connectors/Manga1st.mjs
@@ -3,8 +3,12 @@ export default class Manga1st extends WordPressMadara {
     constructor() {
         super();
         super.id = 'manga1st';
-        super.label = 'Manga1st';
+        super.label = 'MangaVisa';
         this.tags = [ 'manga', 'webtoon', 'english' ];
-        this.url = 'https://manga1st.com';
+        this.url = 'https://mangavisa.com';
+    }
+
+    _createMangaRequest(page) {
+        return new Request(new URL(`/manga/page/${page}/`, this.url), this.requestOptions);
     }
 }
