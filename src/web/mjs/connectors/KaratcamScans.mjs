33706c4868c26e1ce07c186b46483d7ec428b3e2
Tristan <tristan@tnet.moe>
2023-05-29 19:12:16 +0200

    KaratcamScans: fix admin-ajax bad request

--- src/web/mjs/connectors/KaratcamScans.mjs
+++ src/web/mjs/connectors/KaratcamScans.mjs
@@ -9,4 +9,22 @@ export default class KaratcamScans extends WordPressMadara {
         this.tags = [ 'manga', 'french' ];
         this.url = 'https://karatcam-scans.fr';
     }
+
+    async _getChaptersAjaxOld(dataID) {
+        let uri = new URL(this.path + '/wp-admin/admin-ajax.php', this.url);
+        let request = new Request(uri, {
+            method: 'POST',
+            body: 'action=manga_views&manga=' + dataID,
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'x-referer': this.url
+            }
+        });
+        const data = await this.fetchDOM(request, this.queryChapters);
+        if (data.length) {
+            return data;
+        } else {
+            throw new Error('No chapters found (old ajax endpoint)!');
+        }
+    }
 }
