53e8ebebf9f802e6f8bfcb57bdaf879be26619e9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-05-30 13:22:40 +0000

    change for a scripted solution

--- src/web/mjs/connectors/MangaSwat.mjs
+++ src/web/mjs/connectors/MangaSwat.mjs
@@ -15,11 +15,19 @@ export default class MangaSwat extends WordPressMangastream {
     }
 
     async _getPages(chapter) {
+        const script = `
+           new Promise((resolve, reject) => {
+                let t = new RocketLazyLoadScripts;
+                t._loadEverythingNow();
+                setTimeout(() => {
+                    resolve(ts_reader_control.getImages());
+                }, 2500);
+               });
+        `;
         const uri = new URL(chapter.id, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const regex = /ts_reader\.run\(([^)]+)/g;
-        let data = await this.fetchRegex(request, regex);
-        data = JSON.parse(data[0]);
-        return data.sources.shift().images.map(link => this.getAbsolutePath(link, request.url).replace(/\/i\d+\.wp\.com/, '')).filter(link => !link.includes('histats.com'));
+        let request = new Request(uri, this.requestOptions);
+        let data = await Engine.Request.fetchUI(request, script);
+        // HACK: bypass 'i0.wp.com' image CDN to ensure original images are loaded directly from host
+        return data.map(link => this.getAbsolutePath(link, request.url).replace(/\/i\d+\.wp\.com/, '')).filter(link => !link.includes('histats.com'));
     }
 }
