a2baff061aa21bfd8529b1223c8e7ccf9d5ebfc1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-05-30 12:14:23 +0000

    MangaSwat: domain and _getPages
    Cant return ts_reader params? Fetch the JSON directly  🤷 
    
    Fixes https://github.com/manga-download/hakuneko/issues/5855

--- src/web/mjs/connectors/MangaSwat.mjs
+++ src/web/mjs/connectors/MangaSwat.mjs
@@ -7,10 +7,19 @@ export default class MangaSwat extends WordPressMangastream {
         super.id = 'mangaswat';
         super.label = 'SWAT Manga';
         this.tags = ['webtoon', 'arabic'];
-        this.url = 'https://swatmanga.me';
-        this.path = '/manga/?list';
+        this.url = 'https://swatmanga.net';
+        this.path = '/manga/list-mode';
 
         this.queryChapters = 'div.bxcl ul li span.lchx a';
         this.queryChaptersTitle = undefined;
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const regex = /ts_reader\.run\(([^)]+)/g;
+        let data = await this.fetchRegex(request, regex);
+        data = JSON.parse(data[0]);
+        return data.sources.shift().images.map(link => this.getAbsolutePath(link, request.url).replace(/\/i\d+\.wp\.com/, '')).filter(link => !link.includes('histats.com'));
     }
 }
