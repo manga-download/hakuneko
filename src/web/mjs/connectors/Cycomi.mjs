0d5b6b2a5fa4ed5f1c9653a40b690f112da2717d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-11 09:36:54 +0000

    recoded according to Haruneko

--- src/web/mjs/connectors/Cycomi.mjs
+++ src/web/mjs/connectors/Cycomi.mjs
@@ -8,123 +8,143 @@ export default class CyComi extends Connector {
         super.id = 'cycomi';
         super.label = 'CyComi';
         this.tags = ['manga', 'japanese'];
-        this.url = 'https://web.cycomi.com';
+        this.url = 'https://cycomi.com';
+        this.apiUrl = 'https://web.cycomi.com/api';
+    }
+
+    canHandleURI(uri) {
+        return /https?:\/\/cycomi\.com\/title\/\d+$/.test(uri.origin);
     }
 
     async _getMangaFromURI(uri) {
-        const request = new Request(uri, this.requestOptions);
-        const id = uri.pathname.split("/").pop();
-        const title = (await this.fetchDOM(request, 'h2.css-m40re5'))[0].textContent.trim();
-        return new Manga(this, id, title);
+        const id = new URL(uri).pathname.split('/').pop();
+        const request = new Request(`${this.apiUrl}/title/detail?titleId=${id}`);
+        const { data } = await this.fetchJSON(request);
+        return new Manga(this, data.titleId.toString(), data.titleName.trim());
     }
 
     async _getMangas() {
-        const uri = new URL('/api/title/serialization/list', this.url);
-        const request = new Request(uri, this.requestOptions);
-        const response = await this.fetchJSON(request, 'div.card-content > a.card');
         const mangaList = [];
-        for(let page = 0; page < 8; page += 1) {
-            const mangas = response.data.serialization[page].titles.map(title => {
-                return {
-                    id: title.titleId,
-                    title: title.titleName
-                };
-            });
-            mangaList.push(...mangas);
+        for(let page = 0, run = true; run; page++) {
+            const request = new Request(`${this.apiUrl}/home/paginatedList?limit=${50}&page=${page}`, this.requestOptions);
+            const { data } = await this.fetchJSON(request);
+            const mangas = data.reduce((accumulator, entry) => {
+                if(entry) {
+                    const titles = entry.titles.map(manga => new Manga(this, manga.titleId.toString(), manga.titleName));
+                    accumulator.push(...titles);
+                }
+                return accumulator;
+            }, []);
+            mangas.length ? mangaList.push(...mangas) : run = false;
+
         }
         return mangaList;
     }
 
     async _getChapters(manga) {
-        let cursor = undefined;
-        const chapterList = [];
-        do {
-            const [chapters, nextCursor] = await this._getChaptersFromCursor(manga, cursor);
-            chapterList.push(...chapters);
-            cursor = nextCursor;
-        } while (cursor);
-        return chapterList;
+        return [
+            ... await this._fetchMangaVolumes(manga),
+            ... await this._fetchMangaChapters(manga),
+        ];
     }
 
-    async _getChaptersFromCursor(manga, cursor) {
-        const params = new URLSearchParams({
-            limit: 40,
-            titleId: manga.id,
-            sort: 2,
+    async _fetchMangaVolumes(manga) {
+        const request = new Request(`${this.apiUrl}/singleBook/list?titleId=${manga.id}`, this.requestOptions);
+        const { data, resultCode } = await this.fetchJSON(request);
+        return resultCode !== 1 || !data.singleBooks ? [] : data.singleBooks.map(volume => {
+            const title = [ volume.name, volume.stories ].filter(item => item).join(' - ');
+            return {id : '/singleBook/detail?singleBookId=' + volume.id, title };
         });
-        if (cursor) {
-            params.append("cursor", cursor);
-        }
-        const uri = new URL("/api/chapter/paginatedList?" + params.toString(), this.url);
-        const request = new Request(uri, this.requestOptions);
-        const response = await this.fetchJSON(request);
-        const chapters = response.data.map(element => {
-            return {
-                id: {"chapterId": element.id, "titleId": manga.id},
-                title: element.name + " " + element.subName,
-                language: '',
-            };
-        });
-        return [chapters, response.nextCursor];
     }
 
-    decrypt(e, t) {
-        let n = (e=>{
-                let t = new Uint8Array(256);
-                t.forEach((e, n) =>{
-                    t[n] = n;
-                });
-                let n = 0;
-                return t.forEach((i, r) =>{
-                    n = (n + t[r] + e.charCodeAt(r % e.length)) % 256;
-                    let l = t[r];
-                    t[r] = t[n],
-                    t[n] = l;
-                }),
-                t;
-            }) (t),
-            i = 0,
-            r = 0,
-            l = new Uint8Array(e.length);
-        for (let t = 0, a = e.length; t < a; t++) {
-            r = (r + n[i = (i + 1) % 256]) % 256;
-            let a = n[i % 256];
-            n[i % 256] = n[r],
-            n[r] = a;
-            let o = n[(n[i] + n[r]) % 256];
-            l[t] = o ^ e[t];
-        }
-        return l;
+    async _fetchMangaChapters(manga) {
+        const request = new Request(`${this.apiUrl}/chapter/list?titleId=${manga.id}`, this.requestOptions);
+        const { data, resultCode } = await this.fetchJSON(request);
+        return resultCode !== 1 || !data.chapters ? [] : data.chapters.map(chapter => {
+            const title = [ chapter.name, chapter.subName ].filter(item => item).join(' - ');
+            return {id : `/chapter/page/list?titleId=${manga.id}&chapterId=${chapter.id}`, title};
+        });
     }
 
     async _getPages(chapter) {
-        const uri = new URL("/api/chapter/page/list", this.url);
-        const options = {
-            ...this.requestOptions,
+        if(chapter.id.startsWith('/singleBook/')) {
+            return this._fetchVolumePages(chapter);
+        }
+        if(chapter.id.startsWith('/chapter/')) {
+            return this._fetchChapterPages(chapter);
+        }
+        return [];
+    }
+
+    async _fetchVolumePages(volume) {
+        const request = new Request(this.apiUrl + volume.id, this.requestOptions);
+        const { data: { id, chapters } } = await this.fetchJSON(request);
+        return !chapters ? [] : chapters.reduce(async (accumulator, chapter) => {
+            const url = `${this.apiUrl}/singleBook/page/list?singleBookId=${id}&chapterId=${chapter.id}`;
+            const { data: { pages }, resultCode } = await this.fetchJSON(new Request(url), this.requestOptions);
+            return resultCode !== 1 ? accumulator : (await accumulator).concat(this._mapPages(volume, pages));
+        });
+    }
+
+    async _fetchChapterPages(chapter) {
+        const uri = new URL(this.apiUrl + chapter.id);
+        const request = new Request(uri.origin + uri.pathname, {
             method: 'POST',
-            body: JSON.stringify(chapter.id),
-            headers: {...this.requestOptions.headers, "Content-Type": "application/json"},
-        };
-        const request = new Request(uri, options);
-        const response = await this.fetchJSON(request);
-        let pages = response.data.pages.filter(page => page.type == "image");
-        const r = new RegExp(/\/([0-9a-zA-Z]{32})\//);
-        pages = pages.filter(page=> r.test(page.image));
-        return pages.map(page => this.createConnectorURI(page.image));
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                titleId: parseInt(uri.searchParams.get('titleId')),
+                chapterId: parseInt(uri.searchParams.get('chapterId'))
+            })
+        });
+        const { data: { pages }, resultCode } = await this.fetchJSON(request);
+        return resultCode !== 1 ? [] : this._mapPages(chapter, pages);
+    }
+
+    _mapPages(container, pages) {
+        return pages.filter(page => page.type === 'image')
+            .sort((self, other) => self.pageNumber - other.pageNumber)
+            .map(page => this.createConnectorURI(page.image));
     }
 
     async _handleConnectorURI(payload) {
-        const request = new Request(payload, this.requestOptions);
+        const uri = new URL(payload);
+        const request = new Request(uri, this.requestOptions);
         const response = await fetch(request);
-        const data = await response.arrayBuffer();
-        const enc = new Uint8Array(data);
-        const key = payload.match(/\/([0-9a-zA-Z]{32})\//)[1];
-        const buffer = {
-            mimeType : response.headers.get('content-type'),
-            data : this.decrypt(enc, key)
+        const buffer = await response.arrayBuffer();
+        const passphrase = new URL(payload).pathname.split('/').filter(part => /^[0-9a-zA-Z]{32}$/.test(part)).shift();
+        const data = {
+            mimeType: response.headers.get('content-type'),
+            data: this.decrypt(new Uint8Array(buffer), passphrase)
         };
-        this._applyRealMime(buffer);
-        return buffer;
+        this._applyRealMime(data);
+        return data;
     }
 
+    _makeKey(passphrase) {
+        const key = new Uint8Array(Array(256).keys());
+        for(let index = 0, indexSwap = 0; index < key.length; index++) {
+            indexSwap = (indexSwap + key[index] + passphrase.charCodeAt(index % passphrase.length)) % 256;
+            const temp = key[index];
+            key[index] = key[indexSwap];
+            key[indexSwap] = temp;
+        }
+        return key;
+    }
+
+    decrypt(encrypted, passphrase) {
+        const key = this._makeKey(passphrase);
+        const decrypted = new Uint8Array(encrypted.length);
+        for (let index = 0, indexKeySwapSource = 0, indexKeySwapTarget = 0; index < encrypted.length; index++) {
+            indexKeySwapSource = (indexKeySwapSource + 1) % 256;
+            indexKeySwapTarget = (indexKeySwapTarget + key[indexKeySwapSource]) % 256;
+            const temp = key[indexKeySwapSource % 256];
+            key[indexKeySwapSource % 256] = key[indexKeySwapTarget],
+            key[indexKeySwapTarget] = temp;
+            const xor = key[(key[indexKeySwapSource] + key[indexKeySwapTarget]) % 256];
+            decrypted[index] = encrypted[index] ^ xor;
+        }
+        return decrypted;
+    }
 }
