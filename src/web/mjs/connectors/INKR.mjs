c30430f5bdf8cb5031b8563799885b087526003d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-15 14:10:28 +0000

    INKR : finally it works

--- src/web/mjs/connectors/INKR.mjs
+++ src/web/mjs/connectors/INKR.mjs
@@ -1,5 +1,6 @@
 import Connector from '../engine/Connector.mjs';
 import Manga from '../engine/Manga.mjs';
+
 export default class INKR extends Connector {
     constructor() {
         super();
@@ -7,14 +8,21 @@ export default class INKR extends Connector {
         super.label = 'INKR';
         this.tags = [ 'manga', 'english' ];
         this.url = 'https://comics.inkr.com';
+        this.nextInstance = 'b023df32bc9708e85db0b51983a2e76fce7be924';
+
     }
+
+    async _initializeConnector() {
+        const uri = new URL(this.url);
+        const request = new Request(uri.href, this.requestOptions);
+        this.nextInstance = await Engine.Request.fetchUI(request, `__NEXT_DATA__.buildId`);
+    }
+
     async _getMangaFromURI(uri) {
-        const id = uri.pathname;
+        const id = uri.pathname.match(/\/title\/([^/]+)/)[1];
         const request = new Request(uri, this.requestOptions);
-        let data = await this._getNextData(request);
-        data = LZString.decompressBase64(data.props.pageProps._c);
-        
-        const title = data.props.pageProps.initialState.productHome.productHome.product.title;
+        const data =await this.fetchDOM(request, 'meta[property="og:image:alt"]');
+        const title = data[0].content.trim();
         return new Manga(this, id, title);
     }
     async _getMangas() {
@@ -22,64 +30,274 @@ export default class INKR extends Connector {
         throw new Error(msg);
     }
     async _getChapters(manga) {
-        const uri = new URL(manga.id, this.url);
+        //https://comics.inkr.com/_next/data/b023df32bc9708e85db0b51983a2e76fce7be924/title/1227-a-sign-of-affection/chapters.json
+        const uri = new URL(`/_next/data/${this.nextInstance}/title/${manga.id}/chapters.json`, this.url);
         const request = new Request(uri, this.requestOptions);
-        const nextData = await this._getNextData(request);
-        const episodes = nextData.props.pageProps.initialState.productHome.productHome.episode_list;
-        const productId = manga.id.split('/').pop();
-        return episodes.map(ep => {
-            return {
-                id: `${
-                nextData.buildId}
-                /fr/viewer/${
-                productId}
-                /${
-                ep.id}
-                `,
-                title: ep.title,
-            };
-        })
-        .reverse();
+        let data = await this.fetchJSON(request);
+        data = LZString.decompressFromBase64(data.pageProps._c);
+        data = JSON.parse(data);
+        const obj = unpack(data[0], data[1]);
+
+        const chaptersIndexes = obj.icd.find(element => element[0].startsWith('ik-title'))[1].chapterList;
+        const chapters = obj.icd.filter(element => chaptersIndexes.includes(element[0]));
+
+        return chapters
+            .sort(function(a, b) {
+                return a[1].order - b[1].order;
+            })
+            .map(chapter => {
+                return{
+                    id: chapter[1].oid.match(/\d+/),
+                    title : chapter[1].name.trim()
+                };
+            });
+
     }
     async _getPages(chapter) {
-        const result = await this._fetchChapterNextData(chapter);
-        const pdata = result.pageProps.initialState.viewer.pData;
-        const images = pdata.img;
-        if (images == null) {
-            throw new Error(`The chapter '${chapter.title}' is neither public, nor purchased!`);
-        }
-        return images
-        .filter(img => !!img.path)
-        .map(img => {
-            return this.createConnectorURI({
-                url: img.path,
-                //key: this._getSeed(img.path),
-                pdata
-            });
-        });
+        const url = new URL(`/title/${chapter.manga.id}/chapter/${chapter.id}`, this.url);
+        let data = await this._getNextData(url);
+
+        data = LZString.decompressFromBase64(data.props.pageProps._c);
+        data = JSON.parse(data);
+        const obj = unpack(data[0], data[1]);
+
+        const ikchapter = 'ik-chapter-'+ chapter.id;
+        const chap = obj.icd.find(element=> element[0] == ikchapter);
+        return chap[1].chapterPages.map(page => page.url+'/p.jpg');
     }
-    
+
     /************
     * NEXT
-    *************/    
+    *************/
     async _getNextData(request) {
         const [data] = await this.fetchDOM(request, '#__NEXT_DATA__');
         return JSON.parse(data.textContent);
     }
-    async _fetchChapterNextData(chapter) {
-        const parts = chapter.id.split('/');
-        const productId = parts[3];
-        const episodeId = parts[4];
-        const uri = new URL(`_next/data/${chapter.id}.json`, this.url);
-        const request = new Request(uri, this.requestOptions);
-        try {
-            return await this.fetchJSON(request);
-        }
-        catch (error) {
-            console.error(error);
-            throw new Error(`The chapter '${chapter.title}' is neither public, nor purchased!`);
-        }
-    }
-
 
 }
+
+const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+let magicalMap = {};
+for (let h = 0; h < charset.length; h++) {
+    const t = charset[h];
+    magicalMap[t] = h;
+}
+
+const mapsize = charset.length;
+
+var decodeNum = function (t) {
+    return t = t.replace('n|', ''),
+    s_to_num(t);
+};
+
+var decodeKey= function(t) {
+    return 'number' === typeof t ? t : s_to_int(t);
+};
+
+var decodeBool= function(t) {
+    switch (t) {
+        case 'b|T':
+            return !0;
+        case 'b|F':
+            return !1;
+    }
+    return !!t;
+};
+var decodeStr= function(t) {
+    return 's|' === t[0] + t[1] ? t.substr(2) : t;
+};
+
+var d= function(t) {
+    return ':' === t[0] ? s(t.substring(1)).toString() : s_to_int(t).toString();
+};
+
+var s= function(t) {
+    /*
+        let e = BigInt(0);
+        let n = BigInt(1);
+        const i = BigInt(r);
+        for (let r = t.length - 1; r >= 0; r--) {
+            const s = t[r];
+            let a = BigInt(o[s]);
+            a *= n,
+            e += a,
+            n *= i;
+        }
+        return e;*/
+};
+
+var s_to_int= function(t) {
+    let e = 0,
+        n = 1;
+    for (let i = t.length - 1; i >= 0; i--) {
+        const s = t[i];
+        let a = magicalMap[s];
+        a *= n;
+        e += a;
+        n *= mapsize;
+    }
+    return e;
+};
+
+var s_to_num= function(e) {
+    if ('-' === e[0]) return - s_to_num(e.substr(1));
+    let[n,
+        r,
+        o] = e.split('.');
+    if (!r) return s_to_int(n);
+    n = d(n),
+    r = d(r),
+    r = u(r);
+    let s = n + '.' + r;
+    if (o) {
+        s += 'e';
+        let t = !1;
+        '-' === o[0] &&
+          (t = !0, o = o.slice(1)),
+        o = d(o),
+        o = u(o),
+        s += t ? - o : + o;
+    }
+    return + s;
+};
+
+var u= function(t) {
+    return t.split('').reverse().join('');
+};
+
+var unpack = function (t, e) {
+    if ('' === e || '_' === e) return null;
+    const n = t[decodeKey(e)];
+    if (null === n) return n;
+    switch (typeof n) {
+        case 'undefined':
+        case 'number':
+            return n;
+        case 'string':
+            switch (n[0] + n[1]) {
+                case 'b|':
+                    return decodeBool(n);
+                case 'o|':
+                    return function (t, e) {
+                        if ('o|' === e) return {
+                        };
+                        const n = {},
+                            r = e.split('|');
+                        let o = unpack(t, r[1]);
+                        const i = r.length;
+                        i - 2 !== 1 ||
+                  Array.isArray(o) ||
+                  (o = [
+                      o
+                  ]);
+                        for (let a = 2; a < i; a++) {
+                            const e = o[a - 2];
+                            let i = r[a];
+                            i = unpack(t, i),
+                            n[e] = i;
+                        }
+                        return n;
+                    }(t, n);
+                case 'n|':
+                    return decodeNum(n);
+                case 'a|':
+                    return function (t, e) {
+                        if ('a|' === e) return [];
+                        const n = e.split('|'),
+                            r = n.length - 1,
+                            o = new Array(r);
+                        for (let i = 0; i < r; i++) {
+                            let e = n[i + 1];
+                            e = unpack(t, e),
+                            o[i] = e;
+                        }
+                        return o;
+                    }(t, n);
+                default:
+                    return decodeStr(n);
+            }
+    }
+    return null;
+};
+
+var LZString=function() {
+    function o(o, r) {
+        if(!t[o]) {
+            t[o]={}; for(var n=0; n<o.length; n++)t[o][o.charAt(n)]=n;
+        }return t[o][r];
+    }var r=String.fromCharCode, n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$", t={}, i={compressToBase64:function(o) {
+        if(null==o)return""; var r=i._compress(o, 6, function(o) {
+            return n.charAt(o);
+        }); switch(r.length%4) {
+            default:case 0:return r; case 1:return r+"==="; case 2:return r+"=="; case 3:return r+"=";
+        }
+    }, decompressFromBase64:function(r) {
+        return null==r?"":""==r?null:i._decompress(r.length, 32, function(e) {
+            return o(n, r.charAt(e));
+        });
+    }, compressToUTF16:function(o) {
+        return null==o?"":i._compress(o, 15, function(o) {
+            return r(o+32);
+        })+" ";
+    }, decompressFromUTF16:function(o) {
+        return null==o?"":""==o?null:i._decompress(o.length, 16384, function(r) {
+            return o.charCodeAt(r)-32;
+        });
+    }, compressToUint8Array:function(o) {
+        for(var r=i.compress(o), n=new Uint8Array(2*r.length), e=0, t=r.length; t>e; e++) {
+            var s=r.charCodeAt(e); n[2*e]=s>>>8, n[2*e+1]=s%256;
+        }return n;
+    }, decompressFromUint8Array:function(o) {
+        if(null===o||void 0===o)return i.decompress(o); for(var n=new Array(o.length/2), e=0, t=n.length; t>e; e++)n[e]=256*o[2*e]+o[2*e+1]; var s=[]; return n.forEach(function(o) {
+            s.push(r(o));
+        }), i.decompress(s.join(""));
+    }, compressToEncodedURIComponent:function(o) {
+        return null==o?"":i._compress(o, 6, function(o) {
+            return e.charAt(o);
+        });
+    }, decompressFromEncodedURIComponent:function(r) {
+        return null==r?"":""==r?null:(r=r.replace(/ /g, "+"), i._decompress(r.length, 32, function(n) {
+            return o(e, r.charAt(n));
+        }));
+    }, compress:function(o) {
+        return i._compress(o, 16, function(o) {
+            return r(o);
+        });
+    }, _compress:function(o, r, n) {
+        if(null==o)return""; var e, t, i, s={}, p={}, u="", c="", a="", l=2, f=3, h=2, d=[], m=0, v=0; for(i=0; i<o.length; i+=1)if(u=o.charAt(i), Object.prototype.hasOwnProperty.call(s, u)||(s[u]=f++, p[u]=!0), c=a+u, Object.prototype.hasOwnProperty.call(s, c))a=c; else{
+            if(Object.prototype.hasOwnProperty.call(p, a)) {
+                if(a.charCodeAt(0)<256) {
+                    for(e=0; h>e; e++)m<<=1, v==r-1?(v=0, d.push(n(m)), m=0):v++; for(t=a.charCodeAt(0), e=0; 8>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1;
+                }else{
+                    for(t=1, e=0; h>e; e++)m=m<<1|t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t=0; for(t=a.charCodeAt(0), e=0; 16>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1;
+                }l--, 0==l&&(l=Math.pow(2, h), h++), delete p[a];
+            }else for(t=s[a], e=0; h>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1; l--, 0==l&&(l=Math.pow(2, h), h++), s[c]=f++, a=String(u);
+        }if(""!==a) {
+            if(Object.prototype.hasOwnProperty.call(p, a)) {
+                if(a.charCodeAt(0)<256) {
+                    for(e=0; h>e; e++)m<<=1, v==r-1?(v=0, d.push(n(m)), m=0):v++; for(t=a.charCodeAt(0), e=0; 8>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1;
+                }else{
+                    for(t=1, e=0; h>e; e++)m=m<<1|t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t=0; for(t=a.charCodeAt(0), e=0; 16>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1;
+                }l--, 0==l&&(l=Math.pow(2, h), h++), delete p[a];
+            }else for(t=s[a], e=0; h>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1; l--, 0==l&&(l=Math.pow(2, h), h++);
+        }for(t=2, e=0; h>e; e++)m=m<<1|1&t, v==r-1?(v=0, d.push(n(m)), m=0):v++, t>>=1; for(;;) {
+            if(m<<=1, v==r-1) {
+                d.push(n(m)); break;
+            }v++;
+        }return d.join("");
+    }, decompress:function(o) {
+        return null==o?"":""==o?null:i._decompress(o.length, 32768, function(r) {
+            return o.charCodeAt(r);
+        });
+    }, _decompress:function(o, n, e) {
+        var t, i, s, p, u, c, a, l, f=[], h=4, d=4, m=3, v="", w=[], A={val:e(0), position:n, index:1}; for(i=0; 3>i; i+=1)f[i]=i; for(p=0, c=Math.pow(2, 2), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; switch(t=p) {
+            case 0:for(p=0, c=Math.pow(2, 8), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; l=r(p); break; case 1:for(p=0, c=Math.pow(2, 16), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; l=r(p); break; case 2:return"";
+        }for(f[3]=l, s=l, w.push(l); ;) {
+            if(A.index>o)return""; for(p=0, c=Math.pow(2, m), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; switch(l=p) {
+                case 0:for(p=0, c=Math.pow(2, 8), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; f[d++]=r(p), l=d-1, h--; break; case 1:for(p=0, c=Math.pow(2, 16), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; f[d++]=r(p), l=d-1, h--; break; case 2:return w.join("");
+            }if(0==h&&(h=Math.pow(2, m), m++), f[l])v=f[l]; else{
+                if(l!==d)return null; v=s+s.charAt(0);
+            }w.push(v), f[d++]=s+v.charAt(0), h--, s=v, 0==h&&(h=Math.pow(2, m), m++);
+        }
+    }}; return i;
+}();
