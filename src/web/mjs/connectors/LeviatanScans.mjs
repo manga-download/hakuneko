36410e151301914ab732f807774e4a98fa19f86e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-16 09:12:23 +0000

    LeviatanScans : add WpMangaProtector bypass (#5896)

--- src/web/mjs/connectors/LeviatanScans.mjs
+++ src/web/mjs/connectors/LeviatanScans.mjs
@@ -16,6 +16,24 @@ export default class LeviatanScans extends WordPressMadara {
         return new Request(new URL(`/ranking/page/${page}/`, this.url), this.requestOptions);
     }
 
+    async _getPages(chapter) {
+        return await this._getWProtectedPages(chapter);
+    }
+
+    async _getWProtectedPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const script = `
+            new Promise((resolve, reject) => {
+                var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
+                    format: CryptoJSAesJson
+                }).toString(CryptoJS.enc.Utf8));
+                resolve(JSON.parse(imgdata));
+            });
+        `;
+        return await Engine.Request.fetchUI(request, script);
+    }
+
     async _handleConnectorURI(payload) {
         let request = new Request(payload.url, this.requestOptions);
         request.headers.set('x-referer', payload.referer);
