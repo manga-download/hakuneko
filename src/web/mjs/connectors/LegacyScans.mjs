6eb24202de79e1e287bc3f8524e9cbbcd0fca508
Tristan <tristan@tnet.moe>
2023-06-24 05:47:23 +0200

    LegacyScans: change to Mangastream
    They temporarily rebranded to flamescans.fr but are now back with flamescans mangastream theme.

--- src/web/mjs/connectors/LegacyScans.mjs
+++ src/web/mjs/connectors/LegacyScans.mjs
@@ -1,6 +1,6 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class LegacyScans extends WordPressMadara {
+export default class LegacyScans extends WordPressMangastream {
 
     constructor() {
         super();
@@ -8,5 +8,9 @@ export default class LegacyScans extends WordPressMadara {
         super.label = 'Legacy-Scans';
         this.tags = ['webtoon', 'french'];
         this.url = 'https://legacy-scans.com';
+        this.path = '/manga/list-mode/';
+
+        this.queryMangas = 'div.postbody div.soralist ul li a.series';
+        this.queryChapters = 'div#chapterlist ul li a';
     }
 }
