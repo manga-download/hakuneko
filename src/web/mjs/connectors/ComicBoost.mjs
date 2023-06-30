1734f8fac6a08e08635ae93fe52b1ad706a07bc1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-30 08:48:01 +0000

    ComicBoost : First attempt at new Publus support
    * configuration_pack.json is encrypted
    * filename has to be generated
    * pictures are still scrambled, but block generation method is not the old one

--- src/web/mjs/connectors/ComicBoost.mjs
+++ src/web/mjs/connectors/ComicBoost.mjs
@@ -13,32 +13,206 @@ export default class ComicBoost extends Publus {
 
     async _getMangaFromURI(uri) {
         let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'article#book div.detail h1.name');
+        let data = await this.fetchDOM(request, 'h1.comic-title');
         let id = uri.pathname + uri.search;
         let title = data[0].textContent.trim();
         return new Manga(this, id, title);
     }
 
     async _getMangas() {
-        let request = new Request(new URL('/series?per=1000', this.url), this.requestOptions );
-        let data = await this.fetchDOM(request, 'div.box ul li div.info p.name a');
+        let mangaList = [];
+        const uri = new URL('/genre/', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'ul.pagination-list.right li.to-last a');
+        const pageCount = parseInt(data[0].href.match(/(\d+)$/)[1]);
+        for(let page = 1; page <= pageCount; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/genre/?p=' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.book-list-item-inner');
         return data.map(element => {
             return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.text.trim()
+                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('a.book-list-item-thum-wrapper'), this.url),
+                title: element.querySelector('source.thum').getAttribute('title').trim()
             };
         });
     }
 
     async _getChapters(manga) {
         let request = new Request(new URL(manga.id, this.url), this.requestOptions );
-        let data = await this.fetchDOM(request, 'section#productList ul.list_item--series li div.product_item ul.box_button li a.button');
+        let data = await this.fetchDOM(request, 'a.book-product-list-item');
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.closest('div.product_item').querySelector('p.name').textContent.trim(),
+                title: element.dataset.title.trim(),
                 language: ''
             };
         });
     }
+
+    async _getPages(chapter) {
+
+        //FIRST : get real viewer location
+        let uri = new URL( chapter.id, this.url );
+        let request = new Request( uri.href, this.requestOptions );
+        uri = await Engine.Request.fetchUI(request, `window.location.href`, 5000);
+
+        const script = `
+            new Promise((resolve) => {
+            	  const starttime = Date.now();
+                setTimeout(async () => {
+            
+                    const a2f = NFBR.a2F ? new NFBR.a2F() : new NFBR.a2f();
+                    const params = new URL(window.location).searchParams;
+                    const parameters = await a2f.a5W({
+                        contentId: params.get(NFBR.a5q.Key.CONTENT_ID), // Content ID => 'cid'
+                        a6m: params.get(NFBR.a5q.Key.LICENSE_TOKEN), // License Token => 'lit'
+                        preview: params.get(NFBR.a5q.Key.LOOK_INSIDE) === '1', // Look Inside => 'lin'
+                        contentType: params.get(NFBR.a5q.Key.CONTENT_TYPE) || 1, // Content Type => 'cty'
+                        title: params.get(NFBR.a5q.Key.CONTENT_TITLE), // Content Title => 'cti'
+                        winWidth: 3840,
+                        winHeight: 2160
+                    });
+            
+                    const model = new NFBR.a6G.Model({
+                        'settings': new self.NFBR.Settings('NFBR.SettingData'),
+                        'viewerFontSize': self.NFBR.a0X.a3k,
+                        'viewerFontFace': self.NFBR.a0X.a3k,
+                        'viewerSpreadDouble': true,
+                        'viewerAnimationPattern': self.NFBR.a5p.B1P() ? self.NFBR.a0X.a3m : self.NFBR.a0X.a3L,
+                        'viewerAnimationPatternForFixed': self.NFBR.a5p.B1P() ? self.NFBR.a0X.a9j : self.NFBR.a0X.ANIMATION_PATTERN_FOR_FIXED_DEFAULT,
+                        'viewerAnimationSpeed': self.NFBR.a0X.a3M,
+                        'viewerPageTransitionAxis': self.NFBR.a0X.A1P,
+                        'viewerTapRange': self.NFBR.a0X.a4D,
+                        'A8r': new self.NFBR.a6iMark.b0g('NFBR.ResumeData'),
+                        'A6M': new self.NFBR.a6iMark.A7m('NFBR.a6imarkData'),
+                        'viewerb5c': null,
+                        'viewerSpread': {},
+                    });
+            
+                    const bl = new NFBR.a5n();
+                    bl.B0a = "normal_default";
+                    let data = new NFBR.a6i.Content(parameters.url);
+                    await bl.a5s(data, "configuration", new NFBR.a6G.a6L(model));
+            
+                    const pages = data.configuration.contents.map((page, index) => {
+                 	  
+                        let mode = 'puzzle';
+                        let extension = '.jpeg';
+                        if (data.ct && data.et && data.st) {
+                            mode = 'RC4+puzzle';
+                            extension += '.dat';
+                        }
+            
+                        //let file = page.file + '/0';
+                        //for (let d = v = 0; d < file.length; d++) {
+                        //    v += file.charCodeAt(d);
+                        //}
+                        
+                        //Create a page
+                        const fPage = new NFBR.a6i.Page(index, page.file, "0", extension, "");
+                        //Calculate URL using data from JSON
+                        const realURL = data.url + fPage.a7B(data);
+                        
+                        //Fill infos in the page for a7b to work
+                        const fileinfos = data.files[index].FileLinkInfo.PageLinkInfoList[0].Page;
+                        fPage.width = fileinfos.Size.Width;
+                        fPage.height = fileinfos.Size.Height;
+                        fPage.info = fileinfos;
+                        //Fill more infos needed for b0Q unscrambling
+                        fPage.a7b(data);
+                        
+                        //console.log(fPage);
+                        
+                        //test : get blocks
+                        const blocks = window.NFBR.a6G.a5x.prototype.b0Q(fPage, fPage.width, fPage.height);
+                        //console.log(blocks);
+                        
+                        return {
+                            mode: mode,
+                            imageUrl: realURL,
+                            encryption: {
+                                //pattern: v % NFBR.a0X.a3h + 1,
+                                blocks : blocks,
+                                key: {
+                                    ct: data.ct,
+                                    et: data.et,
+                                    st: data.st,
+                                    bs: data.bs || 128,
+                                    hs: data.hs || 1024,
+                                    useRawContent: undefined
+                                }
+                            }
+                        };
+                    });
+
+                    console.log(Date.now() - starttime / 1000)
+                    
+                    resolve(pages);
+            
+                }, 1000);
+            });
+        `;
+
+        request = new Request( uri, this.requestOptions );
+        const data = await Engine.Request.fetchUI(request, script, 60000, true);
+        return data.map(page => page.mode === 'raw' ? page.imageUrl : this.createConnectorURI(page));
+    }
+
+    async _handleConnectorURI(payload) {
+        const uri = new URL(payload.imageUrl, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const response = await fetch(request);
+        switch (payload.mode) {
+            /*
+            case 'RC4+puzzle': {
+                let data = await response.text();
+                data = this._decryptRC4(data, uri.pathname.split('/').pop(), payload.encryption.key);
+                data = await this._descrambleImage(data, payload.encryption.pattern);
+                return this._blobToBuffer(data);
+            }*/
+            case 'puzzle': {
+                let data = await response.blob();
+                data = await this._descrambleImage(data, payload.encryption.blocks);
+                return this._blobToBuffer(data);
+            }
+            /*
+            case 'xor': {
+                let data = await response.arrayBuffer();
+                data = {
+                    mimeType: response.headers.get('content-type'),
+                    data: await this._decryptXOR(data, payload.encryption.key)
+                };
+                this._applyRealMime(data);
+                return data;
+            }*/
+            default: {
+                let data = await response.blob();
+                return this._blobToBuffer(data);
+            }
+        }
+    }
+
+    async _descrambleImage(scrambled, blocks) {
+        let bitmap = await createImageBitmap(scrambled);
+        return new Promise(resolve => {
+            let canvas = document.createElement('canvas');
+            canvas.width = bitmap.width;
+            canvas.height = bitmap.height;
+            var ctx = canvas.getContext('2d');
+            for (let q of blocks) {
+                ctx.drawImage(bitmap, q.destX, q.destY, q.width, q.height, q.srcX, q.srcY, q.width, q.height);
+            }
+            canvas.toBlob(data => {
+                resolve(data);
+            }, Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
+        } );
+    }
+
 }
