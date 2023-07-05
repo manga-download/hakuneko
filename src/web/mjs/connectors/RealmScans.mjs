ca210ab32a69216a74c55f28c791eb6cb9518896
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-05 07:58:07 +0000

    fix getting mangas list

--- src/web/mjs/connectors/RealmScans.mjs
+++ src/web/mjs/connectors/RealmScans.mjs
@@ -8,16 +8,35 @@ export default class RealmScans extends WordPressMangastream {
         super.label = 'RealmScans';
         this.tags = [ 'manga', 'english' ];
         this.url = 'https://realmscans.xyz';
-        this.path = '/series/?list';
+        this.path = '/series';
     }
+
+    async _getMangas() {
+        const request = new Request(new URL(this.path, this.url), this.requestOptions);
+        const script = `
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    let mangalist = [...document.querySelectorAll('div.bsx a')].map(element => {
+                        return {
+                            id: element.pathname,
+                            title: element.querySelector('div.tt').textContent.trim()
+                        };
+                    });
+                   resolve(mangalist);
+                },2500); 
+            });
+        `;
+        return await Engine.Request.fetchUI(request, script);
+    }
+
     async _getPages(chapter) {
         const data = await super._getPages(chapter);
         return data.map(element => this.createConnectorURI(element));
     }
     async _handleConnectorURI(payload) {
-        let request = new Request(payload, this.requestOptions);
+        const request = new Request(payload, this.requestOptions);
         request.headers.set('x-referer', this.url);
-        let response = await fetch(request);
+        const response = await fetch(request);
         let data = await response.blob();
         data = await this._blobToBuffer(data);
         this._applyRealMime(data);
