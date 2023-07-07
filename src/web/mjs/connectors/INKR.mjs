a5787c1c0014c1bdb9196258a1b6c9705a398104
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-07 17:53:54 +0000

    Update INKR.mjs

--- src/web/mjs/connectors/INKR.mjs
+++ src/web/mjs/connectors/INKR.mjs
@@ -34,7 +34,7 @@ export default class INKR extends Connector {
         const uri = new URL(`/_next/data/${this.nextInstance}/title/${manga.id}/chapters.json`, this.url);
         const request = new Request(uri, this.requestOptions);
         let data = await this.fetchJSON(request);
-        data = LZString.decompressFromBase64(data.pageProps._c);
+        data = new LZSTRING().decompressFromBase64(data.pageProps._c);
         data = JSON.parse(data);
         const obj = unpacker.unpack(data[0], data[1]);
 
@@ -57,7 +57,7 @@ export default class INKR extends Connector {
         const url = new URL(`/title/${chapter.manga.id}/chapter/${chapter.id}`, this.url);
         let data = await this._getNextData(url);
 
-        data = LZString.decompressFromBase64(data.props.pageProps._c);
+        data = new LZSTRING().decompressFromBase64(data.props.pageProps._c);
         data = JSON.parse(data);
         const obj = unpacker.unpack(data[0], data[1]);
 
@@ -263,32 +263,125 @@ var unpacker = function() {
     return instanceObj;
 }();
 
-var LZString=function() {
-    function o(o, r) {
-        if(!t[o]) {
-            t[o]={}; for(var n=0; n<o.length; n++)t[o][o.charAt(n)]=n;
-        }return t[o][r];
-    }var r=String.fromCharCode,
-        n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-        //   e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",
-        t={},
-        i={
-            decompressFromBase64:function(r) {
-                return null==r?"":""==r?null:i._decompress(r.length, 32, function(e) {
-                    return o(n, r.charAt(e));
-                });
-            }, _decompress:function(o, n, e) {
-                var i, s, p, u, c, a, l, f=[], h=4, d=4, m=3, v="", w=[], A={val:e(0), position:n, index:1}; for(i=0; 3>i; i+=1)f[i]=i; for(p=0, c=Math.pow(2, 2), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; switch(t=p) {
-                    case 0:for(p=0, c=Math.pow(2, 8), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; l=r(p); break; case 1:for(p=0, c=Math.pow(2, 16), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; l=r(p); break; case 2:return"";
-                }for(f[3]=l, s=l, w.push(l); ;) {
-                    if(A.index>o)return""; for(p=0, c=Math.pow(2, m), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; switch(l=p) {
-                        case 0:for(p=0, c=Math.pow(2, 8), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; f[d++]=r(p), l=d-1, h--; break; case 1:for(p=0, c=Math.pow(2, 16), a=1; a!=c;)u=A.val&A.position, A.position>>=1, 0==A.position&&(A.position=n, A.val=e(A.index++)), p|=(u>0?1:0)*a, a<<=1; f[d++]=r(p), l=d-1, h--; break; case 2:return w.join("");
-                    }if(0==h&&(h=Math.pow(2, m), m++), f[l])v=f[l]; else{
-                        if(l!==d)return null; v=s+s.charAt(0);
-                    }w.push(v), f[d++]=s+v.charAt(0), h--, s=v, 0==h&&(h=Math.pow(2, m), m++);
+var LZSTRING = function () {
+    var r,
+        o = function () {
+            var t = String.fromCharCode,
+                e = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+                n = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$',
+                r = {};
+            function o(t, e) {
+                if (!r[t]) {
+                    r[t] = {};
+                    for (var n = 0; n < t.length; n++) r[t][t.charAt(n)] = n;
                 }
-            }}; return i;
-}();
+                return r[t][e];
+            }
+            var i = {
+                decompressFromBase64: function (t) {
+                    return null == t ? '' : '' == t ? null : i._decompress(t.length, 32, function (n) {
+                        return o(e, t.charAt(n));
+                    });
+                },
+                _decompress: function (e, n, r) {
+                    var o,
+                        i,
+                        s,
+                        a,
+                        c,
+                        u,
+                        l,
+                        d = [],
+                        h = 4,
+                        f = 4,
+                        p = 3,
+                        g = '',
+                        m = [],
+                        y = {
+                            val: r(0),
+                            position: n,
+                            index: 1
+                        };
+                    for (o = 0; o < 3; o += 1) d[o] = o;
+                    for (s = 0, c = Math.pow(2, 2), u = 1; u != c; ) a = y.val & y.position,
+                    y.position >>= 1,
+                    0 == y.position &&
+            (y.position = n, y.val = r(y.index++)),
+                    s |= (a > 0 ? 1 : 0) * u,
+                    u <<= 1;
+                    switch (s) {
+                        case 0:
+                            for (s = 0, c = Math.pow(2, 8), u = 1; u != c; ) a = y.val & y.position,
+                            y.position >>= 1,
+                            0 == y.position &&
+                (y.position = n, y.val = r(y.index++)),
+                            s |= (a > 0 ? 1 : 0) * u,
+                            u <<= 1;
+                            l = t(s);
+                            break;
+                        case 1:
+                            for (s = 0, c = Math.pow(2, 16), u = 1; u != c; ) a = y.val & y.position,
+                            y.position >>= 1,
+                            0 == y.position &&
+                (y.position = n, y.val = r(y.index++)),
+                            s |= (a > 0 ? 1 : 0) * u,
+                            u <<= 1;
+                            l = t(s);
+                            break;
+                        case 2:
+                            return '';
+                    }
+                    for (d[3] = l, i = l, m.push(l); ; ) {
+                        if (y.index > e) return '';
+                        for (s = 0, c = Math.pow(2, p), u = 1; u != c; ) a = y.val & y.position,
+                        y.position >>= 1,
+                        0 == y.position &&
+              (y.position = n, y.val = r(y.index++)),
+                        s |= (a > 0 ? 1 : 0) * u,
+                        u <<= 1;
+                        switch (l = s) {
+                            case 0:
+                                for (s = 0, c = Math.pow(2, 8), u = 1; u != c; ) a = y.val & y.position,
+                                y.position >>= 1,
+                                0 == y.position &&
+                  (y.position = n, y.val = r(y.index++)),
+                                s |= (a > 0 ? 1 : 0) * u,
+                                u <<= 1;
+                                d[f++] = t(s),
+                                l = f - 1,
+                                h--;
+                                break;
+                            case 1:
+                                for (s = 0, c = Math.pow(2, 16), u = 1; u != c; ) a = y.val & y.position,
+                                y.position >>= 1,
+                                0 == y.position &&
+                  (y.position = n, y.val = r(y.index++)),
+                                s |= (a > 0 ? 1 : 0) * u,
+                                u <<= 1;
+                                d[f++] = t(s),
+                                l = f - 1,
+                                h--;
+                                break;
+                            case 2:
+                                return m.join('');
+                        }
+                        if (0 == h && (h = Math.pow(2, p), p++), d[l]) g = d[l];
+                        else {
+                            if (l !== f) return null;
+                            g = i + i.charAt(0);
+                        }
+                        m.push(g),
+                        d[f++] = i + g.charAt(0),
+                        i = g,
+                        0 == --h &&
+              (h = Math.pow(2, p), p++);
+                    }
+                }
+            };
+            return i;
+        } ();
+        return o
+};
 
 /***************************************************/
 // END INKR
