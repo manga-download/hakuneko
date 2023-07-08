f8d6c0ef5a4ef58d16b7b1bce7ca8e97d4980143
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-08 16:36:05 +0000

    FIX RealmScans: change url (#5981)
    * FIX RealmScans: change url
    
    * fix getting mangas list
    
    * Update RealmScans.mjs

--- src/web/mjs/connectors/RealmScans.mjs
+++ src/web/mjs/connectors/RealmScans.mjs
@@ -7,17 +7,29 @@ export default class RealmScans extends WordPressMangastream {
         super.id = 'realmscans';
         super.label = 'RealmScans';
         this.tags = [ 'manga', 'english' ];
-        this.url = 'https://realmscans.com';
-        this.path = '/series/?list';
+        this.url = 'https://realmscans.xyz';
+        this.path = '/series';
     }
+
+    async _getMangas() {
+        const request = new Request(new URL(this.path, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.bsx a');
+        return data.map(element => {
+            return {
+                id: element.pathname,
+                title: element.querySelector('div.tt').textContent.trim()
+            };
+        });
+    }
+
     async _getPages(chapter) {
         const data = await super._getPages(chapter);
         return data.map(element => this.createConnectorURI(element));
     }
     async _handleConnectorURI(payload) {
-        let request = new Request(payload, this.requestOptions);
+        const request = new Request(payload, this.requestOptions);
         request.headers.set('x-referer', this.url);
-        let response = await fetch(request);
+        const response = await fetch(request);
         let data = await response.blob();
         data = await this._blobToBuffer(data);
         this._applyRealMime(data);
