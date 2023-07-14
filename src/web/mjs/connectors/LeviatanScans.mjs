10234264da1db5b0a39a65e6da33881e5ad010f4
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-14 09:08:10 +0000

    FIX LeviatanScans : no more WPMangaProtector

--- src/web/mjs/connectors/LeviatanScans.mjs
+++ src/web/mjs/connectors/LeviatanScans.mjs
@@ -15,33 +15,4 @@ export default class LeviatanScans extends WordPressMadara {
     _createMangaRequest(page) {
         return new Request(new URL(`/ranking/page/${page}/`, this.url), this.requestOptions);
     }
-
-    async _getPages(chapter) {
-        return await this._getWProtectedPages(chapter);
-    }
-
-    async _getWProtectedPages(chapter) {
-        const url = new URL(chapter.id, this.url);
-        const request = new Request(url, this.requestOptions);
-        const script = `
-            new Promise((resolve, reject) => {
-                var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
-                    format: CryptoJSAesJson
-                }).toString(CryptoJS.enc.Utf8));
-                resolve(JSON.parse(imgdata));
-            });
-        `;
-        return await Engine.Request.fetchUI(request, script);
-    }
-
-    async _handleConnectorURI(payload) {
-        let request = new Request(payload.url, this.requestOptions);
-        request.headers.set('x-referer', payload.referer);
-        request.headers.delete('x-origin');
-        let response = await fetch(request);
-        let data = await response.blob();
-        data = await this._blobToBuffer(data);
-        this._applyRealMime(data);
-        return data;
-    }
 }
