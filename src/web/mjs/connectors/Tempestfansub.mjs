40db0575e41126e4bc20f59b2cffeb19d4ce7628
Furkan Göçmen <97408699+Furkangcmn@users.noreply.github.com>
2023-07-31 12:27:44 +0300

    Update Tempestfansub.mjs (#6084)
    * Update Tempestfansub.mjs
    
    Domain changed
    
    * Update Tempestfansub.mjs
    
    ---------
    
    Co-authored-by: MikeZeDev <MikeZeDev@users.noreply.github.com>

--- src/web/mjs/connectors/Tempestfansub.mjs
+++ src/web/mjs/connectors/Tempestfansub.mjs
@@ -7,8 +7,30 @@ export default class Tempestfansub extends WordPressMangastream {
         super.id = 'tempestfansub';
         super.label = 'Tempestfansub';
         this.tags = [ 'webtoon', 'manga', 'turkish' ];
-        this.url = 'https://manga.tempestfansub.com';
+        this.url = 'https://tempestfansub.com';
 
         this.path = '/manga/list-mode/';
     }
+
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            let mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('/manga/?page=' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, 'div.bs div.bsx > a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title.trim()
+            };
+        });
+    }
+
 }
