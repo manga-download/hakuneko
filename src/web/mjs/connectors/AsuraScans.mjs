d6365ca67ca7fef8209b8f11dd3011a793513790
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-31 13:36:36 +0000

    Update AsuraScans.mjs

--- src/web/mjs/connectors/AsuraScans.mjs
+++ src/web/mjs/connectors/AsuraScans.mjs
@@ -9,16 +9,20 @@ export default class AsuraScans extends WordPressMangastream {
         this.tags = ['webtoon', 'english'];
         this.url = 'https://asura.gg';
         this.path = '/manga/list-mode/';
-
         this.queryPages = 'div#readerarea p img';
         this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+    }
+
+    canHandleURI(uri) {
+        return this.url.includes(uri.hostname);
     }
 
     async _getPages(chapter) {
         const excludes = [
             /panda_gif_large/i,
             /2021\/04\/page100-10\.jpg/i,
-            /2021\/03\/20-ending-page-\.jpg/i
+            /2021\/03\/20-ending-page-\.jpg/i,
+            /ENDING-PAGE/i
         ];
         const images = await super._getPages(chapter);
         return images.filter(link => !excludes.some(rgx => rgx.test(link)));
