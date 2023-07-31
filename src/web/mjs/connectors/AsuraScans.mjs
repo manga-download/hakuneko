63b074a4763235653b3d6fbfe0d21664913d8690
Sheepux <sheepux@live.com>
2023-07-31 14:18:14 +0200

    Revert "Update AsuraScans.mjs (#6078)"
    This reverts commit 97d8dcd6ea8a382600532f8ebc53b0f3a79afc74.

--- src/web/mjs/connectors/AsuraScans.mjs
+++ src/web/mjs/connectors/AsuraScans.mjs
@@ -7,34 +7,11 @@ export default class AsuraScans extends WordPressMangastream {
         super.id = 'asurascans';
         super.label = 'Asura Scans';
         this.tags = ['webtoon', 'english'];
-        this.config = {
-            url: {
-                label: 'URL',
-                description: 'This website changes between those two URL regularly.\n Please select the one to use',
-                input: 'select',
-                options: [
-                    { value: 'https://www.asurascans.com', name: 'asurascans.com' },
-                    { value: 'https://asura.gg', name: 'asura.gg' },
-                ],
-                value: 'https://www.asurascans.com'
-
-            }
-        };
+        this.url = 'https://www.asurascans.com';
         this.path = '/manga/list-mode/';
 
         this.queryPages = 'div#readerarea p img';
         this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
-    }
-
-    get url() {
-        return this.config.url.value;
-    }
-
-    set url(value) {
-        if (this.config && value) {
-            this.config.url.value = value;
-            Engine.Settings.save();
-        }
     }
 
     async _getPages(chapter) {
