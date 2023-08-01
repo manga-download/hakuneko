1b78c7d640680912b369ec1da6d79d1569202fd0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-01 21:21:46 +0000

    FIX Mangarawto pages order (#6065)
    Somehow fetching image nodes by injecting a script fixes messed up image order and format
    
    Fixes https://github.com/manga-download/hakuneko/issues/6064

--- src/web/mjs/connectors/Mangarawto.mjs
+++ src/web/mjs/connectors/Mangarawto.mjs
@@ -13,4 +13,20 @@ export default class Mangarawto extends Manga9 {
     canHandleURI(uri) {
         return /^mangaraw\.(to|vip|io)$/.test(uri.hostname);
     }
+
+    async _getPages(chapter) {
+        const script = `
+            new Promise(resolve => {
+                let pagelist = [...document.querySelectorAll('div.card-wrap img')].map(element => {
+                    return new URL(element.dataset.src, window.location).href
+                });
+                resolve(pagelist);
+            });
+        `;
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        const data = await Engine.Request.fetchUI(request, script);
+        return data.map(image => this.createConnectorURI(image));
+
+    }
+
 }
