d8a5180eec140ced62b4518a34c37530cdce9f12
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-03 07:31:39 +0000

    FIX : Siyahmelek : filter dummy picture (#6013)

--- src/web/mjs/connectors/Siyahmelek.mjs
+++ src/web/mjs/connectors/Siyahmelek.mjs
@@ -9,4 +9,11 @@ export default class Siyahmelek extends WordPressMadara {
         this.tags = [ 'manga', 'webtoon', 'turkish' ];
         this.url = 'https://siyahmelek.net';
     }
+
+    async _getPages(chapter) {
+        return (await super._getPages(chapter)).filter(picture => {
+            const pic= JSON.parse(atob(new URL(picture).searchParams.get('payload')));
+            return !pic.url.endsWith('xxxxx/99.jpg');
+        });
+    }
 }
