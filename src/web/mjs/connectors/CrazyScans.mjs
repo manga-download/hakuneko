b3a5f19b1b529e8f628bdab1787fe1f00884e36c
Tristan <tristan@tnet.moe>
2023-08-14 18:16:46 +0200

    CrazyScans: bypass WP Manga Protector (#6144)
    stol- borrowed the code from the MangaSY (a48270b) connector.
    Thanks to @MikeZeDev for the help.

--- src/web/mjs/connectors/CrazyScans.mjs
+++ src/web/mjs/connectors/CrazyScans.mjs
@@ -9,4 +9,19 @@ export default class CrazyScans extends WordPressMadara {
         this.tags = [ 'webtoon', 'english' ];
         this.url = 'https://mangacultivator.com';
     }
+
+    async _getPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const script = `
+            new Promise((resolve, reject) => {
+                var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
+                    format: CryptoJSAesJson
+                }).toString(CryptoJS.enc.Utf8));
+                resolve(JSON.parse(imgdata));
+            });
+        `;
+        const data = await Engine.Request.fetchUI(request, script);
+        return data.map(picture => this.createConnectorURI({url : picture, referer : url}));
+    }
 }
