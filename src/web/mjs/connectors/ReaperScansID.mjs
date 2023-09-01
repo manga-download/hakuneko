18c11151cb62771ffc0ba98f07cf58a777ebbaec
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-01 09:21:21 +0000

    FIX ReaperScansID : change domain & filter ads
    Fixes https://github.com/manga-download/hakuneko/issues/6181

--- src/web/mjs/connectors/ReaperScansID.mjs
+++ src/web/mjs/connectors/ReaperScansID.mjs
@@ -6,12 +6,19 @@ export default class ReaperScansID extends WordPressMadara {
         super.id = 'reaperscansid';
         super.label = 'Shinigami ID';
         this.tags = ['webtoon', 'indonesian', 'scanlation'];
-        this.url = 'https://shinigami.id';
+        this.url = 'https://shinigami.ae';
         this.queryChapters = 'div.chapter-link > a';
         this.queryChaptersTitleBloat ='span.chapter-release-date';
 
         this.links = {
-            login: 'https://shinigami.id/login'
+            login: 'https://shinigami.ae/login'
         };
+    }
+
+    async _getPages(chapter) {
+        return (await super._getPages(chapter)).filter(picture => {
+            const pic= JSON.parse(atob(new URL(picture).searchParams.get('payload')));
+            return pic.url.match(/WP-Manga/i);
+        });
     }
 }
