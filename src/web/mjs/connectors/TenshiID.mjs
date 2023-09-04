b470126653fb19a3a99f876710ecc89d1a04f7a7
akn19 <akunhitam2019@gmail.com>
2023-09-04 14:04:34 +0700

    add tensiID

---
+++ src/web/mjs/connectors/TenshiID.mjs
@@ -0,0 +1,13 @@
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+
+export default class TenshiID extends WordPressMangastream {
+
+    constructor() {
+        super();
+        super.id = 'tenshiid';
+        super.label = 'TenshiID';
+        this.tags = ['manga', 'indonesian'];
+        this.url = 'https://tenshi.id';
+        this.path = '/komik/?list';
+    }
+}
