2154d94678de3f9c35b1ab12fbaff2b989793247
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-06 09:50:27 +0000

    FIX Mgkomik : change domain
    and adjust manga listing
    
    Fixes https://github.com/manga-download/hakuneko/issues/6208

--- src/web/mjs/connectors/Mgkomik.mjs
+++ src/web/mjs/connectors/Mgkomik.mjs
@@ -7,6 +7,19 @@ export default class Mgkomik extends WordPressMadara {
         super.id = 'mgkomik';
         super.label = 'MGKOMIK';
         this.tags = [ 'webtoon', 'indonesian' ];
-        this.url = 'https://mgkomik.com';
+        this.url = 'https://mgkomik.id/';
+        this.queryMangas = 'div.manga div > a';
     }
+
+    async _getMangasFromPage(page) {
+        let request = this._createMangaRequest(page);
+        let data = await this.fetchDOM(request, this.queryMangas);
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, request.url),
+                title: element.title.trim()
+            };
+        });
+    }
+
 }
