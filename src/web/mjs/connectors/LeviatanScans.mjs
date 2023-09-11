582edd24e6a40ecf0c75e0299bfb9232274bab99
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-11 08:44:33 +0000

    FIx LeviatanScans : change domain (#6213)
    * FIx LeviatanScans : chande domain
    
    Fixes https://github.com/manga-download/hakuneko/issues/6210
    
    * Update LeviatanScans.mjs

--- src/web/mjs/connectors/LeviatanScans.mjs
+++ src/web/mjs/connectors/LeviatanScans.mjs
@@ -7,12 +7,8 @@ export default class LeviatanScans extends WordPressMadara {
         super.id = 'leviatanscans';
         super.label = 'LeviatanScans';
         this.tags = [ 'webtoon', 'high-quality', 'english', 'scanlation' ];
-        this.url = 'https://en.leviatanscans.com';
+        this.url = 'https://lscomic.com';
         this.requestOptions.headers.set('x-origin', this.url);
         this.requestOptions.headers.set('x-referer', `${this.url}/`);
-    }
-
-    _createMangaRequest(page) {
-        return new Request(new URL(`/ranking/page/${page}/`, this.url), this.requestOptions);
     }
 }
