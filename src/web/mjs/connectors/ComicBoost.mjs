5aa3962b152fb4f948bde0c9fe4b032ac89893d8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-03 18:31:15 +0000

    use try catch to properly reject promise

--- src/web/mjs/connectors/ComicBoost.mjs
+++ src/web/mjs/connectors/ComicBoost.mjs
@@ -60,81 +60,87 @@ export default class ComicBoost extends Connector {
 
         const script = `
             new Promise((resolve, reject) => {
-                setTimeout(async () => {
 
-                const a2f = NFBR.a2F ? new NFBR.a2F() : new NFBR.a2f();
-                const params = new URL(window.location).searchParams;
-                const parameters = await a2f.a5W({
-                    contentId: params.get(NFBR.a5q.Key.CONTENT_ID), // Content ID => 'cid'
-                    a6m: params.get(NFBR.a5q.Key.LICENSE_TOKEN), // License Token => 'lit'
-                    preview: params.get(NFBR.a5q.Key.LOOK_INSIDE) === '1', // Look Inside => 'lin'
-                    contentType: params.get(NFBR.a5q.Key.CONTENT_TYPE) || 1, // Content Type => 'cty'
-                    title: params.get(NFBR.a5q.Key.CONTENT_TITLE), // Content Title => 'cti'
-                    winWidth: 3840,
-                    winHeight: 2160
-                });
-           
-                //Create a model
-                const model = new NFBR.a6G.Model({
-                    'settings': new self.NFBR.Settings('NFBR.SettingData'),
-                    'viewerFontSize': self.NFBR.a0X.a3k,
-                    'viewerFontFace': self.NFBR.a0X.a3k,
-                    'viewerSpreadDouble': true,
-                    'viewerb5c': null,
-                    'viewerSpread': {},
-                    'queryParamForContentUrl' : parameters.contentAppendParam,
-                });
-                //Create the bookloader
-                const bl = new NFBR.a5n();
-                bl.B0a = "normal_default";
-           
-                //Create a "Content" that will be filled using the bookloader as5 async function
-                let data = new NFBR.a6i.Content(parameters.url);
-                let v_a6L = new NFBR.a6G.a6L(model); //a6G.a6L seems to be named identical between publus versions
-                await bl.a5s(data, "configuration", v_a6L);
-
-                //data is now our JSON with all the infos
-                const pages = data.configuration.contents.map((page, index) => {
-           
-                    let mode = 'raw';
-                    let extension = '.jpeg';
-
-                    //*****************/
-                    //GETTING PAGE URL
-                    //*****************/
-                    //Create a Page
-                    const fPage = new NFBR.a6i.Page(index, page.file, "0", extension, "");
-                    const realURL = v_a6L.a6T(data, fPage); //get real URL, may change depending on publus version
-
-                    //*****************************/
-                    //GETTING IMAGE SCRAMBLE DATA
-                    //*****************************/
-           
-                    //Fill infos in the page for a7b to work
-                    const fileinfos = data.files[index].FileLinkInfo.PageLinkInfoList[0].Page;
-                    fPage.width = fileinfos.Size.Width;
-                    fPage.height = fileinfos.Size.Height;
-                    fPage.info = fileinfos;
-                    //Fill more infos needed for  unscrambling
-                    fPage.a7b(data); // function names depends on publus version
-                    
-                    let blocks  = [];
-                    if (fileinfos.BlockHeight) //if we have a block size for the page, its a puzzle !
-                    {
-                        mode = 'puzzle';
-                        blocks = window.NFBR.a6G.a5x.prototype.b0Q(fPage, fPage.width, fPage.height)
-                    }
-           
-                    return {
-                        mode: mode,
-                        imageUrl: realURL,
-                        encryption: {
-                            blocks: JSON.stringify(blocks),//stringify the array greatly speed up createConnectorURI :)
+                try {
+                
+                    setTimeout(async () => {
+                   
+                    const a2f = NFBR.a2F ? new NFBR.a2F() : new NFBR.a2f();
+                    const params = new URL(window.location).searchParams;
+                    const parameters = await a2f.a5W({
+                        contentId: params.get(NFBR.a5q.Key.CONTENT_ID), // Content ID => 'cid'
+                        a6m: params.get(NFBR.a5q.Key.LICENSE_TOKEN), // License Token => 'lit'
+                        preview: params.get(NFBR.a5q.Key.LOOK_INSIDE) === '1', // Look Inside => 'lin'
+                        contentType: params.get(NFBR.a5q.Key.CONTENT_TYPE) || 1, // Content Type => 'cty'
+                        title: params.get(NFBR.a5q.Key.CONTENT_TITLE), // Content Title => 'cti'
+                        winWidth: 3840,
+                        winHeight: 2160
+                    });
+                   
+                    //Create a model
+                    const model = new NFBR.a6G.Model({
+                        'settings': new self.NFBR.Settings('NFBR.SettingData'),
+                        'viewerFontSize': self.NFBR.a0X.a3k,
+                        'viewerFontFace': self.NFBR.a0X.a3k,
+                        'viewerSpreadDouble': true,
+                        'viewerb5c': null,
+                        'viewerSpread': {},
+                        'queryParamForContentUrl' : parameters.contentAppendParam,
+                    });
+                    //Create the bookloader
+                    const bl = new NFBR.a5n();
+                    bl.B0a = "normal_default";
+                   
+                    //Create a "Content" that will be filled using the bookloader as5 async function
+                    let data = new NFBR.a6i.Content(parameters.url);
+                    let v_a6L = new NFBR.a6G.a6L(model); //a6G.a6L seems to be named identical between publus versions
+                    await bl.a5s(data, "configuration", v_a6L);
+                   
+                    //data is now our JSON with all the infos
+                    const pages = data.configuration.contents.map((page, index) => {
+                   
+                        let mode = 'raw';
+                        let extension = '.jpeg';
+                   
+                        //*****************/
+                        //GETTING PAGE URL
+                        //*****************/
+                        //Create a Page
+                        const fPage = new NFBR.a6i.Page(index, page.file, "0", extension, "");
+                        const realURL = v_a6L.a6T(data, fPage); //get real URL, may change depending on publus version
+                   
+                        //*****************************/
+                        //GETTING IMAGE SCRAMBLE DATA
+                        //*****************************/
+                   
+                        //Fill infos in the page for a7b to work
+                        const fileinfos = data.files[index].FileLinkInfo.PageLinkInfoList[0].Page;
+                        fPage.width = fileinfos.Size.Width;
+                        fPage.height = fileinfos.Size.Height;
+                        fPage.info = fileinfos;
+                        //Fill more infos needed for  unscrambling
+                        fPage.a7b(data); // function names depends on publus version
+                        
+                        let blocks  = [];
+                        if (fileinfos.BlockHeight) //if we have a block size for the page, its a puzzle !
+                        {
+                            mode = 'puzzle';
+                            blocks = window.NFBR.a6G.a5x.prototype.b0Q(fPage, fPage.width, fPage.height)
                         }
-                    };
-                });
-                resolve(pages);
-                }, 1000);
+                   
+                        return {
+                            mode: mode,
+                            imageUrl: realURL,
+                            encryption: {
+                                blocks: JSON.stringify(blocks),//stringify the array greatly speed up createConnectorURI :)
+                            }
+                        };
+                    });
+                    resolve(pages);
+                    }, 1000);
+                } catch (error) {
+                    reject(error)
+                }
             });
         `;
 
