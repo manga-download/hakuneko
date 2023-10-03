e1de0ecaa2f38ef71f2f962d16ffa83933c3575b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-03 19:26:49 +0000

    FIX: Bilibili - Add quality selection & fix incorrect server data (#5498)
    * BilibiliManhua : Add quality selection
    
    * Bilibilicomics : Add quality selection
    
    * Update BilibiliManhua.mjs
    
    * Update BilibiliManhua.mjs
    
    * Update BilibiliManhua.mjs
    
    * Update BilibiliManhua.mjs
    
    * Bilibili : requested changed

--- src/web/mjs/connectors/BilibiliComics.mjs
+++ src/web/mjs/connectors/BilibiliComics.mjs
@@ -11,33 +11,47 @@ export default class BilibiliComics extends BilibiliManhua {
         this.links = {
             login: 'https://www.bilibilicomics.com/account'
         };
-        //override BilibiliManhua config as we need lanuage switch. Difference is handled in the main code with (this.config.language) ? : test
-        this.config = {
-            quality:  {
-                label: 'Preferred format',
-                description: 'format of images\nwebp (low)\njpg (medium)\npng (high))',
-                input: 'select',
-                options: [
-                    { value: 'webp', name: 'webp' },
-                    { value: 'jpg', name: 'jpg' },
-                    { value: 'png', name: 'png' },
-                ],
-                value: 'png'
-            },
-            language: {
-                label: 'Language Settings',
-                description: 'Choose the language to use. This will affect available manga lists',
-                input: 'select',
-                options: [
-                    { value: 'fr', name: 'French' },
-                    { value: 'en', name: 'English' },
-                    { value: 'cn', name: 'Chinese' },
-                    { value: 'id', name: 'Indonesian' },
-                    { value: 'es', name: 'Spanish' },
-                ],
-                value: 'en'
-            },
+
+        this.config.language.input = 'select';
+        this.config.language.options = [
+            { value: 'fr', name: 'French' },
+            { value: 'en', name: 'English' },
+            { value: 'cn', name: 'Chinese' },
+            { value: 'id', name: 'Indonesian' },
+            { value: 'es', name: 'Spanish' },
+        ];
+        this.config.language.options.value = 'en';
+
+        this.config.picquality.options =
+        [
+            { value: 'raw', name: 'Raw' },
+            { value: 'hd', name: 'HD' },
+            { value: 'sd', name: 'SD' },
+        ];
+        this.config.picquality.options.value = 'raw';
+    }
+
+    //override quality formula cause its different for Bilibilicomics than for BilibiliManhua
+    getImageSizeByQuality(width) {
+
+        //quality is an optional url parameter to put when user select lowest quality
+        let o = {
+            imgWidth : width,
+            quality : undefined
         };
 
+        if (width < 1) return o;
+
+        const choosedQuality = this.config.picquality.value;
+        const WindowWidth = 3000; //r = window.innerWidth, force it to max. Why are they adjusting picture to window width?
+
+        choosedQuality === 'raw' ? o.imgWidth = WindowWidth <= 1000 ? 1000 : WindowWidth <= 1200 ? 1200 : WindowWidth <= 1600 ? 1600 : WindowWidth <= 2000 ? 2000 : width : choosedQuality === 'hd' ? o.imgWidth = WindowWidth <= 1200 ? 800 : 1000 : (o.imgWidth = WindowWidth <= 1200 ? 600 : 800, o.quality = 50);
+
+        if (this.config.forcepicturesize.value) {
+            o.imgWidth = Math.max(o.imgWidth, width);
+            o.quality = undefined;
+        }
+        return o;
     }
+
 }
