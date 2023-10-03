44892a4e545b296a7c32309724e723272db80711
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-03 18:12:42 +0000

    Update PixivComics.mjs

--- src/web/mjs/connectors/PixivComics.mjs
+++ src/web/mjs/connectors/PixivComics.mjs
@@ -105,63 +105,12 @@ export default class PixivComics extends Connector {
 
         ctx.drawImage(image, 0, 0);
         const i = ctx.getImageData(0, 0, payload.width, payload.height);
-        const l = await this.descrambleData(i.data, 4, payload.width, payload.height, payload.gridsize, payload.gridsize, '4wXCKprMMoxnyJ3PocJFs4CYbfnbazNe', payload.key, true );
+        const l = await descrambleData(i.data, 4, payload.width, payload.height, payload.gridsize, payload.gridsize, '4wXCKprMMoxnyJ3PocJFs4CYbfnbazNe', payload.key, true );
         const o = new ImageData(l, payload.width, payload.height);
         ctx.putImageData(o, 0, 0);
         return canvas;
     }
 
-    async descrambleData(e, t, i, r, n, s, a, l, o) {
-
-        let d = Math.ceil(r / s),
-            c = Math.floor(i / n),
-            u = Array(d).fill(null).map(() => Array.from(Array(c).keys()));
-        {
-            let e = a +l;//new TextEncoder().encode(a + l); //key + magicvalue to bytes, for use with subtlecrypto SHA256
-            let t = this.convertWordArrayToUint8Array(CryptoJS.SHA256(e)); //subtle.digest('SHA-256", e) => return arraybuffer
-            let arrayBuffer = t.buffer.slice(t.byteOffset, t.byteLength + t.byteOffset); //we need to properly pass the ArrayBuffer to make the Uint32Array since we used CryptoJS
-            let i = new Uint32Array(arrayBuffer, 0, 4);
-            let r = new PixivShuffler(i);
-
-            for (let e = 0; e < 100; e++) r.next();
-            for (let e = 0; e < d; e++) {
-                let t = u[e];
-                for (let e = c - 1; e >= 1; e--) {
-                    let i = r.next() % (e + 1),
-                        n = t[e];
-                    t[e] = t[i],
-                    t[i] = n;
-                }
-            }
-        }
-        if (o) for (let e = 0; e < d; e++) {
-            let t = u[e],
-                i = t.map((e, i) => t.indexOf(i));
-            if (i.some(e => e < 0)) throw Error('Failed to reverse shuffle table');
-            u[e] = i;
-        }
-        let h = new Uint8ClampedArray(e.length);
-        for (let a = 0; a < r; a++) {
-            let r = Math.floor(a / s),
-                l = u[r];
-            for (let r = 0; r < c; r++) {
-                let s = l[r],
-                    o = r * n,
-                    d = (a * i + o) * t,
-                    c = s * n,
-                    u = (a * i + c) * t,
-                    p = n * t;
-                for (let t = 0; t < p; t++) h[d + t] = e[u + t];
-            }
-            {
-                let r = c * n,
-                    s = (a * i + r) * t,
-                    l = (a * i + i) * t;
-                for (let t = s; t < l; t++) h[t] = e[t];
-            }
-        }
-        return h;
-    }
     async _canvasToBlob(canvas) {
         return new Promise(resolve => {
             canvas.toBlob(data => {
@@ -170,21 +119,6 @@ export default class PixivComics extends Connector {
         });
     }
 
-    convertWordArrayToUint8Array (wordArray) {
-        var len = wordArray.words.length,
-            u8_array = new Uint8Array(len << 2),
-            offset = 0,
-            word,
-            i;
-        for (i = 0; i < len; i++) {
-            word = wordArray.words[i];
-            u8_array[offset++] = word >> 24;
-            u8_array[offset++] = word >> 16 & 255;
-            u8_array[offset++] = word >> 8 & 255;
-            u8_array[offset++] = word & 255;
-        }
-        return u8_array;
-    }
 
 }
 
@@ -213,4 +147,72 @@ class PixivShuffler {
         return (e << (t %= 32) >>> 0 | e >>> 32 - t) >>> 0;
     }
 
+}
+
+function convertWordArrayToUint8Array (wordArray) {
+    var len = wordArray.words.length,
+        u8_array = new Uint8Array(len << 2),
+        offset = 0,
+        word,
+        i;
+    for (i = 0; i < len; i++) {
+        word = wordArray.words[i];
+        u8_array[offset++] = word >> 24;
+        u8_array[offset++] = word >> 16 & 255;
+        u8_array[offset++] = word >> 8 & 255;
+        u8_array[offset++] = word & 255;
+    }
+    return u8_array;
+}
+
+async function descrambleData(e, t, i, r, n, s, a, l, o) {
+
+    let d = Math.ceil(r / s),
+        c = Math.floor(i / n),
+        u = Array(d).fill(null).map(() => Array.from(Array(c).keys()));
+    {
+        let e = a +l;//new TextEncoder().encode(a + l); //key + magicvalue to bytes, for use with subtlecrypto SHA256
+        let t = convertWordArrayToUint8Array(CryptoJS.SHA256(e)); //subtle.digest('SHA-256", e) => return arraybuffer
+        let arrayBuffer = t.buffer.slice(t.byteOffset, t.byteLength + t.byteOffset); //we need to properly pass the ArrayBuffer to make the Uint32Array since we used CryptoJS
+        let i = new Uint32Array(arrayBuffer, 0, 4);
+        let r = new PixivShuffler(i);
+
+        for (let e = 0; e < 100; e++) r.next();
+        for (let e = 0; e < d; e++) {
+            let t = u[e];
+            for (let e = c - 1; e >= 1; e--) {
+                let i = r.next() % (e + 1),
+                    n = t[e];
+                t[e] = t[i],
+                t[i] = n;
+            }
+        }
+    }
+    if (o) for (let e = 0; e < d; e++) {
+        let t = u[e],
+            i = t.map((e, i) => t.indexOf(i));
+        if (i.some(e => e < 0)) throw Error('Failed to reverse shuffle table');
+        u[e] = i;
+    }
+    let h = new Uint8ClampedArray(e.length);
+    for (let a = 0; a < r; a++) {
+        let r = Math.floor(a / s),
+            l = u[r];
+        for (let r = 0; r < c; r++) {
+            let s = l[r],
+                o = r * n,
+                d = (a * i + o) * t,
+                c = s * n,
+                u = (a * i + c) * t,
+                p = n * t;
+            for (let t = 0; t < p; t++) h[d + t] = e[u + t];
+        }
+        {
+            let r = c * n,
+                s = (a * i + r) * t,
+                l = (a * i + i) * t;
+            for (let t = s; t < l; t++) h[t] = e[t];
+        }
+    }
+    return h;
 }
