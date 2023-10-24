73f89aff8609ec6bde32297b1eae40baf1ca224b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-24 08:44:50 +0000

    Update COMICMeDu.mjs

--- src/web/mjs/connectors/COMICMeDu.mjs
+++ src/web/mjs/connectors/COMICMeDu.mjs
@@ -8,5 +8,8 @@ export default class COMICMeDu extends YoungChampion {
         super.label = 'COMIC MeDu (こみっくめづ)';
         this.tags = [ 'manga', 'japanese' ];
         this.url = 'https://comic-medu.com';
+        this.links = {
+            login: 'https://comic-medu.com/signin'
+        };
     }
 }
