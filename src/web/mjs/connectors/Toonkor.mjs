b5a355a49be25fa5f70d3f6822c75001a57eb650
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-24 07:24:16 +0000

    FIx Toonkor : get domain from telegram (#6334)
    Avoid twitter shenanigans.
    
    Fixes https://github.com/manga-download/hakuneko/issues/6332

--- src/web/mjs/connectors/Toonkor.mjs
+++ src/web/mjs/connectors/Toonkor.mjs
@@ -7,17 +7,32 @@ export default class Toonkor extends GnuBoard5BootstrapBasic2 {
         super.id = 'toonkor';
         super.label = 'Toonkor';
         this.tags = ['webtoon', 'korean'];
-        this.url = 'https://tkor.zone';
+        this.url = 'https://t.me/s/new_toonkor';
+        this.hostRegexp = /https?:\/\/(?:toonkor|tkor)([\d]+)?\.[a-z]+/;
     }
 
     canHandleURI(uri) {
-        return /https?:\/\/(?:toonkor|tkor)\.[a-z]+/.test(uri.origin);
+        return this.hostRegexp.test(uri.origin);
     }
 
     async _initializeConnector() {
-        let uri = new URL(await this._twitter.getProfileURL('1202224761771741184') || this.url);
-        let request = new Request(uri.href, this.requestOptions);
-        this.url = await Engine.Request.fetchUI(request, `window.location.origin`);
+        const script = `
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                	  //fetch telegram messages with links, reverse it because last one is more recent one, and get the first matching our regex
+                    const tkLinks = [...document.querySelectorAll('section.tgme_channel_history div.tgme_widget_message_wrap .tgme_widget_message_text a ')].reverse();
+                    for (const link of tkLinks) {
+                        if ( ${this.hostRegexp}.test(link.href)) {
+                            resolve(link.href);
+                            break;
+                        }
+                    }
+                },1500);
+            });
+        `;
+        const uri = new URL(this.url);
+        const request = new Request(uri.href, this.requestOptions);
+        this.url = await Engine.Request.fetchUI(request, script);
         console.log(`Assigned URL '${this.url}' to ${this.label}`);
     }
 }
