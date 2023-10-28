4729408ed247d198d30dc923f6e5fdfbecb68ecf
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-28 19:20:46 +0000

    FlameScansORG : change domain
    and filter junk page

--- src/web/mjs/connectors/FlameScansORG.mjs
+++ src/web/mjs/connectors/FlameScansORG.mjs
@@ -5,12 +5,16 @@ export default class FlameScansORG extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'flamescans-org';
-        super.label = 'Flame Scans';
+        super.label = 'Flame Comics';
         this.tags = [ 'manga', 'webtoon', 'english' ];
-        this.url = 'https://flamescans.org';
+        this.url = 'https://flamecomics.com';
         this.path = '/series/list-mode/';
 
         this.queryMangas = 'div.postbody div.soralist ul li a.series';
         this.queryChapters = 'div#chapterlist ul li a';
+    }
+
+    async _getPages(chapter) {
+        return (await super._getPages(chapter)).filter(page => !page.includes('readonflamescans.png'));
     }
 }
