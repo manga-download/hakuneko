6df2e114e6e4096a9fa2b936bed0e2d015a32a51
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-01 12:29:52 +0000

    Fix KomikCast: change domain & fix getpages
    Fixes https://github.com/manga-download/hakuneko/issues/6380

--- src/web/mjs/connectors/KomikCast.mjs
+++ src/web/mjs/connectors/KomikCast.mjs
@@ -7,13 +7,14 @@ export default class KomikCast extends WordPressMangastream {
         super.id = 'komikcast';
         super.label = 'KomikCast';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://komikcast.vip';
+        this.url = 'https://komikcast.ch';
         this.path = '/daftar-komik/?list';
 
         this.queryMangas = 'div.text-mode_list-items ul li a.series, div.text-mode_list-items ul li a.text-mode_list-item';
         this.queryChapters = 'div.komik_info-chapters ul li.komik_info-chapters-item a.chapter-link-item';
         this.queryChaptersTitle = undefined;
-        this.queryPages = 'div.main-reading-area source[src^="http"], div.separator source[src^="http"]';
+        this.queryPages = 'div.main-reading-area source.lazyload, div.separator source.lazyload';
+        this.requestOptions.headers.set('x-referer', this.url);
     }
 
     async _initializeConnector() {
@@ -24,6 +25,6 @@ export default class KomikCast extends WordPressMangastream {
         const uri = new URL(chapter.id, this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, this.queryPages);
-        return data.map(image => this.createConnectorURI(this.getAbsolutePath(image, request.url)));
+        return data.map(image => this.createConnectorURI(this.getAbsolutePath(image.dataset['src'], this.url)));
     }
 }
