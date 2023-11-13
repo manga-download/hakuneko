6f0c1d252d2bde784cffd0e479d5602af41a86f9
Daniel Spinola <daniel.trindade@al.infnet.edu.br>
2023-11-13 07:23:38 -0300

    feat(Ridibooks): apply requested changes

--- src/web/mjs/connectors/Ridibooks.mjs
+++ src/web/mjs/connectors/Ridibooks.mjs
@@ -1,247 +1,58 @@
 import Connector from "../engine/Connector.mjs";
 import Manga from "../engine/Manga.mjs";
-import Chapter from "../engine/Chapter.mjs";
-
-/**
- * @typedef {{
- *  id: string,
- *  title: string,
- * }} IManga
- * 
- * @typedef {{
- *  id: string,
- *  title: string,
- * }} IChapter
- */
 
 export default class Ridibooks extends Connector {
     constructor() {
         super();
-
-        this.id = 'ridibooks'
-        this.label = 'Ridibooks (Korean)'
+        this.id = 'ridibooks';
+        this.label = 'Ridibooks (Korean)';
         this.tags = ['webtoon', 'korean'];
         this.url = 'https://ridibooks.com';
         this.links = {
             login: `${this.url}/account/login`
-        }
-
+        };
         this.apiUrl = "https://api.ridibooks.com";
         this.pagesApi = "https://view.ridibooks.com";
-        this.bookUrlPattern = /^https:\/\/ridibooks\.com\/books\/(?<id>[a-zA-Z0-9]+)/;
     }
 
-    // OVERRIDE
-
-    /**
-     * Get all mangas from the website.
-     * @returns {Promise<IManga[]>}
-     * @override
-     */
     async _getMangas() {
-        let mangaList = [];
-
-        // legacy takes some time and makes quite a lot of requests
-        // and still it doesn't return the full catogue
-        let legacyList = await this._getMangasLegacy();
-        mangaList.push(...legacyList);
-
-        // TODO: implement graphQL fetch strategy
-        // as of now (11 November 2023), Ridibooks
-        // still uses majoritarily the legacy API
-        // with graphQL used only for a few use cases.
-        // Webtoons not being one of them;
-        let mangaListGql = await this._getMangasGql();
-        mangaList.push(...mangaListGql);
-
-        return mangaList;
-    }   
-
-    /**
-     * Method to get all chapters for a webtoon.
-     * @param {IManga} manga
-     * @returns {IChapter[]}
-     * @override
-     */
-    async _getChapters(manga) {
-        let uri = new URL(`/books/${manga.id}`, this.url);
-
-        let request = new Request(uri, this.requestOptions);
-
-        return Engine.Request.fetchUI(request, 'seriesBookListJson')
-            .then(data => data.map(item => ({
-                id: item.id,
-                title: item.title
-            })));
-    }
-
-    /**
-     * Method to get all pages of a chapter.
-     * @param {Chapter} chapter
-     * @returns {Promise<string[]>}
-     * @override
-     */
-    async _getPages(chapter) {
-        let uri = new URL(
-            `/generate/${chapter.id}`,
-            this.pagesApi
-        );
-
-        let request = new Request(uri, this.requestOptions);
-
-        return this.fetchJSON(request, 3)
-            .then(res => {
-                if (!res.success) {
-                    res.login_required && 
-                        alert("Login required.");
-                    return [];
-                }
-
-                return res.pages.map(page => page.src);
-            });
-    }
-
-    /**
-     * Get the manga from the provided resource.
-     * @param {URL|string} uri
-     * @returns {Promise<Manga>}
-     * @override
-     */
-    async _getMangaFromURI(uri) {
-        if (!this.isBookPageURL(uri)) {
-            alert("Not a valid URL.");
-            return;
-        }
-
-        // first check if it already exists in cache
-        let id = this.getIdFromUri(uri);
-        let manga = await this.findMangaById(id);
-        if (manga) return manga;
-
-        // else, try new request
-        let request = new Request(uri, this.requestOptions);
-
-        return Engine.Request.fetchUI(request, "bookDetail")
-            .then(data => new Manga(
-                this,
-                data.series_id,
-                data.series_title
-            ));
-    }
-
-    // -----------------------------
-
-    /** 
-     * Helper function that checks if a given URI is a valid book page URL for the Ridibooks website.
-     * @param {string|URL} uri - The URI from which to extract the ID.
-     * @returns {boolean}
-     */
-    isBookPageURL(uri) {
-        return this.bookUrlPattern.test(String(uri));
-    }
-
-    /**
-     * Extracts the ID from a URI.
-     * 
-     * @param {string|URL} uri - The URI from which to extract the ID.
-     * @returns {string|undefined} The extracted ID, or undefined if no match is found.
-     */
-    getIdFromUri(uri) {
-        let match = String(uri).match(this.bookUrlPattern);
-        
-        if (match) return match.groups.id;
-    }
-
-    /**
-     * Finds a manga by its ID.
-     * 
-     * @async
-     * @param {string} id - The ID of the manga to find.
-     * @returns {Promise<Manga|undefined>}
-     */
-    async findMangaById(id) {
-        return Engine.Storage.loadMangaList(this.id)
-            .then(mangas => mangas.find(manga => manga.id === id))
-            .catch(() => undefined);
-    }
-
-    /**
-     * Retrieves the list of new releases using GraphQL.
-     * 
-     * @returns {Promise<IManga[]>} The list of manga items.
-     * @async
-     */
-    async _getMangasGql() {
-        // const url = new URL('/graphql', this.apiUrl);
-
-        // const operation = '';
-
-        // const query = ``;
-
-        // const variables = {};
-
-        // const data = await this.fetchGraphQL(
-        //     url,
-        //     operation,
-        //     query,
-        //     variables
-        // );
-
-        return [];
-    }
-
-    /**
-     * Retrieves a list of manga from a legacy API.
-     * 
-     * This method makes multiple requests to the API to fetch all the manga items.
-     * It starts from the first page and continues until there are no more pages left.
-     * 
-     * @returns {Promise<IManga[]>} The list of manga items.
-     * @async
-     */
-    async _getMangasLegacy() {
-        let mangaList = [];
-
-        let nextPage = new URL(
-            "/v2/category/books",
-            this.apiUrl
-        );
-
-        nextPage.search = new URLSearchParams({
+        const mangaList = [];
+        let uri = new URL("/v2/category/books", this.apiUrl);
+        uri.search = new URLSearchParams({
             category_id: 1600,
             tab: "books",
             platform: "web",
             order_by: "popular",
             limit: 60,
-            offset: 0 
+            offset: 0
         }).toString();
-
-        while(nextPage) {
-
-            let request = new Request(
-                nextPage,
-                this.requestOptions
-            );
-
-            let { data } = await this.fetchJSON(request, 3);
-
-            let pageMangaList = data.items.map(
-                ({ book }) => ({
-                    id: book.bookId,
-                    title: book.title,
-                })
-            );
-
+        while(uri) {
+            const request = new Request(uri, this.requestOptions);
+            const { data } = await this.fetchJSON(request);
+            const pageMangaList = data.items.map(({ book }) => ({ id: book.bookId, title: book.title }));
             mangaList.push(...pageMangaList);
-
-            if (nextPage = data.pagination.nextPage) {
-                nextPage = new URL(
-                    nextPage,
-                    this.apiUrl
-                );
-            }
+            uri = data.pagination.nextPage && new URL(data.pagination.nextPage, this.apiUrl);
         }
-
         return mangaList;
-    } 
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(`/books/${manga.id}`, this.url);
+        const request = new Request(uri, this.requestOptions);
+        return Engine.Request.fetchUI(request, 'seriesBookListJson');
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(`/generate/${chapter.id}`, this.pagesApi);
+        const request = new Request(uri, this.requestOptions);
+        let data = await this.fetchJSON(request);
+        if (!data.success) return [];
+        return data.pages.map(page => page.src);
+    }
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await Engine.Request.fetchUI(request, "bookDetail");
+        return new Manga(this, data.series_id, data.series_title);
+    }
 }
