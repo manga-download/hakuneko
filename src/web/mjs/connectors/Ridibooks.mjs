c731dc32dd4f968e6ddcf084b6a27aadc087d2ed
Daniel Spinola <51745663+danspinola@users.noreply.github.com>
2023-11-13 10:17:20 -0300

    Ridibooks Connector (#6426)
    * feat: _getMangas by scraping
    
    * feat: _getMangas by fetch api
    
    * feat(ridibooks):  _getChapters
    
    * feat(ridibooks): _getPages
    
    * fet(ridibooks): _getMangaFromURI
    
    * feat(ridibooks): save manga fetched from uri to cache
    
    * feat(ridibooks): first check cache on _getMangaFromURI
    
    * feat(ridibooks): add connector icon
    
    * feat(ridibooks): add gql skeleton
    
    * feat(ridibooks): final clean up
    
    * feat(Ridibooks): apply requested changes
    
    * refactor(Ridibooks): change let to const
    
    * feat(Ridibooks): change icon file for png
    
    ---------
    
    Co-authored-by: Daniel Spinola <daniel.trindade1@ibm.com>

---
+++ src/web/mjs/connectors/Ridibooks.mjs
@@ -0,0 +1,58 @@
+import Connector from "../engine/Connector.mjs";
+import Manga from "../engine/Manga.mjs";
+
+export default class Ridibooks extends Connector {
+    constructor() {
+        super();
+        this.id = 'ridibooks';
+        this.label = 'Ridibooks (Korean)';
+        this.tags = ['webtoon', 'korean'];
+        this.url = 'https://ridibooks.com';
+        this.links = {
+            login: `${this.url}/account/login`
+        };
+        this.apiUrl = "https://api.ridibooks.com";
+        this.pagesApi = "https://view.ridibooks.com";
+    }
+
+    async _getMangas() {
+        const mangaList = [];
+        let uri = new URL("/v2/category/books", this.apiUrl);
+        uri.search = new URLSearchParams({
+            category_id: 1600,
+            tab: "books",
+            platform: "web",
+            order_by: "popular",
+            limit: 60,
+            offset: 0
+        }).toString();
+        while(uri) {
+            const request = new Request(uri, this.requestOptions);
+            const { data } = await this.fetchJSON(request);
+            const pageMangaList = data.items.map(({ book }) => ({ id: book.bookId, title: book.title }));
+            mangaList.push(...pageMangaList);
+            uri = data.pagination.nextPage && new URL(data.pagination.nextPage, this.apiUrl);
+        }
+        return mangaList;
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(`/books/${manga.id}`, this.url);
+        const request = new Request(uri, this.requestOptions);
+        return Engine.Request.fetchUI(request, 'seriesBookListJson');
+    }
+
+    async _getPages(chapter) {
+        const uri = new URL(`/generate/${chapter.id}`, this.pagesApi);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        if (!data.success) return [];
+        return data.pages.map(page => page.src);
+    }
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await Engine.Request.fetchUI(request, "bookDetail");
+        return new Manga(this, data.series_id, data.series_title);
+    }
+}
