b2215124a32f16f5ec6bba9bf668e2b4e9de684c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-02 16:13:28 +0000

    Fix AzoraWorld/AzoraManga : domain, getpages etc.
    * change domain
    * fix getpages
    * AzoraManga is now a dummy connector telling use to use AzoraWorld (website are the same domain)

--- src/web/mjs/connectors/AzoraManga.mjs
+++ src/web/mjs/connectors/AzoraManga.mjs
@@ -1,6 +1,6 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import Connector from '../engine/Connector.mjs';
 
-export default class AzoraManga extends WordPressMadara {
+export default class AzoraManga extends Connector {
 
     constructor() {
         super();
@@ -9,4 +9,17 @@ export default class AzoraManga extends WordPressMadara {
         this.tags = [ 'webtoon', 'arabic' ];
         this.url = 'https://azoramanga.com';
     }
+
+    async _getMangas() {
+        throw new Error('Please use AzoraWorld connector instead.');
+    }
+
+    async _getChapters() {
+        throw new Error('Please use AzoraWorld connector instead.');
+    }
+
+    async _getPages() {
+        throw new Error('Please use AzoraWorld connector instead.');
+    }
+
 }
