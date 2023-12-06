c672f2c465c474a364dc82614a49293d8d3f8ed6
Tristan <tristan@tnet.moe>
2023-12-06 20:50:18 +0100

    feat(HeanCms): deproxify image urls

--- src/web/mjs/connectors/templates/HeanCms.mjs
+++ src/web/mjs/connectors/templates/HeanCms.mjs
@@ -77,6 +77,18 @@ export default class HeanCms extends Connector {
             throw new Error('This chapter is paywalled. Please login.');
         }
 
+        return data.map((image) => this.DeProxifyStatically(new URL(image)).href);
+    }
         return data;
+    }
+
+    // copy pasted from https://github.com/manga-download/haruneko/blob/master/web/src/engine/transformers/ImageLinkDeProxifier.ts
+    DeProxifyStatically(uri) {
+        const url = uri.href
+            // NOTE: The indonesian developer of statically.io (Frans Allen) seems to be affiliated with BacaKomik
+            //       and added support for some kind of optional URL branding => remove known brands from URL
+            .replace(/cdn\.statically\.io\/img\/(bacakomik\/)?/, '')
+            .replace(/\/(w=\d+|h=\d+|q=\d+|f=auto)(,(w=\d+|h=\d+|q=\d+|f=auto))*\//, '/');
+        return new URL(url);
     }
 }
