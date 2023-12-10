b04a76dc7197e02c7412afa3e27eb7a0d38f1bf9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-10 10:39:23 +0000

    Fix ReaperScans domain (#6521)
    Fixes https://github.com/manga-download/hakuneko/issues/6519

--- src/web/mjs/connectors/ReaperScans.mjs
+++ src/web/mjs/connectors/ReaperScans.mjs
@@ -7,9 +7,9 @@ export default class ReaperScans extends Connector {
         super.id = 'reaperscans';
         super.label = 'Reaper Scans';
         this.tags = ['webtoon', 'english'];
-        this.url = 'https://reaperscans.com';
+        this.url = 'https://reapercomics.com';
         this.links = {
-            login: 'https://reaperscans.com/login'
+            login: 'https://reapercomics.com/login'
         };
         this.path = '/comics';
         this.queryMangas = 'a.my-2.text-sm.font-medium.text-white.hover\\3A text-blue-700';
