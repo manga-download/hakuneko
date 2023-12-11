027265887005ca19e5e9ee99110cbf0ca01dbec9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-11 09:31:01 +0000

    FIX KomikTap : remove custom init (#6525)
    * Need to use JS in init procedure to counter sucuri proxy
    * Custom init not needed anymore
    
    Fixes https://github.com/manga-download/hakuneko/issues/6522

--- src/web/mjs/connectors/KomikTap.mjs
+++ src/web/mjs/connectors/KomikTap.mjs
@@ -10,18 +10,4 @@ export default class KomikTap extends WordPressMangastream {
         this.url = 'https://komiktap.me';
         this.path = '/manga/list-mode/';
     }
-
-    async _initializeConnector() {
-        const response = await fetch(this.url);
-        const newUrl = new URL(response.url);
-        this.url = newUrl.origin;
-        this.hostPattern = newUrl.host;
-        console.log(`Assigned URL '${this.url}' to ${this.label}`);
-    }
-    canHandleURI(uri) {
-        super.initialize()
-            .then(() => {
-                return uri.href.includes(this.url);
-            });
-    }
 }
