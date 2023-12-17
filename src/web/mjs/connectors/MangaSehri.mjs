c7a3716191d2b419125a78e2b851cc73419e57a6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-17 15:06:59 +0000

    FIX MangaSehri: change domain (#6574)
    and add referer
    
    Fixes  https://github.com/manga-download/hakuneko/issues/6565

--- src/web/mjs/connectors/MangaSehri.mjs
+++ src/web/mjs/connectors/MangaSehri.mjs
@@ -7,13 +7,15 @@ export default class MangaSehri extends WordPressMadara {
         super.id = 'mangasehri';
         super.label = 'Manga Şehri';
         this.tags = [ 'manga', 'webtoon', 'turkish' ];
-        this.url = 'https://mangasehri.com';
+        this.url = 'https://manga-sehri.com';
+        this.requestOptions.headers.set('x-referer', this.url);
     }
 
     async _getPages(chapter) {
         const uri = new URL(chapter.id, this.url);
         const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, this.queryPages);
-        return data.map(element => this.getAbsolutePath(element.dataset.src || element, request.url)).filter(image => !/grumpybumpers/.test(image));
+        const images = data.map(element =>this.getAbsolutePath(element.dataset.src || element, request.url)).filter(image => !/grumpybumpers/.test(image));
+        return images.map(image => this.createConnectorURI({url : image, referer : this.url}));
     }
 }
