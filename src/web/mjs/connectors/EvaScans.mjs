94a8597df68b2acea4371953b8ed02595d340c53
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-30 12:17:11 +0000

    Update EvaScans: change label and domain (#6588)
    Fixes https://github.com/manga-download/hakuneko/issues/6573

--- src/web/mjs/connectors/EvaScans.mjs
+++ src/web/mjs/connectors/EvaScans.mjs
@@ -5,9 +5,9 @@ export default class EvaScans extends WordPressMadara {
     constructor() {
         super();
         super.id = 'evascans';
-        super.label = 'EvaScans';
+        super.label = 'ManWe';
         this.tags = [ 'webtoon', 'turkish', 'scanlation' ];
-        this.url = 'https://evascans.com';
+        this.url = 'https://manwe.pro';
         this.requestOptions.headers.set('x-referer', this.url);
     }
 }
