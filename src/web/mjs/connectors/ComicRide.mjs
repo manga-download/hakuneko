54092fe37b2fb893ca561c1eb8a2b874b2174d5d
Ignacio Castro <ignaciocastrom@gmail.com>
2023-12-30 19:57:38 -0300

    [ComicRide] Fix page count from 0

--- src/web/mjs/connectors/ComicRide.mjs
+++ src/web/mjs/connectors/ComicRide.mjs
@@ -13,4 +13,12 @@ export default class ComicRide extends YoungChampion {
             login: 'https://comicride.jp/signin'
         };
     }
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 0, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
 }
