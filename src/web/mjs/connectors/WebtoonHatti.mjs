74a5fc489df60e2402323f7d1354a710e76470e0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-06 11:00:36 +0000

    Fix WebtoonHatti : filter pictures
    Fixes https://github.com/manga-download/hakuneko/issues/6649

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -9,4 +9,17 @@ export default class WebtoonHatti extends WordPressMadara {
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://webtoonhatti.net';
     }
+
+    async _getPages(chapter) {
+        const excludes = [
+            /tr2.png$/i,
+            /b2.jpg$/i,
+            /romantiktr\.tr/i
+        ];
+        const images = await super._getPages(chapter);
+        return images.filter(picture => {
+            const pic= JSON.parse(atob(new URL(picture).searchParams.get('payload')));
+            return !excludes.some(rgx => rgx.test(pic.url));
+        });
+    }
 }
