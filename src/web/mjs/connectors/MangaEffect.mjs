5c1b59456423a5a63567165950e445d26f15639e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-23 10:23:57 +0000

    FIX MangaEffect : no more wpmangaprotector (#6699)
    Fixes https://github.com/manga-download/hakuneko/issues/6698

--- src/web/mjs/connectors/MangaEffect.mjs
+++ src/web/mjs/connectors/MangaEffect.mjs
@@ -12,21 +12,4 @@ export default class MangaEffect extends WordPressMadara {
     _createMangaRequest(page) {
         return new Request(new URL(`/manga/page/${page}/`, this.url), this.requestOptions);
     }
-
-    async _getPages(chapter) {
-        const url = new URL(chapter.id, this.url);
-        const request = new Request(url, this.requestOptions);
-        const script = `
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
-                        format: CryptoJSAesJson
-                    }).toString(CryptoJS.enc.Utf8));
-                    resolve(JSON.parse(imgdata));
-                },3000);
-            });
-        `;
-        const data = await Engine.Request.fetchUI(request, script);
-        return data.map(picture => this.createConnectorURI({url : picture, referer : url}));
-    }
 }
