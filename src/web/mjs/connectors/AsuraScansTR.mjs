8a5d1f414cb83ff7e13e96744102d0e118f8844a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-25 07:59:29 +0000

    FIx AsuraScansTR : domain, Madara, no junk
    Fixes https://github.com/manga-download/hakuneko/issues/6707

--- src/web/mjs/connectors/AsuraScansTR.mjs
+++ src/web/mjs/connectors/AsuraScansTR.mjs
@@ -1,31 +1,16 @@
-import WordPressMangastream from './templates/WordPressMangastream.mjs';
+import WordPressMadara from './templates/WordPressMadara.mjs';
 
-export default class AsuraScansTR extends WordPressMangastream {
+export default class AsuraScansTR extends WordPressMadara {
 
     constructor() {
         super();
         super.id = 'asurascans-tr';
         super.label = 'Asura Scans (TR)';
         this.tags = ['webtoon', 'turkish'];
-        this.url = 'https://armoniscans.com';
-        this.path = '/manga/list-mode/';
-        this.queryPages = 'div#readerarea p img';
-        this.requestOptions.headers.set('x-user-agent', 'Mozilla/5.0 (Linux; Android 9; Pixel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4026.0 Mobile Safari/537.36');
+        this.url = 'https://asurascans.com.tr';
     }
 
     get icon() {
         return '/img/connectors/asurascans';
     }
-
-    async _getPages(chapter) {
-        const excludes = [
-            /panda_gif_large/i,
-            /2021\/04\/page100-10\.jpg/i,
-            /2021\/03\/20-ending-page-\.jpg/i,
-            /ENDING-PAGE/i
-        ];
-        const images = await super._getPages(chapter);
-        return images.filter(link => !excludes.some(rgx => rgx.test(link)));
-    }
-
 }
