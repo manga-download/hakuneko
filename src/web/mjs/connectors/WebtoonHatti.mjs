0faf428cdedccaa0f1c883e250d5bf0ce65dfd7d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-27 11:00:11 +0000

    WebtonHatti better CSS for pictures

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -8,18 +8,6 @@ export default class WebtoonHatti extends WordPressMadara {
         super.label = 'Webtoon Hatti';
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://webtoonhatti.net';
-    }
-
-    async _getPages(chapter) {
-        const excludes = [
-            /tr2.png$/i,
-            /b2.jpg$/i,
-            /romantiktr\.tr/i
-        ];
-        const images = await super._getPages(chapter);
-        return images.filter(picture => {
-            const pic= JSON.parse(atob(new URL(picture).searchParams.get('payload')));
-            return !excludes.some(rgx => rgx.test(pic.url));
-        });
+        this.queryPages = 'div.page-break source.wp-manga-chapter-img';
     }
 }
