8fd724789e975b83c813fa2ba5f41aeedcaa43ba
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-12 11:23:02 +0000

    Update WebtoonHatti.mjs

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -8,28 +8,6 @@ export default class WebtoonHatti extends WordPressMadara {
         super.label = 'Webtoon Hatti';
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://webtoonhatti.net';
-    }
-
-    async _getPages(chapter) {
-        const pages = await super._getPages(chapter);
-        const pageList = [];
-
-        for (const page of pages) {
-            let pageUrl = new URL(page).searchParams.get('payload');
-            pageUrl= JSON.parse(atob(pageUrl)).url;
-            const request = new Request(pageUrl, {
-                method : 'HEAD',
-            });
-
-            try {
-                const response = await fetch(request);
-                if (response.status == 200) pageList.push(page);
-            } catch (error) {
-                //
-            }
-
-            await this.wait(150);
-        }
-        return pageList;
+        this.queryPages = 'div.page-break:not([style*="display:"]) source';
     }
 }
