6b2f6ec64060194a1527b7759601f01a9471d6ce
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-12 10:56:25 +0000

    WebtoonHatti : filter pictures again
    🐈💑🚅

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -8,6 +8,28 @@ export default class WebtoonHatti extends WordPressMadara {
         super.label = 'Webtoon Hatti';
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://webtoonhatti.net';
-        this.queryPages = 'div.page-break source.wp-manga-chapter-img';
+    }
+
+    async _getPages(chapter) {
+        const pages = await super._getPages(chapter);
+        const pageList = [];
+
+        for (const page of pages) {
+            let pageUrl = new URL(page).searchParams.get('payload');
+            pageUrl= JSON.parse(atob(pageUrl)).url;
+            const request = new Request(pageUrl, {
+                method : 'HEAD',
+            });
+
+            try {
+                const response = await fetch(request);
+                if (response.status == 200) pageList.push(page);
+            } catch (error) {
+                //
+            }
+
+            await this.wait(150);
+        }
+        return pageList;
     }
 }
