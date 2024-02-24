16ba380f195115d6184b1aaf6bced734590d84a3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-24 19:51:11 +0000

    FIx WebtoonHatti: fix getting pages, again

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -8,6 +8,30 @@ export default class WebtoonHatti extends WordPressMadara {
         super.label = 'Webtoon Hatti';
         this.tags = [ 'webtoon', 'turkish' ];
         this.url = 'https://webtoonhatti.net';
-        this.queryPages = 'div.page-break:not([style*="display:"]) source';
+    }
+
+    async _getPages(chapter) {
+        const pages = await super._getPages(chapter);
+        const promises = [];
+        for(const page of pages) {
+            const promise = new Promise( (resolve, reject) => {
+                const pageData= JSON.parse(atob(new URL(page).searchParams.get( 'payload' )));
+                const request = new Request(pageData.url, {
+                    method : 'HEAD',
+                });
+                request.headers.set('x-referer', pageData.referer);
+
+                try {
+                    fetch(request)
+                        .then(response => response.status == 200 ? resolve(page) : reject());
+                } catch(error) {
+                    reject();
+                }
+            });
+            promises.push(promise);
+        }
+
+        const results = await Promise.allSettled(promises);
+        return results.filter(promise => /fulfilled/i.test(promise.status)).map(promise => promise.value);
     }
 }
