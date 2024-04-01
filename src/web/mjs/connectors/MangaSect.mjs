c7a1cd85d6b9f932b6e5c9c6691a1def0c69923e
Victor Abrate <me@vab.one>
2024-04-01 20:10:04 +0200

    fix MangaSect source

--- src/web/mjs/connectors/MangaSect.mjs
+++ src/web/mjs/connectors/MangaSect.mjs
@@ -22,23 +22,24 @@ export default class MangaSect extends Connector {
         let mangaList = [];
         const uri = new URL(this.path, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.blog-pager span:last-of-type a');
-        const pageCount = parseInt(data[0].href.match(/\/(\d)+\//)[1]);
+        const data = await this.fetchDOM(request, "ul.pagination");
+        const paginationInfo = data[0].children[0].innerText;
+        const rawPageCount = paginationInfo.split("/");
+        const pageCount = parseInt(rawPageCount[1]);
         for(let page = 1; page <= pageCount; page++) {
-            const mangas = await this._getMangasFromPage(page);
-            mangaList.push(...mangas);
+        const mangas = await this._getMangasFromPage(page);
+        mangaList.push(...mangas);
         }
         return mangaList;
     }
-
     async _getMangasFromPage(page) {
         const uri = new URL(this.path + page, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.grid div.text-center > a');
-        return data.map(element => {
+        const data = await this.fetchDOM(request, "figure.clearfix div.image a");
+        return data.map((element) => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.text.trim()
+                title: element.title,
             };
         });
     }
@@ -46,25 +47,33 @@ export default class MangaSect extends Connector {
     async _getChapters(manga) {
         const uri = new URL(manga.id, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'li.chapter > a');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.text.trim()
-            };
+        const data = await this.fetchDOM(request, "div.chapter a");
+        return data.map((element) => {
+        return {
+            id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+            title: element.text.trim(),
+        };
         });
     }
 
     async _getPages(chapter) {
         const referer = new URL(chapter.id, this.url);
-        const chapterid = chapter.id.match(/\/([\d]+)$/)[1];
-        const uri = new URL ('/ajax/image/list/chap/' + chapterid, this.url);
-        const request = new Request(uri, this.requestOptions);
-        request.headers.set('x-referer', referer);
-        request.headers.set('X-Requested-With', 'XMLHttpRequest');
-        const response = await this.fetchJSON(request);
-        const dom = this.createDOM(response.html);
-        const data = dom.querySelectorAll('source[data-src]');
-        return Array.from(data).map(image => this.getAbsolutePath(image.dataset['src'], request.url));
+        const request = new Request(referer, this.requestOptions);
+        request.headers.set("x-referer", referer);
+        request.headers.set("X-Requested-With", "XMLHttpRequest");
+        const response = await this.fetchDOM(request);
+        const regex = /const\s+CHAPTER_ID\s+=\s+(\d+);/;
+        const match = regex.exec(response.innerText);
+        const chapterId = match[1];
+        const uri = new URL("/ajax/image/list/chap/" + chapterId, this.url);
+        const ajaxReq = new Request(uri, this.requestOptions);
+        ajaxReq.headers.set("x-referer", referer);
+        ajaxReq.headers.set("X-Requested-With", "XMLHttpRequest");
+        const ajaxResponse = await this.fetchJSON(ajaxReq);
+        const dom = this.createDOM(ajaxResponse.html);
+        const ajaxData = dom.querySelectorAll("source[data-real]");
+        return Array.from(ajaxData).map((image) =>
+        this.getAbsolutePath(image.dataset["real"], ajaxReq.url)
+        );
     }
 }
