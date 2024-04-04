51f081cbadcf6e2bfe698293b1240acb998a7968
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-04 18:49:16 +0000

    better mangasect fix

--- src/web/mjs/connectors/MangaSect.mjs
+++ src/web/mjs/connectors/MangaSect.mjs
@@ -1,7 +1,6 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import MojoPortalComic from './templates/MojoPortalComic.mjs';
 
-export default class MangaSect extends Connector {
+export default class MangaSect extends MojoPortalComic {
 
     constructor() {
         super();
@@ -12,68 +11,30 @@ export default class MangaSect extends Connector {
         this.path = '/all-manga/';
     }
 
-    async _getMangaFromURI(uri) {
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'header h1');
-        return new Manga(this, uri.pathname, data[0].textContent.trim());
-    }
-
-    async _getMangas() {
-        let mangaList = [];
-        const uri = new URL(this.path, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, "ul.pagination");
-        const paginationInfo = data[0].children[0].innerText;
-        const rawPageCount = paginationInfo.split("/");
-        const pageCount = parseInt(rawPageCount[1]);
-        for(let page = 1; page <= pageCount; page++) {
-            const mangas = await this._getMangasFromPage(page);
-            mangaList.push(...mangas);
-        }
-        return mangaList;
-    }
-    async _getMangasFromPage(page) {
-        const uri = new URL(this.path + page, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, "figure.clearfix div.image a");
-        return data.map((element) => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.title,
-            };
-        });
-    }
-
-    async _getChapters(manga) {
-        const uri = new URL(manga.id, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, "div.chapter a");
-        return data.map((element) => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.text.trim(),
-            };
-        });
-    }
-
     async _getPages(chapter) {
-        const referer = new URL(chapter.id, this.url);
-        const request = new Request(referer, this.requestOptions);
-        request.headers.set("x-referer", referer);
-        request.headers.set("X-Requested-With", "XMLHttpRequest");
-        const response = await this.fetchDOM(request);
-        const regex = /const\s+CHAPTER_ID\s+=\s+(\d+);/;
-        const match = regex.exec(response.innerText);
-        const chapterId = match[1];
-        const uri = new URL("/ajax/image/list/chap/" + chapterId, this.url);
-        const ajaxReq = new Request(uri, this.requestOptions);
-        ajaxReq.headers.set("x-referer", referer);
-        ajaxReq.headers.set("X-Requested-With", "XMLHttpRequest");
-        const ajaxResponse = await this.fetchJSON(ajaxReq);
-        const dom = this.createDOM(ajaxResponse.html);
-        const ajaxData = dom.querySelectorAll("source[data-real]");
-        return Array.from(ajaxData).map((image) =>
-            this.getAbsolutePath(image.dataset["real"], ajaxReq.url)
-        );
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        const script = `
+            new Promise(resolve => {
+
+                function parseResults(data) {
+                    const dom = new DOMParser().parseFromString(data, 'text/html');
+                    let nodes = [...dom.querySelectorAll('img')];
+                    resolve(nodes.map(element => element.dataset.original));
+                }
+
+                const ajaxendpoint = new URL('/ajax/image/list/chap/' + CHAPTER_ID, window.location.href);
+                fetch(ajaxendpoint, {
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(jsonData => {
+                        parseResults(jsonData.html);
+                    });
+            });
+        `;
+        return Engine.Request.fetchUI(request, script);
     }
+
 }
