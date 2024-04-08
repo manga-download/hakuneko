20bd8c1c811f77c3c0880eb4e1078d182de5658a
akn19 <akunhitam2019@gmail.com>
2024-04-08 15:50:49 +0700

    fixes manga title

--- src/web/mjs/connectors/MonoManga.mjs
+++ src/web/mjs/connectors/MonoManga.mjs
@@ -10,4 +10,10 @@ export default class MonoManga extends WordPressMadara {
         this.url = 'https://monomanga.com';
         this.requestOptions.headers.set('x-referer', this.url);
     }
+
+    async _getMangas() {
+        const mangas = await super._getMangas();
+        mangas.forEach(manga => manga.title = manga.title.replace(/Bahasa Indonesia$/i, '').trim());
+        return mangas;
+    }
 }
