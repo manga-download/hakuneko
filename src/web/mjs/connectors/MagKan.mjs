80ba83b8db3f6f684692db3839ac9920c63ca7b3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-17 07:27:21 +0000

    update from master (#6983)
    * Fix MangaToon: change CSS for chapters (#6887)
    
    get rid of duplicate chapters
    
    * Fix ComicK: update domain (#6897)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6896
    
    * Fix MangaSail : change domain (#6898)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6894
    
    * Fix MangaBat : change domain (#6899)
    
    * Fix MangaStarz : change domain (#6901)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6900
    
    * change domain (#6910)
    
    Co-authored-by: Abdullah-KS <78895454+Abdullah-KS@users.noreply.github.com>
    
    * FIx Siyahmelek: change domain (#6915)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6908
    
    * Fix MangaTX : domain changed (#6916)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6911
    
    * Fix TopToon : update CSS selectors (#6906)
    
    * Fix TopToon : update CSS selectors
    
    Fixes https://github.com/manga-download/hakuneko/issues/6905
    
    * Update TopToon.mjs
    
    * Update TopToon.mjs
    
    * attempt to fix lint error
    
    * dont use optional chaining
    
    optional chaining doesnt work in stable build because electron version is too old
    
    * Fix MagKan: use Comici Viewer (#6927)
    
    Similar to YoungChampion, YoungAnimal, Comicride, ComicMedu, etc....
    
    Fixes https://github.com/manga-download/hakuneko/issues/6926
    
    * Fix winterscan : change domain (#6935)
    
    https://github.com/manga-download/hakuneko/issues/6933
    
    * fix MangaSect source (#6930)
    
    * fix MangaSect source
    
    * fix lint
    
    * better mangasect fix
    
    ---------
    
    Co-authored-by: Victor Abrate <me@vab.one>
    Co-authored-by: MikeZeDev <MikeZeDev@users.noreply.github.com>
    
    * Fix MoonWitchInLove : change domain (#6942)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6936
    
    * add connector monzeekomik (#6954)
    
    * add connector monzeekomik
    
    * fix lint
    
    ---------
    
    Co-authored-by: MikeZeDev <MikeZeDev@users.noreply.github.com>
    
    * Fix ComicExtra : change domain (#6951)
    
    * FIx KomikAV: domain type (#6881)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6879
    
    * Fix Saikaiscan : change domain (#6963)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6620
    
    * Fix LxHentai: change domain (#6970)
    
    Fixes https://github.com/manga-download/hakuneko/issues/6968
    
    * Lock ESLint Version (#6971)
    
    * lock eslint version
    
    * fix lint errors
    
    ---------
    
    Co-authored-by: ZIRO <78895454+PD-ZIRO@users.noreply.github.com>
    Co-authored-by: Abdullah-KS <78895454+Abdullah-KS@users.noreply.github.com>
    Co-authored-by: Victor Abrate <victor.abrate@gmail.com>
    Co-authored-by: Victor Abrate <me@vab.one>
    Co-authored-by: akn19 <86169232+akn19@users.noreply.github.com>
    Co-authored-by: Ronny <wegener.ronny@gmail.com>

--- src/web/mjs/connectors/MagKan.mjs
+++ src/web/mjs/connectors/MagKan.mjs
@@ -1,59 +1,12 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import YoungChampion from './YoungChampion.mjs';
 
-export default class MagKan extends Connector {
+export default class MagKan extends YoungChampion {
 
     constructor() {
         super();
         super.id = 'magkan';
         super.label = 'MagKan';
         this.tags = [ 'manga', 'japanese' ];
-        this.url = 'http://kansai.mag-garden.co.jp';
-    }
-
-    async _getMangaFromURI(uri) {
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'meta[property="og:title"]');
-        return new Manga(this, uri.pathname, data[0].content.trim());
-    }
-
-    async _getMangas() {
-        const uri = new URL(this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div#main div.panel div.box div.inner');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('a'), this.url),
-                title: element.querySelector('h2.comic_name').textContent.trim()
-            };
-        });
-    }
-
-    async _getChapters(manga) {
-        const uri = new URL(manga.id, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const body = await this.fetchDOM(request);
-        const current = [...body.querySelectorAll('div#main div.update_summary div.exp ul.btn li a[href*="/assets/files/"]')].map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url).replace(/\/HTML5\/?$/i, ''),
-                title: element.text.replace('を読む', '').trim()
-            };
-        });
-        const previous = [...body.querySelectorAll('div#main div.sam_exp div.exp')].map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element.querySelector('ul.btn li a[href*="/assets/files/"]'), this.url).replace(/\/HTML5\/?$/i, ''),
-                title: element.querySelector('div.back_number_summary div.ttl').textContent.trim()
-            };
-        });
-        return [ ...current, ...previous ];
-    }
-
-    async _getPages(chapter) {
-        const uri = new URL(chapter.id + '/iPhone/ibook.xml', this.url);
-        const request = new Request(uri, this.requestOptions);
-        const response = await fetch(request);
-        const data = await response.text();
-        const pages = parseInt(data.match(/<total>(\d+)<\/total>/)[1]);
-        return new Array(pages).fill().map((_, index) => this.getAbsolutePath(`${chapter.id}/books/images/2/${index + 1}.jpg`, request.url));
+        this.url = 'https://kansai.mag-garden.co.jp';
     }
 }
