16cebe8772583e9d6fedb9d4f2afaf20d1cdd756
Yevu David Efui <42614123+YurDavid@users.noreply.github.com>
2024-04-18 15:17:01 +0000

    updated connector drakescans (#6995)
    * updated connector drakescans
    
    * change to correct class name

--- src/web/mjs/connectors/DrakeScans.mjs
+++ src/web/mjs/connectors/DrakeScans.mjs
@@ -1,12 +1,12 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
 
-export default class DrakeScans extends WordPressMadara {
-
+export default class DrakeScans extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'drakescans';
         super.label = 'DrakeScans';
         this.tags = [ 'webtoon', 'english'];
         this.url = 'https://drakescans.com';
+        this.path = '/manga/list-mode/';
     }
 }
