274fdda5d680849e4897e5d749a80457215b6b45
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-24 18:06:24 +0000

    Fix ShinigamiID: domain & break encryption (#7023)
    custom WPmangaprotector, cute
    Fixes https://github.com/manga-download/hakuneko/issues/6667

--- src/web/mjs/connectors/ReaperScansID.mjs
+++ src/web/mjs/connectors/ReaperScansID.mjs
@@ -6,12 +6,26 @@ export default class ReaperScansID extends WordPressMadara {
         super.id = 'reaperscansid';
         super.label = 'Shinigami ID';
         this.tags = ['webtoon', 'indonesian', 'scanlation'];
-        this.url = 'https://shinigami.sh';
+        this.url = 'https://shinigami.cx';
         this.queryChapters = 'div.chapter-link > a';
         this.queryChaptersTitleBloat ='span.chapter-release-date';
-        this.queryPages = 'div.page-break source[data-src]';
         this.links = {
-            login: 'https://shinigami.sh/login'
+            login: 'https://shinigami.cx/login'
         };
+    }
+
+    async _getPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const script = `
+            new Promise((resolve, reject) => {
+                const key = post_id + 'hJ1nA7qt0fMxGPfW3WlD5QuRy1HBTOnukhP9JE' + post_id + 'aBTSjD3cSKEJEKMI34mSxRUm98Xu4hXp71YTWJ5lUnP' + post_id;
+                const imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, key, {
+                    format: CryptoJSAesJson
+                }).toString(CryptoJS.enc.Utf8));
+                resolve((imgdata));
+            });
+        `;
+        return await Engine.Request.fetchUI(request, script);
     }
 }
