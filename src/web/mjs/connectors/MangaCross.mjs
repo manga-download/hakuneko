26e7aab80b2ab55eb26d82a9d37b47a0e35a959c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-05-04 15:33:06 +0000

    Fix MangaCross: extends Comici & Rebrand (#7007)
    * Fix MangaCross: extends Comici & Rebrand
    
    Fixes https://github.com/manga-download/hakuneko/issues/7006
    
    * update icon

--- src/web/mjs/connectors/MangaCross.mjs
+++ src/web/mjs/connectors/MangaCross.mjs
@@ -1,54 +1,13 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import YoungChampion from './YoungChampion.mjs';
 
-export default class MangaCross extends Connector {
+export default class MangaCross extends YoungChampion {
 
     constructor() {
         super();
         super.id = 'mangacross';
-        super.label = 'MangaCross';
+        super.label = 'ChampionCross';
         this.tags = [ 'manga', 'japanese' ];
-        this.url = 'https://mangacross.jp';
-    }
-
-    async _getMangaFromURI(uri) {
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'head title');
-        let id = uri.pathname.split('/').filter(Boolean).pop();
-        let title = data[0].textContent.split('|')[0].trim();
-        return new Manga(this, id, title);
-    }
-
-    async _getMangas() {
-        let uri = new URL('/api/comics.json', this.url);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchJSON(request);
-        return data.comics.map(comic => {
-            return {
-                id: comic.dir_name,
-                title: comic.title.trim()
-            };
-        });
-    }
-
-    async _getChapters(manga) {
-        let uri = new URL('/api/comics/' + manga.id + '.json', this.url);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchJSON(request);
-        // Is there a way to access the "private" chapters ? Logging in didn't change anything...
-        return data.comic.episodes.filter(episode => episode.status == 'public').map(episode => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(episode.page_url, this.url),
-                title: episode.volume.trim(),
-                language: ''
-            };
-        });
-    }
-
-    async _getPages(chapter) {
-        let uri = new URL(chapter.id + '/viewer.json', this.url);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchJSON(request);
-        return data.episode_pages.map(page => this.createConnectorURI(this.getAbsolutePath(page.image.pc_url, this.url)));
+        this.url = 'https://championcross.jp';
+        this.queryManga = 'div.article-text > a';
     }
 }
