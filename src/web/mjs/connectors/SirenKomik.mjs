6a76f764e0652823adbfa8c28873631bc522e880
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-05-04 15:39:22 +0000

    Fix SirenKomik: fix chapters list + get pictures without JS (#7017)
    Fixes https://github.com/manga-download/hakuneko/issues/6880

--- src/web/mjs/connectors/SirenKomik.mjs
+++ src/web/mjs/connectors/SirenKomik.mjs
@@ -9,5 +9,19 @@ export default class SirenKomik extends WordPressMangastream {
         this.tags = ['manga', 'indonesian'];
         this.url = 'https://sirenkomik.my.id';
         this.path = '/manga/list-mode/';
+        this.queryChapters = 'div#chaptermanga ul li div.kotak-chapter div.episode a';
+        this.queryChaptersTitle = 'span.nomer-chapter';
+    }
+
+    async _initializeConnector() {
+        // for some reasons, fetchui never ends and we reach timeout. I have to put an empty _initializeConnector
+    }
+
+    async _getPages(chapter) {
+        const url = new URL(chapter.id, this.url);
+        const request = new Request(url, this.requestOptions);
+        const [ dom ] = await this.fetchDOM(request, 'body');
+        const matches = dom.innerHTML.match(/"images"\s*:\s*(\[.*\])\s*}/)[1];
+        return JSON.parse(matches);
     }
 }
