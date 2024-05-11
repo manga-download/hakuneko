1afadb3f335ab7dd252bee8fd9bc0b9a55f24b77
yoanhg421 <yoanhg421@gmail.com>
2024-05-11 07:06:41 -0700

    fix linting issues

--- src/web/mjs/connectors/ReadAllComics.mjs
+++ src/web/mjs/connectors/ReadAllComics.mjs
@@ -1,47 +1,47 @@
-import Connector from '../engine/Connector.mjs'
+import Connector from '../engine/Connector.mjs';
 
 export default class ReadAllComics extends Connector {
     constructor() {
-        super()
-        super.id = 'readallcomics'
-        super.label = 'Read All Comics'
-        this.tags = ['comics', 'english']
-        this.url = 'https://readallcomics.com'
-        this.requestOptions.headers.set('x-referer', this.url)
+        super();
+        super.id = 'readallcomics';
+        super.label = 'Read All Comics';
+        this.tags = ['comics', 'english'];
+        this.url = 'https://readallcomics.com';
+        this.requestOptions.headers.set('x-referer', this.url);
     }
 
     async _getMangas() {
         let request = new Request(
             this.url + '/?story=&s=&type=comic',
             this.requestOptions
-        )
-        let data = await this.fetchDOM(request, 'ul.categories li a')
+        );
+        let data = await this.fetchDOM(request, 'ul.categories li a');
         return data.map((element) => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
                 title: element.text.trim(),
-            }
-        })
+            };
+        });
     }
 
     async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions)
-        let data = await this.fetchDOM(request, 'ul.list-story  li  a')
+        let request = new Request(this.url + manga.id, this.requestOptions);
+        let data = await this.fetchDOM(request, 'ul.list-story  li  a');
         return data.map((element) => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
                 title: element.text.trim(),
                 language: '',
-            }
-        })
+            };
+        });
     }
 
     async _getPages(chapter) {
-        let request = new Request(this.url + chapter.id, this.requestOptions)
+        let request = new Request(this.url + chapter.id, this.requestOptions);
         let data = await this.fetchDOM(
             request,
-            'body > center:nth-child(5) > center > div:nth-child(2) source'
-        )
-        return data.map((element) => this.getAbsolutePath(element, request.url))
+            'body > center > center > div source'
+        );
+        return data.map((element) => this.getAbsolutePath(element, request.url));
     }
 }
