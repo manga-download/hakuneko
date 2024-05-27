b153da106058a8f1d63f3e28380dfc7d76f38c88
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-05-27 11:30:15 +0000

    ShueishaMangaPlus: api changes + fix getting chapters
    Fixes https://github.com/manga-download/hakuneko/issues/7110

--- src/web/mjs/connectors/ShueishaMangaPlus.mjs
+++ src/web/mjs/connectors/ShueishaMangaPlus.mjs
@@ -16,48 +16,63 @@ export default class ShueishaMangaPlus extends Publus {
 
     _getLanguage(language) {
         const languages = {
-            1: '[es]', 2: '[fr]', 3: '[id]', 4: '[pt-br]', 5: '[ru]', 6: '[th]'
+            0: ['en'], 1: '[es]', 2: '[fr]', 3: '[id]', 4: '[pt-br]', 5: '[ru]', 6: '[th]', 7: '[de]', 8: '[unk]', 9: '[vi]'
         };
         return languages[language] || '[en]';
     }
 
     async _getMangaFromURI(uri) {
-        let id = uri.pathname.match(/\/(\d+)$/)[1];
-        uri = new URL('/api/title_detail', this.apiURL);
+        const id = uri.pathname.match(/\/(\d+)$/)[1];
+        uri = new URL('/api/title_detailV3', this.apiURL);
         uri.searchParams.set('title_id', id);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
         let title = data.success.titleDetailView.title;
-        title = `${title.name} ${this._getLanguage(title.language)}`;
+        title = `${title.name} ${this._getLanguage(title.language)}`.trim();
         return new Manga(this, id, title);
     }
 
     async _getMangas() {
-        let request = new Request(new URL('/api/title_list/all', this.apiURL), this.requestOptions);
-        let data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
-        return data.success.allTitlesView.titles.map(manga => {
-            return {
-                id: manga.titleId,
-                title: `${manga.name} ${this._getLanguage(manga.language)}`
-            };
-        });
+        const mangalist = [];
+        const request = new Request(new URL('/api/title_list/allV2', this.apiURL), this.requestOptions);
+        const data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
+        for (const group of data.success.allTitlesViewV2.alltitlegroups) {
+            mangalist.push(...group.titles.map(manga => {
+                return {
+                    id: manga.titleId.toString(),
+                    title: `${manga.name} ${this._getLanguage(manga.language)}`.trim(),
+                    language: `${this._getLanguage(manga.language)}`
+                };
+            }));
+        }
+        return mangalist;
     }
 
     async _getChapters(manga) {
-        let uri = new URL('/api/title_detail', this.apiURL);
+        let uri = new URL('/api/title_detailV3', this.apiURL);
         uri.searchParams.set('title_id', manga.id);
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
-        return [
-            ...data.success.titleDetailView.firstChapterList || [],
-            ...data.success.titleDetailView.lastChapterList || []
-        ].map(chapter => {
-            return {
-                id: chapter.chapterId,
-                title: chapter.subTitle || chapter.name,
-                language: ''
-            };
-        }).reverse();
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
+        const result = [];
+
+        for (const chapterListGroup of data.success.titleDetailView.chapterListGroup) {
+            const chapters = [
+                ...chapterListGroup.firstChapterList || [],
+                ...chapterListGroup.midChapterList || [],
+                ...chapterListGroup.lastChapterList || []
+            ].map(chapter => {
+                return {
+                    id: chapter.chapterId,
+                    title: chapter.subTitle || chapter.name,
+                    language: ''
+                };
+            }).reverse();
+
+            result.push(...chapters);
+        }
+
+        return result;
+
     }
 
     async _getPages(chapter) {
@@ -67,18 +82,23 @@ export default class ShueishaMangaPlus extends Publus {
         uri.searchParams.set('split', 'yes');
         let request = new Request(uri, this.requestOptions);
         let data = await this.fetchPROTO(request, this.protoTypes, this.rootType);
-        return data.success.mangaViewer.pages
-            .filter(page => page.mangaPage)
-            .map(page => {
-                let image = page.mangaPage;
-                image = Object.assign(image, {
-                    mode: image.encryptionKey ? 'xor' : 'raw',
-                    encryption: {
-                        pattern: null,
-                        key: image.encryptionKey
-                    }
+
+        try {
+            return data.success.mangaViewer.pages
+                .filter(page => page.mangaPage)
+                .map(page => {
+                    let image = page.mangaPage;
+                    image = Object.assign(image, {
+                        mode: image.encryptionKey ? 'xor' : 'raw',
+                        encryption: {
+                            pattern: null,
+                            key: image.encryptionKey
+                        }
+                    });
+                    return image.mode === 'raw' ? image.imageUrl : this.createConnectorURI(image);
                 });
-                return image.mode === 'raw' ? image.imageUrl : this.createConnectorURI(image);
-            });
+        } catch( error) {
+            return [];
+        }
     }
 }
