25af70bd59de25951b8c787e4edbfd38adce9fc7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-05-29 06:39:31 +0000

    ManhuaScan => KaliScan (#7107)
    basically, change domain

--- src/web/mjs/connectors/ManhuaScan.mjs
+++ src/web/mjs/connectors/ManhuaScan.mjs
@@ -5,9 +5,9 @@ export default class ManhuaScan extends MadTheme {
     constructor() {
         super();
         super.id = 'manhuascan';
-        super.label = 'ManhuaScan';
+        super.label = 'KaliScan';
         this.tags = ['manga', 'webtoon', 'hentai', 'multi-lingual'];
-        this.url = 'https://manhuascan.io';
+        this.url = 'https://kaliscan.io';
         this.requestOptions.headers.set('x-referer', this.url + '/');
     }
 
