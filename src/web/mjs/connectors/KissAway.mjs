b68fbb1e3496fe6c9e9aea9485bd91733570979b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-01 09:57:32 +0000

    fix lint and stuff

--- src/web/mjs/connectors/KissAway.mjs
+++ src/web/mjs/connectors/KissAway.mjs
@@ -9,16 +9,13 @@ export default class KissAway extends FlatManga {
         this.tags = [ 'manga', 'raw', 'japanese' ];
         this.url = 'https://klz9.com';
         this.requestOptions.headers.set('x-referer', this.url);
-        this.TO_PATH_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     }
 
     generateRandomStr(length) {
-        let list = [... Array(length).keys()];
-        return list.map(i => {
-            const max = this.TO_PATH_CHARACTERS.length;
-            const randomIndex = Math.floor(Math.random() * max);
-            return this.TO_PATH_CHARACTERS.charAt(randomIndex);
-        }).join('');
+        const charmap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result ='';
+        for (let o = 0; o < length; o++) result += charmap.charAt(Math.floor(Math.random() * charmap.length));
+        return result;
     }
 
     async _getChapters(manga) {
@@ -48,7 +45,7 @@ export default class KissAway extends FlatManga {
 
     async _getPages(chapter) {
         const uri = new URL(chapter.id, this.url);
-        const randomStr = this.generateRandomStr(30)
+        const randomStr = this.generateRandomStr(30);
         const script = `
             new Promise(async resolve => {
             	const chapId = document.querySelector('input#chapter').value;
@@ -65,5 +62,4 @@ export default class KissAway extends FlatManga {
         const data = await Engine.Request.fetchUI(request, script);
         return data.map(link => this.createConnectorURI(link));
     }
-
 }
