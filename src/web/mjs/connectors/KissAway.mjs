e5596ef961eeed35dffc6021f961d6723eaf038e
realaber <realabertian@gmail.com>
2024-06-01 08:30:45 +0800

    fix with random extra character

--- src/web/mjs/connectors/KissAway.mjs
+++ src/web/mjs/connectors/KissAway.mjs
@@ -9,17 +9,33 @@ export default class KissAway extends FlatManga {
         this.tags = [ 'manga', 'raw', 'japanese' ];
         this.url = 'https://klz9.com';
         this.requestOptions.headers.set('x-referer', this.url);
+        this.TO_PATH_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    }
+
+    generateRandomStr(length) {
+        let list = [... Array(length).keys()]
+        return list.map(i => {
+            const max = this.TO_PATH_CHARACTERS.length;
+            const randomIndex = Math.floor(Math.random() * max);
+            return this.TO_PATH_CHARACTERS.charAt(randomIndex);
+        }).join('');
     }
 
     async _getChapters(manga) {
+        const randomStr = this.generateRandomStr(25)
         const script = `
             new Promise(async resolve => {
-                const nodes = document.querySelectorAll("div#list-chapter a")
-                const chapters = Array.prototype.map.call(nodes, item => {
+                const uri = new URL('${randomStr}.lstc', window.location.origin);
+                uri.searchParams.set('slug', dataL);
+                const response = await fetch(uri);
+                data = await response.text();
+                const dom = new DOMParser().parseFromString(data, "text/html");
+                const nodes = [...dom.querySelectorAll('a.chapter[title]')];
+                const chapters= nodes.map(chapter => {
                     return {
-                        id : item.href,
-                        title : item.title
-                    }
+                        id : chapter.pathname,
+                        title : chapter.title
+                    };
                 });
                 resolve(chapters);
             });
@@ -32,10 +48,17 @@ export default class KissAway extends FlatManga {
 
     async _getPages(chapter) {
         const uri = new URL(chapter.id, this.url);
+        const randomStr = this.generateRandomStr(30)
         const script = `
             new Promise(async resolve => {
-            	const pages = document.querySelectorAll("div#list-imga img.chapter-img");
-                resolve(Array.prototype.map.call(pages, item => item.src));
+            	const chapId = document.querySelector('input#chapter').value;
+                const uri = new URL('${randomStr}.iog', window.location.origin);
+                uri.searchParams.set('cid', chapId);
+                const response = await fetch(uri);
+                const data = await response.text();
+                const dom = new DOMParser().parseFromString(data, "text/html");
+                const nodes = [...dom.querySelectorAll('img.chapter-img')];
+                resolve(nodes.map(picture => picture.src));
             });
         `;
         const request = new Request(uri, this.requestOptions);
