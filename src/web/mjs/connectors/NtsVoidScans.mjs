b92e8f4224ea2c79edac6911134fd3028aabf27d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-07 18:34:40 +0000

    Update NtsVoidScans.mjs
    remove space

--- src/web/mjs/connectors/NtsVoidScans.mjs
+++ src/web/mjs/connectors/NtsVoidScans.mjs
@@ -4,7 +4,7 @@ export default class NtsVoidScans extends WordPressMadara {
     constructor() {
         super();
         super.id = 'ntsvoidscans';
-        super.label = 'Void Scans ';
+        super.label = 'Void Scans';
         this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://voidscans.com';
     }
