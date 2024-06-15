821eadf7988f064bc6254b13b147d368e24f60cf
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-15 08:12:51 +0000

    MangaGeko: domain change & mangalist fix (#7140)
    Fixes https://github.com/manga-download/hakuneko/issues/7138

--- src/web/mjs/connectors/MangaRaw.mjs
+++ src/web/mjs/connectors/MangaRaw.mjs
@@ -8,22 +8,22 @@ export default class MangaRaw extends Connector {
         super.id = 'mangaraw';
         super.label = 'MangaGeko';
         this.tags = ['multi-lingual', 'manga', 'webtoon'];
-        this.url = 'https://www.mgeko.com';
+        this.url = 'https://www.mgeko.cc';
     }
 
     async _getMangaFromURI(uri) {
-        let request = new Request(uri, this.requestOptions);
-        let data = await this.fetchDOM(request, 'div.main-head h1[itemprop="name"]');
-        let id = uri.pathname + uri.search;
-        let title = data[0].textContent.trim();
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.main-head h1[itemprop="name"]');
+        const id = uri.pathname + uri.search;
+        const title = data[0].textContent.trim();
         return new Manga(this, id, title);
     }
 
     async _getMangas() {
-        let mangaList = [];
-        let request = new Request(this.url + '/browse-comics/', this.requestOptions);
-        let data = await this.fetchDOM(request, 'ul.pagination li:nth-child(2) a');
-        let pageCount = parseInt(data[0].textContent);
+        const mangaList = [];
+        const request = new Request(this.url + '/browse-comics/', this.requestOptions);
+        const [ data ] = await this.fetchDOM(request, 'span.mg-pagination-last');
+        const pageCount = parseInt(data.textContent.match(/\d+/)[0]);
         for (let page = 1; page <= pageCount; page++) {
             await this.wait(250);
             let mangas = await this._getMangasFromPage(page);
@@ -33,8 +33,8 @@ export default class MangaRaw extends Connector {
     }
 
     async _getMangasFromPage(page) {
-        let request = new Request(`${this.url}/browse-comics/?results=${page}`, this.requestOptions);
-        let data = await this.fetchDOM(request, 'li.novel-item a', 3);
+        const request = new Request(`${this.url}/browse-comics/?results=${page}`, this.requestOptions);
+        const data = await this.fetchDOM(request, 'li.novel-item a', 3);
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
@@ -59,8 +59,8 @@ export default class MangaRaw extends Connector {
     }
 
     async _getPages(chapter) {
-        let request = new Request(new URL(chapter.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, 'section.page-in div source[onerror]');
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
+        const data = await this.fetchDOM(request, 'section.page-in div source[onerror]');
         return data.map(element => this.getAbsolutePath(element.src, request.url));
     }
 }
