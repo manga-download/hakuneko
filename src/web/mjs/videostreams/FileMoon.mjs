4cf7327f5921fdc27217d9ad0ef8837577620247
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-18 06:54:04 +0000

    Aniwave : fix checksums & getting playlist (#7196)
    * Aniwave : fix checksums & getting playlist
    
    * Fliemoon : fix getting playlist url
    
    * Aniwave ; fix WAF antiscraper check
    
    * try...catch in async promise

--- src/web/mjs/videostreams/FileMoon.mjs
+++ src/web/mjs/videostreams/FileMoon.mjs
@@ -8,9 +8,13 @@ export default class FileMoon {
     async getPlaylist() {
         const script = `
             new Promise((resolve, reject) => {
-                setTimeout(() => {
-                      resolve(window.videop.hls.url); 
-                }, 3000);
+                try {
+                    setTimeout(() => {
+                          resolve(videop.getConfig().sources[0].file); 
+                    }, 3000);
+                } catch (error) {
+                    reject(error);
+                }
             });
         `;
         const request = new Request(this._uri);
