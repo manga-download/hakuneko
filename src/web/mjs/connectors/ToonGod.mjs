cd76f56c2b3d0b1c36ac25a7f3b2fef947c4197b
ainsworth35 <ahmet_turgut35@outlook.com>
2024-07-03 14:28:07 +0300

    Domain Change ToonGod

--- src/web/mjs/connectors/ToonGod.mjs
+++ src/web/mjs/connectors/ToonGod.mjs
@@ -1,12 +1,11 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
+import WordPressMadara from "./templates/WordPressMadara.mjs";
 
 export default class ToonGod extends WordPressMadara {
-
-    constructor() {
-        super();
-        super.id = 'toongod';
-        super.label = 'ToonGod';
-        this.tags = [ 'webtoon', 'hentai', 'english' ];
-        this.url = 'https://www.toongod.com';
-    }
+  constructor() {
+    super();
+    super.id = "toongod";
+    super.label = "ToonGod";
+    this.tags = ["webtoon", "hentai", "english"];
+    this.url = "https://www.toongod.org";
+  }
 }
