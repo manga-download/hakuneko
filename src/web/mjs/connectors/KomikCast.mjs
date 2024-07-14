4f47803d9aa202d216f928277ba382e7bbb0d46f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-14 20:11:37 +0000

    some domain changes (#7246)
    * KomikTap : change domainn
    
    * KomikCast : change domain and fix getting pages & clipboard

--- src/web/mjs/connectors/KomikCast.mjs
+++ src/web/mjs/connectors/KomikCast.mjs
@@ -7,13 +7,14 @@ export default class KomikCast extends WordPressMangastream {
         super.id = 'komikcast';
         super.label = 'KomikCast';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://komikcast.ch';
+        this.url = 'https://komikcast.cz';
         this.path = '/daftar-komik/?list';
 
+        this.querMangaTitleFromURI = 'h1.komik_info-content-body-title';
         this.queryMangas = 'div.text-mode_list-items ul li a.series, div.text-mode_list-items ul li a.text-mode_list-item';
         this.queryChapters = 'div.komik_info-chapters ul li.komik_info-chapters-item a.chapter-link-item';
         this.queryChaptersTitle = undefined;
-        this.queryPages = 'div.main-reading-area source.lazyload, div.separator source.lazyload';
+        this.queryPages = 'div.main-reading-area img';
         this.requestOptions.headers.set('x-referer', this.url);
     }
 
@@ -21,10 +22,4 @@ export default class KomikCast extends WordPressMangastream {
         // do nothing on purpose
     }
 
-    async _getPages(chapter) {
-        const uri = new URL(chapter.id, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, this.queryPages);
-        return data.map(image => this.createConnectorURI(this.getAbsolutePath(image.dataset['src'], this.url)));
-    }
 }
