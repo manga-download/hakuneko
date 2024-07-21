21f6b89c54b37d49b841f023e1dafef5e481e772
Peter <peter.noordijk@gmail.com>
2024-07-21 20:46:03 +0200

    FIX : ReadComicOnline.li : updated script to get image URLs as the old JS variable is removed
    Fixes 'lstImages is not defined' error and also catches the error instead of failing with a timeout

--- src/web/mjs/connectors/KissComic.mjs
+++ src/web/mjs/connectors/KissComic.mjs
@@ -55,7 +55,11 @@ export default class KissComic extends Connector {
         const script = `
             new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    resolve(lstImages);
+                    try {
+                        resolve(Array.from(document.querySelectorAll('#divImage img')).map(img => img.src));
+                    } catch (err) {
+                        reject(err);
+                    }
                 }, 1000);
             });
         `;
