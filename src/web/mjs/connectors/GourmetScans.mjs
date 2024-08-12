c57d57a393ad4d96f213c319028b6d2a63f2eea0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-08-12 12:27:40 +0000

    FIx GourmetScans: fix getting pages (#7324)
    * FIx GourmetScans: fix getting pages
    
    Fixes https://github.com/manga-download/hakuneko/issues/7322
    
    Same fix than Haruneko
    
    * Update GourmetScans.mjs
    
    * Update GourmetScans.mjs

--- src/web/mjs/connectors/GourmetScans.mjs
+++ src/web/mjs/connectors/GourmetScans.mjs
@@ -14,18 +14,29 @@ export default class GourmetScans extends WordPressMadara {
         const url = new URL(chapter.id, this.url);
         const request = new Request(url, this.requestOptions);
         const script = `
-            new Promise((resolve, reject) => {
-                try {
-                    let rocketscript = new RocketLazyLoadScripts;
-                    rocketscript._loadEverythingNow();
-                } catch (error) {}
-           
-                setTimeout(() => {
-                    var imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
-                        format: CryptoJSAesJson
-                    }).toString(CryptoJS.enc.Utf8));
-                    resolve(JSON.parse(imgdata));
-                }, 2500);
+            new Promise( (resolve, reject) => {
+                const start = Date.now();
+                const interval = setInterval(function () {
+                    try {
+                        if (CryptoJS) {
+                            clearInterval(interval);
+                            let imgdata = JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {
+                                format: CryptoJSAesJson
+                            }).toString(CryptoJS.enc.Utf8));
+                            resolve(JSON.parse(imgdata));
+                        }
+                    } catch (error) {
+                        clearInterval(interval);
+                        reject(error);
+                    } finally {
+                        if(Date.now() - start > 10_000) {
+                            clearInterval(interval);
+                            reject(new Error('Unable to get pictures after more than 10 seconds !'));
+                        }
+                     }
+                }, 1000);
+               window.dispatchEvent(new KeyboardEvent('keydown'));
+        
             });
         `;
         const data = await Engine.Request.fetchUI(request, script);
