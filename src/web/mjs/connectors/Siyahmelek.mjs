f605bd017efa4a42340b44a0e946653f625b0ab4
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-07 07:03:54 +0000

    GriMelek change domain (#7370)

--- src/web/mjs/connectors/Siyahmelek.mjs
+++ src/web/mjs/connectors/Siyahmelek.mjs
@@ -7,9 +7,9 @@ export default class Siyahmelek extends WordPressMadara {
         super.id = 'siyahmelek';
         super.label = 'Gri Melek (Siyahmelek)';
         this.tags = [ 'manga', 'webtoon', 'turkish' ];
-        this.url = 'https://grimelek.mom';
+        this.url = 'https://grimelek.pro';
         this.links = {
-            login : 'https://grimelek.mom'//this website needs login to see content !
+            login : 'https://grimelek.pro'//this website needs login to see content !
         };
     }
 
