a142a3228deaa185631b20d1f7df2d2cc456400d
Tristan <tristan@tnet.moe>
2024-10-06 05:08:17 +0200

    feat(Vortex_Scans): create connector

---
+++ src/web/mjs/connectors/Vortex_Scans.mjs
@@ -0,0 +1,15 @@
+import Iken from './templates/Iken.mjs';
+
+export default class Vortex_Scans extends Iken {
+    constructor() {
+        super();
+        super.id = 'vortex_scans';
+        super.label = 'Vortex_Scans';
+        this.tags = [ 'manga', 'english', 'scanlation' ];
+        this.url = 'https://vortexscans.org';
+        this.api = 'https://vortexscans.org';
+        this.links = {
+            login: 'https://vortexscans.org/auth/signin',
+        }
+    }
+}
