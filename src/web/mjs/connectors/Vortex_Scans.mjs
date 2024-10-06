3aea8e956f999a8403dc7d411f6fc4b4c3317d1c
Tristan <tristan@tnet.moe>
2024-10-06 16:09:33 +0200

    chore: rename to Iken

--- src/web/mjs/connectors/Vortex_Scans.mjs
+++ src/web/mjs/connectors/Vortex_Scans.mjs
@@ -7,7 +7,6 @@ export default class Vortex_Scans extends Iken {
         super.label = 'Vortex_Scans';
         this.tags = [ 'manga', 'english', 'scanlation' ];
         this.url = 'https://vortexscans.org';
-        this.api = 'https://vortexscans.org';
         this.links = {
             login: 'https://vortexscans.org/auth/signin',
         };
