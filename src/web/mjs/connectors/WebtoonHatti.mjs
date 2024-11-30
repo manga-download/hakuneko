f5452a94a4dbcbd57ceca2f5ec24a0ef5513804d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-30 13:29:00 +0000

    WebtoonHatti : change domain (#7572)
    Closes https://github.com/manga-download/hakuneko/issues/7570

--- src/web/mjs/connectors/WebtoonHatti.mjs
+++ src/web/mjs/connectors/WebtoonHatti.mjs
@@ -7,7 +7,7 @@ export default class WebtoonHatti extends WordPressMadara {
         super.id = 'webtoonhatti';
         super.label = 'Webtoon Hatti';
         this.tags = [ 'webtoon', 'turkish' ];
-        this.url = 'https://webtoonhatti.net';
+        this.url = 'https://webtoonhatti.com';
     }
 
     async _getPages(chapter) {
