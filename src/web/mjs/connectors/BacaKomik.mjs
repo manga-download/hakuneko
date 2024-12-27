3a5b802b5c5b4692814da82b977ea03dd83adb4e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-12-27 09:41:54 +0000

    BacaKomik: change domain (#7628)

--- src/web/mjs/connectors/BacaKomik.mjs
+++ src/web/mjs/connectors/BacaKomik.mjs
@@ -7,12 +7,12 @@ export default class BacaKomik extends WordPressMangastream {
         super.id = 'bacakomik';
         super.label = 'BacaKomik';
         this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://bacakomik.co';
-        this.path = '/daftar-manga/?list';
+        this.url = 'https://bacakomik.one';
+        this.path = '/daftar-komik/?list';
 
         this.queryMangas = 'div.cpp div.daftarkartun div.jdlbar ul li a.tip';
         this.queryChapters = 'div.eps_lst ul li span.lchx a';
         this.queryChaptersTitle = undefined;
-        this.queryPages = 'div.chapter-area div.chapter-content div.chapter-image img';
+        this.queryPages = 'div#anjay_ini_id_kh img';
     }
 }
