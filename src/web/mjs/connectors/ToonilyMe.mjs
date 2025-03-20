dff1c5e9fe9ba16b8c590553b00832c27c7425ed
YurDavid <davidyevu18@gmail.com>
2025-03-20 20:14:52 +0000

    refactoring for code quality

--- src/web/mjs/connectors/ToonilyMe.mjs
+++ src/web/mjs/connectors/ToonilyMe.mjs
@@ -10,21 +10,21 @@ export default class ToonilyMe extends MadTheme {
     }
 
     async _getChapters(manga) {
-        let request = new Request(this.url + manga.id, this.requestOptions);
-        let data = await this.fetchDOM(request, 'ul#chapter-list > li > a');
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'ul#chapter-list > li > a');
         return data.map(element => {
-            let titleElement = element.querySelector('strong.chapter-title');
-            let chapterTitle = titleElement ? titleElement.textContent.trim() : element.text.trim();
+
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: chapterTitle,
-                language: ''
+                title:element.querySelector(this.queryChapterTitle).textContent.trim(),
             };
         });
     }
 
     async _getPages(chapter) {
-        let request = new Request(this.url + chapter.id, this.requestOptions);
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
         const data = await this.fetchDOM(request, 'div.chapter-image source');
         return data.map(element => this.createConnectorURI(this.getAbsolutePath(element, request.url)));
     }
