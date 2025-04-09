0fdca854d17fafa2e867b9fc67175363a7dbf7fd
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-09 09:50:47 +0000

    FlameScansFR => rebred to Mangas Scans
    Closes https://github.com/manga-download/hakuneko/issues/7861

--- src/web/mjs/connectors/FlameScansFR.mjs
+++ src/web/mjs/connectors/FlameScansFR.mjs
@@ -5,17 +5,9 @@ export default class FlameScansFR extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'flamescans-fr';
-        super.label = 'Flame Scans.fr';
+        super.label = 'Mangas Scans';
         this.tags = [ 'manga', 'webtoon', 'french' ];
-        this.url = 'https://flamescans.fr';
+        this.url = 'https://mangas-scans.com';
         this.path = '/manga/list-mode/';
-
-        this.queryMangas = 'div.postbody div.soralist ul li a.series';
-        this.queryChapters = 'div#chapterlist ul li a';
     }
-
-    get icon() {
-        return '/img/connectors/flamescans-org';
-    }
-
 }
