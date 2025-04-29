01c139907eb5d9db61574a7d3d3524e63bc09219
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-29 08:23:19 +0000

    MangaDenizi : change domain (#7873)
    https://github.com/manga-download/hakuneko/issues/7872

--- src/web/mjs/connectors/MangaDenizi.mjs
+++ src/web/mjs/connectors/MangaDenizi.mjs
@@ -13,6 +13,6 @@ export default class MangaDenizi extends MangaReaderCMS {
         super.id = 'mangadenizi';
         super.label = 'MangaDenizi';
         this.tags = [ 'manga', 'high-quality', 'turkish', 'scanlation' ];
-        this.url = 'http://www.mangadenizi.com';
+        this.url = 'http://www.mangadenizi.net';
     }
 }
