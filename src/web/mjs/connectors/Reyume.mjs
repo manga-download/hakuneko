64ab71186cfec2514ecbaad2b0a9b870c3c74fee
akn19 <86169232+akn19@users.noreply.github.com>
2025-05-21 16:55:44 +0700

    improves code readability & maintainability

--- src/web/mjs/connectors/Reyume.mjs
+++ src/web/mjs/connectors/Reyume.mjs
@@ -7,7 +7,6 @@ export default class Reyume extends Connector {
         super.label = 'ReYume';
         this.tags = [ 'manga', 'scanlation', 'indonesian' ];
         this.url = 'https://www.re-yume.my.id';
-        this.queryMangaTitle = 'h1[itemprop="name"]';
     }
 
     async _getMangas() {
@@ -45,30 +44,29 @@ export default class Reyume extends Connector {
         const request = new Request(new URL('/feeds/posts/default/-/'+mangaid+'?orderby=published&alt=json&max-results=999', this.url), this.requestOptions);
         const { feed } = await this.fetchJSON(request);
 
-        let chapterslist = feed.entry.map(entry => {
-            const goodLink = entry.link.find(link => link.rel === 'alternate');
+        const chapterslist = feed.entry.map((entry) => {
+            const goodLink = entry.link.find((link) => link.rel === "alternate");
             return {
                 id: this.getRootRelativeOrAbsoluteLink(goodLink.href, request.url),
-                title: goodLink.title.replace(/.*(?=Chapter)/g, '').trim()
-            };
-        }).filter(chap => chap.id != manga.id);
+                title: goodLink.title.replace(/.*(?=Chapter)/g, "").trim(),
+            }
+        }).filter((chap) => chap.id != manga.id);
         return chapterslist;
     }
 
     async _getPages(chapter) {
         const scriptPages = `
-        new Promise(resolve => {
-            resolve([...document.querySelectorAll('article#reader img')].map(img => img.src));
-        });
+            new Promise(resolve => {
+                resolve([...document.querySelectorAll('article#reader img')].map(img => img.src));
+            });
         `;
-        const request = new Request(this.url + chapter.id, this.requestOptions);
+        const request = new Request(new URL(chapter.id, this.url), this.requestOptions);
         return await Engine.Request.fetchUI(request, scriptPages);
     }
 
     async _getMangaFromURI(uri) {
         const request = new Request(uri, this.requestOptions);
-        const id = uri.pathname;
-        const title = (await this.fetchDOM(request, this.queryMangaTitle))[0].textContent.trim();
-        return new Manga(this, id, title);
+        const title = (await this.fetchDOM(request, 'h1[itemprop="name"]'))[0].textContent.trim();
+        return new Manga(this, uri.pathname, title);
     }
 }
