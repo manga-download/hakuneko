02623dc27aaeda16b80b0284225d54c1fc510e8d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-15 08:52:22 +0000

    Quantum Scans : change template
    Closes https://github.com/manga-download/hakuneko/issues/7990

--- src/web/mjs/connectors/LyraScans.mjs
+++ src/web/mjs/connectors/LyraScans.mjs
@@ -1,13 +1,13 @@
-import WordPressMangastream from './templates/WordPressMangastream.mjs';
+import HeanCMS from './templates/HeanCMS.mjs';
 
-export default class LyraScans extends WordPressMangastream {
+export default class LyraScans extends HeanCMS {
 
     constructor() {
         super();
         super.id = 'lyrascans';
         super.label = 'Quantum Scans';
         this.tags = [ 'manga', 'english', 'scanlation' ];
-        this.url = 'https://readers-point.space';
-        this.path = '/series/list-mode/';
+        this.url = 'https://quantumscans.org';
+        this.api = 'https://api.quantumscans.org';
     }
 }
