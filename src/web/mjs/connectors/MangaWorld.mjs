59a21d568f4c294ef27f4de0840b6ed9c58b4ee2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-23 17:47:30 +0000

    Update MangaWorld domain (#8000)
    Closes https://github.com/manga-download/hakuneko/issues/7998

--- src/web/mjs/connectors/MangaWorld.mjs
+++ src/web/mjs/connectors/MangaWorld.mjs
@@ -8,7 +8,7 @@ export default class MangaWorld extends Connector {
         super.id = 'mangaworld';
         super.label = 'Manga World';
         this.tags = [ 'manga', 'webtoon', 'italian'];
-        this.url = 'https://www.mangaworld.bz';
+        this.url = 'https://www.mangaworld.nz';
     }
 
     async _getMangaFromURI(uri) {
