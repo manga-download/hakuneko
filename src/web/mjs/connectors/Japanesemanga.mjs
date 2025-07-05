54d1967812dfa3778d99bd65168cc0ad32283771
thiraphat98 <thiraphat9845@gmail.com>
2025-07-05 09:59:40 +0700

    Refactor Japanesemanga and Onemanga constructors to set properties using super

--- src/web/mjs/connectors/Japanesemanga.mjs
+++ src/web/mjs/connectors/Japanesemanga.mjs
@@ -4,8 +4,8 @@ export default class Japanesemanga extends WordPressMangastream {
 
     constructor() {
         super();
-        this.id = 'japanesemanga';
-        this.label = 'มังงะญี่ปุ่น';
+        super.id = 'japanesemanga';
+        super.label = 'มังงะญี่ปุ่น';
         this.tags = [ 'webtoon', 'thai', 'manga' ];
         this.url = 'https://xn--72cas2cj6a4hf4b5a8oc.com';
         this.path = '/manga/list-mode/';
--- src/web/mjs/connectors/Onemanga.mjs
+++ src/web/mjs/connectors/Onemanga.mjs
@@ -4,8 +4,8 @@ export default class Onemanga extends WordPressMangastream {
 
     constructor() {
         super();
-        this.id = 'onemanga';
-        this.label = 'One-Manga';
+        super.id = 'onemanga';
+        super.label = 'One-Manga';
         this.tags = [ 'webtoon', 'thai', 'manga' ];
         this.url = 'https://one-manga.com/';
         this.path = '/manga/manga/list-mode/';
