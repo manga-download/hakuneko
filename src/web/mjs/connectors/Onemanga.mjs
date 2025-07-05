7a0663b2311cb4a0d7512e6f8f0e0a4e0bd0576b
thiraphat98 <thiraphat9845@gmail.com>
2025-07-05 09:45:30 +0700

    Refactor Japanesemanga and Onemanga connectors to set properties directly in the constructor

--- src/web/mjs/connectors/Onemanga.mjs
+++ src/web/mjs/connectors/Onemanga.mjs
@@ -4,9 +4,9 @@ export default class Onemanga extends WordPressMangastream {
 
     constructor() {
         super();
-        super.id = 'onemanga';
-        super.label = 'One-Manga';
-        this.tags = [ 'webtoon', 'thai','manga' ];
+        this.id = 'onemanga';
+        this.label = 'One-Manga';
+        this.tags = [ 'webtoon', 'thai', 'manga' ];
         this.url = 'https://one-manga.com/';
         this.path = '/manga/manga/list-mode/';
     }
