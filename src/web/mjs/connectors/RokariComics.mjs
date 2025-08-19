8fba7f0333120c683b75d3f062d72c6dbfe85fb0
Eshan <2016mehrab@gmail.com>
2025-08-19 17:36:46 +0600

    refactor(RokariComics): Filter out premium chapters by checking for href attribute
    - Added direct chapter query selector
    - Removed unnecessary async function

--- src/web/mjs/connectors/RokariComics.mjs
+++ src/web/mjs/connectors/RokariComics.mjs
@@ -3,6 +3,7 @@ import WordPressMangastream from "./templates/WordPressMangastream.mjs";
 export default class RokariComics extends WordPressMangastream {
     constructor() {
         super();
+        this.queryChapters = 'div#chapterlist ul li div.eph-num a[href]';
         super.id = "rokaricomics";
         super.label = "Rokari Comics";
         this.tags = ["webtoon", "english"];
@@ -12,21 +13,4 @@ export default class RokariComics extends WordPressMangastream {
             login: 'https://rokaricomics.com/login/'
         };
     }
-
-    async _getChapters(manga) {
-        let request = new Request(new URL(manga.id, this.url), this.requestOptions);
-        let data = await this.fetchDOM(request, this.queryChapters);
-        return data
-        // filtering out premium chapters
-            .filter(element => element.href)
-            .map(element => {
-                this.adLinkDecrypt(element);
-                const title = this.queryChaptersTitle ? element.querySelector(this.queryChaptersTitle).textContent : element.text;
-                return {
-                    id: this.getRootRelativeOrAbsoluteLink(element, request.url),
-                    title: title.replace(manga.title, '').trim() || manga.title
-                };
-            });
-    }
-
 }
