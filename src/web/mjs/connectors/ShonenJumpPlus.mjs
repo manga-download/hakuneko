13c3135081c56ead79c5191c213d9ccd1eba9e28
undo <100544250+00undo@users.noreply.github.com>
2025-09-07 17:30:27 +0100

    ShonenJumpPlus: support pasting volume (#8161)
    * Update ShonenJumpPlus.mjs
    
    Fixes #8160
    
    added volume to conditional for _getChapters
    
    * Update ShonenJumpPlus.mjs
    
    Removed unneeded Regexp
    
    * Update ShonenJumpPlus.mjs
    
    *included brackets

--- src/web/mjs/connectors/ShonenJumpPlus.mjs
+++ src/web/mjs/connectors/ShonenJumpPlus.mjs
@@ -11,7 +11,7 @@ export default class ShonenJumpPlus extends CoreView {
     }
 
     async _getChapters(manga) {
-        if(/^\/magazine\/\d+$/.test(manga.id)) {
+        if(/^\/(magazine|volume)\/\d+$/.test(manga.id)) {
             let request = new Request(new URL(this.url+manga.id), this.requestOptions);
             let data = await this.fetchDOM(request, '.episode-header-title');
             return [{
