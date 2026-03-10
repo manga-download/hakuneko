49f116f31dafbe17a32a36d2f82f0d5e7a1bba53
AryanXtra2 <more.aryanb@gmail.com>
2026-03-11 01:58:26 +0530

    Update FlameScansORG.mjs
    Update Flame Comics (FlameScansORG) for new site architecture

--- src/web/mjs/connectors/FlameScansORG.mjs
+++ src/web/mjs/connectors/FlameScansORG.mjs
@@ -1,59 +1,64 @@
-import WordPressMangastream from './templates/WordPressMangastream.mjs';
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-export default class FlameScansORG extends WordPressMangastream {
-
+export default class FlameScansORG extends Connector {
     constructor() {
         super();
         super.id = 'flamescans-org';
         super.label = 'Flame Comics';
-        this.tags = [ 'manga', 'webtoon', 'english' ];
+        this.tags = ['manga', 'webtoon', 'english'];
         this.url = 'https://flamecomics.xyz';
-        this.path = '/series/list-mode/';
-        this.queryMangas = 'div.postbody div.soralist ul li a.series';
-        this.queryChapters = 'div#chapterlist ul li a';
+        this.path = '/browse';
+    }
+
+    async _getMangas() {
+        const uri = new URL(this.path, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#__NEXT_DATA__');
+        const json = JSON.parse(data[0].textContent);
+
+        return json.props.pageProps.series
+            .filter(manga => manga.series_id) 
+            .map(manga => {
+                return {
+                    id: `/series/${manga.series_id}`,
+                    title: manga.title.trim()
+                };
+            });
+    }
+
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#__NEXT_DATA__');
+        const json = JSON.parse(data[0].textContent);
+        
+        return new Manga(this, uri.pathname, json.props.pageProps.series.title.trim());
+    }
+
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#__NEXT_DATA__');
+        const json = JSON.parse(data[0].textContent);
+
+        return json.props.pageProps.chapters.map(chapter => {
+            const title = chapter.title ? `Chapter ${chapter.chapter} - ${chapter.title}` : `Chapter ${chapter.chapter}`;
+            return {
+                id: `/series/${chapter.series_id}/${chapter.token}`,
+                title: title.trim()
+            };
+        });
     }
 
     async _getPages(chapter) {
-        const url = new URL(chapter.id, this.url);
-        const request = new Request(url, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.composed_figure');
-        if (data.length == 0) return (await super._getPages(chapter)).filter(page => !page.includes('readonflamescans.png'));
-
-        //need to combine pictures
-        return data.map(page => {
-            const images = [...page.querySelectorAll('source')].map(image => image.getAttribute('src'));
-            return this.createConnectorURI({ images : images });
-        });
-    }
-
-    async _handleConnectorURI(payload) {
-        const promises = payload.images.map(async part => {
-            const request = new Request(part, this.requestOptions);
-            request.headers.set('x-referer', this.url);
-            const response = await fetch(request);
-            const data = await response.blob();
-            return createImageBitmap(data);
-        });
-        const bitmaps = await Promise.all(promises);
-        const data = await this.composePuzzle(bitmaps);
-        return this._blobToBuffer(data);
-    }
-
-    async composePuzzle(bitmaps) {
-        return new Promise(resolve => {
-            let canvas = document.createElement('canvas');
-            //combine 2 pictures
-            const b1 = bitmaps[0];
-            const b2 = bitmaps[1];
-            canvas.width = b1.width+b2.width;
-            canvas.height = b1.height;
-            let ctx = canvas.getContext('2d');
-            ctx.drawImage(b1, 0, 0);
-            ctx.drawImage(b2, b1.width, 0);
-            canvas.toBlob(data => {
-                resolve(data);
-            },
-            Engine.Settings.recompressionFormat.value, parseFloat(Engine.Settings.recompressionQuality.value)/100);
+        const uri = new URL(chapter.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '#__NEXT_DATA__');
+        const json = JSON.parse(data[0].textContent);
+        const chapterData = json.props.pageProps.chapter;
+        
+        return Object.values(chapterData.images).map(image => {
+            return `https://cdn.flamecomics.xyz/uploads/images/series/${chapterData.series_id}/${chapterData.token}/${image.name}`;
         });
     }
 }
